de2dfda12061dc3d3a1d2b5844ad5b3ca70d49ed
hongli <hongli@3e610c8b-890d-0410-8877-b158ba2967cd>
2004-07-08 12:40:28 +0000

    * Support wildcard searches. * Updated TODO list. * Fix status display.

--- trunk/grftool/win32/grftool.dpr
+++ trunk/grftool/win32/grftool.dpr
@@ -2,10 +2,11 @@ program grftool;
 
 uses
   Forms,
-  main in 'main.pas' {Form1},
+  main in 'Main.pas' {Form1},
   grf in 'grf.pas',
   about in 'about.pas' {AboutBox},
-  ExtractThread in 'ExtractThread.pas';
+  ExtractThread in 'ExtractThread.pas',
+  GPattern in 'GPattern.pas';
 
 {$R *.res}
 
