664de8980b1ba10b8e5815d785f6381ef9066cc7
hongli <hongli@3e610c8b-890d-0410-8877-b158ba2967cd>
2004-07-13 07:37:01 +0000

    New about box.

--- trunk/grftool/win32/About.pas
+++ trunk/grftool/win32/About.pas
@@ -4,13 +4,21 @@ interface
 
 uses
   Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
-  Dialogs, StdCtrls, Buttons;
+  Dialogs, StdCtrls, Buttons, ExtCtrls;
 
 type
   TAboutBox = class(TForm)
+    Panel1: TPanel;
+    Panel2: TPanel;
     Label1: TLabel;
     Label2: TLabel;
-    BitBtn1: TBitBtn;
+    Label3: TLabel;
+    SpeedButton1: TSpeedButton;
+    Label4: TLabel;
+    Bevel1: TBevel;
+    Image1: TImage;
+    procedure FormCreate(Sender: TObject);
+    procedure SpeedButton1Click(Sender: TObject);
   private
     { Private declarations }
   public
@@ -23,5 +31,16 @@ var
 implementation
 
 {$R *.dfm}
+
+procedure TAboutBox.FormCreate(Sender: TObject);
+begin
+  Font.Name := 'Tahoma';
+  Label2.Font.Name := 'Tahoma';
+end;
+
+procedure TAboutBox.SpeedButton1Click(Sender: TObject);
+begin
+  Close;
+end;
 
 end.
