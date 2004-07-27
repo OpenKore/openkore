0868e327f7c68782f0c02fa963b4b2b91ebc316c
hongli <hongli@users.sourceforge.net>
2004-07-27 08:49:30 +0000

    Fix example1.pl; remove example2.pl - completely useles

--- trunk/plugins/example.pl
+++ trunk/plugins/example.pl
@@ -2,24 +2,32 @@ package ExamplePlugin;
 
 use strict;
 use Plugins;
-my $count = 0 if (!defined my $count);
+use Log;
+
+my $ai_hook;
+
+# Take a look at http://openkore.sourceforge.net/srcdoc/Plugins.html
+# for more info about these functions
+Plugins::register('example', 'Example Plugin', \&on_unload, \&on_reload);
+$ai_hook = Plugins::addHook('AI_pre', \&ai_alled);
 
 
-Plugins::register('example', 'Example Plugin', \&Unload, \&Reload);
-Plugins::addHook('AI_pre', \&Called);
-
-sub Unload {
-	print "I have just been unloaded\n";
+sub on_unload {
+	Plugins::delHook('AI_pre', $ai_hook);
+	Log::message "Example plugin unloaded\n";
 }
 
-sub Reload {
-	print "I have just been reloaded\n";
+sub on_reload {
+	# For now, do the same thing as unload
+	&on_unload;
 }
 
+
+my $count = 0;
 sub Called {
 	if ($count == 0) {
 		$count++;
-		print "I am example.pl and I will spam this message the first time the AI sequence is started.\n";
+		Log::message "I am example.pl and I will spam this message the first time the AI() function is called.\n";
 	}
 }
 
