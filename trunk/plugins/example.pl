4224655c8372756627ac190497fd914ff0c8fc80
hongli <hongli@users.sourceforge.net>
2004-07-27 08:53:01 +0000

    *** empty log message ***

--- trunk/plugins/example.pl
+++ trunk/plugins/example.pl
@@ -1,3 +1,4 @@
+# Always give plugins a package name, in order to avoid naming conflicts with the rest of Kore
 package ExamplePlugin;
 
 use strict;
@@ -9,22 +10,24 @@ my $ai_hook;
 # Take a look at http://openkore.sourceforge.net/srcdoc/Plugins.html
 # for more info about these functions
 Plugins::register('example', 'Example Plugin', \&on_unload, \&on_reload);
-$ai_hook = Plugins::addHook('AI_pre', \&ai_alled);
+$ai_hook = Plugins::addHook('AI_pre', \&ai_called);
 
 
 sub on_unload {
+	# This function is called when kore tells the plugin to unload.
 	Plugins::delHook('AI_pre', $ai_hook);
 	Log::message "Example plugin unloaded\n";
 }
 
 sub on_reload {
-	# For now, do the same thing as unload
+	# This function is called when kore tells the plugin to reload.
+	# For now, do the same thing as unload.
 	&on_unload;
 }
 
 
 my $count = 0;
-sub Called {
+sub ai_called {
 	if ($count == 0) {
 		$count++;
 		Log::message "I am example.pl and I will spam this message the first time the AI() function is called.\n";
