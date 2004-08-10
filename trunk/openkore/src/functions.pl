124d39ae11c292a111565e975968b152aa76cc8f
pmak <pmak@3e610c8b-890d-0410-8877-b158ba2967cd>
2004-08-10 09:37:58 +0000

    Picked up items could not be equipped (type_equip was parsed wrong)

--- trunk/openkore/src/functions.pl
+++ trunk/openkore/src/functions.pl
@@ -5880,7 +5880,7 @@ sub parseMsg {
 				$item->{index} = $index;
 				$item->{nameID} = unpack("S1", substr($msg, 6, 2));
 				$item->{type} = unpack("C1", substr($msg, 21, 1));
-				$item->{type_equip} = unpack("C1", substr($msg, 19, 2));
+				$item->{type_equip} = unpack("S1", substr($msg, 19, 2));
 				$item->{amount} = $amount;
 				$item->{identified} = unpack("C1", substr($msg, 8, 1));
 				$item->{upgrade} = unpack("C1", substr($msg, 10, 1));
