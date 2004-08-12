0c65010555d3c9bd6a02490e918e02c6fb856d89
pmak <pmak@users.sourceforge.net>
2004-08-12 07:42:50 +0000

    Support more area effect spells with ss/sp command. Detect Resurrection correctly.

--- trunk/openkore/openkore.pl
+++ trunk/openkore/openkore.pl
@@ -104,6 +104,7 @@ addConfigFile("$Settings::tables_folder/skillsstatus.txt", \%skillsStatus, \&par
 addConfigFile("$Settings::tables_folder/skillsailments.txt", \%skillsAilments, \&parseDataFile2);
 addConfigFile("$Settings::tables_folder/skillsstate.txt", \%skillsState, \&parseDataFile2);
 addConfigFile("$Settings::tables_folder/skillslooks.txt", \%skillsLooks, \&parseDataFile2);
+addConfigFile("$Settings::tables_folder/skillsarea.txt", \%skillsArea, \&parseDataFile2);
 addConfigFile("$Settings::tables_folder/cards.txt", \%cards_lut, \&parseROLUT);
 addConfigFile("$Settings::tables_folder/elements.txt", \%elements_lut, \&parseROLUT);
 addConfigFile("$Settings::tables_folder/recvpackets.txt", \%rpackets, \&parseDataFile2);
