975a1fd632ad1927ec8a290c88d2d6b7f82bc2f6
hongli <hongli@3e610c8b-890d-0410-8877-b158ba2967cd>
2004-08-20 09:44:03 +0000

    Update version.

--- trunk/openkore/src/Settings.pm
+++ trunk/openkore/src/Settings.pm
@@ -42,7 +42,7 @@ our @EXPORT_OK = qw(parseArguments addConfigFile delConfigFile);
 # Constants
 our $NAME = 'OpenKore';
 our $VERSION = '1.3.1';
-our $CVS = ' (CVS version)';
+our $CVS = ' (beta version)';
 our $WEBSITE = 'http://openkore.sourceforge.net';
 our $versionText = "*** $NAME ${VERSION}${CVS} - Custom Ragnarok Online client ***\n***   $WEBSITE   ***\n";
 our $welcomeText = "Welcome to X-$NAME.";
