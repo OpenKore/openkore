64ec75002e9acd18ad5df3eeec5c1661d77bc9fe
hongli <hongli@users.sourceforge.net>
2004-10-12 18:59:08 +0000

    Improve documentation

--- trunk/openkore/src/doc/createdoc.pl
+++ trunk/openkore/src/doc/createdoc.pl
@@ -93,7 +93,7 @@ sub makeupText {
 	}
 
 	# Functions
-	$text =~ s/([a-z0-9_:]+\(\))/&createFuncLink($1)/gie;
+	$text =~ s/([a-z0-9_:\->]+\(\))/&createFuncLink($1)/gie;
 	# Variables
 	$text =~ s/(\$[a-z0-9_{\'}:]+)/<code>$1<\/code>/gi;
 	# Links to modules
