f9d129fb7dd7742896c0cb891deb55082d5feb86
hongli <hongli@3e610c8b-890d-0410-8877-b158ba2967cd>
2004-11-06 15:15:31 +0000

    Initial import

---
+++ trunk/openkore/src/IPC/Processors.pm
@@ -0,0 +1,38 @@
+package IPC::Processors;
+
+use strict;
+use Globals qw($conState %field $char);
+use IPC;
+use Log qw(message debug);
+use Utils qw(calcPosition);
+
+my %handlers = (
+	'where are you' => \&ipcWhereAreYou
+);
+
+sub process {
+	my $ipc = shift;
+	my $msg = shift;
+
+	if (defined $handlers{$msg->{ID}}) {
+		$handlers{$msg->{ID}}->($ipc, $msg->{ID}, $msg->{params});
+	} else {
+		debug "Unhandled IPC message '$msg->{ID}' from client $msg->{clientID}\n", "ipc";
+	}
+}
+
+sub ipcWhereAreYou {
+	my ($ipc, $ID, $params) = @_;
+	return unless $conState == 5;
+
+	my $pos = calcPosition($char);
+	$ipc->broadcast("i am here", {
+		clientID => $ipc->ID,
+		name	=> $char->{name},
+		field	=> $field{name},
+		x	=> $pos->{x},
+		y	=> $pos->{y}
+		});
+}
+
+1;
