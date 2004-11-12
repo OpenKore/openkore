fb34dccadeff149115705d477a0fadab280bde5a
nic0nac <nic0nac@3e610c8b-890d-0410-8877-b158ba2967cd>
2004-11-12 09:55:17 +0000

    moved long comment to readme.txt

--- trunk/mercdb/mercdb.pl
+++ trunk/mercdb/mercdb.pl
@@ -19,129 +19,17 @@
 # Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 # 
 #
-# The idea: 
-# running around an manually checking prices from other mercs 
-# is a pain. let a bot do the job. check on a web-site what he found.
-# when the bot finds very cheap offers (hot-deals) let him buy it.
-#
-# What it does:
-# when walked around in a given map with merchants the bot 
-# checks at 'packet_vender' hook for available merchants. Now
-# it searches in database table 'shopvisit' for the merchants user ID
-# to check when the merc was visited the last time. if the last check 
-# wasn't longer ago than $visitTimeout seconds, the bot will ask RO 
-# for the mercs shop list and write the present date and time into the 
-# database. 
-# 
-# at the hook 'packet_vender_store' mercDbFill will be called. it parses  
-# the venderList and writes it into the mysql table 'shopcont'.
-# 
-# HOT DEALS:
-# when you put in your config.txt item-names or 'all' after 'merchantDB_shoppinglist'
-# the bot will buy it, when it is discovered as hot deal. it buys all hot-deals with 'all'.
-# hot-deals are offered items with a price that is under the average price minus it's
-# standard deviation. 
-#
-# the webfrontend is done in php, because i know php better than perl ;)
-# it shows for all found items and cards the min, max, average price and the standard 
-# deviation of the price, and the "hot-deal" barrier.
-# you can search for cards inserted into items, as well as see what merc
-# offered which items. and so on. should be self explaining.
-#
-# requirements:
-# - any, even a new born RO char :)
-# - openkore cvs
-# - mysql database (can probably be changed to another database due to Mysql)
-# - webserver with php-4 support
-# - perl modules: Mysql, CGI::Enurl, Date::Format, TimeDate, DBD::mysql, DBI
-#
-# <quote=tofu_soldier>
-# so, if any of you intended to get the plugin mercDB.pl running correctly
-# in win32, get to ppm and have these installed:
-# install CGI::Enurl
-# (not sure if you need to do this as well)install CGI
-# install TimeDate
-# install Mysql
-# install DBD::mysql
-# </quote>
-#
-# Installation:
-# - copy this file into the plugin folder of openkore
-# - create a mysql database ($database)
-# - create a new mysql-user that can read and write the roshop tables 
-#   ($dbUser and $dbPassword)
-# - set up the mysql tables with the roshop.sql file
-# - change the mysql access informations here and in the index.php
-# - put the following into your config.txt
-#   merchantDB 1
-#   # items you wanna buy, must be written like in the Vender Store List,
-#   # separated by comma; make it "all" to buy all hot deals
-#   merchantDB_shoppinglist Aquamarine, Red Potion, +6 Stiletto [Drops*2] [2]
-# - copy the index.php on your webserver 
-# - send your char into a city or other place where merchants are
-# - have it walk around the merchants
-# - check index.php for the results
-#
-# To-do:
-# - performance problems
-# - add NPC dealer prices 
-#
-# Fixed:
-# - the name of the shop owner and the shops are right. the shop owner
-#   might be updateted later but should be as good as openkore gives em
-#   to the plugin
-# - elemental weapons are recognized
-# - update existing entries in the database (price changes etc.)
-# - the number of slots is correct now
-# - it works with the changed vender store list in cvs (openkore 1.3.0pre)
-# - didn't occure to me for 2 weeks: after some time running, openkore crashes 
-#   with an "segmentation fault" i can't tell if it is an problem caused by my 
-#   plugin or a general openkore problem. parallel running bots not using this 
-#   plugin do crash as well.
-# 
-# Known bugs:
-#
-# Planned features:
-# - waypoint plugin: the bots walks along a predefined route
-# - upload a store.txt to the web-frontend and get a version with best / 
-#   average prices from the database
-# - check the prices in-game via chat-commands send to the bot
-#
-# Note: 
-# i tested this only on linux 2.6.5-7.95-default with mysql MySQL 4.0.18-Max, 
-# PHP Version 4.3.4 so your mileage may vary.
-#
-# Thanks:
-# - my bot-hating, RO-playing friends, who love the results of what i do with openkore ;)
-# - the developers of openkore, without i would have stopped playing RO a long time ago
-# - the members of the openkore-forum for theire support and feedback
-# 
-# Apologies:
-# this plugin may be a total waste of time for you, because it only checks the 
-# prices goods are offered for and not the prices of the sales. i offer my 
-# excuses to you, but i found no way to check the real sales-prices.
-# and i apologise for my bad english. my english teacher would laugh her ass of
-# if she could read this ;) 
-
-
+############################################################
 
 package merchantdb;
 
-# i dont know if i need all of them
 use Globals;
 use strict;
 use Plugins;
 use Settings;
 use Log qw(message warning error debug);
-
-#use Mysql;
 use DBI;
 use POSIX qw(strftime);
-#use CGI::Enurl;
-#use Date::Format;
-#use Date::Language;
-#use Statistics::Basic::Mean;
-#use Statistics::Basic::StdDev;
 
 Plugins::register('merchantdb', 'little shopping helper.', \&Unload);
 my $startHook = Plugins::addHook('start3', \&init);
@@ -247,11 +135,6 @@ sub Called{
 ### writes shop informations into database
 ###
 ###
-
-###
-### Database Structure 
-###
-### s. ro_database.sql
 	
 sub mercDbFill{
 	my $itemName;
