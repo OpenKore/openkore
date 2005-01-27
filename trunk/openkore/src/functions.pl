df787e9f32058331322853c00f6e51df29bb09b5
hongli <hongli@3e610c8b-890d-0410-8877-b158ba2967cd>
2005-01-27 12:36:59 +0000

    Fix statsAddAuto_dontUseBonus

--- trunk/openkore/src/functions.pl
+++ trunk/openkore/src/functions.pl
@@ -2860,7 +2860,8 @@ sub AI {
 			$st = lc $st;
 			# If stat needs to be raised to match desired amount
                         $statAmount = $char->{$st};
-			$statAmount += $char->{"${st}_bonus"} if ($config{statsAddAuto_dontUseBonus});
+			$statAmount += $char->{"${st}_bonus"} if (!$config{statsAddAuto_dontUseBonus});
+
 			if ($statAmount < $num && $char->{$st} < 99) {
 				# If char has enough stat points free to raise stat
 				if ($char->{points_free} >= $char->{"points_$st"}) {
