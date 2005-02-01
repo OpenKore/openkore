838575b64d0cd21028bc5f98f53a02debb7120f7
hongli <hongli@3e610c8b-890d-0410-8877-b158ba2967cd>
2005-02-01 17:25:19 +0000

    Fix arrow craft; add ability to automatically create arrows

--- trunk/openkore/openkore.pl
+++ trunk/openkore/openkore.pl
@@ -189,6 +189,7 @@ addConfigFile("$Settings::control_folder/avoid.txt", \%avoid, \&parseAvoidContro
 addConfigFile("$Settings::control_folder/priority.txt", \%priority, \&parsePriority);
 addConfigFile("$Settings::control_folder/consolecolors.txt", \%consoleColors, \&parseSectionedFile);
 addConfigFile("$Settings::control_folder/routeweights.txt", \%routeWeights, \&parseDataFile);
+addConfigFile("$Settings::control_folder/arrowcraft.txt", \%arrowcraft_items, \&parseDataFile_lc);
 
 addConfigFile("$Settings::tables_folder/cities.txt", \%cities_lut, \&parseROLUT);
 addConfigFile("$Settings::tables_folder/directions.txt", \%directions_lut, \&parseDataFile2);
