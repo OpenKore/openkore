20011fa61a74238809a65a77ff2ff27fcfca1f15
hongli <hongli@3e610c8b-890d-0410-8877-b158ba2967cd>
2005-05-16 16:12:28 +0000

    First working version

--- trunk/dataserver/plugin/dataserver.pl
+++ trunk/dataserver/plugin/dataserver.pl
@@ -1,17 +1,33 @@
 #!/usr/bin/env perl
-# Just a testing script at the moment.
+package SharedMemoryPlugin;
 
 use strict;
 use IO::Socket::UNIX;
 use Time::HiRes qw(time);
 
-my $sock = new IO::Socket::UNIX(Type => SOCK_STREAM, Peer => 
-"/tmp/dataserver.socket");
+use Plugins;
+
+our ($sock);
+
+
+Plugins::register("shm", "Shared Memory");
+my $hooks = Plugins::addHooks(["FileParsers::RODescLUT", \&parseRODescLUT]);
+
+start() if (!$sock);
+
+
+sub start {
+	$sock = new IO::Socket::UNIX(
+		Type => SOCK_STREAM,
+		Peer => "/tmp/kore-dataserver.socket"
+	);
+}
 
 sub fetch {
-	my ($sock, $type, $name) = @_;
+	my ($type, $name) = @_;
 	my $buf = '';
 
+	return if (!$sock);
 	send($sock, chr($type) . pack("n", length($name)) . $name, 0);
 
 	while (1) {
@@ -27,8 +43,68 @@ sub fetch {
 	}
 }
 
-my $begin = time;
-for (my $i = 0; $i < 50000; $i++) {
-	fetch($sock, 1, "DC_FORTUNEKISS");
+sub parseRODescLUT {
+	my (undef, $args) = @_;
+	my $type;
+
+	if ($args->{file} =~ /itemsdescriptions/i) {
+		$type = 0;
+	} elsif ($args->{file} =~ /skillsdescriptions/i) {
+		$type = 1;
+	}
+
+	if (defined $type) {
+		tie %{$args->{hash}}, "SharedMemoryPlugin::RODescHandler", $type;
+		$args->{return} = 1;
+	}
 }
-printf "Time spent: %.3f\n", time - $begin;
+
+
+package SharedMemoryPlugin::RODescHandler;
+
+
+sub TIEHASH {
+	my ($class, $type) = @_;
+	my %self;
+
+	$self{type} = $type;
+	bless \%self, $class;
+	return \%self;
+}
+
+sub FETCH {
+	my ($self, $key) = @_;
+	return SharedMemoryPlugin::fetch($self->{type}, $key);
+}
+
+sub STORE {
+	# Do nothing; values in the server are read-only.
+}
+
+sub DELETE {
+	# Ditto
+}
+
+sub CLEAR {
+	# Ditto
+}
+
+sub EXISTS {
+	my ($self, $key) = @_;
+	return $self->FETCH($key) ne '';
+}
+
+sub FIRSTKEY {
+	# Not implemented yet.
+}
+
+sub NEXTKEY {
+	# Ditto.
+}
+
+sub SCALAR {
+	return "%HASH";
+}
+
+
+1;
