f814443cfc5037037283473703c778f56afaeb23
hongli <hongli@users.sourceforge.net>
2005-06-04 08:32:23 +0000

    Document Actor.pm

--- trunk/openkore/src/Actor.pm
+++ trunk/openkore/src/Actor.pm
@@ -1,3 +1,27 @@
+#########################################################################
+#  OpenKore - Base class for all actor objects
+#  Copyright (c) 2005 OpenKore Team
+#
+#  This software is open source, licensed under the GNU General Public
+#  License, version 2.
+#  Basically, this means that you're allowed to modify and distribute
+#  this software. However, if you distribute modified versions, you MUST
+#  also distribute the source code.
+#  See http://www.gnu.org/licenses/gpl.html for the full license.
+#
+#  $Revision$
+#  $Id$
+#
+#########################################################################
+##
+# MODULE DESCRIPTION: Base class for all actor objects
+#
+# The Actor class is a base class for all actor objects.
+# An actor object is a monster or player (all members of %monsters and
+# %players).
+#
+# See also: Monster.pm and Player.pm
+
 package Actor;
 use strict;
 
