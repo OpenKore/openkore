707e4420084e38047e543fc7e881e79e69efaf4e
pmak <pmak@users.sourceforge.net>
2005-06-04 12:02:43 +0000

    Actor::getActor() function! Endure sitting fix for 0114/01DE. Actor::Unknown.pm

---
+++ trunk/openkore/src/Actor/Unknown.pm
@@ -0,0 +1,41 @@
+#########################################################################
+#  OpenKore - Unknown actor object
+#  Copyright (c) 2005 OpenKore Team
+#
+#  This software is open source, licensed under the GNU General Public
+#  License, version 2.
+#  Basically, this means that you're allowed to modify and distribute
+#  this software. However, if you distribute modified versions, you MUST
+#  also distribute the source code.
+#  See http://www.gnu.org/licenses/gpl.html for the full license.
+#
+#  $Revision$
+#  $Id$
+#
+#########################################################################
+##
+# MODULE DESCRIPTION: Unknown actor object
+#
+# The Actor::Unknown class represents any actors who are off-screen.
+#
+# Actor.pm is the base class for this class.
+package Actor::Unknown;
+use strict;
+our @ISA = qw(Actor);
+
+sub new {
+	my (undef, $ID) = @_;
+	return bless({
+		type => 'Unknown',
+		ID => $ID,
+		name => '#'.unpack("L1", $ID)
+	});
+}
+
+sub name {
+	my ($self) = @_;
+
+	return "Unknown $self->{name}";
+}
+
+1;
