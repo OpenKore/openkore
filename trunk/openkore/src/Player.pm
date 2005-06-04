abb3a25db8a7bff48ad258077b93981344d131ed
hongli <hongli@users.sourceforge.net>
2005-06-04 08:32:41 +0000

    Add copyright notice

--- trunk/openkore/src/Player.pm
+++ trunk/openkore/src/Player.pm
@@ -1,5 +1,22 @@
+#########################################################################
+#  OpenKore - Base class for all actor objects
+#  Copyright (c) 2005 OpenKore Team
+#
+#  This software is open source, licensed under the GNU General Public
+#  License, version 2.
+#  Basically, this means that you're allowed to modify and distribute
+#  this software. However, if you distribute modified versions, you MUST
+#  also distribute the source code.
+#  See http://www.gnu.org/licenses/gpl.html for the full license.
+#
+#  $Revision$
+#  $Id$
+#
+#########################################################################
 package Player;
+
 use strict;
+
 our @ISA = qw(Actor);
 
 sub new {
