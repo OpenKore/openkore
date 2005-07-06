8a705faf814b7d3d58cfd3e30f190fcd08ed9f03
kaliwanagan <kaliwanagan@users.sourceforge.net>
2005-07-06 17:16:38 +0000

    replaced huge GPL notice with a shorter one

--- trunk/plugins/exchange.pl
+++ trunk/plugins/exchange.pl
@@ -2,20 +2,8 @@
 #
 # itemexchange
 # 
-# This program is free software; you can redistribute it and/or
-# modify it under the terms of the GNU General Public License
-# as published by the Free Software Foundation; either version 2
-# of the License, or (at your option) any later version.
-# 
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
-# 
+# This software is open source, licensed under the 
+# GNU General Public License, version 2.
 
 # See this thread for detailed config information:
 # http://openkore.sourceforge.net/forum/viewtopic.php?t=8668
