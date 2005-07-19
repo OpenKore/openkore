d4f13eae4ba45470066458f79239342f855443ed
jtwen <jtwen@3e610c8b-890d-0410-8877-b158ba2967cd>
2005-07-19 16:28:13 +0000

    add gmhide, gmcreate (thanks isieo), gmwarpto

--- trunk/plugins/gamemaster.pl
+++ trunk/plugins/gamemaster.pl
@@ -13,7 +13,10 @@ Plugins::register('Game Master', 'Enables usage of GM commands', \&on_unload);
 my $commands = Commands::register(
 	['gmb',       'Broadcast a global message.', \&gmb],
 	['gmmapmove', 'Move to the specified map.',  \&gmmapmove],
-	['gmkillall', 'Disconnect all users.', \&gmkillall]
+	['gmcreate',  'Create items or monsters.',   \&gmcreate],
+	['gmhide',    'Toggle perfect GM hide.',     \&gmhide],
+	['gmwarpto',  'Warp to a player.',           \&gmwarpto],
+	['gmkillall', 'Disconnect all users.',       \&gmkillall]
 );
 
 sub on_unload {
@@ -38,19 +41,55 @@ sub gmmapmove {
 	my (undef, $args) = @_;
 	return unless ($conState == 5);
 
-	if ($args eq '') {
+	my ($map_name) = $args =~ /(\S+)/;
+	# this will pack as 0 if it fails to match
+	my ($x, $y) = $args =~ /\w+ (\d+) (\d+)/;
+
+	if ($map_name eq '') {
 		error "Usage: gmmapmove <FIELD>\n";
 		error "FIELD is a field name including .gat extension, like: gef_fild01.gat\n";
 		return;
 	}
 
-	my $packet = pack("C*", 0x40, 0x01) . pack("a20", $args);
+	my $packet = pack("C*", 0x40, 0x01) . pack("a16", $map_name) . pack("v1 v1", $x, $y);
 	sendMsgToServer(\$remote_socket, $packet);
 }
 
 sub gmkillall {
 	return unless ($conState == 5);
 	my $packet = pack("C*", 0xCE, 0x00);
+	sendMsgToServer(\$remote_socket, $packet);
+}
+
+sub gmcreate {
+	my (undef, $args) = @_;
+	return unless ($conState == 5);
+
+	if ($args eq '') {
+		error "Usage: gmcreate (<MONSTER_NAME> || <Item_Name>) \n";
+		return;
+	}
+
+	my $packet = pack("C*", 0x3F, 0x01).pack("a24", $args);
+	sendMsgToServer(\$remote_socket, $packet);
+}
+
+sub gmhide {
+	return unless ($conState == 5);
+	my $packet = pack("C*", 0x9D, 0x01, 0x40, 0x00, 0x00, 0x00);
+	sendMsgToServer(\$remote_socket, $packet);
+}
+
+sub gmwarpto {
+	my (undef, $args) = @_;
+	return unless ($conState == 5);
+
+	if ($args eq '') {
+		error "Usage: gmwarpto <Player Name>\n";
+		return;
+	}
+
+	my $packet = pack("C*", 0xBB, 0x01).pack("a24", $args);
 	sendMsgToServer(\$remote_socket, $packet);
 }
 
