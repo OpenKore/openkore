53cb9f652415d076bfb8bcf6aeae0e5578a3bc03
hongli <hongli@3e610c8b-890d-0410-8877-b158ba2967cd>
2005-08-16 18:18:18 +0000

    Update copyright notices; add a Perl implementation of the Crypton encryption algorithm

--- trunk/openkore/src/Utils/Downloader.pm
+++ trunk/openkore/src/Utils/Downloader.pm
@@ -1,3 +1,15 @@
+#########################################################################
+#  OpenKore - HTTP downloader
+#
+#  Copyright (c) 2005 OpenKore Development Team
+#
+#  This software is open source, licensed under the GNU General Public
+#  License, version 2.
+#  Basically, this means that you're allowed to modify and distribute
+#  this software. However, if you distribute modified versions, you MUST
+#  also distribute the source code.
+#  See http://www.gnu.org/licenses/gpl.html for the full license.
+#########################################################################
 package Utils::Downloader;
 
 use strict;
