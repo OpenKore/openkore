2a486ef95171bb8a538f0b944dd49875d23d298d
nobody <nobody@3e610c8b-890d-0410-8877-b158ba2967cd>
2005-08-27 14:00:50 +0000

    This commit was manufactured by cvs2svn to create tag 'script_capa'.

---
+++ tags/script_capa/macro/Macro/Data.pm
@@ -0,0 +1,31 @@
+# $Header$
+package Macro::Data;
+
+use strict;
+use warnings;
+
+require Exporter;
+our @ISA = qw(Exporter);
+our @EXPORT = qw(%macro %automacro %varStack $queue $cvs %logfac);
+
+our %macro;
+our %automacro;
+our %varStack;
+our $queue;
+our $cvs;
+
+our %logfac = (
+  "variable_trace"      => 1,    # tracks variables
+  "command_preparsed"   => 2,    # unparsed command line
+  "command_parsed"      => 4,    # parsed command line
+  "parser_steps"        => 8,    # parser steps
+  "function_call_macro" => 16,   # functions with low traffic, macro functions
+  "function_call_auto"  => 32,   # functions with high traffic, automacro functions
+  "automacro_checks"    => 64,   # automacro checks
+  "developers"          => 128,  # debugging messages useful for developers
+  "full"                => 255   # full debugging
+);
+# redefine return()?
+# return_value => 256;
+
+1;
