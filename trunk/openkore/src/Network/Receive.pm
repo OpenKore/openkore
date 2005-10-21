978040878a6fc30ebc966a0f53a9be4442c56a63
kaliwanagan <kaliwanagan@3e610c8b-890d-0410-8877-b158ba2967cd>
2005-10-21 19:51:51 +0000

    new party_location packet (01eb) not tested

--- trunk/openkore/src/Network/Receive.pm
+++ trunk/openkore/src/Network/Receive.pm
@@ -203,6 +203,7 @@ sub new {
 #01E9 81 -
 		'01E9' => ['party_join', 'a4 x4 v1 v1 C1 Z24 Z24 Z16', [qw(ID x y type name user map)]],
 #01EB 10 Party move checksum : 0107. 
+		'01EB' => ['party_location', 'a4 v1 v1', [qw(ID x y)]],
 		'01EA' => ['married', 'a4', [qw(ID)]],
 		'01EE' => ['inventory_items_stackable'],
 		'01F4' => ['deal_request', 'Z24 x4 v1', [qw(user level)]],
