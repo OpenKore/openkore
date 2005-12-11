f4296276386f84629c3d19349e3d10d15ee20bed
hongli <hongli@3e610c8b-890d-0410-8877-b158ba2967cd>
2005-12-11 15:25:17 +0000

    Allow more than 255 characters for global announcement

--- forum/trunk/includes/openkore.php
+++ forum/trunk/includes/openkore.php
@@ -3,6 +3,15 @@ if ( !defined('IN_PHPBB') ) {
 	die("Hacking attempt");
 }
 
+define('OPENKORE_FORUM_CONFIG_TABLE', 'openkore_forum_config');
+
+/*
+CREATE TABLE openkore_forum_config (
+    config_name VARCHAR(255) NOT NULL,
+    config_value TEXT NOT NULL
+);
+ */
+
 // Users who have more than x posts are considered good citizen.
 // Users with less than this number of posts will be shown all kinds of warnings.
 define('OPENKORE_MIN_USER_POSTS', 40);
@@ -10,7 +19,7 @@ define('OPENKORE_MIN_USER_POSTS', 40);
 function load_openkore_options()
 {
 	global $db;
-	$sql = "SELECT * FROM " . CONFIG_TABLE . " WHERE config_name = 'important_announcement';";
+	$sql = "SELECT * FROM " . OPENKORE_FORUM_CONFIG_TABLE . " WHERE config_name = 'important_announcement';";
 	if (!($result = $db->sql_query($sql))) {
 		message_die(GENERAL_ERROR, 'Cannot fetch configuration values.', 'Error', __LINE__, __FILE__, $sql);
 	}
