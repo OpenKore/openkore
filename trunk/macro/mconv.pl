01ba314d3d125e32330c2389f5fcfca0b514936c
arachnophobia <arachnophobia@3e610c8b-890d-0410-8877-b158ba2967cd>
2005-12-12 17:44:35 +0000

    support for 0.8 macros (automacros still incomplete), workaround for @return and stuff

--- trunk/macro/mconv.pl
+++ trunk/macro/mconv.pl
@@ -1,7 +1,7 @@
 #!/usr/bin/perl
 #
 # mconv
-# utility for converting version 0.9.x macros to 1.0.x.
+# utility for converting version 0.8.x & 0.9.x macros to 1.0.x.
 #
 
 use strict;
@@ -15,7 +15,7 @@ use Macro::Data;
 my $Version = sprintf("%d.%02d", q$Revision 1.1.1.1$ =~ /(\d+)\.(\d+)/);
 
 if (!defined $ARGV[0]) {
-  print "macro converter\n\tconvert 0.9.x macros to 1.0.x.\n\trelease $Version\n";
+  print "macro converter\n\tconvert 0.8.x and 0.9.x macros to 1.0.x.\n\trelease $Version\n";
   die "usage: $0 <filename>\n"
 }
 
@@ -23,6 +23,8 @@ my $file = $ARGV[0];
 
 if (! -r $file) {die "file $file does not exist or is not readable.\n"}
 if (!parseMacroFile($file)) {die "parsing $file failed.\n"}
+
+print "# converted by mconv.pl $Version\n\n";
 
 my $warning_tpl = "# WARNING: variable names must begin with a letter and\n".
                    "\t# must not contain anything other than letters or digits.\n".
@@ -46,6 +48,9 @@ foreach my $am (keys %automacro) {
         print "\t$cnt ".$automacro{$am}->{$cnt}[0]."\n";
       }
     } else {
+      if ($cnt eq 'pm' && $automacro{$am}->{$cnt} =~ /\|/) {
+        $automacro{$am}->{$cnt} =~ s/^(.*?)\|(.*?)$/"$1", $2/;
+      }
       print "\t$cnt ".$automacro{$am}->{$cnt}."\n";
     }
   }
@@ -54,6 +59,7 @@ foreach my $am (keys %automacro) {
 
 foreach my $m (keys %macro) {
   print "macro $m {\n";
+  my @mbuffer;
   foreach my $cnt (@{$macro{$m}}) {
     my $warning = 0;
     
@@ -65,6 +71,17 @@ foreach my $m (keys %macro) {
 #      while ($cnt =~ s/\$\$(\.?[a-z][a-z\d]*)/\$\{\$$1\}/gi) {} # varvars
     }
 
+    # keywords for 0.8 macros
+    while (my ($var) = $cnt =~ /\@(cart|inventory|storage|store|player|vender)\s+[^(]/) {
+      my ($arg) = $cnt =~ /$var\s+(.*?)(\s|$)/;
+      my $arg2 = $arg; $arg2 =~ s/_/ /g;
+      $arg = quotemeta $arg;
+      $cnt =~ s/\@$var $arg/\@$var ($arg2)/g;
+    }
+    while ($cnt =~ /\@npc\s+[^(]/) {
+      my ($x, $y) = $cnt =~ /\@npc\s+(.*?)\s+(.*?)(\s|$)/;
+      $cnt =~ s/\@npc\s+$x\s+$y/\@npc \($x $y\)/g;
+    }
     # @pause, @release, @log, @call
     if ($cnt =~ /^\@pause\s/ || $cnt =~ /^\@release\s/ ||
         $cnt =~ /^\@log\s/ || $cnt =~ /^\@call\s/) {
@@ -78,11 +95,17 @@ foreach my $m (keys %macro) {
       if ($var =~ /^[a-z][a-z\d]*$/i) {$cnt = "\$$var = $val"}
 #      elsif ($var =~ /^\$\.?[a-z][a-z\d]*/i) {$cnt = "\${$var} = $val"} # varvars
       else {$cnt = $warning_tpl."\$$var = $val"}
+    } elsif ($cnt eq '@return') {
+      unshift @mbuffer, "\$atReturn = \$.pos \$.map";
+      $cnt = "do move \$atReturn";
     } else {
       if (!$warning) {$cnt = "do $cnt"}
       else {$cnt = $warning_tpl."do ".$cnt}
     }
-    print "\t$cnt\n";
+    push @mbuffer, "$cnt";
+  }
+  foreach my $mb (@mbuffer) {
+    print "\t$mb\n";
   }
   print "}\n\n";
 }
