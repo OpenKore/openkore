5d81e3ee0323b704b2b2ad27cacf616eea5f16d7
kaliwanagan <kaliwanagan@users.sourceforge.net>
2006-01-11 15:02:34 +0000

    add illusionist's portuguese translation

--- trunk/openkore/src/po/update.sh
+++ trunk/openkore/src/po/update.sh
@@ -3,7 +3,7 @@
 # updates openkore.pot and *.po, and compiles *.po to .mo.
 set -e
 
-LANGUAGES="tl id"
+LANGUAGES="tl id pt"
 
 echo "Extracting messages from source..."
 xgettext -L perl --force-po -o openkore.pot --keyword=T --keyword=TF \
