386ff76f14b773d66f4b7ea5b5b7d204c45c7822
hakore <hakore@3e610c8b-890d-0410-8877-b158ba2967cd>
2006-01-15 18:07:25 +0000

    Removed the auto-generation of adminPassword when it is blank; authentication is now disabled by default when adminPassword is not set

--- trunk/openkore/openkore.pl
+++ trunk/openkore/openkore.pl
@@ -273,10 +273,10 @@ Plugins::callHook('start3');
 if ($config{'adminPassword'} eq 'x' x 10) {
 	Log::message(T("\nAuto-generating Admin Password due to default...\n"));
 	configModify("adminPassword", vocalString(8));
-} elsif ($config{'adminPassword'} eq '') {
-	# This is where we protect the stupid from having a blank admin password
-	Log::message(T("\nAuto-generating Admin Password due to blank...\n"));
-	configModify("adminPassword", vocalString(8));
+#} elsif ($config{'adminPassword'} eq '') {
+#	# This is where we protect the stupid from having a blank admin password
+#	Log::message(T("\nAuto-generating Admin Password due to blank...\n"));
+#	configModify("adminPassword", vocalString(8));
 } elsif ($config{'secureAdminPassword'} eq '1') {
 	# This is where we induldge the paranoid and let them have session generated admin passwords
 	Log::message(T("\nGenerating session Admin Password...\n"));
