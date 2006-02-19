c1fc1345f2bf0783572e5d947294d86064627ecf
hongli <hongli@3e610c8b-890d-0410-8877-b158ba2967cd>
2006-02-19 21:19:27 +0000

    Fix compatibility with old ActivePerl.

--- trunk/openkore/src/Interface/Console/Win32.pm
+++ trunk/openkore/src/Interface/Console/Win32.pm
@@ -335,73 +335,107 @@ sub title {
 	}
 }
 
-#IRGB
-#8421
+# IRGB
+# 8421
 
-%fgcolors = (
-	'reset'		=> $ATTR_NORMAL,
-	'default'	=> $ATTR_NORMAL,
-
-	'black'		=> $FG_BLACK,
+# Deal with ActivePerl version incompatibilities
+if (defined($main::ATTR_NORMAL)) {
+	%fgcolors = (
+	'reset'		=> $main::ATTR_NORMAL,
+	'default'	=> $main::ATTR_NORMAL,
+	'black'		=> $main::FG_BLACK,
 	'darkgray'	=> FOREGROUND_INTENSITY(),
 	'darkgrey'	=> FOREGROUND_INTENSITY(),
+	'darkred'	=> $main::FG_RED,
+	'red'		=> $main::FG_LIGHTRED,
+	'darkgreen'	=> $main::FG_GREEN,
+	'green'		=> $main::FG_LIGHTGREEN,
+	'brown'		=> $main::FG_BROWN,
+	'yellow'	=> $main::FG_YELLOW,
+	'darkblue'	=> $main::FG_BLUE,
+	'blue'		=> $main::FG_LIGHTBLUE,
+	'darkmagenta'	=> $main::FG_MAGENTA,
+	'magenta'	=> $main::FG_LIGHTMAGENTA,
+	'darkcyan'	=> $main::FG_CYAN,
+	'cyan'		=> $main::FG_LIGHTCYAN,
+	'gray'		=> $main::FG_GRAY,
+	'grey'		=> $main::FG_GRAY,
+	'white'		=> $main::FG_WHITE,
+	);
+} else {
+	%fgcolors = (
+	'reset'		=> $Win32::Console::ATTR_NORMAL,
+	'default'	=> $Win32::Console::ATTR_NORMAL,
+	'black'		=> $Win32::Console::FG_BLACK,
+	'darkgray'	=> FOREGROUND_INTENSITY(),
+	'darkgrey'	=> FOREGROUND_INTENSITY(),
+	'darkred'	=> $Win32::Console::FG_RED,
+	'red'		=> $Win32::Console::FG_LIGHTRED,
+	'darkgreen'	=> $Win32::Console::FG_GREEN,
+	'green'		=> $Win32::Console::FG_LIGHTGREEN,
+	'brown'		=> $Win32::Console::FG_BROWN,
+	'yellow'	=> $Win32::Console::FG_YELLOW,
+	'darkblue'	=> $Win32::Console::FG_BLUE,
+	'blue'		=> $Win32::Console::FG_LIGHTBLUE,
+	'darkmagenta'	=> $Win32::Console::FG_MAGENTA,
+	'magenta'	=> $Win32::Console::FG_LIGHTMAGENTA,
+	'darkcyan'	=> $Win32::Console::FG_CYAN,
+	'cyan'		=> $Win32::Console::FG_LIGHTCYAN,
+	'gray'		=> $Win32::Console::FG_GRAY,
+	'grey'		=> $Win32::Console::FG_GRAY,
+	'white'		=> $Win32::Console::FG_WHITE,
+	);
+}
 
-	'darkred'	=> $FG_RED,
-	'red'		=> $FG_LIGHTRED,
-
-	'darkgreen'	=> $FG_GREEN,
-	'green'		=> $FG_LIGHTGREEN,
-
-	'brown'		=> $FG_BROWN,
-	'yellow'	=> $FG_YELLOW,
-	
-	'darkblue'	=> $FG_BLUE,
-	'blue'		=> $FG_LIGHTBLUE,
-
-	'darkmagenta'	=> $FG_MAGENTA,
-	'magenta'	=> $FG_LIGHTMAGENTA,
-	
-	'darkcyan'	=> $FG_CYAN,
-	'cyan'		=> $FG_LIGHTCYAN,
-
-	'gray'		=> $FG_GRAY,
-	'grey'		=> $FG_GRAY,
-	'white'		=> $FG_WHITE,
-);
-
-#  I  R  G  B
-#128 64 32 16
-
-%bgcolors = (
-	''		=> $BG_BLACK,
-	'default'	=> $BG_BLACK,
-
-	'black'		=> $BG_BLACK,
+#   I  R  G  B
+# 128 64 32 16
+if (defined($main::BG_BLACK)) {
+	%bgcolors = (
+	''		=> $main::BG_BLACK,
+	'default'	=> $main::BG_BLACK,
+	'black'		=> $main::BG_BLACK,
 	'darkgray'	=> BACKGROUND_INTENSITY(),
 	'darkgrey'	=> BACKGROUND_INTENSITY(),
-
-	'darkred'	=> $BG_RED,
-	'red'		=> $BG_LIGHTRED,
-
-	'darkgreen'	=> $BG_GREEN,
-	'green'		=> $BG_LIGHTGREEN,
-
-	'brown'		=> $BG_BROWN,
-	'yellow'	=> $BG_YELLOW,
-	
-	'darkblue'	=> $BG_BLUE,
-	'blue'		=> $BG_LIGHTBLUE,
-
-	'darkmagenta'	=> $BG_MAGENTA,
-	'magenta'	=> $BG_LIGHTMAGENTA,
-	
-	'darkcyan'	=> $BG_CYAN,
-	'cyan'		=> $BG_LIGHTCYAN,
-
-	'gray'		=> $BG_GRAY,
-	'grey'		=> $BG_GRAY,
-	'white'		=> $BG_WHITE,
-);
+	'darkred'	=> $main::BG_RED,
+	'red'		=> $main::BG_LIGHTRED,
+	'darkgreen'	=> $main::BG_GREEN,
+	'green'		=> $main::BG_LIGHTGREEN,
+	'brown'		=> $main::BG_BROWN,
+	'yellow'	=> $main::BG_YELLOW,
+	'darkblue'	=> $main::BG_BLUE,
+	'blue'		=> $main::BG_LIGHTBLUE,
+	'darkmagenta'	=> $main::BG_MAGENTA,
+	'magenta'	=> $main::BG_LIGHTMAGENTA,
+	'darkcyan'	=> $main::BG_CYAN,
+	'cyan'		=> $main::BG_LIGHTCYAN,
+	'gray'		=> $main::BG_GRAY,
+	'grey'		=> $main::BG_GRAY,
+	'white'		=> $main::BG_WHITE,
+	);
+} else {
+	%bgcolors = (
+	''		=> $Win32::Console::BG_BLACK,
+	'default'	=> $Win32::Console::BG_BLACK,
+	'black'		=> $Win32::Console::BG_BLACK,
+	'darkgray'	=> BACKGROUND_INTENSITY(),
+	'darkgrey'	=> BACKGROUND_INTENSITY(),
+	'darkred'	=> $Win32::Console::BG_RED,
+	'red'		=> $Win32::Console::BG_LIGHTRED,
+	'darkgreen'	=> $Win32::Console::BG_GREEN,
+	'green'		=> $Win32::Console::BG_LIGHTGREEN,
+	'brown'		=> $Win32::Console::BG_BROWN,
+	'yellow'	=> $Win32::Console::BG_YELLOW,
+	'darkblue'	=> $Win32::Console::BG_BLUE,
+	'blue'		=> $Win32::Console::BG_LIGHTBLUE,
+	'darkmagenta'	=> $Win32::Console::BG_MAGENTA,
+	'magenta'	=> $Win32::Console::BG_LIGHTMAGENTA,
+	'darkcyan'	=> $Win32::Console::BG_CYAN,
+	'cyan'		=> $Win32::Console::BG_LIGHTCYAN,
+	'gray'		=> $Win32::Console::BG_GRAY,
+	'grey'		=> $Win32::Console::BG_GRAY,
+	'white'		=> $Win32::Console::BG_WHITE,
+	);
+}
 
 
 1 #end of module
