f99c6a5b6fbea8b224d285703ba12a03929502b2
hongli <hongli@users.sourceforge.net>
2006-04-23 10:54:50 +0000

    Split the AI() function into a seperate file

--- openkore/trunk/src/AI.pm
+++ openkore/trunk/src/AI.pm
@@ -1,3 +1,18 @@
+#########################################################################
+#  OpenKore - AI
+#  Copyright (c) OpenKore Team
+#
+#  This software is open source, licensed under the GNU General Public
+#  License, version 2.
+#  Basically, this means that you're allowed to modify and distribute
+#  this software. However, if you distribute modified versions, you MUST
+#  also distribute the source code.
+#  See http://www.gnu.org/licenses/gpl.html for the full license.
+#
+#  $Revision$
+#  $Id$
+#
+#########################################################################
 # Helper functions for managing @ai_seq.
 #
 # Eventually, @ai_seq should never be referenced directly, and then it can be
