b68a9d60f73b0477aee7e4ea4d961ad1620dd55f
hongli <hongli@3e610c8b-890d-0410-8877-b158ba2967cd>
2006-05-27 10:59:10 +0000

    HttpReader::pullData: - Make sure buffer is null-terminated. - Correctly handle errors. - Write unit test for testing pullData under error conditions.

--- openkore/trunk/src/test/unittests.pl
+++ openkore/trunk/src/test/unittests.pl
@@ -10,8 +10,16 @@ if ($^O eq 'MSWin32') {
 	push @tests, qw(HttpReaderTest);
 }
 
+@tests = @ARGV if (@ARGV);
 foreach my $module (@tests) {
-	require "${module}.pm";
+	eval {
+		require "${module}.pm";
+	};
+	if ($@) {
+		$@ =~ s/\(\@INC contains: .*?\) //s;
+		print STDERR "Cannot load unit test $module:\n$@\n";
+		exit 1;
+	}
 	my $start = "${module}::start";
 	$start->();
 }
