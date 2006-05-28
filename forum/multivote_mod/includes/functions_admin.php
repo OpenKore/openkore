0ea57f5229ceee4f3742c324c11640a491e34282
hongli <hongli@3e610c8b-890d-0410-8877-b158ba2967cd>
2006-05-28 08:49:32 +0000

    Branch phpbb 2.0.20 to multivote_mod 1.4.2

---
+++ forum/multivote_mod/includes/functions_admin.php
@@ -0,0 +1,191 @@
+<?php
+/***************************************************************************
+ *                            functions_admin.php
+ *                            -------------------
+ *   begin                : Saturday, Feb 13, 2001
+ *   copyright            : (C) 2001 The phpBB Group
+ *   email                : support@phpbb.com
+ *
+ *   $Id: functions_admin.php,v 1.5.2.5 2005/09/14 19:16:21 acydburn Exp $
+ *
+ *
+ ***************************************************************************/
+
+/***************************************************************************
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 2 of the License, or
+ *   (at your option) any later version.
+ *
+ *
+ ***************************************************************************/
+
+//
+// Simple version of jumpbox, just lists authed forums
+//
+function make_forum_select($box_name, $ignore_forum = false, $select_forum = '')
+{
+	global $db, $userdata;
+
+	$is_auth_ary = auth(AUTH_READ, AUTH_LIST_ALL, $userdata);
+
+	$sql = 'SELECT f.forum_id, f.forum_name
+		FROM ' . CATEGORIES_TABLE . ' c, ' . FORUMS_TABLE . ' f
+		WHERE f.cat_id = c.cat_id 
+		ORDER BY c.cat_order, f.forum_order';
+	if ( !($result = $db->sql_query($sql)) )
+	{
+		message_die(GENERAL_ERROR, 'Couldn not obtain forums information', '', __LINE__, __FILE__, $sql);
+	}
+
+	$forum_list = '';
+	while( $row = $db->sql_fetchrow($result) )
+	{
+		if ( $is_auth_ary[$row['forum_id']]['auth_read'] && $ignore_forum != $row['forum_id'] )
+		{
+			$selected = ( $select_forum == $row['forum_id'] ) ? ' selected="selected"' : '';
+			$forum_list .= '<option value="' . $row['forum_id'] . '"' . $selected .'>' . $row['forum_name'] . '</option>';
+		}
+	}
+
+	$forum_list = ( $forum_list == '' ) ? '<option value="-1">-- ! No Forums ! --</option>' : '<select name="' . $box_name . '">' . $forum_list . '</select>';
+
+	return $forum_list;
+}
+
+//
+// Synchronise functions for forums/topics
+//
+function sync($type, $id = false)
+{
+	global $db;
+
+	switch($type)
+	{
+		case 'all forums':
+			$sql = "SELECT forum_id
+				FROM " . FORUMS_TABLE;
+			if ( !($result = $db->sql_query($sql)) )
+			{
+				message_die(GENERAL_ERROR, 'Could not get forum IDs', '', __LINE__, __FILE__, $sql);
+			}
+
+			while( $row = $db->sql_fetchrow($result) )
+			{
+				sync('forum', $row['forum_id']);
+			}
+		   	break;
+
+		case 'all topics':
+			$sql = "SELECT topic_id
+				FROM " . TOPICS_TABLE;
+			if ( !($result = $db->sql_query($sql)) )
+			{
+				message_die(GENERAL_ERROR, 'Could not get topic ID', '', __LINE__, __FILE__, $sql);
+			}
+
+			while( $row = $db->sql_fetchrow($result) )
+			{
+				sync('topic', $row['topic_id']);
+			}
+			break;
+
+	  	case 'forum':
+			$sql = "SELECT MAX(post_id) AS last_post, COUNT(post_id) AS total 
+				FROM " . POSTS_TABLE . "  
+				WHERE forum_id = $id";
+			if ( !($result = $db->sql_query($sql)) )
+			{
+				message_die(GENERAL_ERROR, 'Could not get post ID', '', __LINE__, __FILE__, $sql);
+			}
+
+			if ( $row = $db->sql_fetchrow($result) )
+			{
+				$last_post = ( $row['last_post'] ) ? $row['last_post'] : 0;
+				$total_posts = ($row['total']) ? $row['total'] : 0;
+			}
+			else
+			{
+				$last_post = 0;
+				$total_posts = 0;
+			}
+
+			$sql = "SELECT COUNT(topic_id) AS total
+				FROM " . TOPICS_TABLE . "
+				WHERE forum_id = $id";
+			if ( !($result = $db->sql_query($sql)) )
+			{
+				message_die(GENERAL_ERROR, 'Could not get topic count', '', __LINE__, __FILE__, $sql);
+			}
+
+			$total_topics = ( $row = $db->sql_fetchrow($result) ) ? ( ( $row['total'] ) ? $row['total'] : 0 ) : 0;
+
+			$sql = "UPDATE " . FORUMS_TABLE . "
+				SET forum_last_post_id = $last_post, forum_posts = $total_posts, forum_topics = $total_topics
+				WHERE forum_id = $id";
+			if ( !$db->sql_query($sql) )
+			{
+				message_die(GENERAL_ERROR, 'Could not update forum', '', __LINE__, __FILE__, $sql);
+			}
+			break;
+
+		case 'topic':
+			$sql = "SELECT MAX(post_id) AS last_post, MIN(post_id) AS first_post, COUNT(post_id) AS total_posts
+				FROM " . POSTS_TABLE . "
+				WHERE topic_id = $id";
+			if ( !($result = $db->sql_query($sql)) )
+			{
+				message_die(GENERAL_ERROR, 'Could not get post ID', '', __LINE__, __FILE__, $sql);
+			}
+
+			if ( $row = $db->sql_fetchrow($result) )
+			{
+				if ($row['total_posts'])
+				{
+					// Correct the details of this topic
+					$sql = 'UPDATE ' . TOPICS_TABLE . ' 
+						SET topic_replies = ' . ($row['total_posts'] - 1) . ', topic_first_post_id = ' . $row['first_post'] . ', topic_last_post_id = ' . $row['last_post'] . "
+						WHERE topic_id = $id";
+
+					if (!$db->sql_query($sql))
+					{
+						message_die(GENERAL_ERROR, 'Could not update topic', '', __LINE__, __FILE__, $sql);
+					}
+				}
+				else
+				{
+					// There are no replies to this topic
+					// Check if it is a move stub
+					$sql = 'SELECT topic_moved_id 
+						FROM ' . TOPICS_TABLE . " 
+						WHERE topic_id = $id";
+
+					if (!($result = $db->sql_query($sql)))
+					{
+						message_die(GENERAL_ERROR, 'Could not get topic ID', '', __LINE__, __FILE__, $sql);
+					}
+
+					if ($row = $db->sql_fetchrow($result))
+					{
+						if (!$row['topic_moved_id'])
+						{
+							$sql = 'DELETE FROM ' . TOPICS_TABLE . " WHERE topic_id = $id";
+			
+							if (!$db->sql_query($sql))
+							{
+								message_die(GENERAL_ERROR, 'Could not remove topic', '', __LINE__, __FILE__, $sql);
+							}
+						}
+					}
+
+					$db->sql_freeresult($result);
+				}
+			}
+			break;
+	}
+	
+	return true;
+}
+
+?>
