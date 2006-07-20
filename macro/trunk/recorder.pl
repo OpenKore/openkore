726012823792a6c32c18ed0cf57c1e2ad6f88e64
arachnophobia <arachnophobia@users.sourceforge.net>
2006-07-20 18:12:33 +0000

    removed the "read documentation" check. Users don°t read the documentation anyway and they get the "password" from other installation guides.

--- macro/trunk/recorder.pl
+++ macro/trunk/recorder.pl
@@ -8,8 +8,8 @@
 package recorder;
 
 my $Version = sprintf("1.0 %s %s %s",
-   q$Date$
-   =~ /(\d{4}-\d{2}-\d{2}) (\d{2}:\d{2}:\d{2}) ([+-]\d{4})/);
+	q$Date$
+	=~ /(\d{4}-\d{2}-\d{2}) (\d{2}:\d{2}:\d{2}) ([+-]\d{4})/);
 
 use strict;
 use Plugins;
@@ -20,77 +20,77 @@ use Log qw(message error);
 Plugins::register('recorder', 'records a macro', \&Unload);
 
 my $chooks = Commands::register(
-            ['record', "Macro recorder", \&commandHandler]
+	['record', "Macro recorder", \&commandHandler]
 );
 
 my $hooks = Plugins::addHooks(
-            ['Commands::run/post', \&cmdRun, undef],
-            ['Command_post', \&cmdRun, undef]
+	['Commands::run/post', \&cmdRun, undef],
+	['Command_post', \&cmdRun, undef]
 );
 
 my %macros;
 my %current = (
-  'name' => undef,
-  'recording' => 0,
-  'time' => 0,
-  'macro' => []
+	'name' => undef,
+	'recording' => 0,
+	'time' => 0,
+	'macro' => []
 );
 
 sub Unload {
-  message "macro recorder unloading, cleaning up.\n", "macro";
-  Plugins::delHooks($hooks);
-  Commands::unregister($chooks);
+	message "macro recorder unloading, cleaning up.\n", "macro";
+	Plugins::delHooks($hooks);
+	Commands::unregister($chooks)
 }
 
 sub commandHandler {
-  my (undef, $arg) = @_;
-  if (!defined $arg) {
-    message "Macro recorder\n", "list";
-    message "start recording with 'record NAME', end with 'record stop'\n";
-    message "save to file with 'record save'\n";
-  } elsif ($arg eq 'stop') {
-    if ($current{recording}) {
-      message "stopped recording ".$current{name}."\n", "list";
-      $macros{$current{name}} = $current{macro};
-      $current{recording} = 0;
-      $current{macro} = [];
-    } else {
-      error "nothing to do.\n";
-    }
-  } elsif ($arg eq 'save') {
-    my $filename = $Settings::control_folder."/macros-".time.".rec";
-    open MACRO, "> $filename";
-    foreach my $m (keys %macros) {
-      print MACRO "macro $m {\n";
-      foreach my $s (@{$macros{$m}}) {print MACRO "\t$s\n"}
-      print MACRO "}\n";
-    }
-    close MACRO;
-    message "macros written to $filename.\n";
-  } else {
-    message "recording macro $arg\n", "list";
-    $current{name} = $arg;
-    $current{recording} = 1;
-    $current{time} = time;
-  }
+	my (undef, $arg) = @_;
+	if (!defined $arg) {
+		message "Macro recorder\n", "list";
+		message "start recording with 'record NAME', end with 'record stop'\n";
+		message "save to file with 'record save'\n"
+	} elsif ($arg eq 'stop') {
+		if ($current{recording}) {
+			message "stopped recording ".$current{name}."\n", "list";
+			$macros{$current{name}} = $current{macro};
+			$current{recording} = 0;
+			$current{macro} = []
+		} else {
+			error "nothing to do.\n"
+		}
+	} elsif ($arg eq 'save') {
+		my $filename = $Settings::control_folder."/macros-".time.".rec";
+		open MACRO, "> $filename";
+		foreach my $m (keys %macros) {
+			print MACRO "macro $m {\n";
+			foreach my $s (@{$macros{$m}}) {print MACRO "\t$s\n"}
+			print MACRO "}\n"
+		}
+		close MACRO;
+		message "macros written to $filename.\n"
+	} else {
+		message "recording macro $arg\n", "list";
+		$current{name} = $arg;
+		$current{recording} = 1;
+		$current{time} = time
+	}
 }
-                                                    
+
 sub cmdRun {
-  return unless $current{recording};
-  my ($trigger, $args) = @_;
-  my $cmd = "do ";
-  if ($trigger eq 'Commands::run/post') {
-    $cmd .= $args->{switch}." ".$args->{args}
-  } else {
-    $cmd .= $args->{input}
-  }
-  $cmd =~ s/\s*$//g;
-  return if $cmd =~ /^do record/ || $cmd =~ /^do macro/;
-  my $pause = time - $current{time};
-  if ($pause == 1) {push @{$current{macro}}, "pause"}
-  if ($pause > 1)  {push @{$current{macro}}, "pause $pause"}
-  push @{$current{macro}}, $cmd;
-  $current{time} = time;
+	return unless $current{recording};
+	my ($trigger, $args) = @_;
+	my $cmd = "do ";
+	if ($trigger eq 'Commands::run/post') {
+		$cmd .= $args->{switch}." ".$args->{args}
+	} else {
+		$cmd .= $args->{input}
+	}
+	$cmd =~ s/\s*$//g;
+	return if $cmd =~ /^do record/ || $cmd =~ /^do macro/;
+	my $pause = time - $current{time};
+	if ($pause == 1) {push @{$current{macro}}, "pause"}
+	if ($pause > 1)  {push @{$current{macro}}, "pause $pause"}
+	push @{$current{macro}}, $cmd;
+	$current{time} = time
 }
 
 1;
