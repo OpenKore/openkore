63d0c61524e0bd20ceb98921656545833fa238d1
hongli <hongli@3e610c8b-890d-0410-8877-b158ba2967cd>
2006-08-20 11:53:45 +0000

    Backport the Whirlpool-based statistics reporting system from SVN; bump version to 1.6.9

--- openkore/branches/branch-1-6/src/Misc.pm
+++ openkore/branches/branch-1-6/src/Misc.pm
@@ -87,6 +87,7 @@ our @EXPORT = (
 	getResponse
 	getSpellName
 	headgearName
+	initUserSeed
 	itemLog_clear
 	look
 	lookAtPosition
@@ -1257,6 +1258,38 @@ sub headgearName {
 	}
 
 	return main::itemName({nameID => $itemID});
+}
+
+##
+# void initUserSeed()
+#
+# Generate a unique seed for the current user and save it to
+# a file, or load the seed from that file if it exists.
+sub initUserSeed {
+	my $seedFile = "$Settings::logs_folder/seed.txt";
+	my $f;
+
+	if (-f $seedFile) {
+		if (open($f, "<", $seedFile)) {
+			binmode $f;
+			$userSeed = <$f>;
+			$userSeed =~ s/\n.*//s;
+			close($f);
+		} else {
+			$userSeed = '0';
+		}
+	} else {
+		$userSeed = '';
+		for (0..10) {
+			$userSeed .= rand(2 ** 49);
+		}
+
+		if (open($f, ">", $seedFile)) {
+			binmode $f;
+			print $f $userSeed;
+			close($f);
+		}
+	}
 }
 
 sub itemLog_clear {
