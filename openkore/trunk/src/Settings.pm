f0d692b06bb0b03eac722eb776bb637afb977d3e
hongli <hongli@3e610c8b-890d-0410-8877-b158ba2967cd>
2006-09-09 15:25:17 +0000

    Bump version number to 1.9.3.

--- openkore/trunk/src/Settings.pm
+++ openkore/trunk/src/Settings.pm
@@ -43,9 +43,9 @@ our @EXPORT_OK = qw(parseArguments addConfigFile delConfigFile %sys $VERSION);
 # Constants
 # Translation Comment: Strings for the name and version of the application
 our $NAME = 'OpenKore';
-our $VERSION = '1.9.2';
+our $VERSION = '1.9.3';
 # Translation Comment: Version String
-our $CVS = Translation::T(" (beta)");
+our $CVS = Translation::T(" (SVN version)");
 our $WEBSITE = 'http://www.openkore.com/';
 # Translation Comment: Version String
 our $versionText = "*** $NAME ${VERSION}${CVS} - " . Translation::T("Custom Ragnarok Online client") . " ***\n***   $WEBSITE   ***\n";
