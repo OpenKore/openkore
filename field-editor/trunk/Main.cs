87a1edf475485853c250ac12aec492de4bb0f794
hongli <hongli@users.sourceforge.net>
2006-10-02 14:20:19 +0000

    Catch unexpected exceptions.

--- field-editor/trunk/Main.cs
+++ field-editor/trunk/Main.cs
@@ -1,4 +1,5 @@
 using System;
+using System.IO;
 using Gtk;
 
 namespace FieldEditor {
@@ -6,8 +7,26 @@ namespace FieldEditor {
 class MainClass	{
 	public static void Main(string[] args) {
 		Application.Init();
-		new MainWindow();
-		Application.Run();
+		try {
+			new MainWindow();
+			Application.Run();
+		} catch (Exception e) {
+			string msg = "An unexpected occured:\n{0}";
+
+			try { 
+				TextWriter s = new StreamWriter("exception.txt");
+				s.WriteLine(e.Message + ":\n" + e.StackTrace);
+				s.Close();
+				msg += "\n\nA stack trace has been written to exception.txt.";
+			} finally {
+				Dialog d = new MessageDialog(null, DialogFlags.Modal,
+					MessageType.Error, ButtonsType.Ok,
+					msg, e.Message);
+				d.Resizable = false;
+				d.Run();
+				d.Destroy();
+			}
+		}
 	}
 }
 
