d7b57dc931a0f12152a54a632ad2dca660693074
bibian <bibian@users.sourceforge.net>
2006-10-23 04:37:51 +0000

    Initial import of tele-search-v2.pl

---
+++ plugins/trunk/tele-search-v2.pl
@@ -0,0 +1,72 @@
+package telesearchV2;
+# Made by Bibian
+# Version 1.0.4
+# OpenKore 1.9.3 or higher required
+
+use strict;
+use Plugins;
+use Settings;
+use Log qw(message error);
+use Utils;
+use AI;
+use Globals;
+
+Plugins::register('Tele-Search v2', 'Alternative tele-search v2.', \&unload);
+my $hooks = Plugins::addHooks(
+	['AI_pre',\&search, undef],
+	['map_loaded', \&MapLoaded, undef]
+	['packet/sendMapLoaded', \&MapLoaded, undef]    
+);
+
+my ($maploaded,$allow_tele,$disallow);
+
+sub unload {
+    Plugins::delHooks($hooks);
+	message("Unloaded Teleport search v2.\n","info");
+}
+       
+sub MapLoaded {
+	$maploaded = 1;
+}
+
+sub checkIdle {
+	if (AI::action eq "move" && AI::action(1) eq "route" || AI::action eq "route" && !AI::inQueue("attack","skill_use")) {
+		return 1;
+	} else {
+		return 0;
+	}
+}
+
+sub search {
+	if ($config{'teleport_search'} && Misc::inLockMap() && $timeout{'ai_teleport_search'}{'timeout'}) {
+	
+		if ($maploaded && !$allow_tele)  {
+			$timeout{'ai_teleport_search'}{'time'} = time;
+			$allow_tele = 1;
+                        
+		# Check if we're allowed to teleport, if map is loaded, timeout has passed and we're just looking for targets.
+		} elsif ($maploaded && $allow_tele && timeOut($timeout{'ai_teleport_search'}) && checkIdle()) {
+			message("Attemping to tele-search.\n","info");
+			$allow_tele = 0;
+			$maploaded = 0;
+			# Attempt to teleport, give error and unload plugin if we cant.
+			if (!Misc::useTeleport(1)) {
+				error ("Fatal error, we dont have the skill nor items to teleport! - Unloading plugin.\n");
+				unload();
+				return;
+			} 
+
+		# We're doing something else besides looking for monsters, reset the timeout.
+		} elsif (!checkIdle()) {
+			$timeout{'ai_teleport_search'}{'time'} = time;
+		}
+		
+        # Oops! timeouts.txt is missing a crucial value, we'll kill the plugin here...
+        } elsif (!$timeout{'ai_teleport_search'}{'timeout'}) {
+			error ("timeouts.txt missing setting! Add 'ai_teleport_search 5' to make the plugin work.\n");
+			unload();
+			return;
+        }
+}
+
+return 1;
