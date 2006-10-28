fcb7f834bb742b53e7c9c71ebafa5bbeeadde208
darkfate_ <darkfate_@3e610c8b-890d-0410-8877-b158ba2967cd>
2006-10-28 22:50:59 +0000

    Rewritten to "less messy" code, add a warning

--- plugins/trunk/pp_workaround/pp_workaround.pl
+++ plugins/trunk/pp_workaround/pp_workaround.pl
@@ -23,50 +23,40 @@
 
 package PP_Workaround;
 
+BEGIN {
+	push(@INC,"$Plugins::current_plugin_folder");
+}
+
 use strict;
 use Plugins;
 use Log qw(message warning error debug);
 use Globals;
 use Skills;
-
-BEGIN {
-	push(@INC,"$Plugins::current_plugin_folder");
-}
-
 use Win32::GuiTest;
 
-my $packSize0089 = 0;
-my $packet0113 = 0;
+my $packSize0089;
+my $packet0113;
 
 my $hooks = Plugins::addHooks(
-		['RO_sendMsg_pre', \&getPacketProto],
-	  ['packet_pre/sendAttack', \&sendAttack],
-	  ['packet_pre/sendSit', \&sendAttack],
-	  ['packet_pre/sendStand', \&sendAttack],
-	  ['packet_pre/sendSkillUse', \&sendSkillUse],
-	  ['packet_outMangle/0089', \&manglePacket0089],
-	 	['packet_outMangle/0113', \&manglePacket0113],
+		['start3', \&startWarning],
+		['RO_sendMsg_pre', \&main],
+	  ['packet_pre/sendAttack', \&control_0089],
+	  ['packet_pre/sendSkillUse', \&control_0113],
+	  ['packet_pre/sendSit', \&control_0089],
+	  ['packet_pre/sendStand', \&control_0089],
+	  ['packet_outMangle/0089', \&manglePackets],
+	  ['packet_outMangle/0113', \&manglePackets],
 );
+
+
 
 sub Reload { Unload(); }
 sub Unload { Plugins::delHooks($hooks); }
+sub manglePackets { 1; }
+sub startWarning { warning("\nTHIS PLUGIN IS HIGHLY EXPERIMENTAL. USE AT YOUR OWN RISK.\n"); }
 
 Plugins::register('PP_Workaround', 'Packet Padding workaround', \&Unload, \&Reload);
 
-########## Mangle all attack and skill packets except teleport ##########
-sub manglePacket0113 {
-	my ($hook, $args) = @_;
-	
-	if (unpack("H*", $args->{data}) =~ /1A000000/i) { # Don't Mangle Teleport Packets (Fly and BfWing)
-		# I think the chances that this pattern matches on an other Packet is low (Users should report if it happens)
-		return 0;
-	}	
-	return 1;
-}
-
-sub manglePacket0089 { 1; }
-
-########## Sends a key to the Ragnarok client ##########
 sub sendKey {
 	my $key = shift;
 	
@@ -75,123 +65,144 @@ sub sendKey {
 		Win32::GuiTest::SetForegroundWindow($_);
 		Win32::GuiTest::SendKeys("{$key}");
 	}
-	message "$key key pressed \n";
+	#message "$key key pressed \n";
 }
 
-########## Captures the packet(0113) and length (0089) ##########
-sub getPacketProto {
-	my ($hook, $args) = @_;
+sub main {
+	my $hook = shift;
+	my $args = shift;
 
 	my $switch = $args->{switch};
 	my $msg = $args->{msg};
 	my $sendMsg = $args->{realMsg};
 	
-	if ($switch eq '007E') { # Client sync
-		message "SYNC by RO client\n";
-    # Reset packetsize
-		$packSize0089 = 0;
-		$packet0113 = 0;
-		#sendKey("INSERT");
-		#sendKey("F1")
+	if ($switch eq '007E') {
+		#message "SYNC by RO client\n";
+		undef $packSize0089;
+		undef $packet0113;
 		
-	} elsif ($switch eq '0072') { # If map loaded
-		$packSize0089 = 0; # Reset packet size
-		$packet0113 = 0;
+	} elsif ($switch eq '0072') {
+		undef $packSize0089;
+		undef $packet0113;
 	
-	} elsif ($switch eq '0089') { # Attack/Sit/Stand
-		if (!$packSize0089) { # Packet length not yet captured
+	} elsif ($switch eq '0089') {
+		if (!$packSize0089) {
 			$packSize0089 = length($msg);
+			#message "len_pre: $packSize0089\n";
 		} 
-				
-	} elsif ($switch eq '0113') { # Skill use
-		if (!$packet0113) { # Packet length not yet captured
-			$packet0113 = unpack("H*", $msg);
+	} elsif ($switch eq '0113') {
+		if (!$packet0113) {
+			$packet0113 = $msg;
 		}
 	} # End if
 }
 
-########## Checks condition and calls the attack function ##########
-sub sendAttack {
-	my ($hook, $args) = @_;
+sub control_0089 {
+	my $hook = shift;
+	my $args = shift;
 	
-	message "$hook\n";
+	#message "$hook\n";
 	$args->{return} = 1;
-	$args->{msg} = "";
-	message "packet prototype: $packSize0089\n";
-	if ($packSize0089 <= 0) { # Don't continue if packet length is unavailable
-			sendKey("INSERT");
-			return;
-	}
-	$args->{msg} = getPadding0089($hook, $args->{monID}, $args->{flag});
-}
-
-########## Checks condition and calls the skill function ##########
-sub sendSkillUse {
-	my ($hook, $args) = @_;
 	
-	message "$hook\n";
-	$args->{return} = 1;
-	$args->{msg} = "";
-	message "packet prototype: $packet0113\n";
-	if (!$packet0113) {
-		sendKey("F1");
-		return;
-	}
-	$args->{msg} = getPadding0113($hook, $args->{ID}, $args->{lv}, $args->{targetID});
-}
-
-########## Creates and returns the attack packet ##########	
-sub getPadding0089 {
-	my ($hook, $monID, $flag) = @_;
-	my $n;
-	my $sRecord;
-	my $msg;
+	if ($packSize0089) {
 	
-	if ($packSize0089 > 0) {
-		my $sFlag = pack("V", $flag);
-		
-		if ($flag == 0x02 || $flag == 0x03) { # Sit/Stand
-			$sRecord = "\x00" x ($packSize0089 - 8) . $sFlag;
-			$msg = pack("S", 0x0089) . pack("S", $packSize0089) . $sRecord;
+		if ($hook eq 'packet_pre/sendSit') {
+			$args->{msg} = sendSit();
 			
-		} else { # End if # Attack
-			if ($packSize0089 & 1) {
-				$n = ($packSize0089 + 2 - 45) >> 1; # 45 -> Minimum odd number
-				$sRecord = "\x00" x ($n + 31) . $monID . "\x00" x $n . $sFlag;
-				
-			} else {
-				$n = ($packSize0089 - 20) >> 1; # 20 -> Minimum even number
-				$sRecord = "\x00" x $n . $monID . "\x00" x ($n + 8). $sFlag;
-				}
-			$msg = pack("S", 0x0089) . pack("S", $packSize0089) . $sRecord;
+		} elsif ($hook eq 'packet_pre/sendStand') {
+			$args->{msg} = sendStand();
+		
+		} elsif ($hook eq 'packet_pre/sendAttack') {
+			#message "0089: $packSize0089\n";
+			$args->{msg} = sendAttack($args->{monID}, $args->{flag});
 		}
-		return $msg;
-	} # End if
-	return "";
+			
+	} else {
+		sendKey("INSERT");
+	}
 }
 
-########## Creates (substitution) and returns the skill packet ##########
-sub getPadding0113 {
-	my ($hook, $ID, $lv, $targetID) = @_;
-	my $msg;
+sub control_0113 {
+	my $hook = shift;
+	my $args = shift;
 	
-	my $selfSkill = Skills->new(name => $config{packetPadding_0_selfSkill});
-	my $selfSkill_ID = unpack("H*", pack("v", $selfSkill->id)); # Get the ID of the self skill
-	my $selfSkill_lvl = unpack("H*", pack("v", $config{packetPadding_0_selfSkill_lvl})); # Get the lvl of the self skill from the config
+	#message "$hook\n";
+	$args->{return} = 1;
 	
 	if ($packet0113) {
-		$ID = unpack("H*", pack("v", $ID)); # Unpack it as string, reversed
-		$lv = unpack("H*", pack("v", $lv)); # Unpack it as string, reversed
-
-		$msg = $packet0113;
-		$msg =~ s/$selfSkill_ID/$ID/i; # Replace the skill ID
-		$msg =~ s/$selfSkill_lvl/$lv/i; # Replace the skill lvl
-		substr($msg, -8) = unpack("H*", $targetID); # Replace the target
+		$args->{msg} = sendSkillUse($args->{ID}, $args->{lv}, $args->{targetID});
 		
-		$msg = pack("H*", $msg);
-		return $msg;
-		} # End if	
-	return "";
-	}
+	} else {
+		sendKey("F1");
+	} # End if 
+}
+
+sub sendSit {
+	my $msg =
+	pack("S", 0x0089) .
+	pack("S", $packSize0089) .
+	"\x00" x ($packSize0089 - 8) .
+	pack("V", 0x02);
+}
+
+sub sendStand {
+	my $msg =
+	pack("S", 0x0089) .
+	pack("S", $packSize0089) .
+	"\x00" x ($packSize0089 - 8) .
+	pack("V", 0x03);
+}
+
+
+sub sendAttack {
+	my $monID = shift;
+	my $flag = shift;
+	my $msg;
+	
+	if ($packSize0089 & 1) {
+		my $n = ($packSize0089 + 2 - 45) >> 1;
+		
+		$msg =
+			pack("S", 0x0089) .
+			pack("S", $packSize0089) .
+			"\x00" x ($n + 31) . $monID .
+			"\x00" x $n .
+			pack("V", $flag);
+			
+		} else {
+			my $n = ($packSize0089 - 20) >> 1;
+				
+			$msg =
+				pack("S", 0x0089) .
+				pack("S", $packSize0089) .
+				"\x00" x $n . $monID .
+				"\x00" x ($n + 8).
+				pack("V", $flag);
+				
+		} # End if
+	return $msg;
+}
+
+sub sendSkillUse {
+	my $ID = shift;
+	my $lv = shift;
+	my $targetID = shift;
+	
+	
+	my $selfSkill = Skills->new(name => $config{packetPadding_0_selfSkill});
+	my $selfSkill_ID = unpack("H*", pack("v", $selfSkill->id));
+	my $selfSkill_lvl = unpack("H*", pack("v", $config{packetPadding_0_selfSkill_lvl}));
+	
+	$ID = unpack("H*", pack("v", $ID));
+	$lv = unpack("H*", pack("v", $lv));
+
+	my $msg = unpack("H*", $packet0113);
+	$msg =~ s/$selfSkill_ID/$ID/i;
+	$msg =~ s/$selfSkill_lvl/$lv/i;
+	substr($msg, -8) = unpack("H*", $targetID) if $targetID;
+
+	$msg = pack("H*", $msg);
+	return $msg;
+}
 
 1;
