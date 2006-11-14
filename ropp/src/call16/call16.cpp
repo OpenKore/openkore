3205e6eee12f4acb7813459270356f16740a2d03
hongli <hongli@users.sourceforge.net>
2006-11-14 22:10:47 +0000

    Correctly set cdecl for processor functions.

--- ropp/src/call16/call16.cpp
+++ ropp/src/call16/call16.cpp
@@ -14,7 +14,9 @@
 
 #include "call16.h"
 
-dword (*funcs[])(dword)={
+typedef CDECL dword (*ProcessorFunction)(dword);
+
+ProcessorFunction funcs[] ={
 	func0, func1, func2, func3, func4, func5, func6, func7,
 	func8, func9, funcA, funcB, funcC, funcD, funcE, funcF
 };
