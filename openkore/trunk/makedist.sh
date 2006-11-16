e63aa8f52a49be89daeda73db1830f4f8ae94a8e
hongli <hongli@users.sourceforge.net>
2006-11-16 10:35:08 +0000

    Upgrade to SCons 0.96.93. Add hints for developers in Makefile.

--- openkore/trunk/makedist.sh
+++ openkore/trunk/makedist.sh
@@ -7,16 +7,16 @@ VERSION=1.9.3
 DIRS=(.
 	src
 	src/build
-	src/scons-local-0.96.91
-	src/scons-local-0.96.91/SCons
-	src/scons-local-0.96.91/SCons/Node
-	src/scons-local-0.96.91/SCons/Optik
-	src/scons-local-0.96.91/SCons/Options
-	src/scons-local-0.96.91/SCons/Platform
-	src/scons-local-0.96.91/SCons/Scanner
-	src/scons-local-0.96.91/SCons/Script
-	src/scons-local-0.96.91/SCons/Sig
-	src/scons-local-0.96.91/SCons/Tool
+	src/scons-local-0.96.93
+	src/scons-local-0.96.93/SCons
+	src/scons-local-0.96.93/SCons/Node
+	src/scons-local-0.96.93/SCons/Optik
+	src/scons-local-0.96.93/SCons/Options
+	src/scons-local-0.96.93/SCons/Platform
+	src/scons-local-0.96.93/SCons/Scanner
+	src/scons-local-0.96.93/SCons/Script
+	src/scons-local-0.96.93/SCons/Sig
+	src/scons-local-0.96.93/SCons/Tool
 	src/po
 	src/test
 	src/Actor
