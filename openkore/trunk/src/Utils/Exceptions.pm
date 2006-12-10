32f6c7fbbec8da9e0b37b2600fd3b6afe69289eb
hongli <hongli@3e610c8b-890d-0410-8877-b158ba2967cd>
2006-12-10 17:32:39 +0000

    Remove Utils::CommonExceptions to Utils::Exceptions and put our modified caught() method in there, to prevent conflicts with the original Exception::Class.

---
+++ openkore/trunk/src/Utils/Exceptions.pm
@@ -0,0 +1,68 @@
+#########################################################################
+#  OpenKore - Exception utility functions and common exception objects
+#
+#  Copryight (c) 2006 OpenKore Development Team
+#
+#  This software is open source, licensed under the GNU General Public
+#  License, version 2.
+#  Basically, this means that you're allowed to modify and distribute
+#  this software. However, if you distribute modified versions, you MUST
+#  also distribute the source code.
+#  See http://www.gnu.org/licenses/gpl.html for the full license.
+#########################################################################
+##
+# MODULE DESCRIPTION: Exception utility functions and common exception objects.
+#
+# This module provides some exception utility functions, to be used in combination
+# with <a href="http://cpan.uwinnipeg.ca/htdocs/Exception-Class/Exception/Class.html">Exeption::Class</a>.
+#
+# It also defines the following commonly-used exception objects:
+# `l
+# - IOException - Input/output exception occured.
+# - FileNotFoundException - A file is not found.
+# - UTF8MalformedException - Invalid UTF-8 data encountered.
+# `l`
+package Utils::Exceptions;
+
+use strict;
+use Exporter;
+use base qw(Exporter);
+use Scalar::Util;
+
+use Exception::Class (
+	'IOException',
+	'FileNotFoundException'  => { isa => 'IOException' },
+	'UTF8MalformedException' => { fields => 'line' }
+);
+
+our @EXPORT = qw(caught);
+
+##
+# Object caught(class)
+# exception: The class name of an exception object.
+#
+# Checks whether the currently caught exception ($@) is of type $class.
+# Returns $@ if it is, undef otherwise. This function is allows you to
+# write in try-catch-style syntax.
+#
+# This symbol is exported by default.
+#
+# Example:
+# eval {
+#     SomeException->throw(error => "foo");
+# };
+# if (my $e = caught("SomeException")) {
+#     print "SomeException caught: " . $e->error . "\n";
+# } elsif (my $e = caught("OtherException")) {
+#     print "OtherException caught: " . $e->error . "\n";
+# } elsif ($@) {
+#     # Rethrow exception.
+#     die $@;
+# }
+sub caught {
+    my $e = $@;
+    return unless Scalar::Util::blessed($e) && $e->isa( $_[0] );
+    return $e;
+}
+
+1;
