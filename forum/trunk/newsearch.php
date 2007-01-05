24efe054a7f05d997f27490539d61aed584e0d31
vcl_kore <vcl_kore@3e610c8b-890d-0410-8877-b158ba2967cd>
2007-01-05 09:53:46 +0000

    Use Google instead of Yahoo as search engine.

--- forum/trunk/newsearch.php
+++ forum/trunk/newsearch.php
@@ -6,6 +6,6 @@ if (empty($_GET['q'])) {
 	$template->pparse('body');
 	PhpBB::finalize();
 } else {
-	header("Location: http://search.yahoo.com/search?p=site%3Aopenkore.com+" . urlencode($_GET['q']));
+	header("Location: http://www.google.com/search?as_q=" . urlencode($_GET['q']) . "&hl=en&hs=nsk&num=10&btnG=Google+Search&as_epq=&as_oq=&as_eq=&lr=&as_ft=i&as_filetype=&as_qdr=all&as_nlo=&as_nhi=&as_occt=any&as_dt=i&as_sitesearch=openkore.com&as_rights=&safe=off");
 }
 ?>
