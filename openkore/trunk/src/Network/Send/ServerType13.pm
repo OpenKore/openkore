e1a89ae4b9a1b92b9b501c4f077f12662b527401
vcl_kore <vcl_kore@users.sourceforge.net>
2007-02-02 07:46:04 +0000

    Change module descriptions

--- openkore/trunk/src/Network/Send/ServerType13.pm
+++ openkore/trunk/src/Network/Send/ServerType13.pm
@@ -1,6 +1,6 @@
 #########################################################################
-#  OpenKore - Packet sending
-#  This module contains functions for sending packets to the server.
+#  OpenKore - Network subsystem
+#  This module contains functions for sending messages to the server.
 #
 #  This software is open source, licensed under the GNU General Public
 #  License, version 2.
@@ -8,7 +8,6 @@
 #  this software. However, if you distribute modified versions, you MUST
 #  also distribute the source code.
 #  See http://www.gnu.org/licenses/gpl.html for the full license.
-#
 #########################################################################
 #
 #  ServerType for rRO Server
