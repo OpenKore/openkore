301b6d42cc6730243c6aecacbdd87424428024fe
roger-kore <roger-kore@3e610c8b-890d-0410-8877-b158ba2967cd>
2007-02-05 11:18:43 +0000

    Now working on SVN 1.9 Thanks to h4rry84 Added a commented line to use this plugin on private serves

--- plugins/trunk/arrowCraft.pl
+++ plugins/trunk/arrowCraft.pl
@@ -8,11 +8,11 @@ package arrowCraft;
 # How to install this thing..:
 #
 # in control\config.txt add:
-#  
+# 
 #arrowCraft Zargon {
-#	delay 3 # specify number of seconds to wait before crafting another arrow; defaults to 1
-#	inInventory Silver Arrow <10
-#	onAction attack
+#       delay 3 # specify number of seconds to wait before crafting another arrow; defaults to 1
+#       inInventory Silver Arrow <10
+#       onAction attack
 #}
  
 use strict;
@@ -28,30 +28,32 @@ Plugins::register('arrowCraft', 'use arrow craft without SP', \&Unload);
 my $hook = Plugins::addHook('AI_pre', \&cast);
  
 sub Unload {
-	Plugins::delHook('AI_pre', $hook);
+        Plugins::delHook('AI_pre', $hook);
 }
  
 my %delay;
  
 sub cast {
-	my $prefix = "arrowCraft_";
-	my $i = 0;
-	
-	while (exists $config{$prefix.$i}) {
-		my $invIndex = main::findIndexString_lc($char->{'inventory'}, "name", $config{$prefix.$i});
-		my $item = $char->{'inventory'}[$invIndex];
-		$delay{'timeout'} = $config{$prefix.$i."_delay"} || 1;
-		if ((defined $invIndex) && 
-			(main::timeOut(\%delay)) &&
-			($item->{'amount'} > 0) && 
-			(main::checkSelfCondition($prefix.$i)))
-		{
-			sendArrowCraft($net,$item->{'nameID'});
-			message ("You use Arrow Craft on item: " . $item->{'name'} . "\n", "selfSkill");
-			$delay{'time'} = time;
-		}
-		$i++;
-	}
+        my $prefix = "arrowCraft_";
+        my $i = 0;
+       
+        while (exists $config{$prefix.$i}) {
+                my $invIndex = main::findIndexString_lc($char->{'inventory'}, "name", $config{$prefix.$i});
+                my $item = $char->{'inventory'}[$invIndex];
+                $delay{'timeout'} = $config{$prefix.$i."_delay"} || 1;
+                if ((defined $invIndex) &&
+                        (main::timeOut(\%delay)) &&
+                        ($item->{'amount'} > 0) &&
+                        (main::checkSelfCondition($prefix.$i)))
+                {
+						#Remove the comment bellow if you play on eathena server
+						#Commands::run('arrowcraft use');
+                        sendArrowCraft($net,$item->{'nameID'});
+                        message ("You use Arrow Craft on item: " . $item->{'name'} . "\n", "selfSkill");
+                        $delay{'time'} = time;
+                }
+                $i++;
+        }
 }
  
 return 1;
