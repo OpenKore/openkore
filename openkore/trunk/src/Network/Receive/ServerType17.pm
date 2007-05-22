88b4973c6ef8f59274cdf3941456b875d2219b89
kLabMouse <kLabMouse@users.noreply.github.com>
2007-05-22 14:22:46 +0000

    Fix another bug. =((

--- openkore/trunk/src/Network/Receive/ServerType17.pm
+++ openkore/trunk/src/Network/Receive/ServerType17.pm
@@ -2,13 +2,54 @@
 package Network::Receive::ServerType17;
 
 use strict;
-use Network::Receive::ServerType0;
-use base qw(Network::Receive::ServerType0);
+use Network::Receive ();
+use base qw(Network::Receive);
+use Time::HiRes qw(time usleep);
+
+use AI;
+use Globals qw($char %timeout $net %config @chars $conState $conState_tries $messageSender);
+use Log qw(message warning error debug);
+use Translation;
+use Network;
+use Utils qw(makeCoords);
 
 sub new {
 	my ($class) = @_;
 	my $self = $class->SUPER::new;
 	return $self;
+}
+
+sub map_loaded {
+	my ($self, $args) = @_;
+	$net->setState(Network::IN_GAME);
+	undef $conState_tries;
+	$char = $chars[$config{char}];
+
+	if ($net->version == 1) {
+		$net->setState(4);
+		message(T("Waiting for map to load...\n"), "connection");
+		ai_clientSuspend(0, 10);
+		main::initMapChangeVars();
+	} else {
+		message	T("Requesting guild information...\n"), "info";
+		$messageSender->sendGuildInfoRequest($net);
+
+		# Replies 01B6 (Guild Info) and 014C (Guild Ally/Enemy List)
+		$messageSender->sendGuildRequest($net, 0);
+
+		# Replies 0166 (Guild Member Titles List) and 0154 (Guild Members List)
+		$messageSender->sendGuildRequest($net, 1);
+		message(T("You are now in the game\n"), "connection");
+		$messageSender->sendMapLoaded();
+		$timeout{'ai'}{'time'} = time;
+	}
+
+	$char->{pos} = {};
+	makeCoords($char->{pos}, $args->{coords});
+	$char->{pos_to} = {%{$char->{pos}}};
+	message(TF("Your Coordinates: %s, %s\n", $char->{pos}{x}, $char->{pos}{y}), undef, 1);
+
+	$messageSender->sendIgnoreAll("all") if ($config{ignoreAll});
 }
 
 sub account_server_info {
