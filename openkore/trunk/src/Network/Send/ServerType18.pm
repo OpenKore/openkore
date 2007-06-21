75b08cb56a215f6c3f3fd9fd6a643a0abba90fa4
vcl_kore <vcl_kore@users.sourceforge.net>
2007-06-21 13:37:12 +0000

    Add servertype 18 for iRO map login support; Bump version number to 'what-will-become-2.0.0'. Don't call this 2.0.0 because people just don't seem to get that SVN is the development version and not the final release.

---
+++ openkore/trunk/src/Network/Send/ServerType18.pm
@@ -0,0 +1,56 @@
+#########################################################################
+#  OpenKore - Network subsystem
+#  This module contains functions for sending messages to the server.
+#
+#  This software is open source, licensed under the GNU General Public
+#  License, version 2.
+#  Basically, this means that you're allowed to modify and distribute
+#  this software. However, if you distribute modified versions, you MUST
+#  also distribute the source code.
+#  See http://www.gnu.org/licenses/gpl.html for the full license.
+#########################################################################
+# iRO (International) as of June 21 2007.
+package Network::Send::ServerType18;
+
+use strict;
+use Globals qw($char $syncSync $net %config);
+use Network::Send::ServerType11;
+use base qw(Network::Send::ServerType11);
+use Log qw(error debug);
+use I18N qw(stringToBytes);
+use Utils qw(getTickCount getHex getCoordString);
+
+sub new {
+	my ($class) = @_;
+	return $class->SUPER::new(@_);
+}
+
+# Create a random byte string of a specified length.
+sub createRandomBytes {
+	my ($length) = @_;
+	my $str = '';
+	for (1..$length) {
+		$str .= chr(rand(256));
+	}
+	return $str;
+}
+
+sub sendMapLogin {
+	my ($self, $accountID, $charID, $sessionID, $sex) = @_;
+	my $msg;
+
+	$sex = 0 if ($sex > 1 || $sex < 0); # Sex can only be 0 (female) or 1 (male)
+	$msg = pack("C*", 0xF3, 0x00) .
+		createRandomBytes(3) .
+		$charID .
+		$accountID .
+		chr(0) .
+		$sessionID .
+		pack("V", getTickCount()) .
+		createRandomBytes(9) .
+		chr($sex) .
+		createRandomBytes(5);
+	$self->sendToServer($msg);
+}
+
+1;
