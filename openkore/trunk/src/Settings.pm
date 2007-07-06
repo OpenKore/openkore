d3283fed4f0d89d55c38ecc5ec67b7caf139dc73
vcl_kore <vcl_kore@3e610c8b-890d-0410-8877-b158ba2967cd>
2007-07-06 10:29:01 +0000

    Change version number to 2.0.0. Update README.

--- openkore/trunk/src/Settings.pm
+++ openkore/trunk/src/Settings.pm
@@ -59,9 +59,10 @@ our @EXPORT_OK = qw(parseArguments addConfigFile delConfigFile %sys $VERSION);
 
 # Translation Comment: Strings for the name and version of the application
 our $NAME = 'OpenKore';
-our $VERSION = 'what-will-become-2.0.0';
+our $VERSION = '2.0.0';
 # Translation Comment: Version String
-our $CVS = T(" (SVN version)");
+#our $CVS = T(" (SVN version)");
+our $CVS = "";
 our $WEBSITE = 'http://www.openkore.com/';
 # Translation Comment: Version String
 our $versionText = "*** $NAME ${VERSION}${CVS} - " . T("Custom Ragnarok Online client") . " ***\n***   $WEBSITE   ***\n";
