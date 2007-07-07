89ea72fbb3ff4aa6817575e02c40d9018947f152
vcl_kore <vcl_kore@users.sourceforge.net>
2007-07-07 07:43:23 +0000

    Bump version to what-will-become-2.0.1

--- openkore/trunk/src/Settings.pm
+++ openkore/trunk/src/Settings.pm
@@ -59,10 +59,9 @@ our @EXPORT_OK = qw(parseArguments addConfigFile delConfigFile %sys $VERSION);
 
 # Translation Comment: Strings for the name and version of the application
 our $NAME = 'OpenKore';
-our $VERSION = '2.0.0';
+our $VERSION = 'what-will-become-2.0.1';
 # Translation Comment: Version String
-#our $CVS = T(" (SVN version)");
-our $CVS = "";
+our $CVS = T(" (SVN version)");
 our $WEBSITE = 'http://www.openkore.com/';
 # Translation Comment: Version String
 our $versionText = "*** $NAME ${VERSION}${CVS} - " . T("Custom Ragnarok Online client") . " ***\n***   $WEBSITE   ***\n";
