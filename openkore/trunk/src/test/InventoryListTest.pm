60b80d88a5f3409ffa10caed04f96967a8cd97ba
vcl_kore <vcl_kore@users.sourceforge.net>
2007-07-09 13:16:50 +0000

    Watch for item name changes in InventoryList.

--- openkore/trunk/src/test/InventoryListTest.pm
+++ openkore/trunk/src/test/InventoryListTest.pm
@@ -22,11 +22,13 @@ sub run {
 	my ($self) = @_;
 	$self->SUPER::run();
 	$self->testGetAndRemoveByName();
+	$self->testNameChange();
 }
 
 # overloaded
 sub init {
 	my ($self) = @_;
+	$count = 0;
 	$self->{list} = new InventoryList();
 	for (my $i = 1; $i <= 6; $i++) {
 		$self->{"item$i"} = $self->createTestObject();
@@ -96,6 +98,37 @@ sub testGetAndRemoveByName {
 	ok(!defined $list->getByName($self->{item4}{name}));
 	ok(!defined $list->getByName($self->{item5}{name}));
 	ok(!defined $list->getByName($self->{item6}{name}));
+	$list->checkValidity();
+}
+
+sub testNameChange {
+	my ($self) = @_;
+	$self->init();
+	my $list = $self->{list};
+	my $item1 = $self->{item1};
+	my $item2 = $self->{item2};
+	my $item3 = $self->{item3};
+
+	$list->add($item1);
+	$item1->setName("Jellopy");
+	ok($list->getByName("Jellopy") == $item1);
+	ok(!$list->getByName("Item 1"));
+	$list->checkValidity();
+
+	$list->add($item2);
+	$list->add($item3);
+	ok($list->getByName("Jellopy") == $item1);
+	ok($list->getByName("Item 2") == $item2);
+	ok($list->getByName("Item 3") == $item3);
+	ok(!$list->getByName("Item 1"));
+	$list->checkValidity();
+
+	$item2->setName("Gold");
+	ok($list->getByName("Jellopy") == $item1);
+	ok($list->getByName("Gold") == $item2);
+	ok($list->getByName("Item 3") == $item3);
+	ok(!$list->getByName("Item 1"));
+	ok(!$list->getByName("Item 2"));
 	$list->checkValidity();
 }
 
