f6c3f48eecf2583737ba6dabfee07ae5650b0565
vcl_kore <vcl_kore@3e610c8b-890d-0410-8877-b158ba2967cd>
2007-07-20 15:13:18 +0000

    Bump version number to 2.0.1.

--- openkore/trunk/src/ErrorHandler.pm
+++ openkore/trunk/src/ErrorHandler.pm
@@ -80,7 +80,7 @@ sub errorHandler {
 
 	# Create the errors.txt error log.
 	my $log = '';
-	$log .= "$Settings::NAME version ${Settings::VERSION}${Settings::CVS}\n" if (defined $Settings::VERSION);
+	$log .= "$Settings::NAME version ${Settings::VERSION}${Settings::SVN}\n" if (defined $Settings::VERSION);
 	$log .= "\@ai_seq = @Globals::ai_seq\n" if (defined @Globals::ai_seq);
 	$log .= "Network state = $Globals::conState\n" if (defined $Globals::conState);
 	$log .= "Network handler = " . Scalar::Util::blessed($Globals::net) . "\n" if ($Globals::net);
--- openkore/trunk/src/Settings.pm
+++ openkore/trunk/src/Settings.pm
@@ -59,12 +59,12 @@ our @EXPORT_OK = qw(parseArguments addConfigFile delConfigFile %sys $VERSION);
 
 # Translation Comment: Strings for the name and version of the application
 our $NAME = 'OpenKore';
-our $VERSION = 'what-will-become-2.0.1';
+our $VERSION = '2.0.1';
 # Translation Comment: Version String
-our $CVS = T(" (SVN version)");
+our $SVN = T(" (SVN version)");
 our $WEBSITE = 'http://www.openkore.com/';
 # Translation Comment: Version String
-our $versionText = "*** $NAME ${VERSION}${CVS} - " . T("Custom Ragnarok Online client") . " ***\n***   $WEBSITE   ***\n";
+our $versionText = "*** $NAME ${VERSION}${SVN} - " . T("Custom Ragnarok Online client") . " ***\n***   $WEBSITE   ***\n";
 our $welcomeText = TF("Welcome to %s.", $NAME);
 our $MAX_READ = 30000;
 
