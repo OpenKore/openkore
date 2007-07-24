a1b58197cf2bfbb17aefde47805d8639ef56ab70
vcl_kore <vcl_kore@3e610c8b-890d-0410-8877-b158ba2967cd>
2007-07-24 09:20:51 +0000

    Fix building of wxstart.exe.

--- openkore/trunk/src/build/compile.bat
+++ openkore/trunk/src/build/compile.bat
@@ -1,3 +1,3 @@
 cd ..\..
-perlapp --trim Pod::Usage --icon src\build\openkore.ico --lib src --norunlib --nologo --force --exe start.exe start.pl
+perlapp --trim Pod::Usage;I18N::Langinfo --icon src\build\openkore.ico --lib src --norunlib --nologo --force --exe start.exe start.pl
 pause
