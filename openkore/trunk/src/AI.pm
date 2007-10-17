521d17581ce6ed0e3d74b5f1afd8aed732a06552
kLabMouse <kLabMouse@users.noreply.github.com>
2007-10-17 07:03:31 +0000

    Added 'StorageZeny' for storage condition, and 'zeny' in 'SelfCondition'. (thanks to 4epT and piroJOKE)

--- openkore/trunk/src/AI.pm
+++ openkore/trunk/src/AI.pm
@@ -595,6 +595,9 @@ sub ai_skillUse2 {
 # Returns 0 otherwise.
 sub ai_storageAutoCheck {
 	return 0 if ($char->getSkillLevel(new Skill(handle => 'NV_BASIC')) < 6);
+	if ($config{StorageZeny}) {
+		return 0 if ($char->{zenny} < $config{StorageZeny});
+	}
 	foreach my $item (@{$char->inventory->getItems()}) {
 		next if ($item->{equipped});
 		my $control = Misc::items_control($item->{name});
