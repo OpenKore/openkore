2960f9ca8f4c70cf86da75f3904d5a43ac0e88f5
kLabMouse <kLabMouse@users.noreply.github.com>
2007-11-09 18:12:05 +0000

    Another Fix...

--- openkore/trunk/src/Network/Receive.pm
+++ openkore/trunk/src/Network/Receive.pm
@@ -5934,7 +5934,7 @@ sub init_prefix_enc {
 	}
 	my $w = ($c[6]<<12) + ($c[4]<<8) + ($c[7]<<4) + $c[1];
 	$enc_val1 = ($c[2]<<12) + ($c[3]<<8) + ($c[5]<<4) + $c[8];
-	$enc_val2 = (((($enc_val1 ^ 0x0000F3AC) + $w) << 16) | (($args->{param1} ^ 0x000049DF) + $w)) ^ $args->{param2};
+	$enc_val2 = (((($enc_val1 ^ 0x0000F3AC) + $w) << 16) | (($enc_val1 ^ 0x000049DF) + $w)) ^ $args->{param2};
 }
 
 sub switch_character {
