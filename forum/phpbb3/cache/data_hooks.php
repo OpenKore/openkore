6be27bde27497337dcb4e728fd791405660a2387
bibian <bibian@3e610c8b-890d-0410-8877-b158ba2967cd>
2008-04-02 09:37:52 +0000

    Delete cache files

--- forum/phpbb3/cache/data_hooks.php
+++
@@ -1,7 +0,0 @@
-<?php
-$expired = (time() > 1238589308) ? true : false;
-if ($expired) { return; }
-
-$data = array (
-);
-?>
--- forum/phpbb3/cache/tpl_prosilver_mcp_footer.html.php
+++
@@ -1,8 +0,0 @@
-</div>
-	<div class="clear"></div>
-
-	</div>
-	<span class="corners-bottom"><span></span></span></div>
-</div>
-
-<?php $this->_tpl_include('overall_footer.html'); ?>
