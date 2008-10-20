fdc0f8e2ba1d7fe310f16ffc66fd53a5e6f4f900
technologyguild <Technologyguild@users.sourceforge.net>
2008-10-20 13:29:45 +0000

    changed special keywords: - @venderprice(<indexID>) new special keywords: - @venderItem(<itemName>) - @listlenght(<$list>) - @listitem(<$listIndex>,<$list>) topic: http://forums.openkore.com/viewtopic.php?f=58&t=2986#p13300

--- macro/trunk/Macro/Parser.pm
+++ macro/trunk/Macro/Parser.pm
@@ -13,8 +13,8 @@ use List::Util qw(max min sum);
 use Log qw(message warning error);
 use Macro::Data;
 use Macro::Utilities qw(refreshGlobal getnpcID getItemIDs getItemPrice getStorageIDs getInventoryIDs
-	getPlayerID getVenderID getRandom getRandomRange getInventoryAmount getCartAmount
-	getShopAmount getStorageAmount getConfig getWord q4rx);
+	getPlayerID getVenderID getRandom getRandomRange getInventoryAmount getCartAmount getShopAmount
+	getStorageAmount getVendAmount getConfig getWord q4rx getArgFromList getListLenght);
 
 our ($rev) = q$Revision$ =~ /(\d+)/;
 
@@ -191,7 +191,9 @@ sub parseCmd {
 		elsif ($kw eq 'player')     {$ret = getPlayerID($arg)}
 		elsif ($kw eq 'vender')     {$ret = getVenderID($arg)}
 		elsif ($kw eq 'venderitem') {($ret) = getItemIDs($arg, \@::venderItemList)}
-		elsif ($kw eq 'venderprice'){($ret) = getItemPrice($arg, \@::venderItemList)}
+		elsif ($kw eq 'venderItem') {$ret = join ',', getItemIDs($arg, \@::venderItemList)}
+		elsif ($kw eq 'venderprice'){$ret = getItemPrice($arg, \@::venderItemList)}
+		elsif ($kw eq 'venderamount'){$ret = getVendAmount($arg, \@::venderItemList)}
 		elsif ($kw eq 'random')     {$ret = getRandom($arg)}
 		elsif ($kw eq 'rand')       {$ret = getRandomRange($arg)}
 		elsif ($kw eq 'invamount')  {$ret = getInventoryAmount($arg)}
@@ -201,6 +203,8 @@ sub parseCmd {
 		elsif ($kw eq 'config')     {$ret = getConfig($arg)}
 		elsif ($kw eq 'arg')        {$ret = getWord($arg)}
 		elsif ($kw eq 'eval')       {$ret = eval($arg)}
+		elsif ($kw eq 'listitem')   {$ret = getArgFromList($arg)}
+		elsif ($kw eq 'listlenght') {$ret = getListLenght($arg)}
 		return unless defined $ret;
 		return $cmd if $ret eq '_%_';
 		$targ = q4rx $targ;
