3c5abbb50a91f43810122fe4a5a14f5cda6d66ce
kaliwanagan <kaliwanagan@users.sourceforge.net>
2008-10-28 16:46:49 +0000

    Implement sli's change http://forums.openkore.com/viewtopic.php?p=9212#p9212

--- plugins/playerRecorder/trunk/playerRecorder.pl
+++ plugins/playerRecorder/trunk/playerRecorder.pl
@@ -1,76 +1,53 @@
-######################################################
-# This plugin is licensed under the GNU GPL          #
-# Copyright 2005 by isieo                            #
-# contact : - isieo <AT> *NOSPAM* G*MAIL <DOT> COM   #
-# -------------------------------------------------- #
-# -------------------------------------------------- #
-# playerrecorder.pl                                  #
-# Records Player's name together with AIDs           #
-# Usefull for players to findout other players' other#
-# characters...                                      #
-#                                                    #
-######################################################
+########################################################
+# This plugin is licensed under the GNU GPL            #
+# Copyright 2005 by isieo                              #
+# contact : - isieo <AT> *NOSPAM* G*MAIL <DOT> COM     #
+#                                                      #
+# Modified by sli (signedlongint@gmail.com)            #
+# ---------------------------------------------------- #
+# ---------------------------------------------------- #
+# playerrecorder.pl                                    #
+# Records Player's name together with AIDs             #
+# Usefull for players to find out other players' other #
+# characters...                                        #
+########################################################
 
 package playerRecord;
+
 use strict;
 use Plugins;
 use Log qw(message);
 use Globals;
 use Settings;
+use Actor;
 
 Plugins::register("prec", "playerRecord", \&on_unload, \&on_reload);
-my $hooks = Plugins::addHooks(
-        ['charNameUpdate', \&write_player],
-);
-my $datadir = $Plugins::current_plugin_folder;
+my $hook = Plugins::addHook('charNameUpdate', \&write_player, undef);
 
 sub on_unload {
-        # This plugin is about to be unloaded; remove hooks
-        Plugins::delHook("charNameUpdate", $hooks);
-}
-
-sub on_reload {
+   Plugins::delHook("charNameUpdate", $hook);
 }
 
 sub write_player {
-        my $hookname = shift;
-        my $args = shift;
-        my $targetId = unpack("V1",$args->{ID});
-        my $targetName = $args->{name};
-        my $file = "$datadir/players.txt";
+   my $hookname = shift;
+   my $args = shift;
+   my $targetId = unpack("V1",$args->{ID});
+   my $targetName = $args->{name};
+   my $aYou = Actor::get($accountID);
+   my $selfName = $char->name();
+   my $file = "$Settings::logs_folder/players_$selfName.txt";
 
-        my ($uId, $name);
-        my $exist=0;
-        my $line;
+   my ($uId, $name);
+   my $exist=0;
+   my $line;
 
-        if ($Settings::VERSION cmp '1.9.1' >= 0) {
-                open FILE, "<:utf8", $file;
-        } else {
-                open FILE, "< $file";
-        }
+   message "Player Exists: $targetName ($targetId)\n";
 
-        foreach (<FILE>){
-                next if (/^#/);
-                s/[\r\n]//g;
-                s/\s+$//g;
-                $line = $_;
-           ($uId, $name ) = $line =~ /^(\d+) (.*)$/;
-           if ($uId eq $targetId && $name eq $targetName){
-                $exist=1;
-           }
-        }
-        close FILE;
-
-        if (!$exist) {
-            message $name.$targetName."\n";
-            if ($Settings::VERSION cmp '1.9.1' >= 0) {
-                open FILE, ">>:utf8", $file;
-            } else {
-                open FILE, ">> $file";
-            }
-            print FILE "$targetId $targetName\n" if ($targetName) ;
-            close FILE;
-        }
+   open FILE, ">>:utf8", $file;
+   my $time=localtime time;
+   print FILE "[$time] $field{name}\t$targetId $targetName\n";
+   close FILE;
 }
 
-1; 
+1;
+
