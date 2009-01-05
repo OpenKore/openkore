905ca3a7ad62554f031d32619fa79aa919d831da
kLabMouse <kLabMouse@3e610c8b-890d-0410-8877-b158ba2967cd>
2009-01-05 17:21:49 +0000

    AI 2008 Project Fixed AI::EnvironmentQueue (loaded default parsers wrongly) Updated AI::AImoduleManager (It's in final stage) One more hit, and we will ROCK!

--- openkore/branches/experimental-ai2008/src/AI/AImoduleManager.pm
+++ openkore/branches/experimental-ai2008/src/AI/AImoduleManager.pm
@@ -18,7 +18,11 @@ use strict;
 use threads;
 use threads::shared;
 use Carp::Assert;
+use FindBin qw($RealBin);
+use List::Util qw(first);
+
 use Modules 'register';
+
 use AI::AImodule;
 use Utils::Set;
 use Utils::CallbackList;
@@ -34,29 +38,69 @@ use Utils qw(timeOut);
 # Create a new AI::AImoduleManager.
 sub new {
 	my ($class) = @_;
-	my %self = (
-		# Set<AI::AImodule>
-		# Indexed set of currently active modules.
-		activeModules => new Set(),
+	my $dir = "$RealBin/src/AI/AImodule";
+	my $self = {};
+	bless $self, $class;
 
-		# Array of IDs, that show in witch order to check AI::AIModules.
-		modules_list => [],
+	# Set<AI::AImodule>
+	# Indexed set of currently active modules.
+	$self->{activeModules} = new Set();
 
-		# Postponed mutex list
-		# Every mutex has time, to reactivate.
-		pospone_mutex_list => {},
+	# Array of IDs, that show in witch order to check AI::AIModules.
+	$self->{modules_list} = [];
 
-		# Hash of IDs and corresponding index in activeModules Set.
-		cache_modules_id => {},
+	# Postponed mutex list
+	# Every mutex has time, to reactivate.
+	$self->{pospone_mutex_list} = {};
 
-		# Whatever there is active AI module with exlusive marker.
-		# If it's not -1, then it's module ID running Exclusive Task.
-		activeExlusiveTask => -1,
+	# Hash of IDs and corresponding index in activeModules Set.
+	$self->{cache_modules_id} = {};
 
-		# Last generated ID.
-		lastID => 0
-	);
-	return bless \%self, $class;
+	# Whatever there is active AI module with exlusive marker.
+	# If it's not -1, then it's module ID running Exclusive Task.
+	$self->{activeExlusiveTask} = -1;
+
+	# Last generated ID.
+	$self->{lastID} = 0;
+
+	######
+	#
+	# Load default modules
+
+	# Read Directory with AI::AIModule's.
+	return if ( !opendir( DIR, $dir ) );
+	my @items;
+	@items = readdir DIR;
+	closedir DIR;
+
+	# Add all avalable command interpretters
+	foreach my $file (@items) {
+		if ( -f "$dir/$file" && $file =~ /\.(pm)$/ ) {
+			$file =~ s/\.(pm)$//;
+			my $module = "AI::AImodule::$file";
+			eval "use $module;";
+			if ($@) {
+				warning TF("Cannot load AI Module \"%s\".\nError Message: \n%s", $module, $@ );
+				next;
+			}
+			my $constructor = UNIVERSAL::can( $module, 'new' );
+			if ( !$constructor ) {
+				warning TF( "AI Module \"%s\" has no constructor.\n", $module );
+				next;
+			}
+			my $parse_msg = UNIVERSAL::can( $module, 'check' );
+			if ( !$parse_msg ) {
+				warning TF( "AI Module \"%s\" is not default.\n", $module );
+				next;
+			}
+			# call "$module::new($self). So that module can use our functions
+			my $ai_module = $constructor->( $module, $self );
+
+			$self->add($ai_module);
+		};
+	};
+
+	return $self;
 }
 
 ####################################
@@ -183,13 +227,13 @@ sub postpone {
 
 	lock ($self) if (is_shared($self));
 
-	my $time = {};
-	$time->{time} = time;
-	$time->{timeout} = $timeout;
+	my %time;
+	$time{time} = time;
+	$time{timeout} = $timeout;
 
-	$time = shared_clone($time) if (is_shared($self));
+	%time = shared_clone(%time) if (is_shared($self));
 
-	$self->{pospone_mutex_list}->{$mutex} = $time;
+	$self->{pospone_mutex_list}->{$mutex} = %time;
 
 	# Calculate Priorities, and Order all modules.
 	$self->_calc_priority();
@@ -221,12 +265,35 @@ sub iterate {
 
 	# Check every module, until all modules are checked,
 	# or module with exclusive morker will popup.
+	my @mutex_lock;
 	foreach my $id ($self->{modules_list}) {
+		my $index = $self->_get_index_by_id($id);
+		my $module = $self->{activeModules}->get($index);
+
+		# Block running module with locked mutexes
+		my $foud_mutex = undef;
+		foreach my $mutex (@{$module->getMutex()}) {
+			$foud_mutex = first { $_ eq $mutex } @mutex_lock;
+			last if (defined $foud_mutex);
+		};
+		next if (defined $foud_mutex);
+
+		# run module
 		if ($self->_run_module($id) > 0) {
+			# Add mutexes to mutex_lock array
+			push @mutex_lock, [$module->getMutex()];
+
+			# Out module with Exclusive marker produced a Task
+			# So just finish doing stuff
 			if ($self->{activeExlusiveTask} == $id) {
 				return;
 			};
 		};
+	};
+
+	# Do we need to recalc modules run list?
+	if ($self->_check_mutex_postpone_timeout() > 0) {
+		$self->_calc_priority();
 	};
 }
 
@@ -240,8 +307,29 @@ sub iterate {
 sub _calc_priority {
 	my ($self) = @_;
 
+	# Check out postponed mutexes
+	$self->_check_mutex_postpone_timeout();
 
-	# ToDo
+	# Place holder for our active modules
+	my @active_modules;
+
+	# add all ID's to out cative id's array	
+	foreach my $id (keys %{$self->{cache_modules_id}}) {
+		my $index = $self->_get_index_by_id($id);
+		my $module = $self->{activeModules}->get($index);
+
+		my $foud_mutex = undef;
+		foreach my $mutex (@{$module->getMutex()}) {
+			$foud_mutex = 1 if (exists $self->{pospone_mutex_list}->{$mutex});
+			last if (defined $foud_mutex);
+		};
+		next if (defined $foud_mutex);
+
+		push(@active_modules, $id);
+	};
+
+	# now sort active modules by their priority. (The bigger one wins).
+	@{$self->{modules_list}} = sort {$self->{activeModules}->get($self->_get_index_by_id($a))->getPriority() <=> $self->{activeModules}->get($self->_get_index_by_id($b))->getPriority()} @active_modules;
 }
 
 # generate new module ID
@@ -303,8 +391,11 @@ sub _cache_id {
 	my ($self, $id) = @_;
 
 	# Empty our Hash
-	$self->{cache_modules_id} = {};
-
+	foreach my $member (keys %{$self->{cache_modules_id}}) {
+		delete $self->{cache_modules_id}->{$member};
+	};
+	
+	# Fill our Cache
 	foreach my $module (@{$self->{activeModules}}) {
 		my $index = $self->{activeModules}->{keys}{$module};
 		my $id = $module->getID();
@@ -335,6 +426,23 @@ sub _get_index_by_id {
 	};
 
 	return -1;
+}
+
+# Return, whatever we need to recalculate modules order
+sub _check_mutex_postpone_timeout {
+	my ($self) = @_;
+
+	my $result;
+	$result = 0;
+
+	foreach my $mutex (keys %{$self->{pospone_mutex_list}}) {
+		my %time = $self->{pospone_mutex_list}->{$mutex};
+		if (timeOut(\%time)) {
+			delete $self->{pospone_mutex_list}->{$mutex};
+			$result = 1;
+		};
+	};
+	return $result;
 }
 
 #####################################
