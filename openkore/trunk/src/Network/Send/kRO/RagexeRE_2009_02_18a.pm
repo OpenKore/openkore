eae2d88219ebe55cc25d9778084b0ee36e2e56d7
Technologyguild <Technologyguild@users.sourceforge.net>
2009-12-16 05:18:06 +0000

    kRO ST: added a few packets

--- openkore/trunk/src/Network/Send/kRO/RagexeRE_2009_02_18a.pm
+++ openkore/trunk/src/Network/Send/kRO/RagexeRE_2009_02_18a.pm
@@ -21,9 +21,6 @@ use strict;
 use Network::Send::kRO::RagexeRE_2009_01_21a;
 use base qw(Network::Send::kRO::RagexeRE_2009_01_21a);
 
-use Log qw(message warning error debug);
-use Utils qw(getTickCount);
-
 sub new {
 	my ($class) = @_;
 	return $class->SUPER::new(@_);
