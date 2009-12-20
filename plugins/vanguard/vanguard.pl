48b2144b1de9ce5ad34582be1d6e2a5e095c756a
Technologyguild <Technologyguild@3e610c8b-890d-0410-8877-b158ba2967cd>
2009-12-20 19:07:04 +0000

    plugin: vanguard (added another server)

--- plugins/vanguard/vanguard.pl
+++ plugins/vanguard/vanguard.pl
@@ -1,20 +1,25 @@
- # OpenKore - Vanguard packet encryption
- # Copyright (C) 2009 Technology (credits to Sonder Köksal for discovering the algorithm and key in his antisex project)
+# OpenKore - Vanguard packet encryption
+# Copyright (C) 2009 Technology (credits to Soner Köksal for discovering the algorithm and key in his antisex project)
  
- # This program is free software; you can redistribute it and/or modify it under
- # the terms of the GNU General Public License as published by the Free Software
- # Foundation; either version 3 of the License, or (at your option) any later
- # version.
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation; either version 3 of the License, or (at your option) any later
+# version.
 
- # This program is distributed in the hope that it will be useful, but WITHOUT
- # ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- # FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
- # details.
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+# details.
 
- # You should have received a copy of the GNU General Public License along with
- # this program; if not, see <http://www.gnu.org/licenses/>.
+# You should have received a copy of the GNU General Public License along with
+# this program; if not, see <http://www.gnu.org/licenses/>.
  
- # usage: put in servers.txt under your server entry with key being the actual key: vanguard_key key
+# usage: put in servers.txt under your server entry with key being the actual key: vanguard_key key
+
+# the vanguard key consists of a part (len 16) of this entire key:
+#	0x98, 0x7F, 0xB6, 0xE0, 0x90, 0x4E, 0x83, 0xC6, 0xF6, 0x29, 0xC2, 0xE0, 0xBD, 0x22, 0x8A, 0xEA,
+#	0x67, 0x9C, 0xE0, 0x1E, 0x2D, 0xE0, 0x0E, 0xED, 0x9E, 0xE0, 0xB7, 0xF6, 0xE0, 0xBC, 0x75, 0x30,
+#	0xE0, 0xB3, 0x7E, 0xBC, 0x3F, 0x16, 0x00, 0x12, 0x21, 0xA4, 0xE7, 0x28
 
 package Vanguard;
 
@@ -87,7 +92,7 @@ sub encrypt {
 		$len += 14; # 4 bytes for sent packet count, 10 for hdd serial
 		$olen += 10; # 10 bytes for hdd serial
 	}
-	$len = (int($len / 16) + 1) * 16; # set length to the multiple of blocksize 16 >= then len
+	$len = (int($len / 16) + 1) * 16; # set length to the multiple of blocksize 16 >= len
 	$new_packet = $rijndael->Encrypt($new_packet, undef, $len, 0);
 	$len += 6;
 	$$old_packet = pack('v3', $len, $olen, 0xFACE) . $new_packet;
