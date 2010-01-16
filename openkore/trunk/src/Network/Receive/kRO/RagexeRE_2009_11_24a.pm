b043a66377506f2fe2b82e31e6d5bb5ef35369f3
Technologyguild <Technologyguild@users.sourceforge.net>
2010-01-16 10:42:53 +0000

    kRO ST: woops forgot to actually add the serverTypes from r7068 (only in dist)

---
+++ openkore/trunk/src/Network/Receive/kRO/RagexeRE_2009_11_24a.pm
@@ -0,0 +1,48 @@
+#########################################################################
+#  OpenKore - Packet sending
+#  This module contains functions for sending packets to the server.
+#
+#  This software is open source, licensed under the GNU General Public
+#  License, version 2.
+#  Basically, this means that you're allowed to modify and distribute
+#  this software. However, if you distribute modified versions, you MUST
+#  also distribute the source code.
+#  See http://www.gnu.org/licenses/gpl.html for the full license.
+#
+#  $Revision: 6687 $
+#  $Id: kRO.pm 6687 2009-04-19 19:04:25Z technologyguild $
+########################################################################
+# Korea (kRO)
+# The majority of private servers use eAthena, this is a clone of kRO
+
+package Network::Receive::kRO::RagexeRE_2009_11_24a;
+
+use strict;
+use base qw(Network::Receive::kRO::RagexeRE_2009_11_18c);
+
+sub new {
+	my ($class) = @_;
+	my $self = $class->SUPER::new(@_);
+	my %packets = (
+		#//0x07fc,10
+		#//0x07fd,-1
+		#//0x07fe,26
+		#//0x07ff,-1
+	);
+
+	foreach my $switch (keys %packets) {
+		$self->{packet_list}{$switch} = $packets{$switch};
+	}
+
+	return $self;
+}
+
+=pod
+//2009-11-24aRagexeRE
+//0x07fc,10
+//0x07fd,-1
+//0x07fe,26
+//0x07ff,-1
+=cut
+
+1;
---
+++ openkore/trunk/src/Network/Receive/kRO/RagexeRE_2009_12_08a.pm
@@ -0,0 +1,44 @@
+#########################################################################
+#  OpenKore - Packet sending
+#  This module contains functions for sending packets to the server.
+#
+#  This software is open source, licensed under the GNU General Public
+#  License, version 2.
+#  Basically, this means that you're allowed to modify and distribute
+#  this software. However, if you distribute modified versions, you MUST
+#  also distribute the source code.
+#  See http://www.gnu.org/licenses/gpl.html for the full license.
+#
+#  $Revision: 6687 $
+#  $Id: kRO.pm 6687 2009-04-19 19:04:25Z technologyguild $
+########################################################################
+# Korea (kRO)
+# The majority of private servers use eAthena, this is a clone of kRO
+
+package Network::Receive::kRO::RagexeRE_2009_12_08a;
+
+use strict;
+use base qw(Network::Receive::kRO::RagexeRE_2009_11_24a);
+
+sub new {
+	my ($class) = @_;
+	my $self = $class->SUPER::new(@_);
+	my %packets = (
+		'0800' => ['vender_items_list', 'v a4 x4', [qw(len venderID unknown)]], # -1
+		#//0x0801,-1
+	);
+
+	foreach my $switch (keys %packets) {
+		$self->{packet_list}{$switch} = $packets{$switch};
+	}
+
+	return $self;
+}
+
+=pod
+//2009-12-08aRagexeRE
+//0x0800,-1
+//0x0801,-1
+=cut
+
+1;
