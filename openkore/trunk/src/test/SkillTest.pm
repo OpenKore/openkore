4b8b92298aa4103f0bdc31e494b41db46d16ee59
eternalharvest <eternalharvest@3e610c8b-890d-0410-8877-b158ba2967cd>
2010-02-28 04:40:24 +0000

    add SKILL_id_handle/skillnametable split, duplicate skill names

--- openkore/trunk/src/test/SkillTest.pm
+++ openkore/trunk/src/test/SkillTest.pm
@@ -6,12 +6,14 @@ use Skill;
 
 sub start {
 	print "### Starting SkillTest\n";
-	Skill::StaticInfo::parseSkillsDatabase("skills.txt");
+	Skill::StaticInfo::parseSkillsDatabase_id2handle('SKILL_id_handle.txt');
+	Skill::StaticInfo::parseSkillsDatabase_handle2name('skillnametable.txt');
 	Skill::StaticInfo::parseSPDatabase("skillssp.txt");
 	testStaticInfo();
 	testStaticSPInfo();
 	testDynamicInfo();
 	testUnknownSkills();
+	testDuplicateNames();
 }
 
 sub testStaticInfo {
@@ -53,8 +55,8 @@ sub testStaticSPInfo {
 sub testDynamicInfo {
 	print "Testing dynamic information conversion...\n";
 	Skill::DynamicInfo::clear();
-	Skill::DynamicInfo::add(42, "MC_MAMMONITE", 3, 5, 1, Skill::TARGET_ENEMY);
-	Skill::DynamicInfo::add(456, "ABC_COMBO_BREAKER", 4, 15, 20, Skill::TARGET_ENEMY);
+	Skill::DynamicInfo::add(42, "MC_MAMMONITE", 3, 5, 1, Skill::TARGET_ENEMY, Skill::OWNER_CHAR);
+	Skill::DynamicInfo::add(456, "ABC_COMBO_BREAKER", 4, 15, 20, Skill::TARGET_LOCATION, Skill::OWNER_HOMUN);
 
 	my $skill = new Skill(idn => 42);
 	is($skill->getName(), "Mammonite");
@@ -63,6 +65,8 @@ sub testDynamicInfo {
 	is($skill->getSP(1), undef);
 	is($skill->getSP(3), 5);
 	is($skill->getRange(), 1);
+	is($skill->getTargetType, Skill::TARGET_ENEMY);
+	is($skill->getOwnerType, Skill::OWNER_CHAR);
 
 	$skill = new Skill(handle => "ABC_COMBO_BREAKER");
 	is($skill->getName(), "Combo Breaker");
@@ -71,12 +75,51 @@ sub testDynamicInfo {
 	is($skill->getSP(1), undef);
 	is($skill->getSP(4), 15);
 	is($skill->getRange(), 20);
+	is($skill->getTargetType, Skill::TARGET_LOCATION);
+	is($skill->getOwnerType, Skill::OWNER_HOMUN);
 
 	$skill = new Skill(name => "Mammonite");
 	is($skill->getName(), "Mammonite");
 	is($skill->getIDN(), 42);
 	is($skill->getHandle(), "MC_MAMMONITE");
+	is($skill->getSP(1), undef);
+	is($skill->getSP(3), 5);
 	is($skill->getRange(), 1);
+	is($skill->getTargetType, Skill::TARGET_ENEMY);
+	is($skill->getOwnerType, Skill::OWNER_CHAR);
+}
+
+sub testDuplicateNames {
+	print "Testing duplicate skill names...\n";
+	Skill::DynamicInfo::clear;
+	Skill::DynamicInfo::add(9001, 'DUP_BLESSING', 3, 5, 1, Skill::TARGET_ACTORS, Skill::OWNER_CHAR);
+	
+	my $skill = new Skill(name => 'Blessing');
+	is($skill->getName, 'Blessing (DUP_BLESSING)');
+	is($skill->getIDN, 9001);
+	is($skill->getHandle, 'DUP_BLESSING');
+	
+	$skill = new Skill(name => 'Blessing (DUP_BLESSING)');
+	is($skill->getName, 'Blessing (DUP_BLESSING)');
+	is($skill->getIDN, 9001);
+	is($skill->getHandle, 'DUP_BLESSING');
+	
+	Skill::DynamicInfo::add(34, 'AL_BLESSING', 3, 5, 1, Skill::TARGET_ACTORS, Skill::OWNER_CHAR);
+	
+	$skill = new Skill(name => 'Blessing');
+	is($skill->getName, 'Blessing');
+	is($skill->getIDN, 34);
+	is($skill->getHandle, 'AL_BLESSING');
+	
+	$skill = new Skill(name => 'Blessing (AL_BLESSING)');
+	is($skill->getName, 'Blessing');
+	is($skill->getIDN, 34);
+	is($skill->getHandle, 'AL_BLESSING');
+	
+	$skill = new Skill(name => 'Blessing (DUP_BLESSING)');
+	is($skill->getName, 'Blessing (DUP_BLESSING)');
+	is($skill->getIDN, 9001);
+	is($skill->getHandle, 'DUP_BLESSING');
 }
 
 sub testUnknownSkills {
