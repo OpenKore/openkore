45c35ad70129b4effa9d884df47e6a312978007b
kLabMouse <kLabMouse@3e610c8b-890d-0410-8877-b158ba2967cd>
2010-03-28 21:49:32 +0000

    Update iRO ST. Do not Ever load such a big package tree, just to copy a little function.

--- openkore/trunk/src/Network/Send/iRO.pm
+++ openkore/trunk/src/Network/Send/iRO.pm
@@ -21,9 +21,6 @@ use Log qw(error debug);
 use I18N qw(stringToBytes);
 use Utils qw(getTickCount getHex getCoordString);
 
-use Network::Send::kRO::RagexeRE_2009_12_08a ();
-*sendBuyBulkVender = *Network::Send::kRO::RagexeRE_2009_12_08a::sendBuyBulkVender;
-
 sub new {
    my ($class) = @_;
    return $class->SUPER::new(@_);
@@ -49,6 +46,17 @@ sub sendHomunculusMove {
 	my $msg = pack("C*", 0x32, 0x02) . $homunID . getCoordString($x, $y, 1);
 	$self->sendToServer($msg);
 	debug "Sent Homunculus move to: $x, $y\n", "sendPacket", 2;
+}
+
+# Copy from Network::Send::kRO::RagexeRE_2009_12_08a::sendBuyBulkVender
+sub sendBuyBulkVender {
+	my ($self, $venderID, $r_array, $venderCID) = @_;
+	my $msg = pack('v2 a4 a4', 0x0801, 12+4*@{$r_array}, $venderID, $venderCID); # TODO: is it the vender's charID?
+	for (my $i = 0; $i < @{$r_array}; $i++) {
+		$msg .= pack('v2', $r_array->[$i]{amount}, $r_array->[$i]{itemIndex});
+		debug "Sent bulk buy vender: $r_array->[$i]{itemIndex} x $r_array->[$i]{amount}\n", "d_sendPacket", 2;
+	}
+	$self->sendToServer($msg);
 }
 
 1;
