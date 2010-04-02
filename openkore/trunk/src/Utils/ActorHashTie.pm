9bfea022afba2b114379eb07eb9e37b64a39dfb6
eternalharvest <eternalharvest@users.sourceforge.net>
2010-04-02 19:48:56 +0000

    remove Tie::ExtraHash dep

--- openkore/trunk/src/Utils/ActorHashTie.pm
+++ openkore/trunk/src/Utils/ActorHashTie.pm
@@ -1,8 +1,16 @@
 package Tie::ActorHash;
 use strict;
 
-use Tie::Hash;
-use base 'Tie::ExtraHash';
+# from Tie::ExtraHash
+sub TIEHASH  { my $p = shift; bless [{}, @_], $p }
+#sub STORE    { $_[0][0]{$_[1]} = $_[2] }
+sub FETCH    { $_[0][0]{$_[1]} }
+sub FIRSTKEY { my $a = scalar keys %{$_[0][0]}; each %{$_[0][0]} }
+sub NEXTKEY  { each %{$_[0][0]} }
+sub EXISTS   { exists $_[0][0]->{$_[1]} }
+sub DELETE   { delete $_[0][0]->{$_[1]} }
+sub CLEAR    { %{$_[0][0]} = () }
+sub SCALAR   { scalar %{$_[0][0]} }
 
 sub STORE {
 	my ($hashref, $key, $value) = ($_[0][0], @_[1, 2]);
