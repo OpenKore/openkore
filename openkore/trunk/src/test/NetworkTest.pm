f88edbaffd51184b59273355499ec9ede62c9989
eternalharvest <eternalharvest@users.sourceforge.net>
2010-04-03 02:02:05 +0000

    add use_ok tests for loadable network modules

---
+++ openkore/trunk/src/test/NetworkTest.pm
@@ -0,0 +1,59 @@
+# Unit test for Network
+package NetworkTest;
+
+use Test::More;
+use Network::Receive;
+use Network::Send;
+
+sub start {
+	print "### Starting NetworkTest\n";
+	testServerTypeTree();
+}
+
+sub testServerTypeTree {
+	use_ok($_) for map {("Network::Receive::$_", "Network::Send::$_")} qw(
+		ServerType0
+		ServerType1
+		ServerType2
+		ServerType3
+		ServerType4
+		ServerType5
+		ServerType6
+		ServerType7
+		ServerType8
+		ServerType8_1
+		ServerType8_2
+		ServerType8_3
+		ServerType8_4
+		ServerType8_5
+		ServerType9
+		ServerType10
+		ServerType11
+		ServerType12
+		ServerType13
+		ServerType14
+		ServerType15
+		ServerType16
+		ServerType17
+		ServerType18
+		ServerType19
+		ServerType20
+		ServerType21
+		ServerType22
+		bRO
+		bRO::Thor
+		euRO
+		fRO
+		idRO
+		inRO
+		iRO
+		mRO
+		pRO
+		rRO
+		tRO
+		twRO
+		kRO::RagexeRE_0
+	);
+}
+
+1;
