426a8b0520aa37fe068d5ee8f03279e565644706
eternalharvest <eternalharvest@3e610c8b-890d-0410-8877-b158ba2967cd>
2010-05-14 16:06:07 +0000

    inRO closed 14.04.2008 (ST)

--- openkore/trunk/src/Network/Receive/inRO.pm
+++
@@ -1,7 +0,0 @@
-# inRO (India)
-package Network::Receive::inRO;
-use strict;
-
-use base 'Network::Receive::ServerType0';
-
-1;
--- openkore/trunk/src/Network/Send/inRO.pm
+++
@@ -1,7 +0,0 @@
-# inRO (India)
-package Network::Send::inRO;
-use strict;
-
-use base 'Network::Send::ServerType0';
-
-1;
