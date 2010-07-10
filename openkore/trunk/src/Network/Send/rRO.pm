cac65e54719267971f95aa789e56d3d401a5e886
eternalharvest <eternalharvest@3e610c8b-890d-0410-8877-b158ba2967cd>
2010-07-10 15:11:14 +0000

    rRO CZ 0134->0801 (sendBuyBulkVender)

--- openkore/trunk/src/Network/Send/rRO.pm
+++ openkore/trunk/src/Network/Send/rRO.pm
@@ -4,4 +4,17 @@ use strict;
 
 use base 'Network::Send::ServerType0';
 
+use Log qw(debug);
+
+# Copy from Network::Send::kRO::RagexeRE_2009_12_08a::sendBuyBulkVender
+sub sendBuyBulkVender {
+	my ($self, $venderID, $r_array, $venderCID) = @_;
+	my $msg = pack('v2 a4 a4', 0x0801, 12+4*@{$r_array}, $venderID, $venderCID); # TODO: is it the vender's charID?
+	for (my $i = 0; $i < @{$r_array}; $i++) {
+		$msg .= pack('v2', $r_array->[$i]{amount}, $r_array->[$i]{itemIndex});
+		debug "Sent bulk buy vender: $r_array->[$i]{itemIndex} x $r_array->[$i]{amount}\n", "d_sendPacket", 2;
+	}
+	$self->sendToServer($msg);
+}
+
 1;
