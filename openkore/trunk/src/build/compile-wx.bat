2462fd9ba9f7fd2cff0ded4d768f2360ffda4baf
kLabMouse <kLabMouse@3e610c8b-890d-0410-8877-b158ba2967cd>
2010-07-30 01:19:52 +0000

    Update Win32 Bundle ICO file and compile scripts

--- openkore/trunk/src/build/compile-wx.bat
+++ openkore/trunk/src/build/compile-wx.bat
@@ -1,4 +1,4 @@
 cd ..\..
 set BUILDING_WX=1
-perlapp --trim Pod::Usage;I18N::Langinfo --icon src\build\openkore.ico --lib src --norunlib --nologo --force --exe wxstart.exe start.pl --add Wx;Wx::RichText;Wx::Grid;Wx::Html;Wx::XRC
+perlapp --clean --trim Pod::Usage;I18N::Langinfo;Wx::build::**;XSTools --icon src\build\openkore.ico --lib src --norunlib --nologo --force --exe wxstart.exe start.pl --add List::Util;File::Path;Text::Balanced;Wx::Perl::Packager;Wx;Wx::**
 pause
--- openkore/trunk/src/build/compile.bat
+++ openkore/trunk/src/build/compile.bat
@@ -1,3 +1,3 @@
 cd ..\..
-perlapp --trim Pod::Usage;I18N::Langinfo --icon src\build\openkore.ico --lib src --norunlib --nologo --force --exe start.exe start.pl
+perlapp --trim Pod::Usage;I18N::Langinfo;XSTools --icon src\build\openkore.ico --lib src --norunlib --nologo --force --exe start.exe start.pl --add List::Util;File::Path;Text::Balanced
 pause
