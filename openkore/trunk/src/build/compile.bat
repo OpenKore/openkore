69e8bb00710d29ef8418170e7670ab66f3b6cec2
kLabMouse <kLabMouse@users.noreply.github.com>
2010-08-01 02:08:36 +0000

    Fix WX interface coloring. Fix Saving of Cursor and Selection in WX Console. Reduce size of start.exe build.

--- openkore/trunk/src/build/compile.bat
+++ openkore/trunk/src/build/compile.bat
@@ -1,3 +1,3 @@
 cd ..\..
-perlapp --trim Pod::Usage;I18N::Langinfo;XSTools --icon src\build\openkore.ico --lib src --norunlib --nologo --force --exe start.exe start.pl --add List::Util;File::Path;Text::Balanced
+perlapp --trim Pod::Usage;I18N::Langinfo;Wx;Wx::**;XSTools --icon src\build\openkore.ico --lib src --norunlib --nologo --force --exe start.exe start.pl --add List::Util;File::Path;Text::Balanced
 pause
