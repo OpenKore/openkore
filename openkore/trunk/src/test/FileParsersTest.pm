e3f65608b2026cfa1e68ac1c7bc07523894fc636
eternalharvest <eternalharvest@users.sourceforge.net>
2010-10-16 17:55:38 +0000

    add tests for items_control parser

---
+++ openkore/trunk/src/test/FileParsersTest.pm
@@ -0,0 +1,43 @@
+# Unit test for FileParsers
+package FileParsersTest;
+use strict;
+
+use Test::More;
+use FileParsers;
+use Globals;
+use Misc;
+
+sub start {
+	print "### Starting FileParsersTest\n";
+	
+	my $items = do {
+		use utf8;
+		{
+			501 => 'Red Potion',
+			512 => 'Apple',
+			528 => "Monster's Feed",
+			2784 => 'Caixinha "Noite Feliz"',
+			12080 => "Коктейль 'Дыхание дракона'",
+			12153 => 'Bowman Scroll 1',
+		}
+	};
+	
+	subtest 'items.txt' => sub {
+		parseROLUT('items.txt', \%items_lut);
+		
+		is_deeply(\%items_lut, $items, 'content');
+		
+		done_testing
+	};
+	
+	subtest 'items_control.txt' => sub {
+		parseItemsControl('items_control.txt', \%items_control);
+		
+		is(items_control('Random Item')->{keep}, 9, 'all');
+		is(items_control($_)->{keep}, 2, $_) for values %$items;
+		
+		done_testing
+	};
+}
+
+1;
