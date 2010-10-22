6cdbd44d890d1857140990c1244f745d17945ca8
kLabMouse <kLabMouse@users.noreply.github.com>
2010-10-22 01:13:09 +0000

    Include Digest::MD5 in Win build scripts.

--- openkore/trunk/src/build/compile-wx.bat
+++ openkore/trunk/src/build/compile-wx.bat
@@ -1,4 +1,4 @@
 cd ..\..
 set BUILDING_WX=1
-perlapp --clean --trim Pod::Usage;I18N::Langinfo;Wx::build::**;XSTools --icon src\build\openkore.ico --lib src --norunlib --nologo --force --exe wxstart.exe start.pl --add List::Util;File::Path;Text::Balanced;Wx::Perl::Packager;Wx;Wx::**
+perlapp --clean --trim Pod::Usage;I18N::Langinfo;Wx::build::**;XSTools --icon src\build\openkore.ico --lib src --norunlib --nologo --force --exe wxstart.exe start.pl --add List::Util;File::Path;Text::Balanced;Digest::MD5;Wx::Perl::Packager;Wx;Wx::**;
 pause
