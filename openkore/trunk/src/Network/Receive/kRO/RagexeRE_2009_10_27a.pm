f3dbc40b491c875fb18c80032cd96c4e2a4e52fe
manticora <manticora@users.sourceforge.net>
2011-02-02 20:46:46 +0000

    07F6 (exp) copy-pasted from ServerType0.pm to RagexeRE_2009_10_27a.pm http://rofan.ru/viewtopic.php?f=33&t=9362

--- openkore/trunk/src/Network/Receive/kRO/RagexeRE_2009_10_27a.pm
+++ openkore/trunk/src/Network/Receive/kRO/RagexeRE_2009_10_27a.pm
@@ -19,6 +19,16 @@ package Network::Receive::kRO::RagexeRE_2009_10_27a;
 
 use strict;
 use base qw(Network::Receive::kRO::RagexeRE_2009_10_06a);
+use Globals qw($char);
+use Log qw(message);
+use Translation qw(T TF);
+
+use constant {
+   EXP_FROM_BATTLE => 0x0,
+   EXP_FROM_QUEST => 0x1,
+   VAR_EXP => 0x1,
+   VAR_JOBEXP => 0x2,
+};
 
 sub new {
 	my ($class) = @_;
@@ -35,6 +45,38 @@ sub new {
 	return $self;
 }
 
+# 07F6 (exp) doesn't change any exp information because 00B1 (exp_zeny_info) is always sent with it
+# r7643 - copy-pasted from ServerType0.pm
+sub exp {
+   my ($self, $args) = @_;
+   my $max = {VAR_EXP, $char->{exp_max}, VAR_JOBEXP, $char->{exp_job_max}}->{$args->{type}};
+   $args->{percent} = $max ? $args->{val} / $max * 100 : 0;
+   if ($args->{flag} == EXP_FROM_BATTLE) {
+      if ($args->{type} == VAR_EXP) {
+         message TF("Base Exp gained: %d (%.2f%%)n", @{$args}{qw(val percent)}), 'exp2', 2;
+      } elsif ($args->{type} == VAR_JOBEXP) {
+         message TF("Job Exp gained: %d (%.2f%%)n", @{$args}{qw(val percent)}), 'exp2', 2;
+      } else {
+         message TF("Unknown (type=%d) Exp gained: %dn", @{$args}{qw(type val)}), 'exp2', 2;
+      }
+   } elsif ($args->{flag} == EXP_FROM_QUEST) {
+      if ($args->{type} == VAR_EXP) {
+         message TF("Base Quest Exp gained: %d (%.2f%%)n", @{$args}{qw(val percent)}), 'exp2', 2;
+      } elsif ($args->{type} == VAR_JOBEXP) {
+         message TF("Job Quest Exp gained: %d (%.2f%%)n", @{$args}{qw(val percent)}), 'exp2', 2;
+      } else {
+         message TF("Unknown (type=%d) Quest Exp gained: %dn", @{$args}{qw(type val)}), 'exp2', 2;
+      }
+   } else {
+      if ($args->{type} == VAR_EXP) {
+         message TF("Base Unknown (flag=%d) Exp gained: %d (%.2f%%)n", @{$args}{qw(flag val percent)}), 'exp2', 2;
+      } elsif ($args->{type} == VAR_JOBEXP) {
+         message TF("Job Unknown (flag=%d) Exp gained: %d (%.2f%%)n", @{$args}{qw(flag val percent)}), 'exp2', 2;
+      } else {
+         message TF("Unknown (type=%d) Unknown (flag=%d) Exp gained: %dn", @{$args}{qw(type flag val)}), 'exp2', 2;
+      }
+   }
+}
 
 =pod
 //2009-10-27aRagexeRE
