59ad2fdf3b205ba21a613bd10debf8b3853733fd
windhamwong <windhamwong@3e610c8b-890d-0410-8877-b158ba2967cd>
2011-02-25 15:21:16 +0000

    sitAuto_follow now is used instead of follow_hp_lower and follow_hp_sitAuto now the hp and sp required before following target is determined by sitAuto_hp_lower and sitAuto_sp_lower

--- openkore/trunk/src/AI/CoreLogic.pm
+++ openkore/trunk/src/AI/CoreLogic.pm
@@ -1960,7 +1960,7 @@ sub processFollow {
 	# var/flag about follow should be moved to %ai_v
 
 	return if (!$config{follow});
-	if($config{'follow_hp_sitAuto'} && percent_hp($char) < $config{'follow_hp_lower'} && $field->isCity) {
+	if($config{'sitAuto_follow'} && percent_hp($char) >= $config{'sitAuto_hp_lower'} && percent_sp($char) >= $config{'sitAuto_sp_lower'} && $field->isCity) {
 	my $action = AI::action;
 		if($action eq "sitting" && !$char->{sitting} && $char->{skills}{NV_BASIC}{lv} >= 3){
 			sit();
