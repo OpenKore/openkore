7bae18cfa69468171fe3607362115f639a565a0e
farrainbow <farrainbow@3e610c8b-890d-0410-8877-b158ba2967cd>
2011-06-13 14:09:03 +0000

    more FileParsers tests

--- openkore/trunk/src/test/FileParsersTest.pm
+++ openkore/trunk/src/test/FileParsersTest.pm
@@ -8,38 +8,55 @@ use Globals;
 use Misc;
 
 sub start {
-	print "### Starting FileParsersTest\n";
-	
-	my $items = do {
-		use utf8;
-		{
-			501 => 'Red Potion',
-			512 => 'Apple',
-			528 => "Monster's Feed",
-			2784 => 'Caixinha "Noite Feliz"',
-			12080 => "Коктейль 'Дыхание дракона'",
-			12153 => 'Bowman Scroll 1',
-		}
-	};
-	
-	subtest 'items.txt' => sub {
-		parseROLUT('items.txt', \%items_lut);
+	subtest 'FileParsers' => sub {
+		binmode STDOUT, ':utf8';
 		
-		is_deeply(\%items_lut, $items, 'content');
+		my $items = do {
+			use utf8;
+			{
+				501 => q(Red Potion),
+				512 => q(Apple),
+				528 => q(Monster's Feed),
+				2784 => q(Caixinha "Noite Feliz"),
+				12080 => q(Коктейль 'Дыхание дракона'),
+				12153 => q(Bowman Scroll 1),
+			}
+		};
+		
+		subtest 'items.txt' => sub {
+			parseROLUT('items.txt', \%items_lut);
+			
+			is_deeply(\%items_lut, $items, 'content');
+			
+			done_testing
+		};
+		
+		subtest 'items_control.txt' => sub {
+			parseItemsControl('items_control.txt', \%items_control);
+			
+			is(items_control('Random Item')->{keep}, 9, 'all');
+			is(items_control($_)->{keep}, 2, $_) for values %$items;
+			is(items_control($_)->{keep}, 2, $_) for 'Unknown #502';
+			is(items_control($_)->{keep}, 2, $_) for 'Slotted Item [1]';
+			is(items_control($_)->{keep}, 22, $_) for 'Slotted Item [2]';
+			
+			done_testing
+		};
+		
+		subtest 'pickupitems.txt' => sub {
+			parseDataFile_lc('pickupitems.txt', \%pickupitems);
+			
+			is(pickupitems('Random Item'), 1, 'all');
+			is(pickupitems($_), 2, $_) for values %$items;
+			is(pickupitems($_), 2, $_) for 'Unknown #502';
+			is(pickupitems($_), 2, $_) for 'Slotted Item [1]';
+			is(pickupitems($_), -1, $_) for 'Slotted Item [2]';
+			
+			done_testing
+		};
 		
 		done_testing
-	};
-	
-	subtest 'items_control.txt' => sub {
-		parseItemsControl('items_control.txt', \%items_control);
-		
-		is(items_control('Random Item')->{keep}, 9, 'all');
-		is(items_control($_)->{keep}, 2, $_) for values %$items;
-		is(items_control($_)->{keep}, 2, $_) for 'Slotted Item [1]';
-		is(items_control($_)->{keep}, 22, $_) for 'Slotted Item [2]';
-		
-		done_testing
-	};
+	}
 }
 
 1;
