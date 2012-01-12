b65db3bca399aaaadcd1fa2b48e02938b4d64454
klabmouse <kLabMouse@users.noreply.github.com>
2012-01-12 20:10:23 +0000

    Add Math::BigInt to compile scripts.

--- openkore/trunk/src/build/compile-wx.bat
+++ openkore/trunk/src/build/compile-wx.bat
@@ -1,4 +1,4 @@
 cd ..\..
 set BUILDING_WX=1
-perlapp --clean --trim Pod::Usage;I18N::Langinfo;Wx::build::**;XSTools --icon src\build\openkore.ico --lib src --norunlib --nologo --force --exe wxstart.exe start.pl --add List::Util;File::Path;Text::Balanced;Digest::MD5;Wx::Perl::Packager;Wx;Wx::**;
+perlapp --clean --trim Pod::Usage;I18N::Langinfo;Wx::build::**;XSTools --icon src\build\openkore.ico --lib src --norunlib --nologo --force --exe wxstart.exe start.pl --add List::Util;File::Path;Text::Balanced;Digest::MD5;Math::BigInt;Math::BigInt::Calc;Math::BigInt::CalcEmu;Math::BigInt::FastCalc;Math::BigInt::Trace;Math::BigFloat;Math::BigFloat::Trace;Math::BigRat;Math::Complex;Math::Trig;Wx::Perl::Packager;Wx;Wx::**;
 pause
--- openkore/trunk/src/build/compile.bat
+++ openkore/trunk/src/build/compile.bat
@@ -1,3 +1,3 @@
 cd ..\..
-perlapp --trim Pod::Usage;I18N::Langinfo;Wx;Wx::**;XSTools --icon src\build\openkore.ico --lib src --norunlib --nologo --force --exe start.exe start.pl --add List::Util;File::Path;Text::Balanced;Digest::MD5;
+perlapp --trim Pod::Usage;I18N::Langinfo;Wx;Wx::**;XSTools --icon src\build\openkore.ico --lib src --norunlib --nologo --force --exe start.exe start.pl --add List::Util;File::Path;Text::Balanced;Digest::MD5;Math::BigInt;Math::BigInt::Calc;Math::BigInt::CalcEmu;Math::BigInt::FastCalc;Math::BigInt::Trace;Math::BigFloat;Math::BigFloat::Trace;Math::BigRat;Math::Complex;Math::Trig;
 pause
