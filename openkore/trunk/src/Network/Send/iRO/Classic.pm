bc57804931990383e26eb0a3f32caeda3c37bfcc
farrainbow <farrainbow@3e610c8b-890d-0410-8877-b158ba2967cd>
2012-07-05 20:03:52 +0000

    Classic iRO Pre-Renewal

---
+++ openkore/trunk/src/Network/Send/iRO/Classic.pm
@@ -0,0 +1,18 @@
+#########################################################################
+#  OpenKore - Network subsystem
+#  This module contains functions for sending messages to the server.
+#
+#  This software is open source, licensed under the GNU General Public
+#  License, version 2.
+#  Basically, this means that you're allowed to modify and distribute
+#  this software. However, if you distribute modified versions, you MUST
+#  also distribute the source code.
+#  See http://www.gnu.org/licenses/gpl.html for the full license.
+#########################################################################
+# Servertype overview: http://wiki.openkore.com/index.php/ServerType
+package Network::Send::iRO::Classic;
+
+use strict;
+use base qw(Network::Send::ServerType0);
+
+1;
