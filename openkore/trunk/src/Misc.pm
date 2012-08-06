6ebebdeedea2a87b6f10e802d8808f09b03716b2
windhamwong <windhamwong@3e610c8b-890d-0410-8877-b158ba2967cd>
2012-08-06 05:11:32 +0000

    [Fix] Forged weapon should always shows $elementName when it is present

--- openkore/trunk/src/Misc.pm
+++ openkore/trunk/src/Misc.pm
@@ -1753,12 +1753,11 @@ sub itemName {
 		my $elementID = $cards[1] % 10;
 		my $elementName = $elements_lut{$elementID};
 		my $starCrumbs = ($cards[1] >> 8) / 5;
-		if ( $starCrumbs >= 1 && $starCrumbs <= 3 )
-		{
+		if ($starCrumbs >= 1 && $starCrumbs <= 3 ) {
 			$prefix .= ('V'x$starCrumbs)."S " if $starCrumbs;
-			$prefix .= "$elementName " if ($elementName ne "");
-			$suffix = "$elementName" if ($elementName ne "");
 		}
+		# $prefix .= "$elementName " if ($elementName ne "");
+		$suffix = "$elementName" if ($elementName ne "");
 	} elsif (@cards) {
 		# Carded item
 		#
