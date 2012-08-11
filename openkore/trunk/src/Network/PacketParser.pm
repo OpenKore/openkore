67ec57b67f3df900cf500e014d4f4a1ae0c5bbb7
windhamwong <windhamwong@3e610c8b-890d-0410-8877-b158ba2967cd>
2012-08-11 05:21:56 +0000

    [ADD] config debugPacket_alwaysDisplay added

--- openkore/trunk/src/Network/PacketParser.pm
+++ openkore/trunk/src/Network/PacketParser.pm
@@ -213,6 +213,10 @@ sub parse {
 
 	unless ($handler) {
 		warning "Packet Parser: Unknown switch: $switch\n";
+		if (debugPacket_include_dumpMethod != 0 && $config{'debugPacket_alwaysDisplay'}) {
+			Misc::dumpData($msg, 1);
+			Misc::visualDump($msg);
+		}
 		return undef;
 	}
 
