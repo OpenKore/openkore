f230ef1fb36d916d42999a28326dceb58c1a90d7
keplerbr <keplerbr@3e610c8b-890d-0410-8877-b158ba2967cd>
2012-09-03 03:12:34 +0000

    * Fixbug: If the shop was not created in game, the OpenKore still thought it had been

--- openkore/trunk/src/Misc.pm
+++ openkore/trunk/src/Misc.pm
@@ -4229,9 +4229,7 @@ sub openShop {
 	$shop{title} = $shopnames[int rand($#shopnames + 1)];
 	$shop{title} = ($config{shopTitleOversize}) ? $shop{title} : substr($shop{title},0,36);
 	$messageSender->sendOpenShop($shop{title}, \@items);
-	message TF("Shop opened (%s) with %d selling items.\n", $shop{title}, @items.""), "success";
-	$shopstarted = 1;
-	$shopEarned = 0;
+	message T("Trying to set up shop...\n"), "vending";
 }
 
 sub closeShop {
