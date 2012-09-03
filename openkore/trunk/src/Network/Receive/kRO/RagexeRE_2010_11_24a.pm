1e0221e324d4027717920e13173c7a5f641961b0
klabmouse <klabmouse@3e610c8b-890d-0410-8877-b158ba2967cd>
2012-09-03 14:39:21 +0000

    Someone Forgot to add Headers to SRC Files. Fixed that Issue.

--- openkore/trunk/src/Network/Receive/kRO/RagexeRE_2010_11_24a.pm
+++ openkore/trunk/src/Network/Receive/kRO/RagexeRE_2010_11_24a.pm
@@ -1,3 +1,17 @@
+#########################################################################
+#  OpenKore - Packet Receiveing
+#  This module contains functions for Receiveing packets to the server.
+#
+#  This software is open source, licensed under the GNU General Public
+#  License, version 2.
+#  Basically, this means that you're allowed to modify and distribute
+#  this software. However, if you distribute modified versions, you MUST
+#  also distribute the source code.
+#  See http:#//www.gnu.org/licenses/gpl.html for the full license.
+########################################################################
+# Korea (kRO)
+# The majority of private servers use eAthena, this is a clone of kRO
+
 package Network::Receive::kRO::RagexeRE_2010_11_24a;
 
 use strict;
--- openkore/trunk/src/Network/Send/rRO.pm
+++ openkore/trunk/src/Network/Send/rRO.pm
@@ -1,3 +1,14 @@
+#########################################################################
+#  OpenKore - Network subsystem
+#  This module contains functions for sending messages to the server.
+#
+#  This software is open source, licensed under the GNU General Public
+#  License, version 2.
+#  Basically, this means that you're allowed to modify and distribute
+#  this software. However, if you distribute modified versions, you MUST
+#  also distribute the source code.
+#  See http://www.gnu.org/licenses/gpl.html for the full license.
+#########################################################################
 # rRO (Russia)
 package Network::Send::rRO;
 use strict;
