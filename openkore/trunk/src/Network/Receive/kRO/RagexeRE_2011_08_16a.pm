1a31f3531c5c497a87ed5ff2cd0e7dfb604c3512
ya4ept <ya4ept@3e610c8b-890d-0410-8877-b158ba2967cd>
2012-10-07 20:20:55 +0000

    add ST RagexeRE_2011_08_16a (from http://chibiro.sytes.net)
    need modify:
    RagexeRE_2010_11_24a.pm ???
    RagexeRE_2011_10_05a.pm ???
    RagexeRE_2011_11_02a.pm ???
    RagexeRE_2012_03_07f.pm ???
    RagexeRE_2012_04_10a.pm ???

---
+++ openkore/trunk/src/Network/Receive/kRO/RagexeRE_2011_08_16a.pm
@@ -0,0 +1,78 @@
+#########################################################################
+#  OpenKore - Packet Receiveing
+#  This module contains functions for Receiveing packets to the server.
+#
+#  This software is open source, licensed under the GNU General Public
+#  License, version 2.
+#  Basically, this means that you're allowed to modify and distribute
+#  this software. However, if you distribute modified versions, you MUST
+#  also distribute the source code.
+#  See http://www.gnu.org/licenses/gpl.html for the full license.
+########################################################################
+# Korea (kRO)
+# The majority of private servers use eAthena, this is a clone of kRO
+# See http://subversion.assembla.com/svn/ClientSide/Packets/Packet_db/packets_2011-08-16aRagexeRE.txt
+
+package Network::Receive::kRO::RagexeRE_2011_08_16a;
+
+use strict;
+use base qw(Network::Receive::kRO::RagexeRE_2010_11_24a);
+
+sub new {
+	my ($class) = @_;
+	my $self = $class->SUPER::new(@_);
+	
+	my %packets = (
+		'08B9' => ['account_id', 'x4 a4 x2', [qw(accountID)]], # 12
+	);
+	foreach my $switch (keys %packets) {
+		$self->{packet_list}{$switch} = $packets{$switch};
+	}
+	
+	return $self;
+}
+
+1;
+
+=cut
+//2011-08-16aRagexeRE
+0x01FD,15,repairitem,2
+0x023B,26,friendslistadd,2
+0x0361,5,hommenu,2:4
+0x088F,36,storagepassword,0
+0x0288,-1,cashshopbuy,4:8
+0x0802,26,partyinvite2,2
+0x022D,19,wanttoconnection,2:6:10:14:18
+0x0369,7,actionrequest,2:6
+0x083C,10,useskilltoid,2:4:6
+0x0439,8,useitem,2:4
+0x0281,-1,itemlistwindowselected,2:4:8
+0x0365,18,bookingregreq,2:4:6
+0x0803,4
+0x0804,14,bookingsearchreq,2:4:6:8:12
+0x0805,-1
+0x0806,2,bookingdelreq,0
+0x0807,4
+0x0808,14,bookingupdatereq,2
+0x0809,50
+0x080A,18
+0x080B,6
+0x0815,-1,reqopenbuyingstore,2:4:8:9:89
+0x0817,2,reqclosebuyingstore,0
+0x0360,6,reqclickbuyingstore,2
+0x0811,-1,reqtradebuyingstore,2:4:8:12
+0x0819,-1,searchstoreinfo,2:4:5:9:13:14:15
+0x0835,2,searchstoreinfonextpage,0
+0x0838,12,searchstoreinfolistitemclick,2:6:10
+0x0437,5,walktoxy,2
+0x035F,6,ticksend,2
+0x0202,5,changedir,2:4
+0x07E4,6,takeitem,2
+0x0362,6,dropitem,2:4
+0x07EC,8,movetokafra,2:4
+0x0364,8,movefromkafra,2:4
+0x0438,10,useskilltopos,2:4:6:8
+0x0366,90,useskilltoposinfo,2:4:6:8:10
+0x08AD,6,getcharnamerequest,2
+0x0368,6,solvecharname,2 
+=pod
