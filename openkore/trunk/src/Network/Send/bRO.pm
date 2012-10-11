d11095bae89e3dca4a4af2a73321a6a98a0bcbba
roxleopardo <roxleopardo@3e610c8b-890d-0410-8877-b158ba2967cd>
2012-10-11 18:53:16 +0000

    Update for bRO 10/10/2012

--- openkore/trunk/src/Network/Send/bRO.pm
+++ openkore/trunk/src/Network/Send/bRO.pm
@@ -1,14 +1,3 @@
-#############################################################################
-#  OpenKore - Network subsystem												#
-#  This module contains functions for sending messages to the server.		#
-#																			#
-#  This software is open source, licensed under the GNU General Public		#
-#  License, version 2.														#
-#  Basically, this means that you're allowed to modify and distribute		#
-#  this software. However, if you distribute modified versions, you MUST	#
-#  also distribute the source code.											#
-#  See http://www.gnu.org/licenses/gpl.html for the full license.			#
-#############################################################################
 # bRO (Brazil)
 package Network::Send::bRO;
 use strict;
@@ -22,82 +11,81 @@ use I18N qw(stringToBytes);
 sub new {
 	my ($class) = @_;
 	my $self = $class->SUPER::new(@_);
-	
-	my %packets = (
 
-		'08A5' => ['actor_action', 'a4 C', [qw(targetID type)]],
-		'0880' => ['character_move','a3', [qw(coords)]],		
-		'0898' => ['sync', 'V', [qw(time)]],
-		'0864' => ['actor_look_at', 'v C', [qw(head body)]],				
-		'0943' => ['item_take', 'a4', [qw(ID)]],
-		'0918' => ['item_drop', 'v2', [qw(index amount)]],		
-		'0368' => ['storage_item_add', 'v V', [qw(index amount)]],
-		'0960' => ['storage_item_remove', 'v V', [qw(index amount)]],
-		'0892' => ['skill_use_location', 'v4', [qw(lv skillID x y)]],
-		'087F' => ['actor_info_request', 'a4', [qw(ID)]],	
-		'0366' => ['map_login', 'a4 a4 a4 V C', [qw(accountID charID sessionID tick sex)]],	
-		'035F' => ['party_join_request_by_name', 'Z24', [qw(partyName)]],
-		'0950' => ['homunculus_command', 'v C', [qw(commandType, commandID)]],
+	my %packets = (
+		'022D' => ['map_login', 'a4 a4 a4 V C', [qw(accountID charID sessionID tick sex)]],
+		'035F' => ['sync', 'V', [qw(time)]],
+		'0437' => ['character_move','a3', [qw(coords)]],
+		'096A' => ['actor_info_request', 'a4', [qw(ID)]],
+		'0369' => ['actor_action', 'a4 C', [qw(targetID type)]],
+		'0202' => ['actor_look_at', 'v C', [qw(head body)]],
+		'07E4' => ['item_take', 'a4', [qw(ID)]],
+		'0362' => ['item_drop', 'v2', [qw(index amount)]],
+		'07EC' => ['storage_item_add', 'v V', [qw(index amount)]],
+		'0364' => ['storage_item_remove', 'v V', [qw(index amount)]],
+		'0438' => ['skill_use_location', 'v4', [qw(lv skillID x y)]],
+		'0361' => ['homunculus_command', 'v C', [qw(commandType, commandID)]],
+		'023B' => ['party_join_request_by_name', 'Z24', [qw(partyName)]],
 	);
-	
-	$self->{packet_list}{$_} = $packets{$_} for keys %packets;	
-	
+
+	$self->{packet_list}{$_} = $packets{$_} for keys %packets;
+
 	my %handlers = qw(
 
 		master_login 02B0
 		buy_bulk_vender 0801
 		party_setting 07D7
-		actor_action 08A5
-		character_move 0880
-		sync 0898
-		actor_look_at 0864		
-		item_take 0943
-		item_drop 0918
-		storage_item_add 0368
-		storage_item_remove 0960
-		skill_use_location 0892
-		actor_info_request 087F		
-		map_login 0366
-		party_join_request_by_name 035F
-		homunculus_command 0950	
+		map_login 022D
+		sync 035F
+		character_move 0437
+		actor_info_request 096A
+		actor_action 0369
+		actor_look_at 0202
+		item_take 07E4
+		item_drop 0362
+		storage_item_add 07EC
+		storage_item_remove 0364
+		skill_use_location 0438
+		homunculus_command 0361
+		party_join_request_by_name 023B
 	);
-	
+
 	$self->{packet_lut}{$_} = $handlers{$_} for keys %handlers;
-	
+
 	return $self;
 }
 
 # Local Servertype Globals
 my $map_login = 0;
 my $enc_val3 = 0;
-		
+
 sub encryptMessageID 
 {
 	my ($self, $r_message, $MID) = @_;
-	
+
 	# Checking In-Game State
 	if ($self->{net}->getState() != Network::IN_GAME && !$map_login) { $enc_val1 = 0; $enc_val2 = 0; return; }
-	
+
 	# Turn Off Map Login Flag
 	if ($map_login)	{ $map_login = 0; }
-		
+
 	# Checking if Encryption is Activated
 	if ($enc_val1 != 0 && $enc_val2 != 0) 
 	{
 		# Saving Last Informations for Debug Log
 		my $oldMID = $MID;
 		my $oldKey = ($enc_val1 >> 8 >> 8) & 0x7FFF;
-		
+
 		# Calculating the Encryption Key
 		$enc_val1 = $enc_val1->bmul($enc_val3)->badd($enc_val2) & 0xFFFFFFFF;
-	
+
 		# Xoring the Message ID
 		$MID = ($MID ^ (($enc_val1 >> 8 >> 8) & 0x7FFF)) & 0xFFFF;
 		$$r_message = pack("v", $MID) . substr($$r_message, 2);
 
 		# Debug Log
 		if ($config{debugPacket_sent} == 1) 
-		{		
+		{
 			debug(sprintf("Encrypted MID : [%04X]->[%04X] / KEY : [0x%04X]->[0x%04X]\n", $oldMID, $MID, $oldKey, ($enc_val1 >> 8 >> 8) & 0x7FFF), "sendPacket", 0);
 		}
 	}
@@ -112,9 +100,9 @@ sub sendStoragePassword {
 	my $type = shift;
 	my $msg;
 	if ($type == 3) {
-		$msg = pack("v v", 0x094E, $type).$pass.pack("H*", "EC62E539BB6BBC811A60C06FACCB7EC8");
+		$msg = pack("v v", 0x0942, $type).$pass.pack("H*", "EC62E539BB6BBC811A60C06FACCB7EC8");
 	} elsif ($type == 2) {
-		$msg = pack("v v", 0x094E, $type).pack("H*", "EC62E539BB6BBC811A60C06FACCB7EC8").$pass;
+		$msg = pack("v v", 0x0942, $type).pack("H*", "EC62E539BB6BBC811A60C06FACCB7EC8").$pass;
 	} else {
 		ArgumentException->throw("The 'type' argument has invalid value ($type).");
 	}
@@ -127,7 +115,7 @@ sub sendMapLogin
 	my $msg;
 
 	$sex = 0 if ($sex > 1 || $sex < 0); # Sex can only be 0 (female) or 1 (male)
-	
+
 	if ( $map_login == 0 ) { PrepareKeys(); $map_login = 1; }
 
 	# Reconstructing Packet 
@@ -147,36 +135,36 @@ sub sendMapLogin
 sub sendHomunculusCommand 
 {
 	my ($self, $command, $type) = @_; # $type is ignored, $command can be 0:get stats, 1:feed or 2:fire
-	
+
 	$self->sendToServer($self->reconstruct({
 		switch => 'homunculus_command',
 		commandType => $type,
 		commandID => $command,
-	}));	
-	
+	}));
+
 	debug "Sent Homunculus Command $command", "sendPacket", 2;
 }
 
 sub sendPartyJoinRequestByName 
 {
 	my ($self, $name) = @_;
-	
+
 	$self->sendToServer($self->reconstruct({
 		switch => 'party_join_request_by_name',
 		partyName => stringToBytes ($name),
-	}));	
-	
+	}));
+
 	debug "Sent Request Join Party (by name): $name\n", "sendPacket", 2;
 }
 
 sub PrepareKeys()
 {
 	# K
-	$enc_val1 = Math::BigInt->new('0x5c18014b');
+	$enc_val1 = Math::BigInt->new('0x52FE4ADA');
 	# M
-	$enc_val3 = Math::BigInt->new('0x6e6b29c1');
+	$enc_val3 = Math::BigInt->new('0x747A537A');
 	# A
-	$enc_val2 = Math::BigInt->new('0x503e1282');
+	$enc_val2 = Math::BigInt->new('0x7B7A3B7A');
 }
 
 1;
