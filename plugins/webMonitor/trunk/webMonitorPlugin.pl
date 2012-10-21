eafce0ae518c9c8e54b83dc0641a55ed0c7027eb
keplerbr <keplerbr@3e610c8b-890d-0410-8877-b158ba2967cd>
2012-10-21 03:23:12 +0000

    Plugin webMonitor: * If the WebPort has not been set, uses default port (1025) * Simple bugfiz menu of the inventory * "Config.html" sent (credits: BonScott)

--- plugins/webMonitor/trunk/webMonitorPlugin.pl
+++ plugins/webMonitor/trunk/webMonitorPlugin.pl
@@ -4,6 +4,7 @@ package webMonitorPlugin;
 # Copyright (C) 2012 BonScott
 # thanks to iMikeLance
 #
+# ------------------------------
 # How use:
 # Add in your config.txt
 #
@@ -11,12 +12,14 @@ package webMonitorPlugin;
 # 
 # Where XXXX is a number of your choice. Ex:
 # webPort 1020
-# 
+#
+# If webPort not defined in config, the default port is 1025
+# ------------------------------
 # Set only one port for each bot. For more details, visit:
 # [OpenKoreBR]
-#
+#	http://openkore.com.br/index.php?/topic/3189-webmonitor-v2-by-bonscott/
 # [OpenKore International]
-#
+#	http://forums.openkore.com/viewtopic.php?f=34&t=18264
 #############################################
 
 use strict;
@@ -42,9 +45,14 @@ my $hook = Plugins::addHooks(['AI_post', \&mainLoop], ['start3', \&post_loading]
 ##### Seting webServer after of plugins loads
 sub post_loading {
 	$port = $config{webPort};
+	
+	if (!$port) {
+		warning "'webPort' not defined in config.txt! Using the default port!\n";
+		$port = 1025;
+	}
+	
 	$bind = "localhost";
-    warning "webPort: ".$config{webPort}."\n";
-	print "$config{webPort}\n";
+    warning "webPort: $port\n";
 	$webserver = new webMonitorServer($port, $bind);
 }
 sub Unload {
