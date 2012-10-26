573e67513a24fd2242fed41de62fbdb122d5999d
Windham Wong <windhamwong@nva-hk.com>
2012-10-26 14:25:06 +0000

    [FIX] RandomWalk coordinate won't be 0 now.

--- openkore/trunk/src/AI/CoreLogic.pm
+++ openkore/trunk/src/AI/CoreLogic.pm
@@ -1913,13 +1913,13 @@ sub processRandomWalk {
 		my ($randX, $randY);
 		my $i = 500;
 		do {
-			$randX = int(rand($field->width));
+			$randX = int(rand($field->width-1)+1);
 			$randX = $config{'lockMap_x'} if ($char->{pos}{x} == $config{'lockMap_x'} && !($config{'lockMap_randX'} > 0));
 			$randX = $config{'lockMap_x'} - $config{'lockMap_randX'} + int(rand(2*$config{'lockMap_randX'}+1)) if ($config{'lockMap_x'} ne '' && $config{'lockMap_randX'} >= 0);
-			$randY = int(rand($field->height));
+			$randY = int(rand($field->height-1)+1);
 			$randY = $config{'lockMap_y'} if ($char->{pos}{y} == $config{'lockMap_y'} && !($config{'lockMap_randY'} > 0));
 			$randY = $config{'lockMap_y'} - $config{'lockMap_randY'} + int(rand(2*$config{'lockMap_randY'}+1)) if ($config{'lockMap_y'} ne '' && $config{'lockMap_randY'} >= 0);
-		} while (--$i && !$field->isWalkable($randX, $randY));
+		} while (--$i && (!$field->isWalkable($randX, $randY) || $randX == 0 || $randY == 0));
 		if (!$i) {
 			error T("Invalid coordinates specified for randomWalk (coordinates are unwalkable); randomWalk disabled\n");
 			$config{'route_randomWalk'} = 0;
