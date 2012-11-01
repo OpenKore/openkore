58a54a752a5dde1c29a1d84952ce033f49d45ee0
farrainbow <farrainbow@3e610c8b-890d-0410-8877-b158ba2967cd>
2012-11-01 10:17:39 +0000

    Can't drop equipped items

--- plugins/webMonitor/trunk/webMonitorServer.pm
+++ plugins/webMonitor/trunk/webMonitorServer.pm
@@ -252,7 +252,7 @@ sub request {
 			if ($item->{equipped}) {
 				push @equipmentID, $item->{nameID};
 				push @equipment, $item->{name};
-				push @equipmentJS, '<td><a class="btn btn-mini btn-inverse" href="/handler?csrf=' . $csrf . '&command=eq+' . $item->{invIndex} . '">Unequip</a></td><td><a class="btn btn-mini btn-danger" href="/handler?csrf=' . $csrf . '&command=drop+' . $item->{invIndex} . '">Drop</a></td>';
+				push @equipmentJS, '<td><a class="btn btn-mini btn-inverse" href="/handler?csrf=' . $csrf . '&command=eq+' . $item->{invIndex} . '">Unequip</a></td><td></td>';
 			} else {
 				push @uequipmentID, $item->{nameID};
 				push @uequipment, $item->{name};
