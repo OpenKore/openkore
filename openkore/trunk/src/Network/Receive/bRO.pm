384323e4885239128710b95aeb8c4b247eaf8909
keplerbr <keplerbr@3e610c8b-890d-0410-8877-b158ba2967cd>
2013-01-23 15:59:49 +0000

    * bRO: update 23/01/2013 All credits for the snowflakes http://forum.xkore.com.br/index.php?/topic/113-atualizacao-23-01-13/

--- openkore/trunk/src/Network/Receive/bRO.pm
+++ openkore/trunk/src/Network/Receive/bRO.pm
@@ -1,5 +1,4 @@
-#################################################################################################
-#  OpenKore - Network subsystem									#
+#  OpenKore - Network subsystem	#
 #  This module contains functions for sending messages to the server.				#
 #												#
 #  This software is open source, licensed under the GNU General Public				#
@@ -10,6 +9,7 @@
 #  See http://www.gnu.org/licenses/gpl.html for the full license.				#
 #################################################################################################
 # bRO (Brazil)
+
 package Network::Receive::bRO;
 use strict;
 use Log qw(message warning error debug);
@@ -27,99 +27,98 @@ sub new {
 	my %packets = (
 	
 		'0097' => ['private_message', 'v Z24 V Z*', [qw(len privMsgUser flag privMsg)]], # -1
+		'085E' => ['sync_request_ex'],  
+		'091D' => ['sync_request_ex'],  
+		'092C' => ['sync_request_ex'],  
+		'0932' => ['sync_request_ex'],  
+		'092D' => ['sync_request_ex'],  
+		'085D' => ['sync_request_ex'],  
+		'0919' => ['sync_request_ex'],  
+		'0865' => ['sync_request_ex'],  
+		'0866' => ['sync_request_ex'],  
+		'0933' => ['sync_request_ex'],  
+		'0867' => ['sync_request_ex'],  
+		'0936' => ['sync_request_ex'],  
+		'087C' => ['sync_request_ex'],  
+		'093A' => ['sync_request_ex'],  
+		'0876' => ['sync_request_ex'],  
+		'0367' => ['sync_request_ex'],  
+		'0922' => ['sync_request_ex'],  
+		'0868' => ['sync_request_ex'],  
+		'0882' => ['sync_request_ex'],  
+		'091E' => ['sync_request_ex'],  
+		'0861' => ['sync_request_ex'],  
+		'091B' => ['sync_request_ex'],  
+		'0863' => ['sync_request_ex'],  
+		'0923' => ['sync_request_ex'],  
+		'0920' => ['sync_request_ex'],  
+		'0918' => ['sync_request_ex'],  
+		'091C' => ['sync_request_ex'],  
+		'087B' => ['sync_request_ex'],  
+		'092A' => ['sync_request_ex'],  
+		'0875' => ['sync_request_ex'],  
+		'0938' => ['sync_request_ex'],  
+		'093F' => ['sync_request_ex'],  
+		'093C' => ['sync_request_ex'],  
+		'093E' => ['sync_request_ex'],  
+		'0931' => ['sync_request_ex'],  
+		'0926' => ['sync_request_ex'],  
+		'092E' => ['sync_request_ex'],  
+		'0921' => ['sync_request_ex'],  
+		'0864' => ['sync_request_ex'],  
+		'0935' => ['sync_request_ex'],  
+		'085F' => ['sync_request_ex'],  
+		'0930' => ['sync_request_ex'],  
+		'091F' => ['sync_request_ex'],  
+		'086D' => ['sync_request_ex'],  
+		'085B' => ['sync_request_ex'],  
+		'0927' => ['sync_request_ex'],  
+		'0934' => ['sync_request_ex'],  
+		'0917' => ['sync_request_ex'],  
+		'093D' => ['sync_request_ex'],  
+		'087F' => ['sync_request_ex'],  
+		'0937' => ['sync_request_ex'],  
+		'087E' => ['sync_request_ex'],  
+		'0925' => ['sync_request_ex'],  
+		'0924' => ['sync_request_ex'],  
+		'0881' => ['sync_request_ex'],  
+		'092F' => ['sync_request_ex'],  
+		'0878' => ['sync_request_ex'],  
+		'086A' => ['sync_request_ex'],  
+		'085A' => ['sync_request_ex'],  
+		'0871' => ['sync_request_ex'],  
+		'086F' => ['sync_request_ex'],  
+		'086C' => ['sync_request_ex'],  
+		'0929' => ['sync_request_ex'],  
+		'092B' => ['sync_request_ex'],  
+		'0883' => ['sync_request_ex'],  
+		'086B' => ['sync_request_ex'],  
+		'0873' => ['sync_request_ex'],  
+		'0869' => ['sync_request_ex'],  
+		'086E' => ['sync_request_ex'],  
+		'0874' => ['sync_request_ex'],  
+		'0860' => ['sync_request_ex'],  
+		'0870' => ['sync_request_ex'],  
+		'087D' => ['sync_request_ex'],  
+		'0880' => ['sync_request_ex'],  
+		'093B' => ['sync_request_ex'],  
+		'0928' => ['sync_request_ex'],  
+		'087A' => ['sync_request_ex'],  
+		'0872' => ['sync_request_ex'],  
+		'091A' => ['sync_request_ex'],  
+		'085C' => ['sync_request_ex'],  
+		'0939' => ['sync_request_ex'],  
+		'0862' => ['sync_request_ex'],  
+		'0877' => ['sync_request_ex'],  
+		'0879' => ['sync_request_ex'],  
 		'08B9' => ['login_pin_code_request', 'V V v', [qw(seed accountID flag)]],
 		'08BB' => ['login_pin_new_code_result', 'v V', [qw(flag seed)]],
-		
-		'0367' => ['sync_request_ex'],
-		'085A' => ['sync_request_ex'],
-		'085B' => ['sync_request_ex'],
-		'085C' => ['sync_request_ex'],
-		'085D' => ['sync_request_ex'],
-		'0436' => ['sync_request_ex'],
-		'085F' => ['sync_request_ex'],
-		'0860' => ['sync_request_ex'],
-		'0861' => ['sync_request_ex'],
-		'0862' => ['sync_request_ex'],
-		'0863' => ['sync_request_ex'],
-		'0864' => ['sync_request_ex'],
-		'0865' => ['sync_request_ex'],
-		'022D' => ['sync_request_ex'],
-		'0867' => ['sync_request_ex'],
-		'0868' => ['sync_request_ex'],
-		'0869' => ['sync_request_ex'],
-		'086A' => ['sync_request_ex'],
-		'086B' => ['sync_request_ex'],
-		'086C' => ['sync_request_ex'],
-		'086D' => ['sync_request_ex'],
-		'086E' => ['sync_request_ex'],
-		'086F' => ['sync_request_ex'],
-		'0870' => ['sync_request_ex'],
-		'0202' => ['sync_request_ex'],
-		'0872' => ['sync_request_ex'],
-		'0873' => ['sync_request_ex'],
-		'0874' => ['sync_request_ex'],
-		'0875' => ['sync_request_ex'],
-		'0876' => ['sync_request_ex'],
-		'0877' => ['sync_request_ex'],
-		'0878' => ['sync_request_ex'],
-		'0879' => ['sync_request_ex'],
-		'087A' => ['sync_request_ex'],
-		'087B' => ['sync_request_ex'],
-		'087C' => ['sync_request_ex'],
-		'087D' => ['sync_request_ex'],
-		'087E' => ['sync_request_ex'],
-		'087F' => ['sync_request_ex'],
-		'0880' => ['sync_request_ex'],
-		'0881' => ['sync_request_ex'],
-		'0882' => ['sync_request_ex'],
-		'0883' => ['sync_request_ex'],
-		'0917' => ['sync_request_ex'],
-		'0918' => ['sync_request_ex'],
-		'0919' => ['sync_request_ex'],
-		'091A' => ['sync_request_ex'],
-		'091B' => ['sync_request_ex'],
-		'091C' => ['sync_request_ex'],
-		'091D' => ['sync_request_ex'],
-		'091E' => ['sync_request_ex'],
-		'091F' => ['sync_request_ex'],
-		'0920' => ['sync_request_ex'],
-		'0921' => ['sync_request_ex'],
-		'0922' => ['sync_request_ex'],
-		'0923' => ['sync_request_ex'],
-		'0924' => ['sync_request_ex'],
-		'0925' => ['sync_request_ex'],
-		'0926' => ['sync_request_ex'],
-		'0927' => ['sync_request_ex'],
-		'0928' => ['sync_request_ex'],
-		'0929' => ['sync_request_ex'],
-		'092A' => ['sync_request_ex'],
-		'092B' => ['sync_request_ex'],
-		'092C' => ['sync_request_ex'],
-		'092D' => ['sync_request_ex'],
-		'092E' => ['sync_request_ex'],
-		'092F' => ['sync_request_ex'],
-		'0930' => ['sync_request_ex'],
-		'0931' => ['sync_request_ex'],
-		'0932' => ['sync_request_ex'],
-		'0933' => ['sync_request_ex'],
-		'0934' => ['sync_request_ex'],
-		'0935' => ['sync_request_ex'],
-		'0936' => ['sync_request_ex'],
-		'0937' => ['sync_request_ex'],
-		'0938' => ['sync_request_ex'],
-		'0939' => ['sync_request_ex'],
-		'093A' => ['sync_request_ex'],
-		'093B' => ['sync_request_ex'],
-		'093C' => ['sync_request_ex'],
-		'093D' => ['sync_request_ex'],
-		'093E' => ['sync_request_ex'],
-		'093F' => ['sync_request_ex'],
 	);
-
+	
 	foreach my $switch (keys %packets) {
 		$self->{packet_list}{$switch} = $packets{$switch};
 	}
-
+	
 	Plugins::addHook('packet_pre/received_characters' => sub {
 		$self->{lockCharScreen} = 2;
 	});
@@ -157,7 +156,6 @@ sub items_nonstackable {
 		warning("items_nonstackable: unsupported packet ($args->{switch})!\n");
 	}
 }
-
 sub sync_request_ex {
 	my ($self, $args) = @_;
 	
@@ -168,91 +166,92 @@ sub sync_request_ex {
 	my $PacketID = $args->{switch};
 	
 	# Sync Ex Reply Array
-	my %sync_ex_question_reply = (
-		'0367' => '02C4',
-		'085A' => '0884',
-		'085B' => '0885',
-		'085C' => '0886',
-		'085D' => '0887',
-		'0436' => '0888',
-		'085F' => '0889',
-		'0860' => '088A',
-		'0861' => '088B',
-		'0862' => '088C',
-		'0863' => '088D',
-		'0864' => '088E',
-		'0865' => '088F',
-		'022D' => '0890',
-		'0867' => '0891',
-		'0868' => '023B',
-		'0869' => '0893',
-		'086A' => '0894',
-		'086B' => '0895',
-		'086C' => '0896',
-		'086D' => '0897',
-		'086E' => '0898',
-		'086F' => '0899',
-		'0870' => '089A',
-		'0202' => '089B',
-		'0872' => '089C',
-		'0873' => '089D',
-		'0874' => '089E',
-		'0875' => '0361',
-		'0876' => '08A0',
-		'0877' => '08A1',
-		'0878' => '08A2',
-		'0879' => '08A3',
-		'087A' => '08A4',
-		'087B' => '08A5',
-		'087C' => '08A6',
-		'087D' => '08A7',
-		'087E' => '08A8',
-		'087F' => '08A9',
-		'0880' => '08AA',
-		'0881' => '08AB',
-		'0882' => '08AC',
-		'0883' => '08AD',
-		'0917' => '0941',
-		'0918' => '0942',
-		'0919' => '0943',
-		'091A' => '0944',
-		'091B' => '0945',
-		'091C' => '0946',
-		'091D' => '0947',
-		'091E' => '0948',
-		'091F' => '0949',
-		'0920' => '094A',
-		'0921' => '094B',
-		'0922' => '094C',
-		'0923' => '094D',
-		'0924' => '094E',
-		'0925' => '094F',
-		'0926' => '0950',
-		'0927' => '0951',
-		'0928' => '0952',
-		'0929' => '0953',
-		'092A' => '0954',
-		'092B' => '0955',
-		'092C' => '0956',
-		'092D' => '0957',
-		'092E' => '0958',
-		'092F' => '0959',
-		'0930' => '095A',
-		'0931' => '095B',
-		'0932' => '0940',
-		'0933' => '095C',
-		'0934' => '095E',
-		'0935' => '095F',
-		'0936' => '0960',
-		'0937' => '0961',
-		'0938' => '0962',
-		'0939' => '0963',
-		'093A' => '0964',
-		'093B' => '0965',
-		'093C' => '0966',
-		'093D' => '0967',
-		'093E' => '0968',
-		'093F' => '0969',
+	my %sync_ex_question_reply = (	
+		'085E' => '0888',  
+		'091D' => '0947',  
+		'092C' => '0956',  
+		'0932' => '095C',  
+		'092D' => '0957',  
+		'085D' => '0887',  
+		'0919' => '0943',  
+		'0865' => '088F',  
+		'0866' => '0890',  
+		'0933' => '095D',  
+		'0867' => '0891',  
+		'0936' => '0960',  
+		'087C' => '08A6',  
+		'093A' => '0964',  
+		'0876' => '08A0',  
+		'0367' => '02C4',  
+		'0922' => '094C',  
+		'0868' => '0892',  
+		'0882' => '08AC',  
+		'091E' => '0948',  
+		'0861' => '088B',  
+		'091B' => '0945',  
+		'0863' => '088D',  
+		'0923' => '094D',  
+		'0920' => '094A',  
+		'0918' => '0942',  
+		'091C' => '0946',  
+		'087B' => '08A5',  
+		'092A' => '0954',  
+		'0875' => '089E',  
+		'0938' => '0962',  
+		'093F' => '0969',  
+		'093C' => '0966',  
+		'093E' => '0968',  
+		'0931' => '095B',  
+		'0926' => '0950',  
+		'092E' => '0958',  
+		'0921' => '094B',  
+		'0864' => '088E',  
+		'0935' => '095F',  
+		'085F' => '0889',  
+		'0930' => '095A',  
+		'091F' => '0949',  
+		'086D' => '0897',  
+		'085B' => '0885',  
+		'0927' => '0951',  
+		'0934' => '095E',  
+		'0917' => '0941',  
+		'093D' => '0967',  
+		'087F' => '08A9',  
+		'0937' => '0961',  
+		'087E' => '08A8',  
+		'0925' => '094F',  
+		'0924' => '094E',  
+		'0881' => '08AB',  
+		'092F' => '0959',  
+		'0878' => '08A2',  
+		'086A' => '0894',  
+		'085A' => '0884',  
+		'0871' => '089B',  
+		'086F' => '0899',  
+		'086C' => '0896',  
+		'0929' => '0953',  
+		'092B' => '0955',  
+		'0883' => '08AD',  
+		'086B' => '0895',  
+		'0873' => '089D',  
+		'0869' => '0893',  
+		'086E' => '0898',  
+		'0874' => '0940',  
+		'0860' => '088A',  
+		'0870' => '089A',  
+		'087D' => '08A7',  
+		'0880' => '08AA',  
+		'093B' => '0965',  
+		'0928' => '0952',  
+		'087A' => '08A4',  
+		'0872' => '089C',  
+		'091A' => '0944',  
+		'085C' => '0886',  
+		'0939' => '0963', 
+		'0862' => '088C',  
+		'0877' => '08A1', 
+		'0879' => '08A3',  
+		
 	);
 	
 	# Getting Sync Ex Reply ID from Table
@@ -272,8 +271,7 @@ sub sync_request_ex {
 
 	# Dispatching Sync Ex Reply
 	$messageSender->sendReplySyncRequestEx($SyncID);
-}
-
+}	
 sub login_pin_new_code_result {
 	my ($self, $args) = @_;
 	
@@ -296,7 +294,6 @@ sub login_pin_new_code_result {
 	}
 
 }
-
 sub login_pin_code_request {
 	my ($self, $args) = @_;
 	
@@ -332,13 +329,15 @@ sub login_pin_code_request {
 		return if (!($self->queryAndSaveLoginPinCode(T("The login PIN code that you entered is invalid. Please re-enter your login PIN code."))));
 		$messageSender->sendLoginPinCode($args->{seed}, 0);
 	} elsif ($args->{flag} == 8) {
+		# TODO: This part needs to be improved!
 		# PIN code incorrect.
 		error T("PIN code is incorrect.\n");
-		configModify('loginPinCode', '', 1);
-		return if (!($self->queryAndSaveLoginPinCode(T("The login PIN code that you entered is incorrect. Please re-enter your login PIN code."))));
-		$messageSender->sendLoginPinCode($args->{seed}, 0);
+		Commands::run("relog");
+		#configModify('loginPinCode', '', 1);
+		#return if (!($self->queryAndSaveLoginPinCode(T("The login PIN code that you entered is incorrect. Please re-enter your login PIN code."))));
+		#$messageSender->sendLoginPinCode($args->{seed}, 0);
 	} else {
-		debug("login_pin_code_request: unknown flag $args->{flag}\n");
+		debug("login_pin_code_request: unknown flag $args->{flag}");
 	}
 	$timeout{master}{time} = time;
 }
@@ -346,4 +345,5 @@ sub login_pin_code_request {
 *parse_quest_update_mission_hunt = *Network::Receive::ServerType0::parse_quest_update_mission_hunt_v2;
 *reconstruct_quest_update_mission_hunt = *Network::Receive::ServerType0::reconstruct_quest_update_mission_hunt_v2;
 
-1;
+1;	
+
