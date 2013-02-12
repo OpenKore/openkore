312ad427b8a01b05f162ce275181b0f35a864700
keplerbr <keplerbr@users.noreply.github.com>
2013-02-12 02:34:12 +0000

    * Receive/kRO and Send/kRO: Add packet_db in some files that had not yet (by LututuiBR)

--- openkore/trunk/src/Network/Receive/kRO/RagexeRE_2011_11_02a.pm
+++ openkore/trunk/src/Network/Receive/kRO/RagexeRE_2011_11_02a.pm
@@ -18,3 +18,47 @@ use strict;
 use base qw(Network::Receive::kRO::RagexeRE_2011_10_05a);
 
 1;
+
+=cut
+//2011-11-02aRagexeRE
+0x01FD,15,repairitem,2
+0x0436,26,friendslistadd,2
+0x0898,5,hommenu,2:4
+0x0281,36,storagepassword,0
+0x0288,-1,cashshopbuy,4:8
+0x088D,26,partyinvite2,2
+0x083C,19,wanttoconnection,2:6:10:14:18
+0x08AA,7,actionrequest,2:6
+0x02C4,10,useskilltoid,2:4:6
+0x0439,8,useitem,2:4
+0x0811,-1,itemlistwindowselected,2:4:8
+0x08A5,18,bookingregreq,2:4:6
+0x0803,4
+0x0804,14,bookingsearchreq,2:4:6:8:12
+0x0805,-1
+0x0806,2,bookingdelreq,0
+0x0807,4
+0x0808,14,bookingupdatereq,2
+0x0809,50
+0x080A,18
+0x080B,6
+0x0835,-1,reqopenbuyingstore,2:4:8:9:89
+0x089B,2,reqclosebuyingstore,0
+0x08A1,6,reqclickbuyingstore,2
+0x089E,-1,reqtradebuyingstore,2:4:8:12
+0x08AB,-1,searchstoreinfo,2:4:5:9:13:14:15
+0x088B,2,searchstoreinfonextpage,0
+0x08A2,12,searchstoreinfolistitemclick,2:6:10
+0x0364,5,walktoxy,2
+0x0817,6,ticksend,2
+0x0366,5,changedir,2:4
+0x0815,6,takeitem,2
+0x0885,6,dropitem,2:4
+0x0893,8,movetokafra,2:4
+0x0897,8,movefromkafra,2:4
+0x0369,10,useskilltopos,2:4:6:8
+0x08AD,90,useskilltoposinfo,2:4:6:8:10
+0x088A,6,getcharnamerequest,2
+0x0838,6,solvecharname,2
+0x08D7,28,battlegroundreg,2:4 //Added to prevent disconnections
+=pod
--- openkore/trunk/src/Network/Send/kRO/RagexeRE_2012_03_07f.pm
+++ openkore/trunk/src/Network/Send/kRO/RagexeRE_2012_03_07f.pm
@@ -140,4 +140,61 @@ sub sendSkillUse {
 		'08A5' => undef,
 		'08AB' => undef,
 		'08AD' => undef,
+
+//2012-03-07fRagexeRE
+0x01FD,15,repairitem,2
+0x0369,26,friendslistadd,2
+0x0863,5,hommenu,2:4
+0x0861,36,storagepassword,0
+0x0288,-1,cashshopbuy,4:8
+0x0929,26,partyinvite2,2
+0x086A,19,wanttoconnection,2:6:10:14:18
+0x0885,7,actionrequest,2:6
+0x0889,10,useskilltoid,2:4:6
+0x0439,8,useitem,2:4
+0x0870,-1,itemlistwindowselected,2:4:8
+0x0815,-1,reqopenbuyingstore,2:4:8:9:89
+0x0817,2,reqclosebuyingstore,0
+0x0360,6,reqclickbuyingstore,2
+0x0811,-1,reqtradebuyingstore,2:4:8:12
+0x0884,-1,searchstoreinfo,2:4:5:9:13:14:15
+0x0835,2,searchstoreinfonextpage,0
+0x0838,12,searchstoreinfolistitemclick,2:6:10
+0x0437,5,walktoxy,2
+0x0887,6,ticksend,2
+0x0890,5,changedir,2:4
+0x0865,6,takeitem,2
+0x02C4,6,dropitem,2:4
+0x093B,8,movetokafra,2:4
+0x0963,8,movefromkafra,2:4
+0x0438,10,useskilltopos,2:4:6:8
+0x0366,90,useskilltoposinfo,2:4:6:8:10
+0x096A,6,getcharnamerequest,2
+0x0368,6,solvecharname,2
+0x08E5,41,bookingregreq,2:4	//Added to prevent disconnections
+0x08E6,4
+0x08E7,10,bookingsearchreq,2
+0x08E8,-1
+0x08E9,2,bookingdelreq,2
+0x08EA,4
+0x08EB,39,bookingupdatereq,2
+0x08EC,73
+0x08ED,43
+0x08EE,6
+0x08EF,6,bookingignorereq,2
+0x08F0,6
+0x08F1,6,bookingjoinpartyreq,2
+0x08F2,36
+0x08F3,-1
+0x08F4,6
+0x08F5,-1,bookingsummonmember,2:4
+0x08F6,22
+0x08F7,3
+0x08F8,7
+0x08F9,6
+0x08FA,6
+0x08FB,6,bookingcanceljoinparty,2
+0x0907,5,moveitem,2:4
+0x0908,5
+0x08D7,28,battlegroundreg,2:4 //Added to prevent disconnections
 =pod
