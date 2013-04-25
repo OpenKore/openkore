7f033752d32e9885695ae6d2aa44f8a1b27d6089
ya4ept <ya4ept@3e610c8b-890d-0410-8877-b158ba2967cd>
2013-04-25 13:46:52 +0000

    Update plugin xConf

--- plugins/xconf/trunk/xconf.pl
+++ plugins/xconf/trunk/xconf.pl
@@ -1,7 +1,7 @@
 # xConf plugin by 4epT (ICQ 2227733)
 # Based on Lims idea
-# Edited by Dairey, manticora
-# Last changes 12.09.2010
+# Version: 4
+# Last changes 24.04.2013
 # Plug-in for change mon_control/pickupitems/items_control/priority files, using console commands.
 #
 # Examples of commands:
@@ -16,17 +16,15 @@
 #
 # priconf Pupa, Poring, Lunatic
 
-
 package xConf;
 
+use strict;
 use Plugins;
 use Globals;
-use Log qw(message error debug);
+use Log qw(message error debug warning);
 
-## startup
-Plugins::register('xConf', 'commands for change items_control, mon_control, pickupitems, priority', \&Unload, \&Reload);
+Plugins::register('xConf', 'commands for change items_control, mon_control, pickupitems, priority', \&Unload, \&Unload);
 
-## Register command 'xConf'
 my $chooks = Commands::register(
 	['iconf', 'edit items_control.txt', \&xConf],
 	['mconf', 'edit mon_control.txt', \&xConf],
@@ -35,141 +33,160 @@ my $chooks = Commands::register(
 	['priconf', 'edit priority.txt', \&priconf],
 );
 
-sub Reload {
-	Commands::unregister($chooks);
-	message "xConf plugin reloading\n", 'success'
-}
 sub Unload {
 	Commands::unregister($chooks);
-	message "xConf plugin unloading\n", 'success'
+	message "xConf plugin reloading or unloading\n", 'success'
 }
 
 sub xConf {
 	my ($cmd, $args) = @_;
-	my ($key, $value) = $args =~ /([\s\S]+?)(?: |   )([\-\d\.]+[\s\S]*)/;
+	my ($file,$file2,$found,$key,$oldval,$shopname,$type,$value, %inf_hash, %ctrl_hash);
+	if ($cmd eq 'sconf') {
+		($key, $value) = $args =~ /(name)(?:\s)(.*)/;
+		$shopname = 1 if $key eq 'name';
+	}
+	($key, $value) = $args =~ /([\s\S]+?)(?:\s)([\-\d\.]+[\s\S]*)/ if !$shopname;
 	$key = $args if !$key;
+	$key =~ s/\s+$//g;
 debug "KEY: $key, VALUE: $value\n";
 	if (!$key) {
-		error "Syntax Error in function '$cmd'. Not found <key>\nUsage: $cmd <key> [<value>]\n";
-		return
+		error "Syntax Error in function '$cmd'. Not found <key>\n".
+				"Usage: $cmd <key> [<value>]\n";
+		return;
 	}
-	my ($file,$found,$name, %inf_hash, %ctrl_hash) = undef;
-
-	if ($cmd eq 'mconf') {
+	if ($cmd eq 'iconf') {
+		%inf_hash  = %items_lut;
+		%ctrl_hash = %items_control;
+		$file = 'items_control.txt';
+		$file2 = 'tables\..\items.txt';
+		$type = 'Item';
+	} elsif ($cmd eq 'mconf') {
 		%inf_hash  = %monsters_lut;
 		%ctrl_hash = %mon_control;
-		$file = 'mon_control.txt'
+		$file = 'mon_control.txt';
+		$file2 = 'tables\..\monsters.txt';
+		$type = 'Monster';
 	} elsif ($cmd eq 'pconf') {
 		%inf_hash  = %items_lut;
 		%ctrl_hash = %pickupitems;
-		$file = 'pickupitems.txt'
-	} elsif ($cmd eq 'iconf') {
-		%inf_hash  = %items_lut;
-		%ctrl_hash = %items_control;
-		$file = 'items_control.txt'
+		$file = 'pickupitems.txt';
+		$file2 = 'tables\..\items.txt';
+		$type = 'Item';
 	} elsif ($cmd eq 'sconf') {
 		%inf_hash = %items_lut;
 		%ctrl_hash = %shop;
-		$file = 'shop.txt'
+		$file = 'shop.txt';
+		$file2 = 'tables\..\items.txt';
+		$type = 'Item';
 	}
 
-	## Check $key in tables\monsters.txt & items.txt
-
+## Check $key in tables\monsters.txt & tables\items.txt
 	if ($key ne "all") {
+## Search name by ID
 		if ($inf_hash{$key}) {
-debug "'$inf_hash{$key}' ID: $key is found in 'tables\\monsters.txt'.\n";
+debug "$type '$inf_hash{$key}' ID: $key is found in file '$file2'.\n";
 		$found = 1;
 		$key = $inf_hash{$key}
 		} else {
-			foreach $name (values %inf_hash) {
+## Search name by $key
+			foreach my $name (values %inf_hash) {
 				if ($found = (lc($key) eq lc($name))) {
 					$key = $name;
-debug "'$name' is found in 'tables\\monsters.txt'.\n";
+debug "$type '$name' is found in file '$file2'.\n";
 					last
 				}
 			}
 		}
-		if (!$found) {error "WARNING: '$key' is not found in 'tables\\monsters.txt' and in 'tables\\items.txt'!\n"}
-	}
-	
-	if ($value eq '') {
-		if ($cmd eq 'sconf') {
-			my $i = 0;
-			$found = 0;
-			until ($ctrl_hash{items}[$i]{name} eq "") {
-				if ($ctrl_hash{items}[$i]{name} eq $args) {
-					$found = 1;
-					message "$file: $ctrl_hash{items}[$i]{name} $ctrl_hash{items}[$i]{price} $ctrl_hash{items}[$i]{amount}\n", 'list'
-				}
-			$i++;
-			}
-			if (!$found) {
-				error "The key '$key' is not found in '$file'\n";
-			}
-		} else {
-			if ($ctrl_hash{lc($key)}) {
-				$key = lc($key);
-				if ($cmd eq 'mconf') {
-				message "$file: $key $ctrl_hash{$key}{attack_auto} $ctrl_hash{$key}{teleport_auto} $ctrl_hash{$key}{teleport_search} $ctrl_hash{$key}{skillcancel_auto} $ctrl_hash{$key}{attack_lvl} $ctrl_hash{$key}{attack_jlvl} $ctrl_hash{$key}{attack_hp} $ctrl_hash{$key}{attack_sp} $ctrl_hash{$key}{weight}\n", 'list'
-				} elsif ($cmd eq 'pconf') {
-					message "$file: $key $pickupitems{$key}\n", 'list'
-				} elsif ($cmd eq 'iconf') {
-					message "$file: $key $items_control{$key}{keep} $items_control{$key}{storage} $items_control{$key}{sell} $items_control{$key}{cart_add} $items_control{$key}{cart_get}\n", 'list'
-				} elsif ($cmd eq 'priconf') {
-					message "$file: $key $priority{$key}\n", 'list'
-				}
+		if (!$found and !$shopname) {
+			if ($cmd eq 'mconf') {
+				warning "WARNING: $type '$key' is not found in file '$file2'!\n";
 			} else {
-				error "The key '$key' is not found in '$file'\n"
+				error "$type '$key' is not found in file '$file2'!\n";
+				return;
 			}
 		}
-		return
 	}
-	filewrite($file, $key, $value)
+
+	if ($cmd eq 'iconf') {$oldval = "$items_control{lc($key)}{keep} $items_control{lc($key)}{storage} $items_control{lc($key)}{sell} $items_control{lc($key)}{cart_add} $items_control{lc($key)}{cart_get}"}
+	if ($cmd eq 'mconf') {$oldval = "$ctrl_hash{lc($key)}{attack_auto} $ctrl_hash{lc($key)}{teleport_auto} $ctrl_hash{lc($key)}{teleport_search} $ctrl_hash{lc($key)}{skillcancel_auto} $ctrl_hash{lc($key)}{attack_lvl} $ctrl_hash{lc($key)}{attack_jlvl} $ctrl_hash{lc($key)}{attack_hp} $ctrl_hash{lc($key)}{attack_sp} $ctrl_hash{lc($key)}{weight}"}
+	if ($cmd eq 'pconf') {$oldval = $pickupitems{lc($key)}}
+	if ($cmd eq 'sconf') {
+		if ($shopname) {
+			$oldval = $shop{title_line};
+			$oldval =~ s/;;/,,/g;
+		} else {
+			for my $sale (@{$shop{items}}) {
+				if (lc($sale->{name}) eq lc($key)) {
+					$oldval = "$sale->{price} $sale->{amount}";
+					last;
+				}
+			}
+		}
+	}
+	$oldval =~ s/\s+$//g;
+	$value =~ s/\s+$//g;
+debug "VALUE: '$value', OLDVALUE: '$oldval'\n";
+	if (not defined $value and $oldval eq '') {
+		error "$type '$key' is not found in file '$file'!\n";
+	} elsif (not defined $value or $value eq $oldval) {
+		message "$file: '$key' is $oldval\n", "info";
+	} else {
+		filewrite($file, $key, $value, $oldval,$shopname);
+	}
 }
 
 ## write FILE
 sub filewrite {
-	my ($file, $key, $value) = @_;
+	my ($file, $key, $value, $oldval,$shopname) = @_;
+	my @value;
 	my $controlfile = Settings::getControlFilename($file);
-debug "sub WRITE = FILE: $file, KEY: $key, VALUE: $value\n";
+debug "sub WRITE = FILE: $file, KEY: $key, VALUE: $value, OLDVALUE: $oldval\n";
 	open(FILE, "<:utf8", $controlfile);
 	my @lines = <FILE>;
 	close(FILE);
 	chomp @lines;
 
 	my $used = 0;
-	my $nochanges = 1;
+	if ($shopname) {
+		@value = split(/,,/, $value);
+		foreach my $line (@lines) {
+			next if $line =~ /^$/ || $line =~ /^#/;
+			if ($line eq $shop{title_line}) {
+				$line = join (';;', @value);
+				last;
+			}
+		}
+	} else {
+		@value = split(/\s+/, $value);
 	foreach my $line (@lines) {
-		my ($what) = $line =~ /([\s\S]+?)(?: |   )(?:[\-\d\.]+[\s\S]*)/;
-		my $tmp = "";
+		my ($what) = $line =~ /([\s\S]+?)(?:\s[\-\d\.]+[\s\S]*)/;
+		$what =~ s/\s+$//g;
+		my $tmp;
 		if (lc($what) eq lc($key)) {
+debug "Change old record: ";
 			if ($file eq 'shop.txt') {
-				$tmp = join ('	', $key, $value);
+				$tmp = join ('	', $key, @value);
 			} else {
-				$tmp = join (' ', $key, $value);
+				$tmp = join (' ', $key, @value);
 			}
-			if ($line ne $tmp) {
-				$nochanges = 0;
-				$line = $tmp;
-			}
+			$line = $tmp;
 			$used = 1;
 		}
 	}
 	if ($used == 0) {
-		$nochanges = 0;
+debug "New record: ";
 		if ($file eq 'shop.txt') {
-			push (@lines, $key.'	'.$value)
+			push (@lines, $key.'	'.@value)
 		} else {
-			push (@lines, $key.' '.$value)
+			push (@lines, $key.' '.@value)
 		}
 	}
-	message "$file: $key $value\n", 'system';
-	if ($nochanges == 0) {
-		open(WRITE, ">:utf8", $controlfile);
-		print WRITE join ("\n", @lines);
-		close(WRITE);
-		Commands::run("reload $file")
 	}
+	open(WRITE, ">:utf8", $controlfile);
+	print WRITE join ("\n", @lines);
+	close(WRITE);
+	message "$file: '$key' set to @value (was $oldval)\n", 'info';
+	Commands::run("reload $file")
 }
 
 
@@ -177,43 +194,24 @@ sub priconf {
 	my ($cmd, $args) = @_;
 	my @mobs = split /\s*,\s*/, $args;
 	if (@mobs == 0) { 
-		error "Syntax Error in function 'priconf'.\nUsage: priconf monster1, monster2, ...\n";
-		return
+		error "Syntax Error in function 'priconf'.\n".
+				"Usage: priconf monster1, monster2, ...\n";
+		return;
 	}
 	my $controlfile = Settings::getControlFilename('priority.txt');
 	open(my $file,"<:utf8",$controlfile);
-	my @lines = ();
-	my $begin = 1;
-	while (my $line = <$file>) {
-		my $tmp = $line;
+	my @lines;
+	while (my $tmp = <$file>) {
 		$tmp =~ s/\x{FEFF}//g;
-		chomp($tmp);
-		
-		if (($tmp =~ /^#/) or ($tmp =~ /^\s*$/)) {
-			push @lines, $line;
-			next;
-		}
-		
-		if ($begin == 1) {
-			foreach (@mobs) { push @lines, $_."\n" }
-			$begin = 0;		
-		}
-		
-		my $ok = 1;
-		foreach (@mobs) {
-			if (lc($_) eq lc($tmp)) {
-				$ok = 0;
-				last;
-			}
-		}
-		if ($ok == 1) {
-			push @lines, $line;
-		}
+		push @lines, $tmp if $tmp =~ /^#/;
 	}
+	push @lines, "\n";
+	foreach (@mobs) { push @lines, $_."\n" }
 	close($file);
 	open($file,">:utf8",$controlfile);
 	print $file @lines;
 	close($file);
 	Commands::run("reload priority.txt");
 }
-return 1;
+
+1;
