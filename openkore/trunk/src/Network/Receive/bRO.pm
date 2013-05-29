4b4e092c2f4d75a36d794a6d4a9aa28c8cd0874c
roxleopardo <roxleopardo@3e610c8b-890d-0410-8877-b158ba2967cd>
2013-05-29 13:31:29 +0000

    bRO Update 2013/05/29

--- openkore/trunk/src/Network/Receive/bRO.pm
+++ openkore/trunk/src/Network/Receive/bRO.pm
@@ -1,14 +1,14 @@
-#############################################################################
-#  OpenKore - Network subsystem												#
-#  This module contains functions for sending messages to the server.		#
-#																			#
-#  This software is open source, licensed under the GNU General Public		#
-#  License, version 2.														#
-#  Basically, this means that you're allowed to modify and distribute		#
-#  this software. However, if you distribute modified versions, you MUST	#
-#  also distribute the source code.											#
-#  See http://www.gnu.org/licenses/gpl.html for the full license.			#
-#############################################################################
+#################################################################################################
+#  OpenKore - Network subsystem									#
+#  This module contains functions for sending messages to the server.				#
+#												#
+#  This software is open source, licensed under the GNU General Public				#
+#  License, version 2.										#
+#  Basically, this means that you're allowed to modify and distribute				#
+#  this software. However, if you distribute modified versions, you MUST			#
+#  also distribute the source code.								#
+#  See http://www.gnu.org/licenses/gpl.html for the full license.				#
+#################################################################################################
 # bRO (Brazil)
 package Network::Receive::bRO;
 use strict;
@@ -26,90 +26,92 @@ sub new {
 	
 	my %packets = (
 		'0097' => ['private_message', 'v Z24 V Z*', [qw(len privMsgUser flag privMsg)]], # -1
-		'0367' => ['sync_request_ex'],  
-		'085A' => ['sync_request_ex'],  
-		'085B' => ['sync_request_ex'],  
-		'085C' => ['sync_request_ex'],  
-		'085D' => ['sync_request_ex'],  
-		'085E' => ['sync_request_ex'],  
-		'0365' => ['sync_request_ex'],  
-		'0364' => ['sync_request_ex'],  
-		'0861' => ['sync_request_ex'],  
-		'0862' => ['sync_request_ex'],  
-		'0863' => ['sync_request_ex'],  
-		'0802' => ['sync_request_ex'],  
-		'0865' => ['sync_request_ex'],  
-		'0866' => ['sync_request_ex'],  
-		'0867' => ['sync_request_ex'],  
-		'0868' => ['sync_request_ex'],  
-		'0869' => ['sync_request_ex'],  
-		'086A' => ['sync_request_ex'],  
-		'086B' => ['sync_request_ex'],  
-		'086C' => ['sync_request_ex'],  
-		'086D' => ['sync_request_ex'],  
-		'086E' => ['sync_request_ex'],  
-		'086F' => ['sync_request_ex'],  
-		'0870' => ['sync_request_ex'],  
-		'0871' => ['sync_request_ex'],  
-		'0872' => ['sync_request_ex'],  
-		'022D' => ['sync_request_ex'],  
-		'0874' => ['sync_request_ex'],  
-		'0875' => ['sync_request_ex'],  
-		'0886' => ['sync_request_ex'],  
-		'0877' => ['sync_request_ex'],  
-		'0878' => ['sync_request_ex'],  
-		'0879' => ['sync_request_ex'],  
-		'087A' => ['sync_request_ex'],  
-		'087B' => ['sync_request_ex'],  
-		'087C' => ['sync_request_ex'],  
-		'087D' => ['sync_request_ex'],  
-		'0887' => ['sync_request_ex'],  
-		'087F' => ['sync_request_ex'],  
-		'0880' => ['sync_request_ex'],  
-		'0881' => ['sync_request_ex'],  
-		'0882' => ['sync_request_ex'],  
-		'0883' => ['sync_request_ex'],  
-		'0917' => ['sync_request_ex'],  
-		'0918' => ['sync_request_ex'],  
-		'0919' => ['sync_request_ex'],  
-		'091A' => ['sync_request_ex'],  
-		'091B' => ['sync_request_ex'],  
-		'091C' => ['sync_request_ex'],  
-		'091D' => ['sync_request_ex'],  
-		'091E' => ['sync_request_ex'],  
-		'091F' => ['sync_request_ex'],  
-		'0920' => ['sync_request_ex'],  
-		'0921' => ['sync_request_ex'],  
-		'0922' => ['sync_request_ex'],  
-		'0923' => ['sync_request_ex'],  
-		'0924' => ['sync_request_ex'],  
-		'0925' => ['sync_request_ex'],  
-		'0926' => ['sync_request_ex'],  
-		'0927' => ['sync_request_ex'],  
-		'0928' => ['sync_request_ex'],  
-		'0929' => ['sync_request_ex'],  
-		'092A' => ['sync_request_ex'],  
-		'023B' => ['sync_request_ex'],  
-		'092C' => ['sync_request_ex'],  
-		'092D' => ['sync_request_ex'],  
-		'092E' => ['sync_request_ex'],  
-		'092F' => ['sync_request_ex'],  
-		'0930' => ['sync_request_ex'],  
-		'02C4' => ['sync_request_ex'],  
-		'0932' => ['sync_request_ex'],  
-		'0933' => ['sync_request_ex'],  
-		'0362' => ['sync_request_ex'],  
-		'0935' => ['sync_request_ex'],  
-		'0936' => ['sync_request_ex'],  
-		'0937' => ['sync_request_ex'],  
-		'0938' => ['sync_request_ex'],  
-		'0939' => ['sync_request_ex'],  
-		'093A' => ['sync_request_ex'],  
-		'093B' => ['sync_request_ex'],  
-		'0202' => ['sync_request_ex'],  
-		'093D' => ['sync_request_ex'],  
-		'093E' => ['sync_request_ex'],  
-		'093F' => ['sync_request_ex'],  
+		'08B9' => ['login_pin_code_request', 'V V v', [qw(seed accountID flag)]],
+		'08BB' => ['login_pin_new_code_result', 'v V', [qw(flag seed)]],
+		'086C' => ['sync_request_ex'],
+		'08AC' => ['sync_request_ex'],
+		'0955' => ['sync_request_ex'],
+		'083C' => ['sync_request_ex'],
+		'0886' => ['sync_request_ex'],
+		'092B' => ['sync_request_ex'],
+		'0369' => ['sync_request_ex'],
+		'08A1' => ['sync_request_ex'],
+		'08A7' => ['sync_request_ex'],
+		'08A5' => ['sync_request_ex'],
+		'089A' => ['sync_request_ex'],
+		'0959' => ['sync_request_ex'],
+		'0934' => ['sync_request_ex'],
+		'0948' => ['sync_request_ex'],
+		'085A' => ['sync_request_ex'],
+		'086D' => ['sync_request_ex'],
+		'092E' => ['sync_request_ex'],
+		'023B' => ['sync_request_ex'],
+		'0862' => ['sync_request_ex'],
+		'0891' => ['sync_request_ex'],
+		'0932' => ['sync_request_ex'],
+		'0876' => ['sync_request_ex'],
+		'0953' => ['sync_request_ex'],
+		'0917' => ['sync_request_ex'],
+		'0892' => ['sync_request_ex'],
+		'0951' => ['sync_request_ex'],
+		'0838' => ['sync_request_ex'],
+		'0802' => ['sync_request_ex'],
+		'0965' => ['sync_request_ex'],
+		'087B' => ['sync_request_ex'],
+		'091C' => ['sync_request_ex'],
+		'0927' => ['sync_request_ex'],
+		'089D' => ['sync_request_ex'],
+		'091D' => ['sync_request_ex'],
+		'0931' => ['sync_request_ex'],
+		'08A0' => ['sync_request_ex'],
+		'089B' => ['sync_request_ex'],
+		'07E4' => ['sync_request_ex'],
+		'0925' => ['sync_request_ex'],
+		'0937' => ['sync_request_ex'],
+		'087C' => ['sync_request_ex'],
+		'088D' => ['sync_request_ex'],
+		'0872' => ['sync_request_ex'],
+		'089C' => ['sync_request_ex'],
+		'0921' => ['sync_request_ex'],
+		'0202' => ['sync_request_ex'],
+		'02C4' => ['sync_request_ex'],
+		'035F' => ['sync_request_ex'],
+		'0936' => ['sync_request_ex'],
+		'085F' => ['sync_request_ex'],
+		'0437' => ['sync_request_ex'],
+		'0884' => ['sync_request_ex'],
+		'0935' => ['sync_request_ex'],
+		'0924' => ['sync_request_ex'],
+		'086F' => ['sync_request_ex'],
+		'093C' => ['sync_request_ex'],
+		'0961' => ['sync_request_ex'],
+		'094B' => ['sync_request_ex'],
+		'088E' => ['sync_request_ex'],
+		'093A' => ['sync_request_ex'],
+		'08AD' => ['sync_request_ex'],
+		'0360' => ['sync_request_ex'],
+		'0926' => ['sync_request_ex'],
+		'0835' => ['sync_request_ex'],
+		'093E' => ['sync_request_ex'],
+		'0941' => ['sync_request_ex'],
+		'08A4' => ['sync_request_ex'],
+		'0869' => ['sync_request_ex'],
+		'08A2' => ['sync_request_ex'],
+		'0438' => ['sync_request_ex'],
+		'0366' => ['sync_request_ex'],
+		'0364' => ['sync_request_ex'],
+		'0895' => ['sync_request_ex'],
+		'091E' => ['sync_request_ex'],
+		'0881' => ['sync_request_ex'],
+		'087A' => ['sync_request_ex'],
+		'0861' => ['sync_request_ex'],
+		'092F' => ['sync_request_ex'],
+		'085C' => ['sync_request_ex'],
+		'093D' => ['sync_request_ex'],
+		'0880' => ['sync_request_ex'],
+		'0945' => ['sync_request_ex'],
+		'0929' => ['sync_request_ex'],
+		'088F' => ['sync_request_ex'],
 	);
 
 	foreach my $switch (keys %packets) {
@@ -124,7 +126,7 @@ sub new {
 	Plugins::addHook(charSelectScreen => sub {
 		$_[1]{return} = $self->{lockCharScreen};
 	});
-	
+
 	return $self;
 }
 
@@ -164,26 +166,92 @@ sub sync_request_ex {
 	# Computing Sync Ex - By Fr3DBr
 	my $PacketID = $args->{switch};
 	
-	
-	# Sync Ex Reply Array 
+	# Sync Ex Reply Array
 	my %sync_ex_question_reply = (
-		'0367', '0438', '085A', '0948', '085B', '0876', '085C', '087E', '085D', '094C', 
-		'085E', '0437', '0365', '0889', '0364', '088A', '0861', '088B', '0862', '088C', 
-		'0863', '088D', '0802', '088E', '0865', '088F', '0866', '0890', '0867', '0891', 
-		'0868', '0892', '0869', '0893', '086A', '0894', '086B', '0895', '086C', '0896', 
-		'086D', '0897', '086E', '07EC', '086F', '0281', '0870', '089A', '0871', '089B', 
-		'0872', '089C', '022D', '089D', '0874', '089E', '0875', '089F', '0886', '08A0', 
-		'0877', '08A1', '0878', '08A2', '0879', '08A3', '087A', '08A4', '087B', '08A5', 
-		'087C', '08A6', '087D', '08A7', '0887', '08A8', '087F', '08A9', '0880', '08AA', 
-		'0881', '08AB', '0882', '08AC', '0883', '08AD', '0917', '0941', '0918', '0942', 
-		'0919', '0943', '091A', '0361', '091B', '0945', '091C', '0946', '091D', '0947', 
-		'091E', '0885', '091F', '0949', '0920', '094A', '0921', '094B', '0922', '0888', 
-		'0923', '094D', '0924', '094E', '0925', '094F', '0926', '0950', '0927', '0951', 
-		'0928', '0952', '0929', '0953', '092A', '0954', '023B', '0955', '092C', '0956', 
-		'092D', '0957', '092E', '0958', '092F', '0959', '0930', '095A', '02C4', '095B', 
-		'0932', '095C', '0933', '095D', '0362', '095E', '0935', '095F', '0936', '0436', 
-		'0937', '0961', '0938', '0962', '0939', '0963', '093A', '0964', '093B', '0965', 
-		'0202', '0966', '093D', '0967', '093E', '0968', '093F', '0969'
+		'086C' => '0920',
+		'08AC' => '088B',
+		'0955' => '0956',
+		'083C' => '087E',
+		'0886' => '0947',
+		'092B' => '0958',
+		'0369' => '0867',
+		'08A1' => '093F',
+		'08A7' => '0868',
+		'08A5' => '0281',
+		'089A' => '0957',
+		'0959' => '0894',
+		'0934' => '0942',
+		'0948' => '0362',
+		'085A' => '091A',
+		'086D' => '091F',
+		'092E' => '086A',
+		'023B' => '086B',
+		'0862' => '095F',
+		'0891' => '0365',
+		'0932' => '08A8',
+		'0876' => '0962',
+		'0953' => '0968',
+		'0917' => '0928',
+		'0892' => '0819',
+		'0951' => '095B',
+		'0838' => '0964',
+		'0802' => '0954',
+		'0965' => '0817',
+		'087B' => '0890',
+		'091C' => '0874',
+		'0927' => '0815',
+		'089D' => '0896',
+		'091D' => '0865',
+		'0931' => '0940',
+		'08A0' => '0919',
+		'089B' => '0870',
+		'07E4' => '092D',
+		'0925' => '096A',
+		'0937' => '0897',
+		'087C' => '0887',
+		'088D' => '095C',
+		'0872' => '0939',
+		'089C' => '088C',
+		'0921' => '093B',
+		'0202' => '0952',
+		'02C4' => '0923',
+		'035F' => '0963',
+		'0936' => '094A',
+		'085F' => '0930',
+		'0437' => '022D',
+		'0884' => '0864',
+		'0935' => '0878',
+		'0924' => '0933',
+		'086F' => '0877',
+		'093C' => '085B',
+		'0961' => '08AB',
+		'094B' => '089F',
+		'088E' => '092C',
+		'093A' => '0888',
+		'08AD' => '095A',
+		'0360' => '0879',
+		'0926' => '07EC',
+		'0835' => '08A9',
+		'093E' => '0966',
+		'0941' => '0898',
+		'08A4' => '0967',
+		'0869' => '085E',
+		'08A2' => '094E',
+		'0438' => '0368',
+		'0366' => '088A',
+		'0364' => '087F',
+		'0895' => '0943',
+		'091E' => '0960',
+		'0881' => '08A3',
+		'087A' => '0361',
+		'0861' => '095D',
+		'092F' => '0885',
+		'085C' => '091B',
+		'093D' => '0922',
+		'0880' => '0944',
+		'0945' => '08AA',
+		'0929' => '094C',
+		'088F' => '0811',
 	);
 	
 	# Getting Sync Ex Reply ID from Table
@@ -203,6 +271,75 @@ sub sync_request_ex {
 
 	# Dispatching Sync Ex Reply
 	$messageSender->sendReplySyncRequestEx($SyncID);
+}
+
+sub login_pin_new_code_result {
+	my ($self, $args) = @_;
+	
+	if ($args->{flag} == 2) {
+		# PIN code invalid.
+		error T("PIN code is invalid, don't use sequences or repeated numbers.\n");
+		configModify('loginPinCode', '', 1);
+		return if (!($self->queryAndSaveLoginPinCode(T("PIN code is invalid, don't use sequences or repeated numbers.\n"))));
+		
+		# there's a bug in bRO where you can use letters or symbols or even a string as your PIN code.
+		# as a result this will render you unable to login again (forever?) using the official client
+		# and this is detectable and can result in a permanent ban. we're using this code in order to
+		# prevent this. - revok 17.12.2012
+		while ((($config{loginPinCode} =~ /[^0-9]/) || (length($config{loginPinCode}) != 4)) &&
+			!($self->queryAndSaveLoginPinCode("Your PIN should never contain anything but exactly 4 numbers.\n"))) {
+			error T("Your PIN should never contain anything but exactly 4 numbers.\n");
+		}
+		
+		$messageSender->sendLoginPinCode($args->{seed}, 0);
+	}
+
+}
+
+sub login_pin_code_request {
+	my ($self, $args) = @_;
+	
+	if (($args->{seed} == 0) && ($args->{accountID} == 0) && ($args->{flag} == 0)) {
+		message T("PIN code is correct.\n"), "success";
+		# call charSelectScreen
+		$self->{lockCharScreen} = 0;
+		if (charSelectScreen(1) == 1) {
+			$firstLoginMap = 1;
+			$startingzeny = $chars[$config{'char'}]{'zeny'} unless defined $startingzeny;
+			$sentWelcomeMessage = 1;
+		}
+	} elsif ($args->{flag} == 1) {
+		# PIN code query request.
+		message T("Server requested PIN password in order to select your character.\n"), "connection";
+		return if ($config{loginPinCode} eq '' && !($self->queryAndSaveLoginPinCode()));
+		$messageSender->sendLoginPinCode($args->{seed}, 0);
+		#$messageSender->sendCharLogin($config{char});
+	} elsif ($args->{flag} == 2) {
+		# PIN code has never been set before, so set it.
+		warning T("PIN password is not set for this account.\n"), "connection";
+		return if ($config{loginPinCode} eq '' && !($self->queryAndSaveLoginPinCode()));
+		
+		while ((($config{loginPinCode} =~ /[^0-9]/) || (length($config{loginPinCode}) != 4)) &&
+			!($self->queryAndSaveLoginPinCode("Your PIN should never contain anything but exactly 4 numbers.\n"))) {
+			error T("Your PIN should never contain anything but exactly 4 numbers.\n");
+		}
+		$messageSender->sendLoginPinCode($args->{seed}, 1);
+	} elsif ($args->{flag} == 5) {
+		# PIN code invalid.
+		error T("PIN code is invalid, don't use sequences or repeated numbers.\n");
+		configModify('loginPinCode', '', 1);
+		return if (!($self->queryAndSaveLoginPinCode(T("The login PIN code that you entered is invalid. Please re-enter your login PIN code."))));
+		$messageSender->sendLoginPinCode($args->{seed}, 0);
+	} elsif ($args->{flag} == 8) {
+		# PIN code incorrect.
+		error T("PIN code is incorrect.\n");
+		configModify('loginPinCode', '', 1);
+		return if (!($self->queryAndSaveLoginPinCode(T("The login PIN code that you entered is incorrect. Please re-enter your login PIN code."))));
+		$messageSender->sendLoginPinCode($args->{seed}, 0);
+	} else {
+		debug("login_pin_code_request: unknown flag $args->{flag}\n");
+	}
+	$timeout{master}{time} = time;
 }
 
 *parse_quest_update_mission_hunt = *Network::Receive::ServerType0::parse_quest_update_mission_hunt_v2;
--- openkore/trunk/src/Network/Send/bRO.pm
+++ openkore/trunk/src/Network/Send/bRO.pm
@@ -1,14 +1,14 @@
-#############################################################################
-#  OpenKore - Network subsystem												#
-#  This module contains functions for sending messages to the server.		#
-#																			#
-#  This software is open source, licensed under the GNU General Public		#
-#  License, version 2.														#
-#  Basically, this means that you're allowed to modify and distribute		#
-#  this software. However, if you distribute modified versions, you MUST	#
-#  also distribute the source code.											#
-#  See http://www.gnu.org/licenses/gpl.html for the full license.			#
-#############################################################################
+#################################################################################################
+#  OpenKore - Network subsystem									#
+#  This module contains functions for sending messages to the server.				#
+#												#
+#  This software is open source, licensed under the GNU General Public				#
+#  License, version 2.										#
+#  Basically, this means that you're allowed to modify and distribute				#
+#  this software. However, if you distribute modified versions, you MUST			#
+#  also distribute the source code.								#
+#  See http://www.gnu.org/licenses/gpl.html for the full license.				#
+#################################################################################################
 # bRO (Brazil)
 package Network::Send::bRO;
 use strict;
@@ -24,44 +24,45 @@ sub new {
 	my $self = $class->SUPER::new(@_);
 	
 	my %packets = (
-		'0940' => ['actor_action', 'a4 C', [qw(targetID type)]],
-		'0369' => ['character_move','a3', [qw(coords)]],
-		'035F' => ['sync', 'V', [qw(time)]],
-		'07E4' => ['actor_look_at', 'v C', [qw(head body)]],
-		'0934' => ['item_take', 'a4', [qw(ID)]],
-		'0819' => ['item_drop', 'v2', [qw(index amount)]],
-		'0860' => ['storage_item_add', 'v V', [qw(index amount)]],
-		'085F' => ['storage_item_remove', 'v V', [qw(index amount)]],
-		'0884' => ['skill_use_location', 'v4', [qw(lv skillID x y)]],
-		'096A' => ['actor_info_request', 'a4', [qw(ID)]],
-		'0960' => ['map_login', 'a4 a4 a4 V C', [qw(accountID charID sessionID tick sex)]],
-		'0931' => ['party_join_request_by_name', 'Z24', [qw(partyName)]], #f
-		'0873' => ['homunculus_command', 'v C', [qw(commandType, commandID)]], #f
-		'092B' => ['storage_password_give', 'v H16 a16', [qw(type key encrypted_password)]],
-		'092B' => ['storage_password_set', 'v a16 H16', [qw(type encrypted_password key)]],
+		'0882' => ['actor_action', 'a4 C', [qw(targetID type)]],
+		'087D' => ['character_move','a3', [qw(coords)]],
+		'0860' => ['sync', 'V', [qw(time)]],
+		'0949' => ['actor_look_at', 'v C', [qw(head body)]],
+		'0899' => ['item_take', 'a4', [qw(ID)]],
+		'0893' => ['item_drop', 'v2', [qw(index amount)]],
+		'089E' => ['storage_item_add', 'v V', [qw(index amount)]],
+		'0969' => ['storage_item_remove', 'v V', [qw(index amount)]],
+		'0918' => ['skill_use_location', 'v4', [qw(lv skillID x y)]],
+		'0363' => ['actor_info_request', 'a4', [qw(ID)]],
+		'08A6' => ['map_login', 'a4 a4 a4 V C', [qw(accountID charID sessionID tick sex)]],
+		'094D' => ['party_join_request_by_name', 'Z24', [qw(partyName)]],
+		'0946' => ['homunculus_command', 'v C', [qw(commandType, commandID)]],
+		'08B8' => ['send_pin_password','a4 Z*', [qw(accountID pin)]],
+		'08BA' => ['new_pin_password','a4 Z*', [qw(accountID pin)]],
+		#'08BE' => ['change_pin_password','a*', [qw(accountID oldPin newPin)]], # TODO: PIN change system/command?
 	);
 	
 	$self->{packet_list}{$_} = $packets{$_} for keys %packets;	
 	
 	my %handlers = qw(
-		actor_action 0940
-		character_move 0369
-		sync 035F
-		actor_look_at 07E4
-		item_take 0934
-		item_drop 0819
-		storage_item_add 0860
-		storage_item_remove 085F
-		skill_use_location 0884
-		actor_info_request 096A
-		map_login 0960
-		party_join_request_by_name 0931
-		homunculus_command 0873
+		actor_action 0882
+		character_move 087D
+		sync 0860
+		actor_look_at 0949
+		item_take 0899
+		item_drop 0893
+		storage_item_add 089E
+		storage_item_remove 0969
+		skill_use_location 0918
+		actor_info_request 0363
+		map_login 08A6
+		party_join_request_by_name 094D
+		homunculus_command 0946
 		master_login 02B0
-		buy_bulk_vender 0801
 		party_setting 07D7
-		storage_password_give 092B
-		storage_password_set 092B
+		buy_bulk_vender 0801
+		send_pin_password 08B8
+		new_pin_password 08BA
 	);
 	
 	$self->{packet_lut}{$_} = $handlers{$_} for keys %handlers;
@@ -69,61 +70,85 @@ sub new {
 	return $self;
 }
 
-sub encryptMessageID {
+# Local Servertype Globals
+my $map_login = 0;
+my $enc_val3 = 0;
+		
+sub encryptMessageID 
+{
 	my ($self, $r_message, $MID) = @_;
 	
-	if (sprintf("%04X",$MID) eq $self->{packet_lut}{map_login}) {
-		# K
-		$self->{encryption}->{key_1} = Math::BigInt->new(498822262);
-		# A
-		$self->{encryption}->{key_2} = Math::BigInt->new(1768126699);
-		# M
-		$self->{encryption}->{key_3} = Math::BigInt->new(1868856914);
-	} elsif ($self->{net}->getState() != Network::IN_GAME) {
-		# Turn off keys
-		$self->{encryption}->{key_1} = 0; $self->{encryption}->{key_2} = 0; return;
-	}
+	# Checking In-Game State
+	if ($self->{net}->getState() != Network::IN_GAME && !$map_login) { $enc_val1 = 0; $enc_val2 = 0; return; }
+	
+	# Turn Off Map Login Flag
+	if ($map_login)	{ $map_login = 0; }
 		
 	# Checking if Encryption is Activated
-	if ($self->{encryption}->{key_1} != 0 && $self->{encryption}->{key_2} != 0) {
+	if ($enc_val1 != 0 && $enc_val2 != 0) 
+	{
 		# Saving Last Informations for Debug Log
 		my $oldMID = $MID;
-		my $oldKey = ($self->{encryption}->{key_1} >> 16) & 0x7FFF;
+		my $oldKey = ($enc_val1 >> 16) & 0x7FFF;
 		
 		# Calculating the Encryption Key
-		$self->{encryption}->{key_1} = $self->{encryption}->{key_1}->bmul($self->{encryption}->{key_3})->badd($self->{encryption}->{key_2}) & 0xFFFFFFFF;
+		$enc_val1 = $enc_val1->bmul($enc_val2)->badd($enc_val3) & 0xFFFFFFFF;
 	
 		# Xoring the Message ID
-		$MID = ($MID ^ (($self->{encryption}->{key_1} >> 16) & 0x7FFF)) & 0xFFFF;
+		$MID = ($MID ^ (($enc_val1 >> 16) & 0x7FFF));
 		$$r_message = pack("v", $MID) . substr($$r_message, 2);
 
-		# Debug Log	
-		debug(sprintf("Encrypted MID : [%04X]->[%04X] / KEY : [0x%04X]->[0x%04X]\n", $oldMID, $MID, $oldKey, ($self->{encryption}->{key_1} >> 16) & 0x7FFF), "sendPacket", 0) if ($config{debugPacket_sent});
+		# Debug Log
+		if ($config{debugPacket_sent} == 1) 
+		{		
+			debug(sprintf("Encrypted MID : [%04X]->[%04X] / KEY : [0x%04X]->[0x%04X]\n", $oldMID, $MID, $oldKey, ($enc_val1 >> 8 >> 8) & 0x7FFF), "sendPacket", 0);
+		}
 	}
 }
 
 sub sendStoragePassword {
-	my ($self, $pass, $type) = @_;
-	my $storage_key = "EC62E539BB6BBC811A60C06FACCB7EC8"; 
-	my $switch;
+	my $self = shift;
+	# 16 byte packed hex data
+	my $pass = shift;
 	# 2 = set password ?
 	# 3 = give password ?
-	if ($type == 3) { $switch = 'storage_password_give'; } elsif ($type == 2) { $switch = 'storage_password_set';
+	my $type = shift;
+	my $msg;
+	if ($type == 3) {
+		$msg = pack("v v", 0x0883, $type).$pass.pack("H*", "EC62E539BB6BBC811A60C06FACCB7EC8");
+	} elsif ($type == 2) {
+		$msg = pack("v v", 0x0883, $type).pack("H*", "EC62E539BB6BBC811A60C06FACCB7EC8").$pass;
 	} else {
 		ArgumentException->throw("The 'type' argument has invalid value ($type).");
 	}
-	
-	my $msg = $self->reconstruct({
-		switch => $switch,
-		key => 'EC62E539BB6BBC811A60C06FACCB7EC8',
-		encrypted_password => $pass, # 16 byte packed hex data
-		type => $type
-	});
-	
 	$self->sendToServer($msg);
 }
 
-sub sendHomunculusCommand {
+sub sendMapLogin 
+{
+	my ($self, $accountID, $charID, $sessionID, $sex) = @_;
+	my $msg;
+
+	$sex = 0 if ($sex > 1 || $sex < 0); # Sex can only be 0 (female) or 1 (male)
+	
+	if ( $map_login == 0 ) { PrepareKeys(); $map_login = 1; }
+
+	# Reconstructing Packet 
+	$msg = $self->reconstruct({
+		switch => 'map_login',
+		accountID => $accountID,
+		charID => $charID,
+		sessionID => $sessionID,
+		tick => getTickCount,
+		sex => $sex,
+	});
+
+	$self->sendToServer($msg);
+	debug "Sent sendMapLogin\n", "sendPacket", 2;
+}
+
+sub sendHomunculusCommand 
+{
 	my ($self, $command, $type) = @_; # $type is ignored, $command can be 0:get stats, 1:feed or 2:fire
 	
 	$self->sendToServer($self->reconstruct({
@@ -145,6 +170,71 @@ sub sendPartyJoinRequestByName
 	}));	
 	
 	debug "Sent Request Join Party (by name): $name\n", "sendPacket", 2;
+}
+
+sub PrepareKeys()
+{
+		# K
+		$enc_val1 = Math::BigInt->new('0x01ae43b9');
+		# M
+		$enc_val2 = Math::BigInt->new('0x26793378');
+		# A
+		$enc_val3 = Math::BigInt->new('0x18a368c1');
+}
+
+sub sendLoginPinCode {
+	my ($self, $seed, $type) = @_;
+	
+	my $pin = randomizePinCode($seed, $config{loginPinCode});
+	my $msg;
+	if ($type == 0) {
+		$msg = $self->reconstruct({
+			switch => 'send_pin_password',
+			accountID => $accountID,
+			pin => $pin,
+		});
+	} elsif ($type == 1) {
+		$msg = $self->reconstruct({
+			switch => 'new_pin_password',
+			accountID => $accountID,
+			pin => $pin,
+		});
+	}
+	$self->sendToServer($msg);
+	$timeout{charlogin}{time} = time;
+	debug "Sent loginPinCode\n", "sendPacket", 2;	
+}
+
+# randomizePin function/algorithm by Kurama, ever_boy_, kLabMouse and Iniro. cleanups by Revok
+sub randomizePinCode {
+	my ($seed, $pin) = @_;
+	$seed =  Math::BigInt->new($seed);
+	my $mulfactor = 0x3498;
+	my $addfactor = 0x881234;
+	my @keypad_keys_order = ('0'..'9');
+	# calculate keys order (they are randomized based on seed value)
+	if (@keypad_keys_order >= 1) {
+		my $k = 2;
+		for (my $pos = 1; $pos < @keypad_keys_order; $pos++) {
+			$seed = $addfactor + $seed * $mulfactor & 0xFFFFFFFF; # calculate next seed value
+			my $replace_pos = $seed % $k;
+			if ($pos != $replace_pos) {
+				my $old_value = $keypad_keys_order[$pos];
+				$keypad_keys_order[$pos] = $keypad_keys_order[$replace_pos];
+				$keypad_keys_order[$replace_pos] = $old_value;
+			}
+			$k++;
+		}
+	}
+	# associate keys values with their position using a hash
+	my %keypad;
+	for (my $pos = 0; $pos < @keypad_keys_order; $pos++) { $keypad{@keypad_keys_order[$pos]} = $pos; }
+	my $pin_reply = '';
+	my @pin_numbers = split('',$pin);
+	foreach (@pin_numbers) {
+		$pin_reply .= $keypad{$_};
+	}
+	return $pin_reply;
 }
 
 1;
