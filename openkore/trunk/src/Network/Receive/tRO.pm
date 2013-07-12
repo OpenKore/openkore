f1fa1903e49f333392df5c40be20e2786e25cd13
roxleopardo <roxleopardo@users.sourceforge.net>
2013-07-12 01:11:10 +0000

    BETA tRO update for 2013-07-09thRagexe Added support for new way to receive character list. Associated new packets with functions.
    Credits:
    ROX_Leopardo
    kLabMouse

--- openkore/trunk/src/Network/Receive/tRO.pm
+++ openkore/trunk/src/Network/Receive/tRO.pm
@@ -9,7 +9,7 @@
 #  also distribute the source code.
 #  See http://www.gnu.org/licenses/gpl.html for the full license.
 #########################################################################
-# tRO (Thai) for 2008-09-16Ragexe12_Th
+# tRO (Thai) for 2013-07-09thRagexe
 # Servertype overview: http://wiki.openkore.com/index.php/ServerType
 package Network::Receive::tRO;
 
@@ -25,11 +25,19 @@ sub new {
 		'007C' => ['actor_connected', 'C a4 v14 C2 a3 C2', [qw(object_type ID walk_speed opt1 opt2 option hair_style weapon lowhead type shield tophead midhead hair_color clothes_color head_dir stance sex coords unknown1 unknown2)]], # 42 # spawning
 		'0097' => ['private_message', 'v Z24 V Z*', [qw(len privMsgUser flag privMsg)]], # -1
 		'022C' => ['actor_moved', 'C a4 v3 V v5 V v5 a4 a2 v V C2 a6 C2 v', [qw(object_type ID walk_speed opt1 opt2 option type hair_style weapon shield lowhead tick tophead midhead hair_color clothes_color head_dir guildID emblemID manner opt3 stance sex coords unknown1 unknown2 lv)]], # 65 # walking
+
+		'082D' => ['sync_received_characters'],
+		'099D' => ['received_characters', 'x2 a*', [qw(charInfo)]],
 	);
 
 	foreach my $switch (keys %packets) {
 		$self->{packet_list}{$switch} = $packets{$switch};
 	}
+
+	my %handlers = qw(
+		received_characters 099D
+	);
+	$self->{packet_lut}{$_} = $handlers{$_} for keys %handlers;
 
 	return $self;
 }
