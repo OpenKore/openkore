a0b87c106f18f09b3d4a2ed8b47e3c835f480844
ya4ept <ya4ept@3e610c8b-890d-0410-8877-b158ba2967cd>
2013-09-25 06:21:23 +0000

    edit packet_extract.bat add RO Tools 1.0.1 (for history)

--- packet-extracter/trunk/packet_extract.bat
+++ packet-extracter/trunk/packet_extract.bat
@@ -1,8 +1,19 @@
-@Echo Off
-set mm=%date:~3,2%
-set dd=%date:~0,2%
-set yyyy=%date:~6,4%
-for %%I In (*.exe) Do (
-if %%I neq start.exe if %%I neq extractor.exe start.exe ! packet_extract.pl "%%I"
-if exist extractor.exe extractor.exe > recvpackets_%%I_%yyyy%-%mm%-%dd%.txt)
-pause
+@ECHO Off
+SET mm=%date:~3,2%
+SET dd=%date:~0,2%
+SET yyyy=%date:~6,4%
+
+IF EXIST extractor.exe (DEL extractor.exe)
+
+FOR %%I IN (*.exe) DO (
+	IF %%I NEQ start.exe IF %%I NEQ extractor.exe (
+		start.exe ! packet_extract.pl "%%I"
+		IF EXIST extractor.exe (
+			extractor.exe > recvpackets_%%I_%yyyy%-%mm%-%dd%.txt
+			echo .
+			DEL extractor.exe
+		)
+	)
+)
+
+PAUSE
