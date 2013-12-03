5056663dbc91faf7b43432b9fff57c91514159f0
marcelofoxes <marcelofoxes@3e610c8b-890d-0410-8877-b158ba2967cd>
2013-12-03 04:02:51 +0000

    * used the same default ports for all zones [fixed]

--- openkore/trunk/src/Network/XKore2.pm
+++ openkore/trunk/src/Network/XKore2.pm
@@ -39,14 +39,14 @@ sub start {
 	$sessionStore = new Base::Ragnarok::SessionStore();
 	$mapServer = new Network::XKore2::MapServer(
 		host => $publicIP,
-		port => $config{XKore_listenPort_map} || 6900,
+		port => $config{XKore_listenPort_map} || 6103,
 		serverType => $masterServer->{serverType},
 		rpackets => \%rpackets,
 		sessionStore => $sessionStore
 	);
 	$charServer = new Network::XKore2::CharServer(
 		host => $publicIP,
-		port => $config{XKore_listenPort_char} || 6900,
+		port => $config{XKore_listenPort_char} || 6978,
 		serverType => $masterServer->{serverType},
 		rpackets => \%rpackets,
 		mapServer => $mapServer,
