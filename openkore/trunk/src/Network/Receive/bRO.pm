7db2bedc35118baea4023fcfecf2e36b01bd4bf9
marcelofoxes <marcelofoxes@3e610c8b-890d-0410-8877-b158ba2967cd>
2013-12-03 20:14:16 +0000

    * bRO network update

--- openkore/trunk/src/Network/Receive/bRO.pm
+++ openkore/trunk/src/Network/Receive/bRO.pm
@@ -14,7 +14,9 @@ package Network::Receive::bRO;
 use strict;
 use Log qw(warning);
 use base 'Network::Receive::ServerType0';
-use Globals qw(%timeout);
+
+
+# Sync_Ex algorithm developed by Fr3DBr
 
 sub new {
 	my ($class) = @_;
@@ -22,115 +24,31 @@ sub new {
 	
 	my %packets = (
 		'0097' => ['private_message', 'v Z24 V Z*', [qw(len privMsgUser flag privMsg)]], # -1
-		'091D' => ['sync_request_ex'],  
-		'0944' => ['sync_request_ex'],  
-		'0860' => ['sync_request_ex'],  
-		'087F' => ['sync_request_ex'],  
-		'0932' => ['sync_request_ex'],  
-		'0938' => ['sync_request_ex'],  
-		'0954' => ['sync_request_ex'],  
-		'095A' => ['sync_request_ex'],  
-		'092D' => ['sync_request_ex'],  
-		'0870' => ['sync_request_ex'],  
-		'0950' => ['sync_request_ex'],  
-		'0893' => ['sync_request_ex'],  
-		'0963' => ['sync_request_ex'],  
-		'08A9' => ['sync_request_ex'],  
-		'0896' => ['sync_request_ex'],  
-		'0925' => ['sync_request_ex'],  
-		'0202' => ['sync_request_ex'],  
-		'0943' => ['sync_request_ex'],  
-		'095D' => ['sync_request_ex'],  
-		'08AD' => ['sync_request_ex'],  
-		'0926' => ['sync_request_ex'],  
-		'094B' => ['sync_request_ex'],  
-		'093C' => ['sync_request_ex'],  
-		'0437' => ['sync_request_ex'],  
-		'0947' => ['sync_request_ex'],  
-		'0365' => ['sync_request_ex'],  
-		'0956' => ['sync_request_ex'],  
-		'086C' => ['sync_request_ex'],  
-		'088F' => ['sync_request_ex'],  
-		'08A3' => ['sync_request_ex'],  
-		'08A1' => ['sync_request_ex'],  
-		'0959' => ['sync_request_ex'],  
-		'0876' => ['sync_request_ex'],  
-		'0951' => ['sync_request_ex'],  
-		'087B' => ['sync_request_ex'],  
-		'0436' => ['sync_request_ex'],  
-		'0875' => ['sync_request_ex'],  
-		'0922' => ['sync_request_ex'],  
-		'0952' => ['sync_request_ex'],  
-		'0867' => ['sync_request_ex'],  
-		'0879' => ['sync_request_ex'],  
-		'0942' => ['sync_request_ex'],  
-		'0817' => ['sync_request_ex'],  
-		'091E' => ['sync_request_ex'],  
-		'0924' => ['sync_request_ex'],  
-		'086F' => ['sync_request_ex'],  
-		'085D' => ['sync_request_ex'],  
-		'085A' => ['sync_request_ex'],  
-		'0877' => ['sync_request_ex'],  
-		'0955' => ['sync_request_ex'],  
-		'088B' => ['sync_request_ex'],  
-		'0882' => ['sync_request_ex'],  
-		'0865' => ['sync_request_ex'],  
-		'094E' => ['sync_request_ex'],  
-		'087C' => ['sync_request_ex'],  
-		'08A4' => ['sync_request_ex'],  
-		'0878' => ['sync_request_ex'],  
-		'0966' => ['sync_request_ex'],  
-		'093E' => ['sync_request_ex'],  
-		'0927' => ['sync_request_ex'],  
-		'095B' => ['sync_request_ex'],  
-		'0891' => ['sync_request_ex'],  
-		'0964' => ['sync_request_ex'],  
-		'0869' => ['sync_request_ex'],  
-		'086B' => ['sync_request_ex'],  
-		'0897' => ['sync_request_ex'],  
-		'0888' => ['sync_request_ex'],  
-		'0935' => ['sync_request_ex'],  
-		'08A6' => ['sync_request_ex'],  
-		'0861' => ['sync_request_ex'],  
-		'0886' => ['sync_request_ex'],  
-		'088C' => ['sync_request_ex'],  
-		'0281' => ['sync_request_ex'],  
-		'0933' => ['sync_request_ex'],  
-		'0945' => ['sync_request_ex'],  
-		'093F' => ['sync_request_ex'],  
-		'0919' => ['sync_request_ex'],  
-		'0862' => ['sync_request_ex'],  
-		'092A' => ['sync_request_ex'],  
-		'0835' => ['sync_request_ex'],  
-		'092E' => ['sync_request_ex'],  
-		'0361' => ['sync_request_ex'],  
-		'08A5' => ['sync_request_ex'],  
-		'0930' => ['sync_request_ex'], 
 	);
-
-	foreach my $switch (keys %packets) {
-		$self->{packet_list}{$switch} = $packets{$switch};
-	}
 	
-		$self->{sync_ex_reply} = {
-			'091D', '0873', '0944', '085C', '0860', '086D', '087F', '0360', '0932', '091F', 
-			'0938', '08AA', '0954', '0923', '095A', '095C', '092D', '091B', '0870', '0890', 
-			'0950', '0362', '0893', '0918', '0963', '091A', '08A9', '0960', '0896', '0363', 
-			'0925', '0928', '0202', '0864', '0943', '0892', '095D', '0917', '08AD', '08AC', 
-			'0926', '08AB', '094B', '0368', '093C', '0920', '0437', '095F', '0947', '0946', 
-			'0365', '0895', '0956', '08A2', '086C', '086A', '088F', '0962', '08A3', '0958', 
-			'08A1', '08A7', '0959', '083C', '0876', '0838', '0951', '0866', '087B', '0811', 
-			'0436', '0872', '0875', '087D', '0922', '0940', '0952', '094C', '0867', '0366', 
-			'0879', '0819', '0942', '089C', '0817', '0939', '091E', '093A', '0924', '0967', 
-			'086F', '0802', '085D', '0871', '085A', '0936', '0877', '0934', '0955', '085B', 
-			'088B', '094F', '0882', '0931', '0865', '085F', '094E', '0887', '087C', '0868', 
-			'08A4', '0894', '0878', '093B', '0966', '093D', '093E', '08A8', '0927', '0815', 
-			'095B', '087A', '0891', '087E', '0964', '096A', '0869', '0949', '086B', '091C', 
-			'0897', '07EC', '0888', '095E', '0935', '088E', '08A6', '0953', '0861', '08A0', 
-			'0886', '0921', '088C', '092C', '0281', '0881', '0933', '089B', '0945', '0969', 
-			'093F', '0364', '0919', '086E', '0862', '0968', '092A', '089A', '0835', '0899', 
-			'092E', '022D', '0361', '0874', '08A5', '088D', '0930', '0961'
-		};
+	# Sync Ex Reply Array 
+	$self->{sync_ex_reply} = {
+		'0367', '088A', '085A', '0368', '085B', '0874', '085C', '0938', '0362', '088E', 
+		'085E', '0888', '085F', '0889', '0860', '0884', '0861', '088B', '0862', '0363', 
+		'0863', '0940', '0864', '0802', '0865', '088F', '0866', '0890', '0867', '0891', 
+		'0868', '0892', '0869', '0893', '086A', '0894', '086B', '0895', '086C', '0896', 
+		'086D', '0897', '086E', '0898', '086F', '0899', '0870', '089A', '0871', '089B', 
+		'0872', '07E4', '0873', '023B', '0886', '089E', '0875', '089F', '0876', '08A0', 
+		'0877', '08A1', '0878', '08A2', '0879', '08A3', '087A', '08A4', '087B', '08A5', 
+		'087C', '08A6', '087D', '08A7', '087E', '08A8', '087F', '08A9', '0880', '08AA', 
+		'0881', '08AB', '0882', '08AC', '0883', '08AD', '0917', '0941', '0918', '0942', 
+		'0919', '022D', '091A', '0944', '091B', '0945', '091C', '0946', '091D', '0947', 
+		'091E', '0948', '091F', '0949', '0920', '094A', '0921', '07EC', '0922', '094C', 
+		'0923', '094D', '0281', '094E', '0925', '094F', '0926', '0950', '0927', '0951', 
+		'0928', '0952', '0929', '0953', '092A', '0954', '092B', '0955', '092C', '0956', 
+		'092D', '0957', '092E', '0958', '092F', '0959', '0930', '095A', '0931', '095B', 
+		'0436', '095C', '0933', '095D', '0934', '095E', '0935', '095F', '0936', '0960', 
+		'0937', '0961', '0887', '0962', '0939', '0963', '093A', '0964', '093B', '0965', 
+		'093C', '0966', '093D', '0967', '093E', '0968', '093F', '0969'
+	};
+	
+	foreach my $key (keys %{$self->{sync_ex_reply}}) { $packets{$key} => ['sync_request_ex']; }
+	foreach my $switch (keys %packets) { $self->{packet_list}{$switch} = $packets{$switch}; }
 	
 	return $self;
 }
--- openkore/trunk/src/Network/Send/bRO.pm
+++ openkore/trunk/src/Network/Send/bRO.pm
@@ -19,46 +19,34 @@ sub new {
 	my $self = $class->SUPER::new(@_);
 	
 	my %packets = (
-		'0941' => ['actor_action', 'a4 C', [qw(targetID type)]],
-		'094A' => ['character_move','a3', [qw(coords)]],
-		'0367' => ['sync', 'V', [qw(time)]],
-		'0898' => ['actor_look_at', 'v C', [qw(head body)]],
-		'0929' => ['item_take', 'a4', [qw(ID)]],
-		'089D' => ['item_drop', 'v2', [qw(index amount)]],
-		'0884' => ['storage_password'],
-		'0957' => ['storage_item_add', 'v V', [qw(index amount)]],
-		'02C4' => ['storage_item_remove', 'v V', [qw(index amount)]],
-		'035F' => ['skill_use_location', 'v4', [qw(lv skillID x y)]],
-		'0369' => ['actor_info_request', 'a4', [qw(ID)]],
-		'089E' => ['map_login', 'a4 a4 a4 V C', [qw(accountID charID sessionID tick sex)]],
-		'0863' => ['party_join_request_by_name', 'Z24', [qw(partyName)]], #f
-		'023B' => ['homunculus_command', 'v C', [qw(commandType, commandID)]], #f
+		'0369' => ['actor_action', 'a4 C', [qw(targetID type)]],
+		'0437' => ['character_move','a3', [qw(coords)]],
+		'035F' => ['sync', 'V', [qw(time)]],
+		'0885' => ['actor_look_at', 'v C', [qw(head body)]],
+		'085D' => ['item_take', 'a4', [qw(ID)]],
+		'088C' => ['item_drop', 'v2', [qw(index amount)]],
+		'089D' => ['storage_password'],
+		'0438' => ['storage_item_add', 'v V', [qw(index amount)]],
+		'0932' => ['storage_item_remove', 'v V', [qw(index amount)]],
+		'0364' => ['skill_use_location', 'v4', [qw(lv skillID x y)]],
+		'096A' => ['actor_info_request', 'a4', [qw(ID)]],
+		'0365' => ['map_login', 'a4 a4 a4 V C', [qw(accountID charID sessionID tick sex)]],
+		'0838' => ['party_join_request_by_name', 'Z24', [qw(partyName)]], #f
+		'0943' => ['homunculus_command', 'v C', [qw(commandType, commandID)]], #f
 	);
 	
 	$self->{packet_list}{$_} = $packets{$_} for keys %packets;	
 	
 	my %handlers = qw(
-		actor_action 0941
-		character_move 094A
-		sync 0367
-		actor_look_at 0898
-		item_take 0929
-		item_drop 089D
-		storage_password 0884
-		storage_item_add 0957
-		storage_item_remove 02C4
-		skill_use_location 035F
-		actor_info_request 0369
-		map_login 089E
-		party_join_request_by_name 0863
-		homunculus_command 023B
 		master_login 02B0
 		buy_bulk_vender 0801
 		party_setting 07D7
 	);
 	
+	while (my ($k, $v) = each %packets) { $handlers{$v->[0]} = $k}
 	$self->{packet_lut}{$_} = $handlers{$_} for keys %handlers;
-	$self->cryptKeys(1785990933, 1701607027, 747196614);
+	$self->cryptKeys(679100536, 306729564, 986793068);
+	
 	return $self;
 }
 
