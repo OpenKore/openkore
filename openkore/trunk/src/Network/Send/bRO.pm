cd3cfaa48502eb9019560f392feea535a8049159
lututui <lututui@3e610c8b-890d-0410-8877-b158ba2967cd>
2014-01-21 19:47:19 +0000

    * bRO update by marcelofoxes

--- openkore/trunk/src/Network/Send/bRO.pm
+++ openkore/trunk/src/Network/Send/bRO.pm
@@ -21,18 +21,18 @@ sub new {
 	my %packets = (
 		'0369' => ['actor_action', 'a4 C', [qw(targetID type)]],
 		'0437' => ['character_move','a3', [qw(coords)]],
-		'0887' => ['sync', 'V', [qw(time)]],
-		'0918' => ['actor_look_at', 'v C', [qw(head body)]],
-		'0360' => ['item_take', 'a4', [qw(ID)]],
-		'0946' => ['item_drop', 'v2', [qw(index amount)]],
-		'08AD' => ['storage_password'],
-		'0819' => ['storage_item_add', 'v V', [qw(index amount)]],
-		'0886' => ['storage_item_remove', 'v V', [qw(index amount)]],
+		'035F' => ['sync', 'V', [qw(time)]],
+		'087C' => ['actor_look_at', 'v C', [qw(head body)]],
+		'0364' => ['item_take', 'a4', [qw(ID)]],
+		'0931' => ['item_drop', 'v2', [qw(index amount)]],
+		'0365' => ['storage_password'],
+		'0883' => ['storage_item_add', 'v V', [qw(index amount)]],
+		'02C4' => ['storage_item_remove', 'v V', [qw(index amount)]],
 		'0438' => ['skill_use_location', 'v4', [qw(lv skillID x y)]],
 		'096A' => ['actor_info_request', 'a4', [qw(ID)]],
-		'0882' => ['map_login', 'a4 a4 a4 V C', [qw(accountID charID sessionID tick sex)]],
-		'0963' => ['party_join_request_by_name', 'Z24', [qw(partyName)]], #f
-		'086E' => ['homunculus_command', 'v C', [qw(commandType, commandID)]], #f
+		'08A3' => ['map_login', 'a4 a4 a4 V C', [qw(accountID charID sessionID tick sex)]],
+		'093A' => ['party_join_request_by_name', 'Z24', [qw(partyName)]], #f
+		'0887' => ['homunculus_command', 'v C', [qw(commandType, commandID)]], #f
 	);
 	
 	$self->{packet_list}{$_} = $packets{$_} for keys %packets;	
@@ -45,7 +45,7 @@ sub new {
 	
 	while (my ($k, $v) = each %packets) { $handlers{$v->[0]} = $k}
 	$self->{packet_lut}{$_} = $handlers{$_} for keys %handlers;
-	$self->cryptKeys(516447572, 1857494034, 242571268);
+	$self->cryptKeys(998317519, 1450250746, 329792115);
 	
 	return $self;
 }
