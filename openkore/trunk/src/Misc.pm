b0cb9b7e49a5d3b475f2ad815b1c429baae3639b
Windham Wong <windhamwong@nva-hk.com>
2014-06-16 21:25:12 +0000

    teleportAuto_item1 & teleportAuto_item2 added to config. This is used when the player needs to use a specific item for doing the teleport.

--- openkore/trunk/src/Misc.pm
+++ openkore/trunk/src/Misc.pm
@@ -3045,14 +3045,20 @@ sub useTeleport {
 	# could lead to problems if the name is different on some servers
 	# 11 Mar 2010 - instead of name, use nameID, names can be different for different servers
 	my $item;
-	if ($use_lvl == 1) {
-		#$item = $char->inventory->getByName("Fly Wing");
-		$item = $char->inventory->getByNameID(601);
-		unless ($item) { $item = $char->inventory->getByNameID(12323); } # only if we don't have any fly wing
-	} elsif ($use_lvl == 2) {
-		#$item = $char->inventory->getByName("Butterfly Wing");
-		$item = $char->inventory->getByNameID(602);
-		unless ($item) { $item = $char->inventory->getByNameID(12324); } # only if we don't have any butterfly wing
+	if ($use_lvl == 1) { #Fly Wing
+		if (!$config{teleportAuto_item1}) {
+			$item = $char->inventory->getByNameID(601);
+			unless ($item) { $item = $char->inventory->getByNameID(12323); } # only if we don't have any fly wing
+		} else {
+			$item = $char->inventory->getByName($config{teleportAuto_item1});
+		}
+	} elsif ($use_lvl == 2) { #Butterfly Wing
+		if (!$config{teleportAuto_item2}) {
+			$item = $char->inventory->getByNameID(602);
+			unless ($item) { $item = $char->inventory->getByNameID(12324); } # only if we don't have any butterfly wing
+		} else {
+			$item = $char->inventory->getByName($config{teleportAuto_item2});
+		}
 	}
 
 	if ($item) {
