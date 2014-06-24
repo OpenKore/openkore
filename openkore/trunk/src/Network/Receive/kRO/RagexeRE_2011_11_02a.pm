aefd70662e9e4efb814ca1a27605faf0631f88b7
ya4ept <ya4ept@3e610c8b-890d-0410-8877-b158ba2967cd>
2014-06-24 11:39:48 +0000

    - add new sT: RagexeRE_2011_10_25a (thanks SkylorD: http://forums.openkore.com/viewtopic.php?p=265080#p265080) - add new recvpackets.txt

--- openkore/trunk/src/Network/Receive/kRO/RagexeRE_2011_11_02a.pm
+++ openkore/trunk/src/Network/Receive/kRO/RagexeRE_2011_11_02a.pm
@@ -15,7 +15,7 @@
 package Network::Receive::kRO::RagexeRE_2011_11_02a;
 
 use strict;
-use base qw(Network::Receive::kRO::RagexeRE_2011_10_05a);
+use base qw(Network::Receive::kRO::RagexeRE_2011_10_25a);
 
 1;
 
