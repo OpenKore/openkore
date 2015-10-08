c5f2eb68551ae0254f219fce561da78e27fd3ddf
Windham Wong <windhamwong@nva-hk.com>
2015-10-08 23:29:41 +0000

    Pet feed fix and portals details adding through command "portals add" * [FIX] Pet feed issue fixed * [ADD] Command: portals add Command usage: portals add izlude_d 122 105 izlude 128 195 0 c r0 portals add mora 56 25 bif_fild02 176 162

--- openkore/trunk/src/Commands.pm
+++ openkore/trunk/src/Commands.pm
@@ -3989,6 +3989,33 @@ sub cmdPortalList {
 	} elsif ($arg eq 'recompile') {
 		Settings::loadByRegexp(qr/portals/);
 		Misc::compilePortals() if Misc::compilePortals_check();
+	} elsif ($arg =~ /^add (.*)$/) { #Manual adding portals
+		#Command: portals add mora 56 25 bif_fild02 176 162
+		#Command: portals add y_airport 143 43 y_airport 148 51 0 c r0 c r0
+		print $args."TEST\n";
+		my ($srcMap, $srcX, $srcY, $dstMap, $dstX, $dstY, $seq) = $args =~ /^add ([a-zA-Z\_\-0-9]*) (\d{1,3}) (\d{1,3}) ([a-zA-Z\_\-0-9]*) (\d{1,3}) (\d{1,3})(.*)$/; #CHECKING
+		my $srcfile = $srcMap.'.fld';
+		$srcfile = File::Spec->catfile($Settings::fields_folder, $srcfile) if ($Settings::fields_folder);
+		my $dstfile = $dstMap.'.fld';
+		$dstfile = File::Spec->catfile($Settings::fields_folder, $dstfile) if ($Settings::fields_folder);
+		print "GOOD\n" if (-f $srcfile && -f $dstfile);
+		if ($srcX > 0 && $srcY > 0 && $dstX > 0 && $dstY > 0
+			&& -f $srcfile && -f $dstfile) { #found map and valid corrdinates	
+			if ($seq) {
+				message TF("Recorded new portal (destination): %s (%s, %s) -> %s (%s, %s) [%s]\n", $srcMap, $srcX, $srcY, $dstMap, $dstX, $dstY, $seq), "portalRecord";
+				
+				FileParsers::updatePortalLUT2(Settings::getTableFilename("portals.txt"),
+					$srcMap, $srcX, $srcY,
+					$dstMap, $dstX, $dstY,
+					$seq);		
+			} else {
+				message TF("Recorded new portal (destination): %s (%s, %s) -> %s (%s, %s)\n", $srcMap, $srcX, $srcY, $dstMap, $dstX, $dstY), "portalRecord";
+				
+				FileParsers::updatePortalLUT(Settings::getTableFilename("portals.txt"),
+					$srcMap, $srcX, $srcY,
+					$dstMap, $dstX, $dstY);		
+			}
+		}
 	}
 }
 
