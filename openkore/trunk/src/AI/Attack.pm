dd373e69193cb2315db0eecf14272e839c5ea2c0
windhamwong <windhamwong@3e610c8b-890d-0410-8877-b158ba2967cd>
2015-11-11 00:39:08 +0000

    attackComboSlot now detects whether the afterSkill is exist or not twRO localisation config update EP15.1 field update

--- openkore/trunk/src/AI/Attack.pm
+++ openkore/trunk/src/AI/Attack.pm
@@ -291,7 +291,8 @@ sub main {
 			next;
 		}
 
-		if (Skill->new(auto => $config{"attackComboSlot_${i}_afterSkill"})->getIDN == $char->{last_skill_used}
+		if ($config{"attackComboSlot_${i}_afterSkill"}
+		 && Skill->new(auto => $config{"attackComboSlot_${i}_afterSkill"})->getIDN == $char->{last_skill_used}
 		 && ( !$config{"attackComboSlot_${i}_maxUses"} || $args->{attackComboSlot_uses}{$i} < $config{"attackComboSlot_${i}_maxUses"} )
 		 && ( !$config{"attackComboSlot_${i}_autoCombo"} || ($char->{combo_packet} && $config{"attackComboSlot_${i}_autoCombo"}) )
 		 && ( !defined($args->{ID}) || $args->{ID} eq $char->{last_skill_target} || !$config{"attackComboSlot_${i}_isSelfSkill"})
