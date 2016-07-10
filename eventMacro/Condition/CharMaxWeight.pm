8ac3b0a04c3851dbf0576613886e0f7e1b774539
Henrybk <henriquebkmohan@gmail.com>
2016-07-10 18:03:58 -0300

    Added 8 new automacro conditions
    Conditions added: CharCurrentWeight, CharMaxWeight, CurrentHP,
    CurrentSP, FreeSkillPoints, FreeStatPoints, MaxHP, MaxSP.
    
    All of them need testing.

---
+++ eventMacro/Condition/CharMaxWeight.pm
@@ -0,0 +1,47 @@
+package eventMacro::Condition::CharMaxWeight;
+
+use strict;
+use Settings;
+use Globals;
+use Log qw(message error warning debug);
+
+use eventMacro::Condition;
+use base qw(eventMacro::Condition);
+
+use eventMacro::Data;
+use eventMacro::Utilities qw(parse_syntax_condition_operator_plus_number_or_variable validate_code_number_operator_compare_number_or_variable);
+
+sub new {
+	my ($class, $condition_code) = @_;
+	my $self = $class->SUPER::new();
+	
+	$self->{Name} = 'CharMaxWeight';
+	$self->{Code_Number} = undef;
+	$self->{Code_Operator} = undef;
+	return undef unless ($self->parse_syntax($condition_code));
+	
+	$self->{is_Unique_Condition} = 0;
+	$self->{Hooks} = ['packet/stat_info'];
+
+	return $self;
+}
+
+sub validate_condition_status {
+	my ($self, $event_name, $args) = @_;
+	
+	return unless (defined $eventMacro);
+	return if ($event_name eq 'packet/stat_info' && $args && $args->{type} != 25);
+	
+	$self->{is_Fulfilled} = validate_code_number_operator_compare_number_or_variable($char->{weight_max}, $self->{Code_Operator}, $self->{Code_Number}, (@{$self->{Variables}} > 0 ? 1 : 0));
+}
+
+sub parse_syntax {
+	my ($self, $condition_code) = @_;
+	unless ( parse_syntax_condition_operator_plus_number_or_variable($condition_code, \$self->{Code_Operator}, \$self->{Code_Number}, $self->{Variables}) ) {
+		error "[eventMacro] Bad syntax in condition '".$self->get_name()."': '".$condition_code."'\n";
+		return 0;
+	}
+	return 1;
+}
+
+1;
---
+++ eventMacro/Condition/CurrentHP.pm
@@ -0,0 +1,48 @@
+package eventMacro::Condition::CurrentHP;
+
+use strict;
+use Settings;
+use Globals;
+use Log qw(message error warning debug);
+
+use eventMacro::Condition;
+use base qw(eventMacro::Condition);
+
+use eventMacro::Data;
+use eventMacro::Utilities qw(parse_syntax_condition_operator_plus_number_or_variable validate_code_number_operator_compare_number_or_variable);
+
+sub new {
+	my ($class, $condition_code) = @_;
+	my $self = $class->SUPER::new();
+	
+	$self->{Name} = 'CurrentHP';
+	$self->{Code_Number} = undef;
+	$self->{Code_Operator} = undef;
+	return undef unless ($self->parse_syntax($condition_code));
+	
+	$self->{is_Unique_Condition} = 0;
+	$self->{Hooks} = ['packet/sendMapLoaded', 'packet/hp_sp_changed', 'packet/stat_info'];
+
+	return $self;
+}
+
+sub validate_condition_status {
+	my ($self, $event_name, $args) = @_;
+	
+	return unless (defined $eventMacro);
+	return if ($event_name eq 'packet/stat_info' && $args && $args->{type} != 5);
+	return if ($event_name eq 'packet/hp_sp_changed' && $args && $args->{type} != 5);
+	
+	$self->{is_Fulfilled} = validate_code_number_operator_compare_number_or_variable($char->{hp}, $self->{Code_Operator}, $self->{Code_Number}, (@{$self->{Variables}} > 0 ? 1 : 0));
+}
+
+sub parse_syntax {
+	my ($self, $condition_code) = @_;
+	unless ( parse_syntax_condition_operator_plus_number_or_variable($condition_code, \$self->{Code_Operator}, \$self->{Code_Number}, $self->{Variables}) ) {
+		error "[eventMacro] Bad syntax in condition '".$self->get_name()."': '".$condition_code."'\n";
+		return 0;
+	}
+	return 1;
+}
+
+1;
