7b7b7854d0da3593cd9d08aaf7e734d0b9ab80c6
DunstleOS <saytanito@gmail.com>
2016-09-01 18:10:01 +0700

    Update PacketParser.pm

--- src/Network/PacketParser.pm
+++ src/Network/PacketParser.pm
@@ -487,7 +487,7 @@ sub unknownMessage {
 	if($masterServer->{serverType} eq 'tRO')
 	{
 		my $h2int = hex($args->{switch}); #แปลง hex string เป็นตัวเลข
-		if(( $h2int >= 2129 && $h2int <= 2635) && ($net->getState() == Network::IN_GAME)) #ถ้า Unknow Packet มีค่าตั้งแต่ 0851 - 0A4B หรือแปลงเป็นตัวเลขได้ตั้งแต่ 2138 - 2635 ให้ทำงานในบล็อคด้านล่าง
+		if(( $h2int >= 2129 && $h2int <= 2448) && ($net->getState() == Network::IN_GAME)) #ถ้า Unknow Packet มีค่าตั้งแต่ 0851 - 0990 หรือแปลงเป็นตัวเลขได้ตั้งแต่ 2138 - 2448 ให้ทำงานในบล็อคด้านล่าง
 		{
 			$messageSender->sendReplySyncRequestEx($h2int+42); #นำตัวเลขมา + 42 แล้วจัดการส่งพวก Unknow กลับหลุม #42 มีค่าเท่ากับ 2A เมื่อแปลงเป็น hex string 
 			my $int2h1 = sprintf("%04X", $h2int);
