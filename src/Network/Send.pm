1faa80c1ef904d381277c70072f0c3a2a2011785
DunstleOS <saytanito@gmail.com>
2016-09-17 23:10:58 +0700

    Update Send.pm

--- src/Network/Send.pm
+++ src/Network/Send.pm
@@ -349,7 +349,7 @@ sub sendToServer {
 			$self->{hmac_enc} = 0;
 			$self->{seq} = 0;
 		} elsif ($messageID eq '007D') {
-			$msg .= pack('V', $self->{flag}) . pack('V', $self->{seq});
+			$msg .= pack('V', $self->{flag}) . pack('V', $self->{seq}++);
 			$msg .= hmac_md5($msg, pack('H*', $hmac_key));
 			$msg = pack('v', length($msg) + 2) . $msg;
 		} elsif ($messageID eq '0360' && $self->{seq} == 0) {
