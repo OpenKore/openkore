b68b635a8b967c638e43ef19f5713ebd674348e9
Henrybk <henriquebkmohan@gmail.com>
2016-10-29 17:05:01 -0200

    bug fix

--- eventMacro/FileParser.pm
+++ eventMacro/FileParser.pm
@@ -9,7 +9,7 @@ our @EXPORT = qw(parseMacroFile isNewCommandBlock);
 our @EKSPORT_OK = qw(isNewCommandBlock);
 
 use Globals;
-use Utils qw/existsInList/
+use Utils qw/existsInList/;
 use Utils::Exceptions;
 use List::Util qw(max min sum);
 use Log qw(message error warning debug);
