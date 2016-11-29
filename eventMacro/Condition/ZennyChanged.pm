2546f3a92fedc0363c2344643e358606be3f70d9
Henrybk <henriquebkmohan@gmail.com>
2016-11-29 16:52:46 -0200

    Changed NumericConditionEvent and added ZennyChanged

---
+++ eventMacro/Condition/ZennyChanged.pm
@@ -0,0 +1,37 @@
+package eventMacro::Condition::ZennyChanged;
+
+use strict;
+use Utils;
+
+use base 'eventMacro::Conditiontypes::NumericConditionEvent';
+
+use Globals qw( $char );
+
+sub _hooks {
+	['zeny_change'];
+}
+
+sub validate_condition {
+	my ( $self, $callback_type, $callback_name, $args ) = @_;
+	
+	if ($callback_type eq 'hook') {
+		$self->{change} = $args->{change};
+		$self->{zeny} = $args->{zeny};
+		$self->SUPER::validate_condition( $self->{change}, $self->{zeny} );
+	} elsif ($callback_type eq 'variable') {
+		$self->SUPER::update_validator_var($callback_name, $args);
+		return 0;
+	}
+}
+
+sub get_new_variable_list {
+	my ($self) = @_;
+	my $new_variables;
+	
+	$new_variables->{".ZennyChangedLastChange"} = $self->{change};
+	$new_variables->{".ZennyChangedLastZennyAfter"} = $self->{zeny};
+	
+	return $new_variables;
+}
+
+1;
