e005a05dcb9c54aeecf9b897c35d2567bec7be2b
Henrybk <henriquebkmohan@gmail.com>
2016-12-19 12:29:28 -0200

    Fix

--- src/Network/Receive/kRO/Ragexe_2014_10_22b.pm
+++ src/Network/Receive/kRO/Ragexe_2014_10_22b.pm
@@ -24,10 +24,11 @@ sub new {
        '0A18' => ['map_loaded', 'V a3 C2 v C', [qw(syncMapSync coords xSize ySize font sex)]], # 13
        '0984' => ['actor_status_active', 'a4 v V5', [qw(ID type tick unknown1 unknown2 unknown3 unknown4)]], # 28
        '097A' => ['quest_all_list2', 'v3 a*', [qw(len count unknown message)]],
-       '09DB' => ['actor_moved', 'v C a4 a4 v3 V v10 a4 a2 v V C2 a3 C3 v2 Z*', [qw(len object_type ID AID walk_speed opt1 opt2 option type hair_style weapon shield lowhead tophead midhead hair_color clothes_color head_dir guildID emblemID manner opt3 stance sex coords xSize ySize act lv font name)]],
-       '09DC' => ['actor_connected', 'v C a4 a4 v3 V v11 a4 a2 v V C2 a3 C2 v2 a9 Z*', [qw(len object_type ID AID walk_speed opt1 opt2 option type hair_style weapon shield lowhead tophead midhead hair_color clothes_color head_dir costume guildID emblemID manner opt3 stance sex coords xSize ySize lv font opt4 name)]],
-       '09DD' => ['actor_exists', 'v C a4 a4 v3 V v11 a4 a2 v V C2 a3 C3 v2 a9 Z*', [qw(len object_type ID AID walk_speed opt1 opt2 option type hair_style weapon shield lowhead tophead midhead hair_color clothes_color head_dir costume guildID emblemID manner opt3 stance sex coords xSize ySize act lv font opt4 name)]],
-       '09DF' => ['private_message_sent', 'C V', [qw(type unknown)]],
+       '09DB' => ['actor_moved', 'v C a4 a4 v3 V v5 a4 v6 a4 a2 v V C2 a6 C2 v2 a9 Z*', [qw(len object_type ID charID walk_speed opt1 opt2 option type hair_style weapon shield lowhead tick tophead midhead hair_color clothes_color head_dir costume guildID emblemID manner opt3 stance sex coords xSize ySize lv font opt4 name)]],
+       '09DC' => ['actor_connected', 'v C a4 a4 v3 V v11 a4 a2 v V C2 a3 C2 v2 a9 Z*', [qw(len object_type ID charID walk_speed opt1 opt2 option type hair_style weapon shield lowhead tophead midhead hair_color clothes_color head_dir costume guildID emblemID manner opt3 stance sex coords xSize ySize lv font opt4 name)]],
+       '09DD' => ['actor_exists', 'v C a4 a4 v3 V v11 a4 a2 v V C2 a3 C3 v2 a9 Z*', [qw(len object_type ID charID walk_speed opt1 opt2 option type hair_style weapon shield lowhead tophead midhead hair_color clothes_color head_dir costume guildID emblemID manner opt3 stance sex coords xSize ySize act lv font opt4 name)]],
+       '09DE' => ['private_message', 'v V Z25 Z*', [qw(len charID privMsgUser privMsg)]],
+       '09DF' => ['private_message_sent', 'C V', [qw(type charID)]],
        '0A00' => ['hotkeys'],
        );
   
