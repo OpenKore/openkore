10bdf1a40e55fcd0347d7b9c380ed3a3abfd2f2f
Henrybk <henriquebkmohan@gmail.com>
2017-01-06 02:04:50 -0200

    All future conditions

---
+++ eventMacro/Condition/QuestHuntCompleted.pm
@@ -0,0 +1,199 @@
+package eventMacro::Condition::QuestHuntCompleted;
+
+use strict;
+use Globals;
+use Utils;
+use eventMacro::Data;
+
+use base 'eventMacro::Condition';
+
+# Using 'inventory_ready' is not optimal, but it works.
+sub _hooks {
+	['packet_mapChange','inventory_ready','packet/quest_all_list2','packet/quest_all_list','packet/quest_all_mission','packet/quest_add','packet/quest_delete','packet/quest_update_mission_hunt','packet/quest_active'];
+}
+
+sub _parse_syntax {
+	my ( $self, $condition_code ) = @_;
+	
+	$self->{fulfilled_quest_id} = undef;
+	$self->{fulfilled_mob_id} = undef;
+	$self->{fulfilled_member_index} = undef;
+	
+	$self->{var_to_member_index_quest_id} = {};
+	$self->{var_to_member_index_mob_id} = {};
+	
+	$self->{members_array} = [];
+	
+	$self->{is_on_stand_by} = 1;
+	
+	my $var_exists_hash = {};
+	
+	my @members = split(/\s*,\s*/, $condition_code);
+	foreach my $member_index (0..$#members) {
+		my $member = $members[$member_index];
+		
+		my ($quest_id, $mob_id) = split(/\s+/, $member);
+		
+		unless (defined $quest_id && defined $mob_id) {
+			$self->{error} = "List member '".$member."' must have a slot and an ID defined";
+			return 0;
+		}
+		
+		if ($quest_id =~ /(?:^|(?<=[^\\]))\$($variable_qr)$/) {
+			my $var = $1;
+			if ($var =~ /^\./) {
+				$self->{error} = "System variables should not be used in automacros (The ones starting with a dot '.')";
+				return 0;
+			} else {
+				push ( @{ $self->{var_to_member_index_quest_id}{$var} }, $member_index );
+				$self->{members_array}->[$member_index]{quest_id} = undef;
+				$var_exists_hash->{$var} = undef;
+			}
+			
+		} elsif ($quest_id =~ /^\d+$/) {
+			$self->{members_array}->[$member_index]{quest_id} = $quest_id;
+			
+		} else {
+			$self->{error} = "List member '".$member."' has an invalid quest ID '".$quest_id."'";
+			return 0;
+		}
+		
+		if ($mob_id =~ /(?:^|(?<=[^\\]))\$($variable_qr)$/) {
+			my $var = $1;
+			if ($var =~ /^\./) {
+				$self->{error} = "System variables should not be used in automacros (The ones starting with a dot '.')";
+				return 0;
+			} else {
+				push ( @{ $self->{var_to_member_index_mob_id}{$var} }, $member_index );
+				$self->{members_array}->[$member_index]{mob_id} = undef;
+				$var_exists_hash->{$var} = undef;
+			}
+			
+		} elsif ($mob_id =~ /^\d+$/) {
+			$self->{members_array}->[$member_index]{mob_id} = $mob_id;
+			
+		} else {
+			$self->{error} = "List member '".$member."' has an invalid mob ID '".$mob_id."'";
+			return 0;
+		}
+	}
+	
+	foreach my $var (keys %{$var_exists_hash}) {
+		push ( @{ $self->{variables} }, $var );
+	}
+	
+	return 1;
+}
+
+sub update_vars {
+	my ( $self, $var_name, $var_value ) = @_;
+	
+	foreach my $member_index ( @{ $self->{var_to_member_index_quest_id}{$var_name} } ) {
+		if ($var_value =~ /^\d+$/) {	
+			$self->{members_array}->[$member_index]{quest_id} = $var_value;
+		} else {
+			$self->{members_array}->[$member_index]{quest_id} = undef;
+		}
+	}
+	
+	foreach my $member_index ( @{ $self->{var_to_member_index_mob_id}{$var_name} } ) {
+		if ($var_value =~ /^\d+$/) {	
+			$self->{members_array}->[$member_index]{mob_id} = $var_value;
+		} else {
+			$self->{members_array}->[$member_index]{mob_id} = undef;
+		}
+	}
+}
+
+sub check_quests {
+	my ( $self, $list ) = @_;
+	$self->{fulfilled_quest_id} = undef;
+	$self->{fulfilled_mob_id} = undef;
+	$self->{fulfilled_member_index} = undef;
+	foreach my $member_index ( 0..$#{ $self->{members_array} } ) {
+	
+		my $counter = 0;
+		Log::warning "[test] number ".$counter++."\n";
+		
+		my $quest_ID = $self->{members_array}->[$member_index]{quest_id};
+		my $mob_ID = $self->{members_array}->[$member_index]{mob_id};
+		next unless (defined $quest_ID);
+		Log::warning "[test] number ".$counter++."\n";
+		next unless (defined $mob_ID);
+		Log::warning "[test] number ".$counter++."\n";
+		next unless (exists $questList->{$quest_ID});
+		Log::warning "[test] number ".$counter++."\n";
+		my $quest = $questList->{$quest_ID};
+		
+		Log::warning "[test] dump ".(Data::Dumper::Dumper($quest))."\n";
+		
+		next unless (exists $quest->{active});
+		Log::warning "[test] number ".$counter++."\n";
+		next unless ($quest->{active});
+		Log::warning "[test] number ".$counter++."\n";
+		
+		next unless (exists $quest->{missions});
+		Log::warning "[test] number ".$counter++."\n";
+		next unless (exists $quest->{missions}->{$mob_ID});
+		Log::warning "[test] number ".$counter++."\n";
+		next unless (exists $quest->{missions}->{$mob_ID}->{count});
+		Log::warning "[test] number ".$counter++."\n";
+		next unless (exists $quest->{missions}->{$mob_ID}->{goal});
+		Log::warning "[test] number ".$counter++."\n";
+		next unless ($quest->{missions}->{$mob_ID}->{count} == $quest->{missions}->{$mob_ID}->{goal});
+		Log::warning "[test] number ".$counter++."\n";
+		
+		$self->{fulfilled_quest_id} = $quest_ID;
+		$self->{fulfilled_mob_id} = $mob_ID;
+		$self->{fulfilled_member_index} = $member_index;
+		last;
+	}
+}
+
+sub validate_condition {
+	my ( $self, $callback_type, $callback_name, $args ) = @_;
+	
+	if ($callback_type eq 'hook') {
+		
+		if ($callback_name eq 'packet_mapChange') {
+			$self->{fulfilled_quest_id} = undef;
+			$self->{fulfilled_mob_id} = undef;
+			$self->{fulfilled_member_index} = undef;
+			$self->{is_on_stand_by} = 1;
+			
+		} elsif ($callback_name eq 'inventory_ready') {
+			return $self->SUPER::validate_condition if ($self->{is_on_stand_by} == 0);
+			$self->{is_on_stand_by} = 0;
+			
+		} else {
+			$self->{is_on_stand_by} = 0;
+		}
+		
+	} elsif ($callback_type eq 'variable') {
+		$self->update_vars($callback_name, $args);
+		
+	} elsif ($callback_type eq 'recheck') {
+		$self->{is_on_stand_by} = 0;
+	}
+	
+	$self->check_quests;
+	
+	if ($self->{is_on_stand_by} == 1) {
+		return $self->SUPER::validate_condition(0);
+	} else {
+		return $self->SUPER::validate_condition( (defined $self->{fulfilled_quest_id} ? 1 : 0) );
+	}
+}
+
+sub get_new_variable_list {
+	my ($self) = @_;
+	my $new_variables;
+	
+	$new_variables->{".".$self->{name}."LastQuestID"} = $self->{fulfilled_quest_id};
+	$new_variables->{".".$self->{name}."LastMobID"} = $self->{fulfilled_mob_id};
+	$new_variables->{".".$self->{name}."LastListIndex"} = $self->{fulfilled_member_index};
+	
+	return $new_variables;
+}
+
+1;
