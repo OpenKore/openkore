a7b0ee96fcfe13bfba573651aa0ca50015c01c23
lututui <lututui@inboxalias.com>
2017-02-02 18:40:43 -0200

    Retrieve address from object
    Useful when setting the port to zero in poseidon.txt

--- src/Poseidon/poseidon.pl
+++ src/Poseidon/poseidon.pl
@@ -42,9 +42,9 @@ sub initialize
 	# Starting Poseidon
 	print "Starting Poseidon 2.1 (26 Sep 2012)...\n";
 	$roServer = new Poseidon::RagnarokServer($config{ragnarokserver_port}, $config{ragnarokserver_ip});
-	print "Ragnarok Online Server Ready At : " . $config{ragnarokserver_ip} . ":" . $config{ragnarokserver_port} . "\n";
+	print "Ragnarok Online Server Ready At : " . $roServer->getHost() . ":" . $roServer->getPort() . "\n";
 	$queryServer = new Poseidon::QueryServer($config{queryserver_port}, $config{queryserver_ip}, $roServer);
-	print "Query Server Ready At : " . $config{queryserver_ip} . ":" . $config{queryserver_port} . "\n";
+	print "Query Server Ready At : " . $queryServer->getHost() . ":" . $queryServer->getPort() . "\n";
 	print ">>> Poseidon 2.1 initialized <<<\n\n";
 	print "Please read " . POSEIDON_SUPPORT_URL . "\n";
 	print "for further instructions.\n";
