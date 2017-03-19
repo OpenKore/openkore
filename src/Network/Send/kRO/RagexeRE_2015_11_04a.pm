a4660a79eaad84bca3e2a898793af856b9966c1d
SkylorÐ <alexandre.vit.nascimento@gmail.com>
2017-03-19 17:18:39 -0300

    Update RagexeRE_2015_11_04a.pm

--- src/Network/Send/kRO/RagexeRE_2015_11_04a.pm
+++ src/Network/Send/kRO/RagexeRE_2015_11_04a.pm
@@ -1,17 +1,17 @@
-```
-#############################################################################
-#  OpenKore - Network subsystem												#
-#  This module contains functions for sending messages to the server.		#
-#																			#
-#  This software is open source, licensed under the GNU General Public		#
-#  License, version 2.														#
-#  Basically, this means that you're allowed to modify and distribute		#
-#  this software. However, if you distribute modified versions, you MUST	#
-#  also distribute the source code.											#
-#  See http://www.gnu.org/licenses/gpl.html for the full license.			#
-#############################################################################
-# bRO (Brazil)
+#########################################################################
+#  OpenKore - Packet sending
+#  This module contains functions for sending packets to the server.
+#
+#  This software is open source, licensed under the GNU General Public
+#  License, version 2.
+#  Basically, this means that you're allowed to modify and distribute
+#  this software. However, if you distribute modified versions, you MUST
+#  also distribute the source code.
+#  See http://www.gnu.org/licenses/gpl.html for the full license.
+########################################################################
+
 package Network::Send::kRO::RagexeRE_2015_11_04a_;
+
 use strict;
 use base 'Network::Send::kRO::RagexeRE_2015_05_13a';
 
