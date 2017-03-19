126f025d62da306eaa5099d55c69fd538e20c00b
SkylorÐ <alexandre.vit.nascimento@gmail.com>
2017-03-19 20:26:04 -0300

    Update RagexeRE_2015_11_04a.pm

--- src/Network/Send/kRO/RagexeRE_2015_11_04a.pm
+++ src/Network/Send/kRO/RagexeRE_2015_11_04a.pm
@@ -21,30 +21,45 @@ sub new {
 	
 	my %packets = (
 		'0369' => ['actor_action', 'a4 C', [qw(targetID type)]],
-		'083C' => ['skill_use', 'v2 a4', [qw(lv skillID targetID)]],
+		'0887' => ['actor_info_request', 'a4', [qw(ID)]],
+		'0928' => ['actor_look_at', 'v C', [qw(head body)]],
+		'0368' => ['actor_name_request', 'a4', [qw(ID)]],
 		'0363' => undef, #old map_login
 		'0363' => ['character_move','a3', [qw(coords)]],
-		'0886' => ['sync', 'V', [qw(time)]],
-		'0928' => ['actor_look_at', 'v C', [qw(head body)]],
-		'0964' => ['item_take', 'a4', [qw(ID)]],
+		'07EC' => ['friend_request', 'a*', [qw(username)]],# len 26
+		'088D' => ['homunculus_command', 'v C', [qw(commandType, commandID)]], #f
 		'0437' => ['item_drop', 'v2', [qw(index amount)]],
+		'0964' => ['item_take', 'a4', [qw(ID)]],
+		'0360' => ['map_login', 'a4 a4 a4 V C', [qw(accountID charID sessionID tick sex)]],
+		'08A5' => ['party_join_request_by_name', 'Z24', [qw(partyName)]], #f
+		'083C' => ['skill_use', 'v2 a4', [qw(lv skillID targetID)]],
+		'0438' => ['skill_use_location', 'v4', [qw(lv skillID x y)]],#
 		'088B' => ['storage_item_add', 'v V', [qw(index amount)]],
 		'0364' => ['storage_item_remove', 'v V', [qw(index amount)]],
-		'0887' => ['actor_info_request', 'a4', [qw(ID)]],
-		'0336' => ['actor_name_request', 'a4', [qw(ID)]],
-		'093A' => ['item_list_res', 'v V2 a*', [qw(len type action itemInfo)]],
-		'0360' => ['map_login', 'a4 a4 a4 V C', [qw(accountID charID sessionID tick sex)]],
-		'07EC' => ['party_join_request_by_name', 'Z24', [qw(partyName)]], #f
-		'088D' => ['homunculus_command', 'v C', [qw(commandType, commandID)]], #f
+		'0886' => ['sync', 'V', [qw(time)]],
+		#'093A' => ['item_list_res', 'v V2 a*', [qw(len type action itemInfo)]],
 		'0951' => ['storage_password'],
 	);
 	
 	$self->{packet_list}{$_} = $packets{$_} for keys %packets;
 	
 	my %handlers = qw(
-		master_login 02B0
-		buy_bulk_vender 0801
-		party_setting 07D7
+		actor_action 0369
+		actor_info_request 0887
+		actor_look_at 0928
+		actor_name_request 0338
+		character_move 0363
+		friend_request 07EC
+		homunculus_command 088D
+		item_drop 0437
+		item_take 0964
+		map_login 0360
+		party_join_request_by_name 08A5
+		skill_use 083C
+		skill_use_location 0438
+		storage_item_add 088B
+		storage_item_remove 0364
+		sync 0886
 	);
 	
 	while (my ($k, $v) = each %packets) { $handlers{$v->[0]} = $k}
