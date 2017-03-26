d4c3b220383898ee07dbc89c2f9f4f82a48f9c16
SkylorÐ <alexandre.vit.nascimento@gmail.com>
2017-03-26 16:23:57 -0300

    Update RagexeRE_2015_05_13a.pm

--- src/Network/Receive/kRO/RagexeRE_2015_05_13a.pm
+++ src/Network/Receive/kRO/RagexeRE_2015_05_13a.pm
@@ -23,6 +23,7 @@ sub new {
 	my $self = $class->SUPER::new(@_);
 	
 	my %packets = (
+		'0A0B' => ['cart_item_added', 'v V v C4 a8 a25', [qw(index amount nameID type identified broken upgrade cards options)]],
 		'0A0C' => ['inventory_item_added', 'v3 C3 a8 V C2 a4 v a25', [qw(index amount nameID identified broken upgrade cards type_equip type fail expire unknown options)]],
 		'0A0D' => ['inventory_items_nonstackable', 'v a*', [qw(len itemInfo)]],
 	);
