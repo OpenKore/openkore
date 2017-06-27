3bafd62f8902b1a8374f70e7b0ef58d2e9ed439e
lututui <lututui@inboxalias.com>
2017-06-27 19:11:34 -0300

    [2017-06-27] [bRO] Connection update

--- src/Network/Send/bRO.pm
+++ src/Network/Send/bRO.pm
@@ -23,19 +23,19 @@ sub new {
 		'083C' => ['skill_use', 'v2 a4', [qw(lv skillID targetID)]],
 		'0437' => ['character_move','a3', [qw(coords)]],
 		'035F' => ['sync', 'V', [qw(time)]],
-		'0961' => ['actor_look_at', 'v C', [qw(head body)]],
-		'0819' => ['item_take', 'a4', [qw(ID)]],
-		'091C' => ['item_drop', 'v2', [qw(index amount)]],
-		'0888' => ['storage_item_add', 'v V', [qw(index amount)]],
-		'085A' => ['storage_item_remove', 'v V', [qw(index amount)]],
+		'0202' => ['actor_look_at', 'v C', [qw(head body)]],
+		'07E4' => ['item_take', 'a4', [qw(ID)]],
+		'0362' => ['item_drop', 'v2', [qw(index amount)]],
+		'07EC' => ['storage_item_add', 'v V', [qw(index amount)]],
+		'0364' => ['storage_item_remove', 'v V', [qw(index amount)]],
 		'0438' => ['skill_use_location', 'v4', [qw(lv skillID x y)]],
 		'096A' => ['actor_info_request', 'a4', [qw(ID)]],
 		'0368' => ['actor_name_request', 'a4', [qw(ID)]],
-		'0877' => ['item_list_res', 'v V2 a*', [qw(len type action itemInfo)]],
-		'0942' => ['map_login', 'a4 a4 a4 V C', [qw(accountID charID sessionID tick sex)]],
-		'08A9' => ['party_join_request_by_name', 'Z24', [qw(partyName)]], #f
-		'089F' => ['homunculus_command', 'v C', [qw(commandType, commandID)]], #f
-		'0892' => ['storage_password'],
+		'0281' => ['item_list_res', 'v V2 a*', [qw(len type action itemInfo)]],
+		'022D' => ['map_login', 'a4 a4 a4 V C', [qw(accountID charID sessionID tick sex)]],
+		'0802' => ['party_join_request_by_name', 'Z24', [qw(partyName)]], #f
+		'0361' => ['homunculus_command', 'v C', [qw(commandType, commandID)]], #f
+		'093F' => ['storage_password'],
 	);
 	
 	$self->{packet_list}{$_} = $packets{$_} for keys %packets;
@@ -49,7 +49,7 @@ sub new {
 	
 	while (my ($k, $v) = each %packets) { $handlers{$v->[0]} = $k}
 	$self->{packet_lut}{$_} = $handlers{$_} for keys %handlers;
-	$self->cryptKeys(2002936943, 1428909809, 108665161);
+	$self->cryptKeys(2030399356, 1811960832, 543189760);
 
 	return $self;
 }
