0dc9272cac4da20929dbd465e084bb61f4587f6a
kaliwanagan <kaliwanagan@openkore.com>
2017-07-12 02:34:24 +0800

    update packet definitions for sending and receibing

--- src/Network/Receive/pRO.pm
+++ src/Network/Receive/pRO.pm
@@ -1,38 +1,37 @@
-#########################################################################
-#  OpenKore - Network subsystem
-#  Copyright (c) 2006 OpenKore Team
-#
-#  This software is open source, licensed under the GNU General Public
-#  License, version 2.
-#  Basically, this means that you're allowed to modify and distribute
-#  this software. However, if you distribute modified versions, you MUST
-#  also distribute the source code.
-#  See http://www.gnu.org/licenses/gpl.html for the full license.
-#########################################################################
+#################################################################################################
+#  OpenKore - Network subsystem									#
+#  This module contains functions for sending messages to the server.				#
+#												#
+#  This software is open source, licensed under the GNU General Public				#
+#  License, version 2.										#
+#  Basically, this means that you're allowed to modify and distribute				#
+#  this software. However, if you distribute modified versions, you MUST			#
+#  also distribute the source code.								#
+#  See http://www.gnu.org/licenses/gpl.html for the full license.				#
+#################################################################################################
 # pRO (Philippines)
 # Servertype overview: http://wiki.openkore.com/index.php/ServerType
 package Network::Receive::pRO;
-
 use strict;
-use base qw(Network::Receive::ServerType0);
+use Time::HiRes;
 
 use Globals;
-use Log qw(message warning error debug);
-use Translation;
+use base qw(Network::Receive::ServerType0);
+use Log qw(message debug warning);
+use Network::MessageTokenizer;
 use Misc;
-use Utils qw(timeOut getFormattedDate);
-use I18N qw(bytesToString stringToBytes);
-
-use Time::HiRes qw(time);
-
+use Utils;
+use Translation;
 sub new {
 	my ($class) = @_;
 	my $self = $class->SUPER::new(@_);
 	my %packets = (
-		'006D' => ['character_creation_successful', 'a4 V9 v V2 v14 Z24 C6 v2', [qw(charID exp zeny exp_job lv_job opt1 opt2 option stance manner points_free hp hp_max sp sp_max walk_speed type hair_style weapon lv points_skill lowhead shield tophead midhead hair_color clothes_color name str agi vit int dex luk slot renameflag)]],
-		'0097' => ['private_message', 'v Z24 V Z*', [qw(len privMsgUser flag privMsg)]], # -1
+		'0276' => ['account_server_info', 'x2 a4 a4 a4 a4 a26 C a4 a*', [qw(sessionID accountID sessionID2 lastLoginIP lastLoginTime accountSex iAccountSID serverInfo)]],
+		'006D' => ['character_creation_successful', 'a4 V9 v V2 v14 Z24 C6 v2 Z*', [qw(charID exp zeny exp_job lv_job opt1 opt2 option stance manner points_free hp hp_max sp sp_max walk_speed type hair_style weapon lv points_skill lowhead shield tophead midhead hair_color clothes_color name str agi vit int dex luk slot renameflag mapname)]],
+		'0097' => ['private_message', 'v Z28 Z*', [qw(len privMsgUser privMsg)]],
 		'082D' => ['received_characters_info', 'x2 C5 x20', [qw(normal_slot premium_slot billing_slot producible_slot valid_slot)]],
-		'099D' => ['received_characters', 'v a*', [qw(len charInfo)]],
+		'099B' => ['map_property3', 'v a4', [qw(type info_table)]],
+		'0A7B' => ['gameguard_request', 'v a*', [qw(len data)]],
 	);
 
 	foreach my $switch (keys %packets) {
@@ -40,134 +39,29 @@ sub new {
 	}
 
 	my %handlers = qw(
-		received_characters 099D
-		actor_exists 0856
-		actor_connected 0857
-		actor_moved 0858
+		actor_moved 0856
+		actor_exists 0857
+		actor_connected 0858
 		account_id 0283
+		received_characters 099D
 	);
 	$self->{packet_lut}{$_} = $handlers{$_} for keys %handlers;
-	
+
 	return $self;
 }
 
-sub received_characters_info {
+sub gameguard_request {
 	my ($self, $args) = @_;
 
-	Scalar::Util::weaken(my $weak = $self);
-	my $timeout = {timeout => 6, time => time};
+	message T ("Receive Gameguard!\n");
+	my $msg = pack('v*', length($args->{data}) + 4, 0xA7B, $args->{len}) . $args->{data};
+	$self->{net}->{xkore_socket}->send($msg);
+	my $msg2;
+	$self->{net}->{xkore_socket}->recv($msg2, 2);
+	my $newSize = unpack('v', $msg2);
+	$self->{net}->{xkore_socket}->recv($msg2, $newSize);
 
-	$self->{charSelectTimeoutHook} = Plugins::addHook('Network::serverConnect/special' => sub {
-		if ($weak && timeOut($timeout)) {
-			$weak->received_characters({charInfo => '', RAW_MSG_SIZE => 4});
-		}
-	});
-
-	$self->{charSelectHook} = Plugins::addHook(charSelectScreen => sub {
-		if ($weak) {
-			Plugins::delHook(delete $weak->{charSelectTimeoutHook}) if $weak->{charSelectTimeoutHook};
-		}
-	});
-
-	$timeout{charlogin}{time} = time;
-
-	$self->received_characters($args);
+	$messageSender->sendToServer($msg2);
 }
-
-sub received_characters {
-	return if ($net->getState() == Network::IN_GAME);
-	my ($self, $args) = @_;
-	$net->setState(Network::CONNECTED_TO_LOGIN_SERVER);
-
-	$charSvrSet{normal_slot} = $args->{normal_slot} if (exists $args->{normal_slot});
-	$charSvrSet{premium_slot} = $args->{premium_slot} if (exists $args->{premium_slot});
-	$charSvrSet{billing_slot} = $args->{billing_slot} if (exists $args->{billing_slot});
-	$charSvrSet{producible_slot} = $args->{producible_slot} if (exists $args->{producible_slot});
-	$charSvrSet{valid_slot} = $args->{valid_slot} if (exists $args->{valid_slot});
-
-	undef $conState_tries;
-
-	Plugins::callHook('parseMsg/recvChars', $args->{options});
-	if ($args->{options} && exists $args->{options}{charServer}) {
-		$charServer = $args->{options}{charServer};
-	} else {
-		$charServer = $net->serverPeerHost . ":" . $net->serverPeerPort;
-	}
-
-	# PACKET_HC_ACCEPT_ENTER2 contains no character info
-	return unless exists $args->{charInfo};
-
-	my $blockSize = $self->received_characters_blockSize();
-	for (my $i = $args->{RAW_MSG_SIZE} % $blockSize; $i < $args->{RAW_MSG_SIZE}; $i += $blockSize) {
-		#exp display bugfix - chobit andy 20030129
-		my $unpack_string = $self->received_characters_unpackString;
-		# TODO: What would be the $unknown ?
-		my ($cID,$exp,$zeny,$jobExp,$jobLevel, $opt1, $opt2, $option, $stance, $manner, $statpt,
-			$hp,$maxHp,$sp,$maxSp, $walkspeed, $jobId,$hairstyle, $weapon, $level, $skillpt,$headLow, $shield,$headTop,$headMid,$hairColor,
-			$clothesColor,$name,$str,$agi,$vit,$int,$dex,$luk,$slot, $rename, $unknown, $mapname, $deleteDate) =
-			unpack($unpack_string, substr($args->{RAW_MSG}, $i));
-		$chars[$slot] = new Actor::You;
-
-		# Re-use existing $char object instead of re-creating it.
-		# Required because existing AI sequences (eg, route) keep a reference to $char.
-		$chars[$slot] = $char if $char && $char->{ID} eq $accountID && $char->{charID} eq $cID;
-
-		$chars[$slot]{ID} = $accountID;
-		$chars[$slot]{charID} = $cID;
-		$chars[$slot]{exp} = $exp;
-		$chars[$slot]{zeny} = $zeny;
-		$chars[$slot]{exp_job} = $jobExp;
-		$chars[$slot]{lv_job} = $jobLevel;
-		$chars[$slot]{hp} = $hp;
-		$chars[$slot]{hp_max} = $maxHp;
-		$chars[$slot]{sp} = $sp;
-		$chars[$slot]{sp_max} = $maxSp;
-		$chars[$slot]{jobID} = $jobId;
-		$chars[$slot]{hair_style} = $hairstyle;
-		$chars[$slot]{lv} = $level;
-		$chars[$slot]{headgear}{low} = $headLow;
-		$chars[$slot]{headgear}{top} = $headTop;
-		$chars[$slot]{headgear}{mid} = $headMid;
-		$chars[$slot]{hair_color} = $hairColor;
-		$chars[$slot]{clothes_color} = $clothesColor;
-		$chars[$slot]{name} = $name;
-		$chars[$slot]{str} = $str;
-		$chars[$slot]{agi} = $agi;
-		$chars[$slot]{vit} = $vit;
-		$chars[$slot]{int} = $int;
-		$chars[$slot]{dex} = $dex;
-		$chars[$slot]{luk} = $luk;
-		$chars[$slot]{sex} = $accountSex2;
-
-		setCharDeleteDate($slot, $deleteDate) if $deleteDate;
-		$chars[$slot]{nameID} = unpack("V", $chars[$slot]{ID});
-		$chars[$slot]{name} = bytesToString($chars[$slot]{name});
-	}
-
-	my $nChars = 0;
-	foreach (@chars) { $nChars++ if($_); }
-
-	# FIXME better support for multiple received_characters packets
-	if ($args->{switch} eq '099D' && $args->{RAW_MSG_SIZE} >= ($blockSize * 3)) {
-		$net->setState(1.5);
-		return;
-	}
-
-	message T("Received characters from Character Server\n"), "connection";
-
-	if (!$masterServer->{pinCode}) {
-		if (charSelectScreen(1) == 1) {
-			$firstLoginMap = 1;
-			$startingzeny = $chars[$config{'char'}]{'zeny'} unless defined $startingzeny;
-			$sentWelcomeMessage = 1;
-		}
-	} else {
-		message T("Waiting for PIN code request\n"), "connection";
-		$timeout{'charlogin'}{'time'} = time;
-	}
-}
-
-*parse_quest_update_mission_hunt = *Network::Receive::parse_quest_update_mission_hunt_v2;
-*reconstruct_quest_update_mission_hunt = *Network::Receive::reconstruct_quest_update_mission_hunt_v2;
 
 1;
--- src/Network/Send/pRO.pm
+++ src/Network/Send/pRO.pm
@@ -1,54 +1,55 @@
-#########################################################################
-#  OpenKore - Network subsystem
-#  This module contains functions for sending messages to the server.
-#
-#  This software is open source, licensed under the GNU General Public
-#  License, version 2.
-#  Basically, this means that you're allowed to modify and distribute
-#  this software. However, if you distribute modified versions, you MUST
-#  also distribute the source code.
-#  See http://www.gnu.org/licenses/gpl.html for the full license.
-#########################################################################
+#################################################################################################
+#  OpenKore - Network subsystem									#
+#  This module contains functions for sending messages to the server.				#
+#												#
+#  This software is open source, licensed under the GNU General Public				#
+#  License, version 2.										#
+#  Basically, this means that you're allowed to modify and distribute				#
+#  this software. However, if you distribute modified versions, you MUST			#
+#  also distribute the source code.								#
+#  See http://www.gnu.org/licenses/gpl.html for the full license.				#
+#################################################################################################
 # pRO (Philippines)
 # Servertype overview: http://wiki.openkore.com/index.php/ServerType
 package Network::Send::pRO;
-
 use strict;
-use Globals;
-use Network::Send::ServerType0;
 use base qw(Network::Send::ServerType0);
-use Log qw(error debug);
-use I18N qw(stringToBytes);
-use Utils qw(getTickCount getHex getCoordString);
+use Log qw(debug);
+use Misc qw(visualDump);
 
 sub new {
 	my ($class) = @_;
 	my $self = $class->SUPER::new(@_);
-	
+
+	my %packets = (
+		'0A76' => ['master_login', 'V Z40 a32 v', [qw(version username password_rijndael master_version)]],
+		'0275' => ['game_login', 'a4 a4 a4 v C x16 v', [qw(accountID sessionID sessionID2 userLevel accountSex iAccountSID)]],
+		);
+	$self->{packet_list}{$_} = $packets{$_} for keys %packets;
+
+
 	my %handlers = qw(
-		sync 0360
-		character_move 035F
-		actor_info_request 0368
-		actor_look_at 0361
-		item_take 0362
-		item_drop 0363
-		storage_item_add 0364
-		storage_item_remove 0365
+		master_login 0A76
+		game_login 0275
 		storage_password 023B
-		skill_use_location 0366
-		party_setting 07D7
-		buy_bulk_vender 0801
 	);
+
 	$self->{packet_lut}{$_} = $handlers{$_} for keys %handlers;
-	
+	$self->cryptKeys(0x0, 0x0, 0x0);
 	return $self;
 }
 
-sub sendCharDelete {
-	my ($self, $charID, $email) = @_;
-	my $msg = pack("C*", 0xFB, 0x01) .
-			$charID . pack("a50", stringToBytes($email));
-	$self->sendToServer($msg);
+sub reconstruct_master_login {
+	my ($self, $args) = @_;
+
+	if (exists $args->{password}) {
+		my $key = pack('C32', (0x06, 0xA9, 0x21, 0x40, 0x36, 0xB8, 0xA1, 0x5B, 0x51, 0x2E, 0x03, 0xD5, 0x34, 0x12, 0x00, 0x06, 0x06, 0xA9, 0x21, 0x40, 0x36, 0xB8, 0xA1, 0x5B, 0x51, 0x2E, 0x03, 0xD5, 0x34, 0x12, 0x00, 0x06));
+		my $chain = pack('C32', (0x3D, 0xAF, 0xBA, 0x42, 0x9D, 0x9E, 0xB4, 0x30, 0xB4, 0x22, 0xDA, 0x80, 0x2C, 0x9F, 0xAC, 0x41, 0x3D, 0xAF, 0xBA, 0x42, 0x9D, 0x9E, 0xB4, 0x30, 0xB4, 0x22, 0xDA, 0x80, 0x2C, 0x9F, 0xAC, 0x41));
+		my $in = pack('a32', $args->{password});
+		my $rijndael = Utils::Rijndael->new;
+		$rijndael->MakeKey($key, $chain, 32, 32);
+		$args->{password_rijndael} = unpack("Z32", $rijndael->Encrypt($in, undef, 32, 0));
+	}
 }
 
 1;
