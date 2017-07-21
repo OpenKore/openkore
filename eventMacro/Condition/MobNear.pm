fcf194efd2b22d503742890e27c1edd7d7ec7f60
Henrybk <henriquebkmohan@gmail.com>
2017-07-21 00:30:36 -0300

    fixed more bugs

--- eventMacro/Condition/MobNear.pm
+++ eventMacro/Condition/MobNear.pm
@@ -36,6 +36,19 @@ sub validate_condition {
 	return $self->SUPER::validate_condition( $callback_type, $callback_name, $args );
 }
 
+sub get_new_variable_list {
+	my ($self) = @_;
+	my $new_variables;
+	
+	$new_variables->{".".$self->{name}."Last"} = $self->{fulfilled_actor}->{name};
+	$new_variables->{".".$self->{name}."Last"."Pos"} = sprintf("%d %d %s", $self->{fulfilled_actor}->{pos_to}{x}, $self->{fulfilled_actor}->{pos_to}{y}, $field->baseName);
+	$new_variables->{".".$self->{name}."Last"."BinId"} = $self->{fulfilled_actor}->{binID};
+	$new_variables->{".".$self->{name}."Last"."Dist"} = distance($char->{pos_to}, $self->{fulfilled_actor}->{pos_to});
+	$new_variables->{".".$self->{name}."Last"."Id"} = $self->{fulfilled_actor}->{binType};
+	
+	return $new_variables;
+}
+
 sub usable {
 	1;
 }
--- eventMacro/Condition/PlayerNear.pm
+++ eventMacro/Condition/PlayerNear.pm
@@ -36,6 +36,21 @@ sub validate_condition {
 	return $self->SUPER::validate_condition( $callback_type, $callback_name, $args );
 }
 
+sub get_new_variable_list {
+	my ($self) = @_;
+	my $new_variables;
+	
+	$new_variables->{".".$self->{name}."Last"} = $self->{fulfilled_actor}->{name};
+	$new_variables->{".".$self->{name}."Last"."Pos"} = sprintf("%d %d %s", $self->{fulfilled_actor}->{pos_to}{x}, $self->{fulfilled_actor}->{pos_to}{y}, $field->baseName);
+	$new_variables->{".".$self->{name}."Last"."BinId"} = $self->{fulfilled_actor}->{binID};
+	$new_variables->{".".$self->{name}."Last"."Dist"} = distance($char->{pos_to}, $self->{fulfilled_actor}->{pos_to});
+	$new_variables->{".".$self->{name}."Last"."Level"} = $self->{fulfilled_actor}->{lv};
+	$new_variables->{".".$self->{name}."Last"."Job"} = $self->{fulfilled_actor}->job;
+	$new_variables->{".".$self->{name}."Last"."AccountId"} = $self->{fulfilled_actor}->{binID};
+	
+	return $new_variables;
+}
+
 sub usable {
 	1;
 }
