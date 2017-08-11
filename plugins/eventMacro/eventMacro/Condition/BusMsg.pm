8fd6e2e55a3e4a1311864c9510bf5e665b7f0a4a
Henrybk <henriquebkmohan@gmail.com>
2017-08-11 20:57:54 -0300

    added

---
+++ plugins/eventMacro/eventMacro/Condition/BusMsg.pm
@@ -0,0 +1,34 @@
+package eventMacro::Condition::BusMsg;
+
+use strict;
+use Globals;
+use Utils;
+
+use base 'eventMacro::Condition::BaseMsg';
+
+sub _hooks {
+	my ( $self ) = @_;
+	my $hooks = $self->SUPER::_hooks;
+	my @other_hooks = ('bus_received');
+	push(@{$hooks}, @other_hooks);
+	return $hooks;
+}
+
+sub validate_condition {
+	my ( $self, $callback_type, $callback_name, $args ) = @_;
+	if ($callback_type eq 'hook') {
+		$self->{message} = $args->{message};
+		if (exists $args->{sender}) {
+			$self->{source} = $args->{sender};
+		} else {
+			$self->{source} = undef;
+		}
+	}
+	return $self->SUPER::validate_condition( $callback_type, $callback_name, $args );
+}
+
+sub usable {
+	1;
+}
+
+1;
