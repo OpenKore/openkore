2ad18f4cb80299b44852b262dab053b3904ca456
lututui <lututuibr@gmail.com>
2017-08-22 19:03:48 -0300

    [2017-08-22] [bRO] Connection update

--- src/Network/Send/bRO.pm
+++ src/Network/Send/bRO.pm
@@ -21,23 +21,23 @@ sub new {
 	my $self = $class->SUPER::new(@_);
 	
 	my %packets = (
-		'0362' => ['actor_action', 'a4 C', [qw(targetID type)]],
-		'083C' => ['skill_use', 'v2 a4', [qw(lv skillID targetID)]],
-		'0437' => ['character_move','a3', [qw(coords)]],
-		'035F' => ['sync', 'V', [qw(time)]],
-		'0892' => ['actor_look_at', 'v C', [qw(head body)]],
-		'0369' => ['item_take', 'a4', [qw(ID)]],
-		'0860' => ['item_drop', 'a2 v', [qw(ID amount)]],
-		'093C' => ['storage_item_add', 'a2 V', [qw(ID amount)]],
-		'092C' => ['storage_item_remove', 'a2 V', [qw(ID amount)]],
-		'0438' => ['skill_use_location', 'v4', [qw(lv skillID x y)]],
-		'096A' => ['actor_info_request', 'a4', [qw(ID)]],
-		'0368' => ['actor_name_request', 'a4', [qw(ID)]],
-		'0943' => ['item_list_res', 'v V2 a*', [qw(len type action itemInfo)]],
-		'0937' => ['map_login', 'a4 a4 a4 V C', [qw(accountID charID sessionID tick sex)]],
-		'0925' => ['party_join_request_by_name', 'Z24', [qw(partyName)]], #f
-		'0960' => ['homunculus_command', 'v C', [qw(commandType, commandID)]], #f
-		'087D' => ['storage_password'],
+		'0893' => ['actor_action', 'a4 C', [qw(targetID type)]],
+		'0367' => ['skill_use', 'v2 a4', [qw(lv skillID targetID)]],
+		'087A' => ['character_move','a3', [qw(coords)]],
+		'0281' => ['sync', 'V', [qw(time)]],
+		'0950' => ['actor_look_at', 'v C', [qw(head body)]],
+		'0966' => ['item_take', 'a4', [qw(ID)]],
+		'0882' => ['item_drop', 'a2 v', [qw(ID amount)]],
+		'0948' => ['storage_item_add', 'a2 V', [qw(ID amount)]],
+		'0933' => ['storage_item_remove', 'a2 V', [qw(ID amount)]],
+		'089E' => ['skill_use_location', 'v4', [qw(lv skillID x y)]],
+		'0894' => ['actor_info_request', 'a4', [qw(ID)]],
+		'093C' => ['actor_name_request', 'a4', [qw(ID)]],
+		'0925' => ['item_list_res', 'v V2 a*', [qw(len type action itemInfo)]],
+		'07E4' => ['map_login', 'a4 a4 a4 V C', [qw(accountID charID sessionID tick sex)]],
+		'094C' => ['party_join_request_by_name', 'Z24', [qw(partyName)]], #f
+		'0897' => ['homunculus_command', 'v C', [qw(commandType, commandID)]], #f
+		'085B' => ['storage_password'],
 	);
 	
 	$self->{packet_list}{$_} = $packets{$_} for keys %packets;
@@ -51,7 +51,7 @@ sub new {
 	
 	while (my ($k, $v) = each %packets) { $handlers{$v->[0]} = $k}
 	$self->{packet_lut}{$_} = $handlers{$_} for keys %handlers;
-	$self->cryptKeys(2041138947, 952071351, 1050549552);
+	$self->cryptKeys(46217982, 1043542121, 1807761116);
 	
 	return $self;
 }
