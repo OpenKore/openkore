3319486666ad0a576a30b6110201df3a431f34e2
c4c1n6kr3m1 <andx.andx@gmail.com>
2017-09-01 12:22:16 -0700

    revert misc.pm

--- src/Misc.pm
+++ src/Misc.pm
@@ -1185,14 +1185,12 @@ sub charSelectScreen {
 			}
 		}
 		
-		push @charNames, TF("Slot %d: %s (%s, %s, level %d/%d, %s)%s",
+		push @charNames, TF("Slot %d: %s (%s, level %d/%d)%s",
 			$num,
 			$chars[$num]{name},
 			$jobs_lut{$chars[$num]{'jobID'}},
-			$sex_lut{$chars[$num]{sex}},
 			$chars[$num]{lv},
 			$chars[$num]{lv_job},
-			$chars[$num]{map_name},
 			$messageDeleteDate);
 		push @charNameIndices, $num;
 	}
