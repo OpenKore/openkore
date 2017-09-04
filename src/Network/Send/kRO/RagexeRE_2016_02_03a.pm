e79261eaf50f657ac182e2e44dcc734e3ed798e4
sctnightcore <nightcoremax@gmail.com>
2017-09-04 20:08:18 +0700

    Update and rename RagexeRE_2016_02_03.pm to RagexeRE_2016_02_03a.pm

--- src/Network/Send/kRO/RagexeRE_2016_02_03a.pm
+++ src/Network/Send/kRO/RagexeRE_2016_02_03a.pm
@@ -10,7 +10,7 @@
 #  See http://www.gnu.org/licenses/gpl.html for the full license.
 ########################################################################
 
-package Network::Send::kRO::RagexeRE_2016_02_03;
+package Network::Send::kRO::RagexeRE_2016_02_03a;
 
 use strict;
 use base 'Network::Send::kRO::RagexeRE_2015_11_04a';
