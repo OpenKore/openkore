7eaa4e06e6585175efe7adbd7dee2c283c563c88
sctnightcore <nightcoremax@gmail.com>
2017-09-05 00:17:40 +0700

    Update RagexeRE_2016_02_03a.pm

--- src/Network/Send/kRO/RagexeRE_2016_02_03a.pm
+++ src/Network/Send/kRO/RagexeRE_2016_02_03a.pm
@@ -13,7 +13,8 @@
 package Network::Send::kRO::RagexeRE_2016_02_03a;
 
 use strict;
-use base 'Network::Send::kRO::RagexeRE_2015_11_04a';
+use base qw(Network::Receive::kRO::RagexeRE_2015_12_02);
+
 
 sub new {
 	my ($class) = @_;
