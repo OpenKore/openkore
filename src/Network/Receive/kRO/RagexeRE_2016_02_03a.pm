705229970f9b64cba967b0e4d361e8aaec1699d5
sctnightcore <nightcoremax@gmail.com>
2017-09-10 03:05:02 +0700

    edit qw rev RagexeRE_2016_02_03a

--- src/Network/Receive/kRO/RagexeRE_2016_02_03a.pm
+++ src/Network/Receive/kRO/RagexeRE_2016_02_03a.pm
@@ -14,7 +14,7 @@
 package Network::Receive::kRO::RagexeRE_2016_02_03a;
 
 use strict;
-use base qw(Network::Receive::kRO::RagexeRE_2015_12_02);
+use base qw(Network::Receive::kRO::RagexeRE_2016_01_06a);
 
 
 1;
