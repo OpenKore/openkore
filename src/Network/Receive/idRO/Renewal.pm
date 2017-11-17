9d091acfb70cf5f73c61f411e29604af92b85ed9
sctnightcore <nightcoremax@gmail.com>
2017-11-17 08:52:33 +0700

    [idRO_RE] Fix network work only x1,x3 (#1402)
    * idRO_Re fix
    
    * tables for idRO_RE
    
    * fix network like iRO
    
    * fix 1 more

---
+++ src/Network/Receive/idRO/Renewal.pm
@@ -0,0 +1,18 @@
+#########################################################################
+#  OpenKore - Network subsystem
+#  Copyright (c) 2006 OpenKore Team
+#
+#  This software is open source, licensed under the GNU General Public
+#  License, version 2.
+#  Basically, this means that you're allowed to modify and distribute
+#  this software. However, if you distribute modified versions, you MUST
+#  also distribute the source code.
+#  See http://www.gnu.org/licenses/gpl.html for the full license.
+#########################################################################
+# Servertype overview: http://wiki.openkore.com/index.php/ServerType
+package Network::Receive::idRO::Renewal;
+
+use strict;
+use base qw(Network::Receive::idRO);
+
+1;
