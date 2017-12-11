1bae53b73e771f752451b6284641c3aaafb765f0
Alison Serafim <alisonserafim@gmail.com>
2017-12-11 20:09:08 -0200

    update
    update

--- src/Network/Send/Zero.pm
+++ src/Network/Send/Zero.pm
@@ -16,7 +16,7 @@ use strict;
 use base qw(Network::Send::ServerType0);
 use Globals; 
 use Network::Send::ServerType0; 
-use Log qw(error debug); 
+use Log qw(error debug message); 
 
 sub new {
 	my ($class) = @_;
@@ -46,6 +46,7 @@ sub new {
 		'0959' => ['storage_password'],
 		'095F' => ['sync', 'V', [qw(time)]],
 		'0ACF' => ['master_login', 'a4 Z25 a32 a5', [qw(game_code username password_rijndael flag)]],
+		'0825' => ['token_login', 'v v x v Z25 a32 Z17 Z15 a*', [qw(len version master_version username password_rijndael mac ip token)]],		
 	);
 	
 	$self->{packet_list}{$_} = $packets{$_} for keys %packets;
@@ -90,16 +91,7 @@ sub new {
 sub sendMasterLogin {
 	my ($self, $username, $password, $master_version, $version) = @_;
 	my $msg;
-	my $password_rijndael;
-	if (defined $password) {
-		my $key = pack('C32', (0x06, 0xA9, 0x21, 0x40, 0x36, 0xB8, 0xA1, 0x5B, 0x51, 0x2E, 0x03, 0xD5, 0x34, 0x12, 0x00, 0x06, 0x06, 0xA9, 0x21, 0x40, 0x36, 0xB8, 0xA1, 0x5B, 0x51, 0x2E, 0x03, 0xD5, 0x34, 0x12, 0x00, 0x06));
-		my $chain = pack('C32', (0x3D, 0xAF, 0xBA, 0x42, 0x9D, 0x9E, 0xB4, 0x30, 0xB4, 0x22, 0xDA, 0x80, 0x2C, 0x9F, 0xAC, 0x41, 0x3D, 0xAF, 0xBA, 0x42, 0x9D, 0x9E, 0xB4, 0x30, 0xB4, 0x22, 0xDA, 0x80, 0x2C, 0x9F, 0xAC, 0x41));
-		my $in = pack('a32', $password);
-		my $rijndael = Utils::Rijndael->new;
-		$rijndael->MakeKey($key, $chain, 32, 32);
-		$password_rijndael = unpack("Z32", $rijndael->Encrypt($in, undef, 32, 0));
-		Misc::visualDump($password_rijndael);
-	}
+	my $password_rijndael = $self->encrypt_password($password);
 
 	$msg = $self->reconstruct({
 		switch => 'master_login',
@@ -114,8 +106,46 @@ sub sendMasterLogin {
 }
 
 sub sendTokenToServer {
-	return 1;
+	my ($self, $username, $password, $master_version, $version, $token, $length) = @_;
+	my $len =  $length + 92;
+	message "len: ".$len."\n";
+	my $password_rijndael = $self->encrypt_password($password);
+	my $ip = '192.168.0.14';
+	my $mac = '20CF3095572A';	
+	my $mac_hyphen_separated = join '-', $mac =~ /(..)/g;
+
+	my $msg = $self->reconstruct({
+		switch => 'token_login',
+		len => $len, # size of packet
+		version => $version,
+		master_version => $master_version,
+		username => $username,
+		password_rijndael => '',
+		mac => $mac_hyphen_separated,
+		ip => $ip,
+		token => $token,		
+	});	
+	
+	$self->sendToServer($msg);
+	debug "Sent sendTokenLogin\n", "sendPacket", 2;
 }
 
+
+sub encrypt_password {
+	my ($self, $password) = @_;
+	my $password_rijndael;
+	if (defined $password) {
+		my $key = pack('C32', (0x06, 0xA9, 0x21, 0x40, 0x36, 0xB8, 0xA1, 0x5B, 0x51, 0x2E, 0x03, 0xD5, 0x34, 0x12, 0x00, 0x06, 0x06, 0xA9, 0x21, 0x40, 0x36, 0xB8, 0xA1, 0x5B, 0x51, 0x2E, 0x03, 0xD5, 0x34, 0x12, 0x00, 0x06));
+		my $chain = pack('C32', (0x3D, 0xAF, 0xBA, 0x42, 0x9D, 0x9E, 0xB4, 0x30, 0xB4, 0x22, 0xDA, 0x80, 0x2C, 0x9F, 0xAC, 0x41, 0x3D, 0xAF, 0xBA, 0x42, 0x9D, 0x9E, 0xB4, 0x30, 0xB4, 0x22, 0xDA, 0x80, 0x2C, 0x9F, 0xAC, 0x41));
+		my $in = pack('a32', $password);
+		my $rijndael = Utils::Rijndael->new;
+		$rijndael->MakeKey($key, $chain, 32, 32);
+		$password_rijndael = unpack("Z32", $rijndael->Encrypt($in, undef, 32, 0));
+		Misc::visualDump($password_rijndael);
+		return $password_rijndael;
+	} else {
+		error("Password is not configured");
+	}
+}
 
 1;
