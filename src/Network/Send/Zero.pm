0f1337bf6a113f5c1ba52151e5bdd95493a3bf3c
Alison Diego Serafim <ads@aloc.com.br>
2018-01-05 18:57:05 -0200

    remove space

--- src/Network/Send/Zero.pm
+++ src/Network/Send/Zero.pm
@@ -23,7 +23,7 @@ sub new {
 	my $self = $class->SUPER::new(@_);
 
 	my %packets = (
-		'0439' => ['item_use', 'a2 a4', [qw(ID targetID)]],		
+		'0439' => ['item_use', 'a2 a4', [qw(ID targetID)]],
 		'0825' => ['token_login', 'v v x v Z24 a27 Z17 Z15 a*', [qw(len version master_version username password_rijndael mac ip token)]],
 		'0ACF' => ['master_login', 'a4 Z25 a32 a5', [qw(game_code username password_rijndael flag)]],
 	);
