4762391f47e2b2486019f45c7e52ca1c2f21442c
Maytichai Saowa <nightcoremax@gmail.com>
2018-01-06 09:18:15 +0700

    fix unpack stat_info,  exp

--- src/Network/Receive/Zero.pm
+++ src/Network/Receive/Zero.pm
@@ -35,8 +35,8 @@ sub new {
 		'0AC4' => ['account_server_info', 'x2 a4 a4 a4 a4 a26 C x17 a*', [qw(sessionID accountID sessionID2 lastLoginIP lastLoginTime accountSex serverInfo)]],
 		'0AC5' => ['received_character_ID_and_Map', 'a4 Z16 a4 v', [qw(charID mapName mapIP mapPort)]], #miss 128 unknow
 		'0AC7' => ['map_changed', 'Z16 v2 a4 v', [qw(map x y IP port)]], # 28
-		'0ACB' => ['stat_info', 'v V', [qw(type val)]],   #need check 
-		'0ACC' => ['exp', 'a4 V v2', [qw(ID val type flag)]], #need check 
+		'0ACB' => ['stat_info', 'v a2', [qw(type val)]],
+		'0ACC' => ['exp', 'a4 a8 v2', [qw(ID val type flag)]],
 		'0ADC' => ['flag', 'a*', [qw(unknown)]],
 		'0ADE' => ['flag', 'a*', [qw(unknown)]],
 		'0ADF' => ['actor_info', 'a4 a4 Z24 Z24', [qw(ID charID name prefix_name)]],
