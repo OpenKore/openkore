21032e7b82bb16a54f3ec8847988777c958edfe9
lututui <lututuibr@gmail.com>
2018-01-06 22:45:20 -0200

    Add player condition target_spirit

--- src/Misc.pm
+++ src/Misc.pm
@@ -4339,6 +4339,10 @@ sub checkPlayerCondition {
 	if ($config{$prefix."_isNotMyDevotee"}) {
 		return 0 if (defined $devotionList->{$accountID}->{targetIDs}->{$id});
 	}
+	
+	if ($config{$prefix."_spirit"}) {
+		return 0 unless inRange(defined $player->{spirits} ? $player->{spirits} : 0, $config{$prefix . "_spirit"});
+	}
 
 	my %args = (
 		player => $player,
