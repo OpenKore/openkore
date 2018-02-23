66a058a6feba763a55553f5b83df8f634588d379
lututui <lututuibr@gmail.com>
2018-02-23 19:50:31 -0300

    Revert "[TalkNPC] fix getting stuck when disconnected"

--- src/Network/DirectConnection.pm
+++ src/Network/DirectConnection.pm
@@ -239,13 +239,9 @@ sub serverDisconnect {
 		message TF("Disconnecting (%s:%s)...", $self->{remote_socket}->peerhost(), 
 			$self->{remote_socket}->peerport()), "connection";
 		close($self->{remote_socket});
-		if (!$self->serverAlive()) {
-			message T("disconnected\n"), "connection";
-			Plugins::callHook("disconnected");
-		} else {
+		!$self->serverAlive() ?
+			message T("disconnected\n"), "connection" :
 			error T("couldn't disconnect\n"), "connection";
-			Plugins::callHook("disconnect_fail");
-		}
 	}
 }
 
@@ -632,8 +628,8 @@ sub checkConnection {
 
 		} elsif (timeOut($timeout{play})) {
 			error T("Timeout on Map Server, "), "connection";
+			Plugins::callHook('disconnected');
 			if ($config{dcOnDisconnect}) {
-				Plugins::callHook('disconnected');
 				error T("Auto disconnecting on Disconnect!\n");
 				chatLog("k", T("*** You disconnected, auto disconnect! ***\n"));
 				$quit = 1;
