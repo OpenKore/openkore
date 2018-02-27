a3889ec51d337d70bbf00ea49c37f75f8a5025a7
Alison Serafim <10372732+alisonrag@users.noreply.github.com>
2018-02-27 18:16:43 -0300

    [Receive.pm] move some actor packets
    move some identical functions related to actor to Receive.pm

--- src/Network/Receive/kRO/Sakexe_0.pm
+++ src/Network/Receive/kRO/Sakexe_0.pm
@@ -272,7 +272,7 @@ sub new {
 		'01A4' => ['pet_info2', 'C a4 V', [qw(type ID value)]], # 11
 		'01A6' => ['egg_list'], # -1
 		'01AA' => ['pet_emotion', 'a4 V', [qw(ID type)]], # 10
-		'01AB' => ['actor_muted', 'a4 v V', [qw(ID duration)]], # 12
+		'01AB' => ['stat_info', 'a4 v V', [qw(ID type val)]], # was "actor_muted"; is struct/handler correct at all?
 		'01AC' => ['actor_trapped', 'a4', [qw(ID)]], # 6
 		'01AD' => ['arrowcraft_list'], # -1
 		'01B0' => ['monster_typechange', 'a4 C V', [qw(ID type nameID)]], # 11
@@ -756,59 +756,6 @@ sub map_loaded {
 	setStatus($char, $char->{opt1}, $char->{opt2}, $char->{option}); # set initial status from data received from the char server (seems needed on eA, dunno about kRO)
 
 	$messageSender->sendIgnoreAll("all") if ($config{ignoreAll});
-}
-
-sub actor_look_at {
-	my ($self, $args) = @_;
-	return unless changeToInGameState();
-
-	my $actor = Actor::get($args->{ID});
-	$actor->{look}{head} = $args->{head};
-	$actor->{look}{body} = $args->{body};
-	debug $actor->nameString . " looks at $args->{body}, $args->{head}\n", "parseMsg";
-}
-
-sub actor_movement_interrupted {
-	my ($self, $args) = @_;
-	return unless changeToInGameState();
-	my %coords;
-	$coords{x} = $args->{x};
-	$coords{y} = $args->{y};
-
-	my $actor = Actor::get($args->{ID});
-	$actor->{pos} = {%coords};
-	$actor->{pos_to} = {%coords};
-	if ($actor->isa('Actor::You') || $actor->isa('Actor::Player')) {
-		$actor->{sitting} = 0;
-	}
-	if ($actor->isa('Actor::You')) {
-		debug "Movement interrupted, your coordinates: $coords{x}, $coords{y}\n", "parseMsg_move";
-		AI::clear("move");
-	}
-	if ($char->{homunculus} && $char->{homunculus}{ID} eq $actor->{ID}) {
-		AI::clear("move");
-	}
-}
-
-sub actor_muted {
-	my ($self, $args) = @_;
-
-	my $ID = $args->{ID};
-	my $duration = $args->{duration};
-	if ($duration > 0) {
-		$duration = 0xFFFFFFFF - $duration + 1;
-		message TF("%s is muted for %d minutes\n", getActorName($ID), $duration), "parseMsg_statuslook", 2;
-	} else {
-		message TF("%s is no longer muted\n", getActorName($ID)), "parseMsg_statuslook", 2;
-	}
-}
-
-sub actor_trapped {
-	my ($self, $args) = @_;
-	# original comment was that ID is not a valid ID
-	# but it seems to be, at least on eAthena/Freya
-	my $actor = Actor::get($args->{ID});
-	debug "$actor->nameString() is trapped.\n";
 }
 
 sub area_spell {
