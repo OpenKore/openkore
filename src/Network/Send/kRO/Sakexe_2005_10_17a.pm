53493fc728115936c67d00496589e57a2ffbc6df
lututui <lututuibr@gmail.com>
2018-04-21 15:23:57 -0300

    [Issue #1060] [Part 1/?] Optimize Sakexe imports

--- src/Network/Send/kRO/Sakexe_2005_10_17a.pm
+++ src/Network/Send/kRO/Sakexe_2005_10_17a.pm
@@ -20,7 +20,7 @@ package Network::Send::kRO::Sakexe_2005_10_17a;
 use strict;
 use base qw(Network::Send::kRO::Sakexe_2005_10_13a);
 
-use Log qw(message warning error debug);
+use Log qw(debug);
 
 sub new {
 	my ($class) = @_;
