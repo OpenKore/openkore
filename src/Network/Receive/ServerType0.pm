f2dbd474c30b18b4cf76f48bdb495fad244ecf65
Alison Serafim <alisonserafim@gmail.com>
2018-06-10 19:03:54 -0300

    [cRO] Request unfreeze

--- src/Network/Receive/ServerType0.pm
+++ src/Network/Receive/ServerType0.pm
@@ -2115,7 +2115,7 @@ sub map_changed {
 		oldMap => $oldMap,
 	});
 	$timeout{ai}{time} = time;
-	$messageSender->sendBlockingPlayerCancel() if(grep { $masterServer->{serverType} eq $_ } qw( Zero idRO_Renewal )); # request to unfreeze char
+	$messageSender->sendBlockingPlayerCancel() if(grep { $masterServer->{serverType} eq $_ } qw( Zero idRO_Renewal cRO)); # request to unfreeze char alisonrag
 }
 
 sub memo_success {
