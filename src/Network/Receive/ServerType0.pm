111a17d851cbc5a588811b5b1c7d308d6518dfdc
sctnightcore <nightcoremax@gmail.com>
2018-08-11 09:10:28 -0700

    Update New 'login_pin_code_request'

--- src/Network/Receive/ServerType0.pm
+++ src/Network/Receive/ServerType0.pm
@@ -647,6 +647,7 @@ sub new {
 		'0AE3' => ['received_login_token', 'v l Z20 Z*', [qw(len login_type flag login_token)]],
 		'0AE4' => ['party_join', 'a4 a4 V v4 C Z24 Z24 Z16 C2', [qw(ID charID role jobID lv x y type name user map item_pickup item_share)]],
  		'0AE5' => ['party_users_info', 'v Z24 a*', [qw(len party_name playerInfo)]],
+		'0AE9' => ['login_pin_code_request', 'V a4 v2', [qw(seed accountID flag lock)]],		
 		'C350' => ['senbei_vender_items_list'], #new senbei vender, need research
 	};
 
