24d6cf2515fc7c90218334eea7102b3bf708dcef
Alison Serafim <10372732+alisonrag@users.noreply.github.com>
2018-09-10 18:15:21 -0300

    merge private_message_sent

--- src/Network/Receive.pm
+++ src/Network/Receive.pm
@@ -7173,4 +7173,25 @@ sub inventory_item_favorite {
 	}
 }
 
+sub private_message_sent {
+	my ($self, $args) = @_;
+	if ($args->{type} == 0) {
+ 		message TF("(To %s) : %s\n", $lastpm[0]{'user'}, $lastpm[0]{'msg'}), "pm/sent";
+		chatLog("pm", "(To: $lastpm[0]{user}) : $lastpm[0]{msg}\n") if ($config{'logPrivateChat'});
+
+		Plugins::callHook('packet_sentPM', {
+			to => $lastpm[0]{user},
+			msg => $lastpm[0]{msg}
+		});
+
+	} elsif ($args->{type} == 1) {
+		warning TF("%s is not online\n", $lastpm[0]{user});
+	} elsif ($args->{type} == 2) {
+		warning TF("Player %s ignored your message\n", $lastpm[0]{user});
+	} else {
+		warning TF("Player %s doesn't want to receive messages\n", $lastpm[0]{user});
+	}
+	shift @lastpm;
+}
+
 1;
