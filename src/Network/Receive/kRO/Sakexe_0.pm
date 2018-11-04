3d9fefd5bdb55501139a79b4674e91e3255850cd
sctnightcore <nightcoremax@gmail.com>
2018-11-04 16:20:53 +0700

    add packet 0AFD
    check Herc PR : https://github.com/HerculesWS/Hercules/pull/2310

--- src/Network/Receive/kRO/Sakexe_0.pm
+++ src/Network/Receive/kRO/Sakexe_0.pm
@@ -625,6 +625,7 @@ sub new {
 		'0ACD' => ['login_error', 'C Z20', [qw(type date)]],
 		'0ADC' => ['flag', 'V', [qw(unknown)]],
  		'0ADE' => ['overweight_percent', 'v V', [qw(len percent)]],#TODO
+		'0AFD' => ['sage_autospell', 'v a*', [qw(len autospell_list)]], #herc PR 2310
 		'0AE4' => ['party_join', 'a4 a4 V v4 C Z24 Z24 Z16 C2', [qw(ID charID role jobID lv x y type name user map item_pickup item_share)]],
 		'0AE5' => ['party_users_info', 'v Z24 a*', [qw(len party_name playerInfo)]],
 		};
