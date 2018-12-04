a0e8f222e363c822b17093c5ede418aa3cbe0488
Felipe Rojas Verasay <35433329+PipeDeveloper@users.noreply.github.com>
2018-12-04 16:28:18 -0300

    change some lines
    also add color

--- checkfiles.sh
+++ checkfiles.sh
@@ -1,16 +1,21 @@
-#This script is taked from rAthena function.sh as a checker for files in folder.
-#Modified for educational purposes.
+#This script is taked from rAthena function.sh as a checker for files in forlder.
+#Coppyrights are to their respective developers.
+#Modified by Zelecktor 2019 for educational purposes.
 #This file is adapted for run shell to run many openkore instances.
 
 O_CLI=openkore.pl
 INST_PATH=/opt
 PKG_PATH=$INST_PATH/$PKG
+NOCOLOR='\033[0m'
+RED='\033[0;31m'
 
 check_files() {
     for i in ${O_CLI}
     do
         if [ ! -f ./$i ]; then
-            echo "$i does not exist... exiting..."
+            echo "$i doesn't exist! or you are not in openkore folder"
+	    echo "Make sure you are on the right directory"
+	    echo "${RED}Exiting...${NOCOLOR}"
             exit 1;
         fi
     done
