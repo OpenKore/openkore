3c587163c7c07c1b9b8a43cbe16b768039d05e83
OvoKore <ovokore@hotmail.com>
2019-03-18 13:06:11 -0300

    teleportAuto_item 3 digit or more

--- src/Misc.pm
+++ src/Misc.pm
@@ -3331,7 +3331,7 @@ sub useTeleport {
 			unless ($item) { $item = $char->inventory->getByNameID(12323); } # only if we don't have any fly wing
 		} else {
 			$item = $char->inventory->getByName($config{teleportAuto_item1});
-			$item = $char->inventory->getByNameID($config{teleportAuto_item1}) if (!($item));
+			$item = $char->inventory->getByNameID($config{teleportAuto_item1}) if (!($item) && length($config{teleportAuto_item1}) >= 3);
 		}
 	} elsif ($use_lvl == 2) { #Butterfly Wing
 		if (!$config{teleportAuto_item2}) {
@@ -3339,7 +3339,7 @@ sub useTeleport {
 			unless ($item) { $item = $char->inventory->getByNameID(12324); } # only if we don't have any butterfly wing
 		} else {
 			$item = $char->inventory->getByName($config{teleportAuto_item2});
-			$item = $char->inventory->getByNameID($config{teleportAuto_item2}) if (!($item));
+			$item = $char->inventory->getByNameID($config{teleportAuto_item2}) if (!($item) && length($config{teleportAuto_item2}) >= 3);
 		}
 	}
 
