6b133a16bce34c716be273ac097e45175eec269f
sctnightcore <23263315+sctnightcore@users.noreply.github.com>
2019-06-10 17:45:53 +0700

    EquipSwitch Packet (#2681)
    * Send Packet of EquipSwitch !
    
    Packet base IRO:RE
    '0A97' => 'equipswitch_add'
    '0A99' => 'equipswitch_remove'
    '0A9C' => 'equipswitch_run']
    
    * Add some Recv
    
    '0A98' => 'equip_item_switch',
    '0A9A' => 'unequip_item_switch'
    '0A9D' => 'equipswitch_run_res'
    
    * merge sub eq / uneq for eq_switch
    
    * Add 0A9B for check equip switch list
    
    '0A9B' => 'eqsw_log'
    
    * Update equip_item for check fail equip / rename sub / add 0ACE
    
    '0ACE' => 'equipswitch_single'
    
    * Update 0A98
    
    * 0ACE is CZ packet
    
    * Add 0ACE
    
    * clear code

--- src/Commands.pm
+++ src/Commands.pm
@@ -2248,6 +2248,7 @@ sub cmdEquip {
 
 	if ($arg1 eq "") {
 		cmdEquip_list();
+		cmdEquipsw_list();
 		return;
 	}
 
@@ -2292,6 +2293,7 @@ sub cmdEquip_list {
 		error T("Character equipment not yet ready\n");
 		return;
 	}
+	message TF("=====[Character Equip List]=====\n"), "info";
 	for my $slot (@Actor::Item::slots) {
 		my $item = $char->{equipment}{$slot};
 		my $name = $item ? $item->{name} : '-';
@@ -2299,7 +2301,25 @@ sub cmdEquip_list {
 			message sprintf("%-15s: %s x %s\n", $slot, $name, $item->{amount}), "list" :
 			message sprintf("%-15s: %s\n", $slot, $name), "list";
 	}
+	message TF("================================\n"), "info";
 }
+
+sub cmdEquipsw_list {
+	if (!$char) {
+		error T("Character equipment not yet ready\n");
+		return;
+	}
+	message TF("=====[Equip Switch List]=====\n"), "info";
+	for my $slot (@Actor::Item::slots) {
+		my $item = $char->{eqswitch}{$slot};
+		my $name = $item ? $item->{name} : '-';
+		($item->{type} == 10 || $item->{type} == 16 || $item->{type} == 17 || $item->{type} == 19) 
+			? message sprintf("%-15s: %s x %s\n", $slot, $name, $item->{amount}), "list" 
+			: message sprintf("%-15s: %s\n", $slot, $name), "list";
+	}
+	message TF("=============================\n"), "info";
+}
+
 
 sub cmdEval {
 	if (!$Settings::lockdown) {
--- src/Network/Receive/ServerType0.pm
+++ src/Network/Receive/ServerType0.pm
@@ -637,7 +637,14 @@ sub new {
 		'0A51' => ['rodex_check_player', 'V v2 Z24', [qw(char_id class base_level name)]],   # 34
 		'0A7D' => ['rodex_mail_list', 'v C3', [qw(len type amount isEnd)]],   # -1
 		'0A89' => ['clone_vender_found', 'a4 v4 C v9 Z24', [qw(ID jobID unknown coord_x coord_y sex head_dir weapon shield lowhead tophead midhead hair_color clothes_color robe title)]],
-		'0A8A' => ['clone_vender_lost', 'v a4', [qw(len ID)]],		
+		'0A8A' => ['clone_vender_lost', 'v a4', [qw(len ID)]],	
+		'0A98' => ($rpackets{'0A98'}{length} == 10) # or 12
+			? ['equip_item_switch', 'a2 V v', [qw(ID type success)]] 
+			: ['equip_item_switch', 'a2 V2', [qw(ID type success)]] #kRO <= 20170502
+		,
+		'0A9A' => ['unequip_item_switch', 'a2 V C', [qw(ID type success)]],
+		'0A9B' => ['equipswitch_log', 'v a*', [qw(len log)]], # -1
+		'0A9D' => ['equipswitch_run_res', 'v', [qw(success)]],
 		'0AA0' => ['refineui_opened', '' ,[qw()]],
 		'0AA2' => ['refineui_info', 'v v C a*' ,[qw(len index bless materials)]],	
 		'0ABE' => ['warp_portal_list', 'v Z16 Z16 Z16 Z16', [qw(type memo1 memo2 memo3 memo4)]], #TODO : MapsCount || size is -1
@@ -2591,6 +2598,26 @@ sub move_interrupt {
 	debug "Movement interrupted by casting a skill/fleeing a mob/etc\n";
 }
 
+sub equipswitch_run_res {
+	my ($self, $args) = @_;
+	if ($args->{success}) {
+		message TF("[Equip Switch] Fail !\n"), "info";
+	} else {
+		message TF("[Equip Switch] Success !\n"), "info";
+	}
+}
+
+sub equipswitch_log {
+    my ($self, $args) = @_;
+    for (my $i = 0; $i < length($args->{log}); $i+= 6) {
+    	my ($index, $position) = unpack('a2 V', substr($args->{log}, $i, 6));
+    	my $item = $char->inventory->getByID($index);
+    	$char->{eqswitch}{$equipSlot_lut{$position}} = $item;
+    }
+}
+
 *changeToInGameState = *Network::Receive::changeToInGameState;
+*equip_item_switch = *Network::Receive::equip_item;
+*unequip_item_switch = *Network::Receive::unequip_item;
 
 1;
