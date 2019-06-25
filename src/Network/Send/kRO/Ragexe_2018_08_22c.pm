88cceacb620f863326b57e3d5ba4726925d1bac1
sctnightcore <23263315+sctnightcore@users.noreply.github.com>
2019-06-26 04:25:32 +0700

    [Add Servertype] Ragexe 20180704 To 20181114 (#2749)
    * Add Ragexe 20180704 To 20181114
    
    * del ragexe_0 / use base RagexeRE_2018_06_21a
    
    * fix error / tested

---
+++ src/Network/Send/kRO/Ragexe_2018_08_22c.pm
@@ -0,0 +1,52 @@
+#########################################################################
+#  OpenKore - Packet sending
+#  This module contains functions for sending packets to the server.
+#
+#  This software is open source, licensed under the GNU General Public
+#  License, version 2.
+#  Basically, this means that you're allowed to modify and distribute
+#  this software. However, if you distribute modified versions, you MUST
+#  also distribute the source code.
+#  See http://www.gnu.org/licenses/gpl.html for the full license.
+########################################################################
+#bysctnightcore
+package Network::Send::kRO::Ragexe_2018_08_22c;
+
+use strict;
+use base qw(Network::Send::kRO::Ragexe_2018_08_08b);
+
+sub new {
+	my ($class) = @_;
+	my $self = $class->SUPER::new(@_);
+	
+	my %handlers = qw(
+		actor_action 0437
+		actor_info_request 0368
+		actor_look_at 0361
+		actor_name_request 0369
+		buy_bulk_buyer 0819
+		buy_bulk_closeShop 0815
+		buy_bulk_openShop 0811
+		buy_bulk_request 0817
+		character_move 035F
+		friend_request 0202
+		homunculus_command 022D
+		item_drop 0363
+		item_list_window_selected 07E4
+		item_take 0362
+		map_login 0436
+		party_join_request_by_name 02C4
+		skill_use 0438
+		skill_use_location 0366
+		storage_item_add 0364
+		storage_item_remove 0365
+		storage_password 023B
+		sync 0360
+	);
+	
+	$self->{packet_lut}{$_} = $handlers{$_} for keys %handlers;
+	
+	return $self;
+}
+
+1;
