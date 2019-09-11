89e3ba77383cfa60a69962c2a81e5dc8932aa894
Alison Serafim <10372732+alisonrag@users.noreply.github.com>
2019-09-11 13:47:00 -0300

    add charblocksize 146

--- src/Network/Receive.pm
+++ src/Network/Receive.pm
@@ -603,6 +603,12 @@ sub received_characters_unpackString {
 				keys => [qw(charID exp zeny exp_job lv_job body_state health_state effect_state stance manner status_point hp hp_max sp sp_max walkspeed jobID hair_style weapon lv skill_point head_bottom shield head_top head_mid hair_pallete clothes_color name str agi vit int dex luk slot hair_color is_renamed last_map delete_date robe slot_addon rename_addon sex)],
 			};
 
+        } elsif ($_ == 146) { # bRO and tRO update the data to be equal to charblocksize 147, but not added sex. (Sep, 2019)
+			$char_info = {
+			    types => 'a4 V9 v V2 v4 V v9 Z24 C8 v a16 V4',
+				keys => [qw(charID exp zeny exp_job lv_job body_state health_state effect_state stance manner status_point hp hp_max sp sp_max walkspeed jobID hair_style weapon lv skill_point head_bottom shield head_top head_mid hair_pallete clothes_color name str agi vit int dex luk slot hair_color is_renamed last_map delete_date robe slot_addon rename_addon)],
+			};
+
         } elsif ($_ == 145) { # PACKETVER >= 20141016 [support to double sex account]
 			$char_info = {
 				types => 'a4 V9 v V2 v14 Z24 C8 v Z16 V4 C',
