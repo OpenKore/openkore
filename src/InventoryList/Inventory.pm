719dd18ee0a17824a7c517c67640a01a8f859d07
Alison Serafim <alisonserafim@gmail.com>
2019-09-14 18:41:01 -0300

    update inventoryList to work with new item_list (#2887)
    * update inventoryList to work with new item_list
    
    * inventory update ins constant name

--- src/InventoryList/Inventory.pm
+++ src/InventoryList/Inventory.pm
@@ -7,15 +7,21 @@ use base qw(InventoryList);
 
 use constant {
 	MAP_LOADED_OR_NEW => 0,
-	RECV_STAT_INFO2 => 1
+	INVENTORT_READY => 1
 };
 
 sub new {
 	my ($class) = @_;
 	my $self = $class->SUPER::new;
-	$self->{hooks} = Plugins::addHooks (
-		['packet/stat_info2',        sub { $self->onStatInfo2; }]
-	);
+	if($masterServer->{itemListType}) {
+		$self->{hooks} = Plugins::addHooks (
+			['packet_pre/item_list_end',        sub { $self->onitemListEnd; }]
+		);
+	} else {
+		$self->{hooks} = Plugins::addHooks (
+			['packet/stat_info2',        sub { $self->onStatInfo2; }]
+		);
+	}
 	#Here we use packet/stat_info2 because it was the only safe hook I (henrybk) found for this function, both 'inventory_items_stackable' and 'inventory_items_nonstackable' are
 	#only sent by the server if we have at least 1 item of that category, while 'stat_info2' is always (at least in my tests) sent.
 	$self->{state} = MAP_LOADED_OR_NEW;
@@ -27,10 +33,20 @@ sub isReady {
 	return $self->{state};
 }
 
+sub onitemListEnd {
+	my ($self) = @_;
+	if($current_item_list == 0x0) {
+		if ($self->{state} == MAP_LOADED_OR_NEW) {
+			$self->{state} = INVENTORT_READY;
+			Plugins::callHook('inventory_ready');
+		}
+	}
+}
+
 sub onStatInfo2 {
 	my ($self) = @_;
 	if ($self->{state} == MAP_LOADED_OR_NEW) {
-		$self->{state} = RECV_STAT_INFO2;
+		$self->{state} = INVENTORT_READY;
 		Plugins::callHook('inventory_ready');
 	}
 }
