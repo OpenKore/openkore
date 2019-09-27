18c9643d59fa7a1eae0f7ee4a7c348be0b48bbbb
sctnightcore <nightcoremax@gmail.com>
2019-09-28 00:01:16 +0700

    Recheck recv packet with recvpacket.txt

--- src/Network/Receive/iRO/Renewal.pm
+++ src/Network/Receive/iRO/Renewal.pm
@@ -21,10 +21,6 @@ sub new {
 	
 	my %packets = (
 		'0097' => ['private_message', 'v Z24 V Z*', [qw(len privMsgUser flag privMsg)]],
-		'0A0A' => ['storage_item_added', 'a2 V V C4 a16 a25', [qw(ID amount nameID type identified broken upgrade cards options)]],
-		'0A0B' => ['cart_item_added', 'a2 V V C4 a16 a25', [qw(ID amount nameID type identified broken upgrade cards options)]],
-		'0A37' => ['inventory_item_added', 'a2 v V C3 a16 V C2 a4 v a25 C v', [qw(ID amount nameID identified broken upgrade cards type_equip type fail expire unknown options favorite viewID)]],
-		'0A05' => ['rodex_add_item', 'C a2 v V C4 a16 a25 v a5', [qw(fail ID amount nameID type identified broken upgrade cards options weight unknow)]],   # 63
 	);
 	
 	$self->{packet_list}{$_} = $packets{$_} for keys %packets;
@@ -38,6 +34,7 @@ sub new {
 
 	$self->{packet_lut}{$_} = $handlers{$_} for keys %handlers;
 
+	# TODO !
 	$self->{vender_items_list_item_pack} = 'V v2 C V C3 a16 a25';
 	$self->{npc_store_info_pack} = "V V C V";
 	$self->{buying_store_items_list_pack} = "V v C V";
