b743f7b02203de88677e8512b210d9a91307d39e
Alison Serafim <alisonserafim@gmail.com>
2019-12-18 16:52:33 -0300

    [Receive] Open UI packet (#3001)

--- src/Network/Receive.pm
+++ src/Network/Receive.pm
@@ -458,6 +458,18 @@ use constant {
 	EXP_FROM_QUEST => 0x1,
 };
 
+# client UI types
+use constant {
+	BANK_UI => 0x0,
+	STYLIST_UI => 0x1,
+	CAPTCHA_UI => 0x2,
+	MACRO_UI => 0x3,
+	UI_UNUSED => 0x4,
+	TIPBOX_UI => 0x5,
+	RENEWQUEST_UI => 0x6,
+	ATTENDANCE_UI => 0x7,
+};
+
 # 07F6 (exp) doesn't change any exp information because 00B1 (exp_zeny_info) is always sent with it
 # r7643 - copy-pasted to RagexeRE_2009_10_27a.pm
 sub exp {
@@ -9298,6 +9310,32 @@ sub stylist_res {
 		message T("[Stylist UI] Success.\n"), "info";
 	} else {
 		error T("[Stylist UI] Fail.\n");
+	}
+}
+
+sub open_ui {
+	my ($self, $args) = @_;
+	
+	debug TF("Received request from server to open UI: %s\n", $args->{type});
+	
+	if($args->{type} == BANK_UI) { # TODO: implement bank system and add Bank open Request
+		message T("Server requested to open Bank UI.\n");
+	} elsif($args->{type} == STYLIST_UI) { # TODO: implement Stylist system and add Stylist open Request
+		message T("Server requested to open Stylist UI.\n");
+	} elsif($args->{type} == CAPTCHA_UI) {
+		message T("Server requested to open Captcha UI.\n");
+	} elsif($args->{type} == MACRO_UI) {
+		message T("Server requested to open Macro Recorder UI.\n");
+	} elsif($args->{type} == UI_UNUSED) {
+		message T("Server requested to open Unused UI.\n"); # why?
+	} elsif($args->{type} == TIPBOX_UI) {
+		message T("Server requested to open Tip Box UI.\n");
+	} elsif($args->{type} == RENEWQUEST_UI) {
+		message T("Server requested to open Quest UI.\n");
+	} elsif($args->{type} == ATTENDANCE_UI) { # TODO: implement Attendance system and add Attendance open Request
+		message T("Server requested to open Attendance UI.\n");
+	} else {
+		error TF("Received request from server to open unknown UI: %s\n", $args->{type});
 	}
 }
 
