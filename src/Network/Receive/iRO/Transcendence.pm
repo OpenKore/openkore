448a155273718860433a01b72558d18af95a89e2
Alison Serafim <10372732+alisonrag@users.noreply.github.com>
2020-01-08 19:01:50 -0300

    iRO RT support

---
+++ src/Network/Receive/iRO/Transcendence.pm
@@ -0,0 +1,41 @@
+#########################################################################
+#  OpenKore - Network subsystem
+#  Copyright (c) 2006 OpenKore Team
+#
+#  This software is open source, licensed under the GNU General Public
+#  License, version 2.
+#  Basically, this means that you're allowed to modify and distribute
+#  this software. However, if you distribute modified versions, you MUST
+#  also distribute the source code.
+#  See http://www.gnu.org/licenses/gpl.html for the full license.
+#########################################################################
+# Servertype overview: http://wiki.openkore.com/index.php/ServerType
+package Network::Receive::iRO::Transcendence;
+
+use strict;
+use base qw(Network::Receive::iRO);
+
+sub new {
+	my ($class) = @_;
+	my $self = $class->SUPER::new(@_);
+	
+	my %packets = (
+		'082D' => ['received_characters_info', 'v C x2 C2 x20', [qw(len total_slot premium_start_slot premium_end_slot)]],
+	);
+	
+	$self->{packet_list}{$_} = $packets{$_} for keys %packets;
+	
+	my %handlers = qw(
+		received_characters 099D
+		received_characters_info 082D
+		sync_received_characters 09A0
+		account_server_info 0AC4
+	);
+
+	$self->{packet_lut}{$_} = $handlers{$_} for keys %handlers;
+
+	return $self;
+}
+
+
+1;
