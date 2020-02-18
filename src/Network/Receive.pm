2b19c4577650ec0c4abaded4ac943ddfc2f59ab3
Alison Serafim <10372732+alisonrag@users.noreply.github.com>
2020-02-18 19:22:22 -0300

    documented errors

--- src/Network/Receive.pm
+++ src/Network/Receive.pm
@@ -5105,6 +5105,40 @@ sub emoticon {
 	});
 }
 
+# Notifies the client of a ban or forced disconnect (SC_NOTIFY_BAN).
+# 0081 <error code>.B
+# error code:
+#     0 = BAN_UNFAIR -> "disconnected from server" -> MsgStringTable[3]
+#     1 = server closed -> MsgStringTable[4]
+#     2 = ID already logged in -> MsgStringTable[5]
+#     3 = timeout/too much lag -> MsgStringTable[241]
+#     4 = server full -> MsgStringTable[264]
+#     5 = underaged -> MsgStringTable[305]
+#     8 = Server sill recognizes last connection -> MsgStringTable[441]
+#     9 = too many connections from this ip -> MsgStringTable[529]
+#     10 = out of available time paid for -> MsgStringTable[530]
+#     11 = BAN_PAY_SUSPEND
+#     12 = BAN_PAY_CHANGE
+#     13 = BAN_PAY_WRONGIP
+#     14 = BAN_PAY_PNGAMEROOM
+#     15 = disconnected by a GM -> if( servicetype == taiwan ) MsgStringTable[579]
+#     16 = BAN_JAPAN_REFUSE1
+#     17 = BAN_JAPAN_REFUSE2
+#     18 = BAN_INFORMATION_REMAINED_ANOTHER_ACCOUNT
+#     100 = BAN_PC_IP_UNFAIR
+#     101 = BAN_PC_IP_COUNT_ALL
+#     102 = BAN_PC_IP_COUNT
+#     103 = BAN_GRAVITY_MEM_AGREE
+#     104 = BAN_GAME_MEM_AGREE
+#     105 = BAN_HAN_VALID
+#     106 = BAN_PC_IP_LIMIT_ACCESS
+#     107 = BAN_OVER_CHARACTER_LIST
+#     108 = BAN_IP_BLOCK
+#     109 = BAN_INVALID_PWD_CNT
+#     110 = BAN_NOT_ALLOWED_JOBCLASS
+#     113 = access is restricted between the hours of midnight to 6:00am.
+#     115 = You are in game connection ban period.
+#     ? = disconnected -> MsgStringTable[3]
 sub errors {
 	my ($self, $args) = @_;
 
