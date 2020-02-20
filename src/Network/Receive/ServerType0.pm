475d88158d4afec208f1bcca038248f0db1366e7
Alison Serafim <10372732+alisonrag@users.noreply.github.com>
2020-02-20 15:05:56 -0300

    [Guild] Change guild packets to emulate client behavior

--- src/Network/Receive/ServerType0.pm
+++ src/Network/Receive/ServerType0.pm
@@ -1292,16 +1292,6 @@ sub guild_notice {
 			"------------------\n", $address, $message);
 		message $msg, "guildnotice";
 	}
-
-	#message	T("Requesting guild information...\n"), "info"; # Lets Disable this, its kinda useless.
-	$messageSender->sendGuildMasterMemberCheck();
-
-	# Replies 01B6 (Guild Info) and 014C (Guild Ally/Enemy List)
-	$messageSender->sendGuildRequestInfo(0);
-
-	# Replies 0166 (Guild Member Titles List) and 0154 (Guild Members List)
-	$messageSender->sendGuildRequestInfo(1);
-
 }
 
 sub identify_list {
