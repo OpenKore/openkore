e8c7b368fb3c70634effadce5c1c9cccc8138e63
Alison Serafim <10372732+alisonrag@users.noreply.github.com>
2020-02-24 02:34:57 -0300

    documented stats_info

--- src/Network/Receive.pm
+++ src/Network/Receive.pm
@@ -1573,6 +1573,9 @@ sub stats_added {
 	});
 }
 
+# Character status (ZC_STATUS).
+# 00BD <stpoint>.W <str>.B <need str>.B <agi>.B <need agi>.B <vit>.B <need vit>.B <int>.B <need int>.B <dex>.B <need dex>.B <luk>.B <need luk>.B 
+# <atk>.W <atk2>.W <matk min>.W <matk max>.W <def>.W <def2>.W <mdef>.W <mdef2>.W <hit>.W <flee>.W <flee2>.W <crit>.W <aspd>.W <aspd2>.W 
 sub stats_info {
 	my ($self, $args) = @_;
 	return unless changeToInGameState();
