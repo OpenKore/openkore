c2752786986599e049f276df8fd68311a7aa0687
koooge <koooooge@gmail.com>
2020-02-26 00:46:22 +0100

    test: Delete obsolete server test
    Signed-off-by: koooge <koooooge@gmail.com>

--- src/test/NetworkTest.pm
+++ src/test/NetworkTest.pm
@@ -35,42 +35,12 @@ sub start {
 			},
 		],
 	);
-	
+
 	for my $serverType (qw(
 		0
-		1
-		2
-		3
-		4
-		5
-		6
-		7
-		8
-		8_1
-		8_2
-		8_3
-		8_4
-		8_5
-		9
-		10
-		11
-		12
-		13
-		14
-		15
-		16
-		17
-		18
-		19
-		20
-		21
-		22
-		aRO
 		bRO
-		fRO
 		idRO
 		iRO
-		mRO
 		pRO
 		rRO
 		tRO
@@ -82,10 +52,9 @@ sub start {
 				SKIP: {
 					# kRO has too many base classes (more than 100), and perl dies trying to load it
 					skip 'known to be broken', 1 if $serverType =~ /^(kRO_RagexeRE_0|twRO)$/;
-
 					my $instance = eval { $module->create(undef, $serverType) };
 					ok($instance, "create $module") or skip 'failed', 1;
-					
+
 					# idRO and pRO have broken packet_list
 					next if $serverType =~ /^(idRO|pRO)$/;
 
@@ -95,29 +64,26 @@ sub start {
 							is($_, $handler->[0], 'matches');
 						}}
 					}
-					
-					# do not test unsupported STs further
-					next if $serverType =~ /^[1-9]/;
-					
+
 					# do not test kRO tree further
 					next if $serverType =~ /^kRO/;
 
 					for my $expected (@{$tests{$module}}) {
 						subtest "reconstruct and parse $expected->{switch}" => sub { SKIP: {
 							my ($reconstruct_callback, $parse_callback);
-							
+
 							subtest 'callbacks exist' => sub {
 								ok($reconstruct_callback = $instance->can("reconstruct_$expected->{switch}"), 'reconstruct');
 								ok($parse_callback = $instance->can("parse_$expected->{switch}"), 'parse');
 							} or skip 'failed', 1;
-							
+
 							my $got = Storable::dclone($expected);
 							$instance->$reconstruct_callback($got);
 							$instance->$parse_callback($got);
-							
+
 							# there may be additional keys after reconstruct_callback
 							$got = reduce_struct($got, $expected);
-							
+
 							is_deeply($got, $expected, 'test data');
 						}}
 					}
@@ -129,7 +95,7 @@ sub start {
 
 sub reduce_struct {
 	my ($got, $expected) = @_;
-	
+
 	ref $got eq 'HASH' ? {map { exists $expected->{$_} ? ($_ => reduce_struct($got->{$_}, $expected->{$_})) : () } keys %$got}
 	: ref $got eq 'ARRAY' ? [List::MoreUtils::pairwise { reduce_struct($a, $b) } @$got, @$expected]
 	: $got
