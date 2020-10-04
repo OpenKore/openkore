a4297ae37a0047147fbcac2703d942d965c63baf
sctnightcore <23263315+sctnightcore@users.noreply.github.com>
2020-10-04 12:19:52 +0700

    add xkore mode check / remover blockingPlayerCancel

--- src/Network/Receive/kRO/RagexeRE_2020_03_04a.pm
+++ src/Network/Receive/kRO/RagexeRE_2020_03_04a.pm
@@ -51,7 +51,6 @@ sub new {
 	);
 
 	$self->{packet_lut}{$_} = $handlers{$_} for keys %handlers;
-	$self->{blockingPlayerCancel} = 1;
 	$self->{vender_items_list_item_pack} = 'V v2 C V C3 a16 a25';
 	$self->{npc_store_info_pack} = "V V C V";
 	$self->{buying_store_items_list_pack} = "V v C V";
