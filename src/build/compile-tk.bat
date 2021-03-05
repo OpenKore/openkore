47402b6e542c51bf9524b5444201f83164199ed6
Alison <alisonserafim@gmail.com>
2021-03-05 20:52:39 -0300

    update compile scripts

--- src/build/compile-tk.bat
+++ src/build/compile-tk.bat
@@ -1,4 +1,4 @@
 cd ..\..
 set BUILDING_WX=2
-perlapp --clean --trim Pod::Usage;I18N::Langinfo;XSTools --icon src\build\openkore.ico --lib src --norunlib --nologo --force --exe tkstart.exe start.pl --add List::Util;File::Path;Text::Balanced;Digest::MD5;Math::BigInt;Math::BigInt::Calc;Math::BigInt::CalcEmu;Math::BigInt::FastCalc;Math::BigInt::Trace;Math::BigFloat;Math::BigFloat::Trace;Math::BigRat;Math::Complex;Math::Trig;Tk;Tk::**;Win32::API;
+perlapp --clean --trim Pod::Usage;I18N::Langinfo;Wx;Wx::**;XSTools;Win32::GUI; --icon src\build\openkore.ico --lib src --nocompress --norunlib --nologo --force --exe tkstart.exe start.pl --add List::Util;File::Path;Text::Balanced;Digest::MD5;Math::BigInt;Math::BigInt::Calc;Math::BigInt::CalcEmu;Math::BigInt::FastCalc;Math::BigInt::Trace;Math::BigFloat;Math::BigFloat::Trace;Math::BigRat;Math::Complex;Math::Trig;Tk;Tk::**;Win32::API;
 pause
--- src/build/compile-vx.bat
+++ src/build/compile-vx.bat
@@ -1,4 +1,4 @@
 cd ..\..
 set BUILDING_WX=2
-perlapp --clean --trim Pod::Usage;I18N::Langinfo;XSTools --icon src\build\openkore.ico --lib src --norunlib --gui --nologo --force --exe vxstart.exe start.pl --add List::Util;File::Path;Text::Balanced;Digest::MD5;Math::BigInt;Math::BigInt::Calc;Math::BigInt::CalcEmu;Math::BigInt::FastCalc;Math::BigInt::Trace;Math::BigFloat;Math::BigFloat::Trace;Math::BigRat;Math::Complex;Math::Trig;Tk;Tk::**;
+perlapp --clean --trim Pod::Usage;I18N::Langinfo;Wx;Wx::**;XSTools;Win32::GUI; --icon src\build\openkore.ico --lib src --nocompress --norunlib --gui --nologo --force --exe vxstart.exe start.pl --add List::Util;File::Path;Text::Balanced;Digest::MD5;Math::BigInt;Math::BigInt::Calc;Math::BigInt::CalcEmu;Math::BigInt::FastCalc;Math::BigInt::Trace;Math::BigFloat;Math::BigFloat::Trace;Math::BigRat;Math::Complex;Math::Trig;Tk;Tk::**;
 pause
