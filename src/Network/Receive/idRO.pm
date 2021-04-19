96bda206cdb1b3674d733b38b4648eb22ece181a
Alison <alisonserafim@gmail.com>
2021-04-18 22:43:17 -0300

    [Network] 2021-04-17 idRO Update (#3403)
    * [2021-04-17] idRO Network Update
    
    * sort alphabetically
    
    Co-authored-by: ya4ept <ya4ept@ya.ru>

--- src/Network/Receive/idRO.pm
+++ src/Network/Receive/idRO.pm
@@ -24,6 +24,13 @@ sub new {
 
 	my %packets = (
 		'0097' => ['private_message', 'v Z24 V Z*', [qw(len privMsgUser flag privMsg)]],
+		'009D' => ['item_exists', 'a4 V C v3 C2', [qw(ID nameID identified x y amount subx suby)]],
+		'01C8' => ['item_used', 'a2 V a4 v C', [qw(ID itemID actorID remaining success)]],
+		'0A09' => ['deal_add_other', 'V C V C3 a16 a25', [qw(nameID type amount identified broken upgrade cards options)]],
+		'0A0A' => ['storage_item_added', 'a2 V V C4 a16 a25', [qw(ID amount nameID type identified broken upgrade cards options)]],
+		'0A0B' => ['cart_item_added', 'a2 V V C4 a16 a25', [qw(ID amount nameID type identified broken upgrade cards options)]],
+		'0A37' => ['inventory_item_added', 'a2 v V C3 a16 V C2 a4 v a25 C v', [qw(ID amount nameID identified broken upgrade cards type_equip type fail expire unknown options favorite viewID)]],
+		'0ADD' => ['item_appeared', 'a4 V v C v2 C2 v C v', [qw(ID nameID type identified x y subx suby amount show_effect effect_type )]],
 	);
 
 	$self->{packet_list}{$_} = $packets{$_} for keys %packets;
@@ -35,6 +42,12 @@ sub new {
 	);
 
 	$self->{packet_lut}{$_} = $handlers{$_} for keys %handlers;
+
+	$self->{vender_items_list_item_pack} = 'V v2 C V C3 a16 a25 V v';
+	$self->{buying_store_items_list_pack} = "V v C V";
+
+	$self->{npc_store_info_pack} = "V V C V";
+	$self->{makable_item_list_pack} = "V4";
 
 	return $self;
 }
