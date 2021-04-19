c036b73f61d104cf3daf8abd9630891df89dc69d
Alison <alisonserafim@gmail.com>
2021-04-18 22:43:17 -0300

    [Network] 2021-04-17 idRO Update (#3403)
    * [2021-04-17] idRO Network Update
    
    * sort alphabetically
    
    Co-authored-by: ya4ept <ya4ept@ya.ru>

--- src/Network/Send/idRO.pm
+++ src/Network/Send/idRO.pm
@@ -23,20 +23,33 @@ sub new {
 	my $self = $class->SUPER::new(@_);
 
 	my %handlers = qw(
-		send_equip 0998
-		storage_password 023B
-		sync 0360
-		character_move 035F
+		actor_action 0437
 		actor_info_request 0368
 		actor_look_at 0361
-		item_take 0362
-		item_drop 0363
-		storage_item_add 0364
-		storage_item_remove 0365
-		skill_use_location 0366
-		party_setting 07D7
+		actor_name_request 0369
+		buy_bulk_buyer 0819
+		buy_bulk_closeShop 0815
+		buy_bulk_openShop 0811
+		buy_bulk_request 0817
 		buy_bulk_vender 0801
 		char_delete2_accept 098F
+		character_move 035F
+		friend_request 0202
+		homunculus_command 022D
+		item_drop 0363
+		item_list_window_selected 07E4
+		item_take 0362
+		map_login 0436
+		party_join_request_by_name 02C4
+		party_setting 07D7
+		pet_capture 08B5
+		send_equip 0998
+		skill_use 0438
+		skill_use_location 0366
+		storage_item_add 0364
+		storage_item_remove 0365
+		storage_password 023B
+		sync 0360
 	);
 	$self->{packet_lut}{$_} = $handlers{$_} for keys %handlers;
 
