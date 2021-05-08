c85724a7f7fb79bae5ba8a38116cfb8196d2a9ef
ya4ept <ya4ept@ya.ru>
2021-05-08 12:34:39 +0300

    replaced OTT with OTP (#3423)
    OTP - One Time Password

--- src/Network/Receive.pm
+++ src/Network/Receive.pm
@@ -7731,7 +7731,7 @@ sub received_login_token {
 	return if ($self->{net}->version == 1);
 	my $master = $masterServers{$config{master}};
 	# rathena use 0064 not 0825
-	$messageSender->sendTokenToServer($config{username}, $config{password}, $master->{master_version}, $master->{version}, $args->{login_token}, $args->{len}, $master->{OTT_ip}, $master->{OTT_port});
+	$messageSender->sendTokenToServer($config{username}, $config{password}, $master->{master_version}, $master->{version}, $args->{login_token}, $args->{len}, $master->{OTP_ip}, $master->{OTP_port});
 }
 
 # this info will be sent to xkore 2 clients
