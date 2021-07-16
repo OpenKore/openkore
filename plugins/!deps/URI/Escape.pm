c4f3131f54addc57c8852bd239677b12cbef8732
tung.pham.lenovo <tung.pham@atadi.vn>
2021-07-16 22:14:52 +0700

    fixed "\C no longer supported in regex" for vRO_auth plugins encoded UTF8 for vRO/maps.txt

--- plugins/!deps/URI/Escape.pm
+++ plugins/!deps/URI/Escape.pm
@@ -202,7 +202,14 @@ sub uri_unescape {
 }
 
 sub escape_char {
-    return join '', @URI::Escape::escapes{$_[0] =~ /(\C)/g};
+    # return join '', @URI::Escape::escapes{$_[0] =~ /(\C)/g};
+    my $dummy = substr($_[0], 0, 0);
+    if (utf8::is_utf8($_[0])) {
+        my $s = shift;
+        utf8::encode($s);
+        unshift(@_, $s);
+    }
+    return join '', @URI::Escape::escapes{split //, $_[0]};
 }
 
 1;
