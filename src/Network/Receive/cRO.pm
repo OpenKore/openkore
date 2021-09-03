8f7b382b5ba26eecb1360cdc65e1b81d7a675d9e
Alison <alisonserafim@gmail.com>
2021-09-03 19:23:12 -0300

    [cRO] September, 2021 update (#3455)
    * [cRO] September, 2021 update
    * update map_login packet
    * update cRO server ip
    * code cleanup
    * update server settings
    * update table files
    Co-authored-by: ya4ept <ya4ept@ya.ru>

--- src/Network/Receive/cRO.pm
+++ src/Network/Receive/cRO.pm
@@ -13,7 +13,6 @@ package Network::Receive::cRO;
 
 use strict;
 use base qw(Network::Receive::ServerType0);
-use Globals;
 
 sub new {
 	my ($class) = @_;
@@ -60,11 +59,11 @@ sub new {
 
 	$self->{packet_lut}{$_} = $handlers{$_} for keys %handlers;
 
-	$self->{vender_items_list_item_pack} = 'V v2 C V C3 a16 a25';
-	$self->{npc_store_info_pack} = "V V C V";
 	$self->{buying_store_items_list_pack} = "V v C V";
 	$self->{makable_item_list_pack} = "V4";
+	$self->{npc_store_info_pack} = "V V C V";
 	$self->{rodex_read_mail_item_pack} = "v V C3 a16 a4 C a4 a25";
+	$self->{vender_items_list_item_pack} = 'V v2 C V C3 a16 a25';
 
 	return $self;
 }
