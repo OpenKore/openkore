b49c553c7517f59fd1229e2bbee695799b77eb0e
ya4ept <ya4ept@ya.ru>
2021-10-02 13:21:12 +0300

    sorting

--- src/Network/Receive/vRO.pm
+++ src/Network/Receive/vRO.pm
@@ -37,11 +37,11 @@ sub new {
 	);
 	$self->{packet_lut}{$_} = $handlers{$_} for keys %handlers;
 
+	$self->{buying_store_items_list_pack} = "V v C V";
 	$self->{npc_store_info_pack} = "V V C V";
 	$self->{rodex_read_mail_item_pack} = "v V C3 a16 a4 C a4 a25";
 	$self->{vender_items_list_item_pack} = 'V v2 C V C3 a16 a25 V v';
-	$self->{buying_store_items_list_pack} = "V v C V";
-	
+
 	return $self;
 }
 
