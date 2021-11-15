3ef10a94b7ec96866767384c44123716c926b85b
ya4ept <ya4ept@ya.ru>
2021-11-16 02:42:23 +0300

    update makedist.sh (#3536)
    - updated the script
    - updated Distfiles

--- makedist.sh
+++ makedist.sh
@@ -1,73 +1,17 @@
 #!/bin/bash
-# This script creates a source tarball for OpenKore, confpack and tablepack.
+# This script creates a source tarball for OpenKore.
 
 PACKAGE=openkore
-VERSION=2.0.6
+VERSION=`date +"%Y-%m-%d"`
 
 DIRS=(.
 	src
-	src/build
-	src/scons-local-2.0.1
-	src/scons-local-2.0.1/SCons
-	src/scons-local-2.0.1/SCons/compat
-	src/scons-local-2.0.1/SCons/Node
-	src/scons-local-2.0.1/SCons/Options
-	src/scons-local-2.0.1/SCons/Platform
-	src/scons-local-2.0.1/SCons/Scanner
-	src/scons-local-2.0.1/SCons/Script
-	src/scons-local-2.0.1/SCons/Tool
-	src/scons-local-2.0.1/SCons/Tool/MSCommon
-	src/scons-local-2.0.1/SCons/Tool/packaging
-	src/scons-local-2.0.1/SCons/Variables
-	src/deps
-	src/deps/Carp
-	src/deps/Class
-	src/deps/Class/Accessor
-	src/deps/Class/Data
-	src/deps/Devel
-	src/deps/Exception
-	src/deps/File
-	src/deps/List
-	src/deps/Text
-	src/deps/Tie
-	src/deps/SVN
-	src/deps/Data/YAML
-	src/po
-	src/test
 	src/Actor
+	src/Actor/Slave
 	src/AI
 	src/AI/Slave
-	src/Base
-	src/Base/Server
-	src/Base/Ragnarok
-	src/Base/WebServer
-	src/Bus
-	src/Bus/Server
-	src/doc
-	src/doc/data
-	src/doc/srcdoc
-	src/Interface
-	src/Interface/Console
-	src/Interface/Win32
-	src/Interface/Wx
-	src/Interface/Wx/DockNotebook
-	src/Interface/Wx/List
-	src/Interface/Wx/List/ItemList
-	src/Interface/Wx/StatView
-	src/MediaServer
-	src/Network
-	src/Network/Receive
-	src/Network/Receive/iRO
-	src/Network/Receive/kRO
-	src/Network/Send
-	src/Network/Send/iRO
-	src/Network/Send/kRO
-	src/Network/XKore2
-	src/Task
-	src/Poseidon
-	src/Utils
-	src/Utils/StartupNotification
 	src/auto/XSTools
+	src/auto/XSTools/darwin/include
 	src/auto/XSTools/misc
 	src/auto/XSTools/OSL
 	src/auto/XSTools/OSL/doc
@@ -79,29 +23,90 @@ DIRS=(.
 	src/auto/XSTools/OSL/Threading
 	src/auto/XSTools/OSL/Threading/Unix
 	src/auto/XSTools/OSL/Threading/Win32
-	src/auto/XSTools/PathFinding
-	src/auto/XSTools/unix
-	src/auto/XSTools/win32
-	src/auto/XSTools/Translation
 	src/auto/XSTools/PaddedPackets
 	src/auto/XSTools/PaddedPackets/Algorithms
+	src/auto/XSTools/PathFinding
+	src/auto/XSTools/Translation
+	src/auto/XSTools/unix
 	src/auto/XSTools/utils
-	src/auto/XSTools/utils/win32
-	src/auto/XSTools/utils/unix
 	src/auto/XSTools/utils/c-bindings
 	src/auto/XSTools/utils/perl
+	src/auto/XSTools/utils/unix
+	src/auto/XSTools/utils/win32
+	src/auto/XSTools/win32
+	src/Base
+	src/Base/Ragnarok
+	src/Base/Server
+	src/Base/WebServer
+	src/build
+	src/Bus
+	src/Bus/Server
+	src/deps
+	src/deps/Carp
+	src/deps/Class
+	src/deps/Class/Accessor
+	src/deps/Class/Data
+	src/deps/Data/YAML
+	src/deps/Devel
+	src/deps/Devel/StackTrace
+	src/deps/Exception
+	src/deps/Exception/Class
+	src/deps/File
+	src/deps/JSON
+	src/deps/List
+	src/deps/Text
+	src/deps/Tie
+	src/doc
+	src/doc/data
+	src/doc/srcdoc
+	src/Interface
+	src/Interface/Console
+	src/Interface/Win32
+	src/Interface/Wx
+	src/Interface/Wx/DockNotebook
+	src/Interface/Wx/List
+	src/Interface/Wx/List/ItemList
+	src/Interface/Wx/StatView
+	src/InventoryList
+	src/MediaServer
+	src/Network
+	src/Network/Receive
+	src/Network/Receive/idRO
+	src/Network/Receive/iRO
+	src/Network/Receive/kRO
+	src/Network/Send
+	src/Network/Send/idRO
+	src/Network/Send/iRO
+	src/Network/Send/kRO
+	src/Network/XKore2
+	src/po
+	src/Poseidon
+	src/scons-local-3.1.2
+	src/scons-local-3.1.2/scons-local-3.1.2
+	src/scons-local-3.1.2/scons-local-3.1.2/SCons
+	src/scons-local-3.1.2/scons-local-3.1.2/SCons/compat
+	src/scons-local-3.1.2/scons-local-3.1.2/SCons/Node
+	src/scons-local-3.1.2/scons-local-3.1.2/SCons/Platform
+	src/scons-local-3.1.2/scons-local-3.1.2/SCons/Scanner
+	src/scons-local-3.1.2/scons-local-3.1.2/SCons/Script
+	src/scons-local-3.1.2/scons-local-3.1.2/SCons/Tool
+	src/scons-local-3.1.2/scons-local-3.1.2/SCons/Tool/clangCommon
+	src/scons-local-3.1.2/scons-local-3.1.2/SCons/Tool/docbook
+	src/scons-local-3.1.2/scons-local-3.1.2/SCons/Tool/MSCommon
+	src/scons-local-3.1.2/scons-local-3.1.2/SCons/Tool/packaging
+	src/scons-local-3.1.2/scons-local-3.1.2/SCons/Variables
+	src/Task
+	src/test
+	src/test/data
+	src/test/data/child
+	src/test/Utils
+	src/Utils
+	src/Utils/StartupNotification
 )
 PACKAGEDIR=$PACKAGE-$VERSION
 
 
-if [[ "$1" == "--help" ]]; then
-	echo "makedist.sh [--bin|--semibin DIR]"
-	echo " --bin      Create a binary distribution archive, including the binaries,"
-	echo "            confpack and tablepack."
-	echo " --semibin  Create a binary distribution, excluding binaries, confpack and"
-	echo "            tablepack. Files will be copied to DIR."
-	exit 1
-elif [[ "$1" == "--bin" ]]; then
+if [[ "$1" == "--bin" ]]; then
 	BINDIST=1
 	if [[ "$2" == "-o" ]]; then
 		PACKAGEDIR="$3"
@@ -116,10 +121,21 @@ elif [[ "$1" == "--semibin" ]]; then
 		echo "The output folder does not exist. See --help"
 		exit 1
 	fi
+elif [[ "$1" == "--help" ]]; then
+	echo "makedist.sh [ --bin | --semibin DIR ]"
+	echo " --bin [ -o DIR ] Create a binary distribution archive, including the binaries,"
+	echo "                  confpack and tablepack."
+	echo " --semibin DIR    Create a binary distribution, excluding binaries, confpack and"
+	echo "                  tablepack. Files will be copied to DIR."
+	exit 1
+else
+	echo "unknown option '$1'"
+	echo "Try 'makedist.sh --help' for more information."
+	exit 1
 fi
 
 if [[ "$BINDIST" == "1" ]]; then
-	for F in start.exe wxstart.exe NetRedirect.dll XSTools.dll; do
+	for F in start.exe wxstart.exe NetRedirect.dll XSTools.dll start-poseidon.exe; do
 		if [[ ! -f "$F" ]]; then
 			echo "Please put $F in the current folder."
 			exit 1
@@ -152,6 +168,8 @@ err() {
 # Preparation: create the dist folder
 rm -rf "$PACKAGEDIR" || err
 mkdir "$PACKAGEDIR"  || err
+mkdir "$PACKAGEDIR/control"  || err
+mkdir "$PACKAGEDIR/fields"  || err
 
 
 # Copy the files to the dist folder
@@ -166,13 +184,13 @@ process() {
 	fi
 	for F in "${ADDITIONAL[@]}"; do
 		if [ -f "$1/$F" ]; then
-			echo "Copying $1/$F"
+			echo "  copying $1/$F"
 			cp "$1/$F" "$TARGET" || err
 		fi
 	done
 
 	for F in ${FILES[@]}; do
-		echo "Copying $1/$F"
+		echo "  copying $1/$F"
 		cp "$1/$F" "$TARGET" || err
 	done
 }
@@ -185,68 +203,36 @@ done
 #######################################
 
 
-function findConfpackDir() {
-	if [[ -d confpack ]]; then
-		confpackDir=confpack
-	elif [[ -d control/confpack ]]; then
-		confpackDir=control/confpack
-	elif [[ -d ../confpack ]]; then
-		confpackDir=../confpack
-	else
-		err "Cannot find the confpack folder. Please put it in the current directory."
-	fi
-}
-
-function findTablepackDir() {
-	if [[ -d tablepack ]]; then
-		tablepackDir=tablepack
-	elif [[ -d tables/tablepack ]]; then
-		tablepackDir=tables/tablepack
-	elif [[ -d ../tablepack ]]; then
-		tablepackDir=../tablepack
-	else
-		err "Cannot find the tablepack folder. Please put it in the current directory."
-	fi
-}
-
-dir=`cd "$PACKAGEDIR"; pwd`
-
 # Copy the confpack and tablepack files to the distribution's folder
-if [[ "$SEMIBINDIST" != "1" ]]; then
-	findConfpackDir
-	findTablepackDir
-	make -C "$confpackDir" distdir DISTDIR="$PACKAGEDIR/control" || err
-	make -C "$tablepackDir" distdir DISTDIR="$PACKAGEDIR/tables" || err
-fi
+cp -v control/*.txt "$PACKAGEDIR/control/" || err
+cp -v fields/*.gz "$PACKAGEDIR/fields/" || err
+cp -vr fields/tools "$PACKAGEDIR/fields/" || err
+cp -vr tables "$PACKAGEDIR/" || err
 
-# Convert openkore.pl to Unix line format, otherwise Unix users can't
-# execute it directly.
+# Convert openkore.pl to Unix line format, otherwise Unix users can't execute it directly.
 perl src/build/dos2unix.pl "$PACKAGEDIR/openkore.pl" || err
-perl src/build/unix2dos.pl "$PACKAGEDIR/README.txt" || err
+perl src/build/unix2dos.pl "$PACKAGEDIR/README.md" || err
 
 if [[ "$BINDIST" == "1" ]]; then
 	# Create binary zipfile
-	cp XSTools.dll NetRedirect.dll "$PACKAGEDIR/src/auto/XSTools/" || err
+	cp -v XSTools.dll NetRedirect.dll start-poseidon.exe "$PACKAGEDIR/" || err
 
 	# Win32 binary
-	cp start.exe "$PACKAGEDIR/" || err
+	cp -v start.exe "$PACKAGEDIR/" || err
 	zip -9r "$PACKAGE-$VERSION-win32.zip" "$PACKAGEDIR" || err
+	echo "$PACKAGE-$VERSION-win32.zip created"
 
 	# Win32 Wx binary
-	cp wxstart.exe "$PACKAGEDIR/" || err
-	rm -f "$PACKAGEDIR/start.exe"
+	cp -v wxstart.exe "$PACKAGEDIR/" || err
+	rm -vf "$PACKAGEDIR/start.exe"
 	zip -9r "$PACKAGE-$VERSION-win32_WX.zip" "$PACKAGEDIR" || err
-
-	echo "$PACKAGE-$VERSION-win32.zip created"
 	echo "$PACKAGE-$VERSION-win32_wx.zip created"
 
-elif [[ "$SEMIBINDIST" != "1" ]]; then
+elif [[ "$SEMIBINDIST" == "1" ]]; then
 	# Create tarball
 	echo "Creating distribution archive..."
 	tar --bzip2 -cf "$PACKAGEDIR.tar.bz2" "$PACKAGEDIR" || err
 	echo "$PACKAGEDIR.tar.bz2"
 fi
 
-if [[ "$SEMIBINDIST" != "1" ]]; then
-	rm -rf "$PACKAGEDIR"
-fi
+rm -rf "$PACKAGEDIR"
