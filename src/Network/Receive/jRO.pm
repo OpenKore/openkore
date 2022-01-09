4ade467f492636654c9ca51ac01c2ca52b3ce371
Yuanxiang Ye <alvin.ye.y@gmail.com>
2022-01-09 08:35:48 -0600

    Changed how 'item_appeared' is parsed on jRO
    nameID is 4 bytes long, and amount is 2 bytes long on jRO

--- src/Network/Receive/jRO.pm
+++ src/Network/Receive/jRO.pm
@@ -20,6 +20,7 @@ sub new {
 	my ($class) = @_;
 	my $self = $class->SUPER::new(@_);
 	my %packets = (
+        '009E' => ['item_appeared', 'a4 V C v2 C2 V', [qw(ID nameID identified x y subx suby amount)]],
 	);
 
 	foreach my $switch (keys %packets) {
@@ -27,6 +28,8 @@ sub new {
 	}
 
 	my %handlers = qw(
+		account_server_info 0B07
+        item_appeared 009E
 	);
 	$self->{packet_lut}{$_} = $handlers{$_} for keys %handlers;
 
