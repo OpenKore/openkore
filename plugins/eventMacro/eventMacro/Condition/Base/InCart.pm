29e87ccd8026c735b2cc1157329aeb94922dd6cb
Alison <alisonserafim@gmail.com>
2022-08-20 14:12:56 -0300

    fix: update eventmacro item list conditions hooks (#3608)
    * fix: update condition hooks

--- plugins/eventMacro/eventMacro/Condition/Base/InCart.pm
+++ plugins/eventMacro/eventMacro/Condition/Base/InCart.pm
@@ -8,24 +8,20 @@ use Globals qw( $char );
 
 
 sub _hooks {
-	['packet_mapChange','cart_ready','packet/cart_item_added','cart_item_removed','packet/cart_off'];
+	['cart_ready','packet/cart_item_added','cart_item_removed','packet/cart_off','packet/item_list_end'];
 }
 
 sub validate_condition {
 	my ( $self, $callback_type, $callback_name, $args ) = @_;
-	
 	if ($callback_type eq 'hook') {
-
-		if ($callback_name eq 'packet_mapChange') {
-			$self->{is_on_stand_by} = 1;
-			
-		} elsif ($callback_name eq 'cart_ready') {
+		if ($callback_name eq 'cart_ready'
+			|| ($callback_name eq 'item_list_end' && $args->{type}== 0x1)) # INVTYPE_CART
+		{
 			$self->{is_on_stand_by} = 0;
 			
 		} elsif ($callback_name eq 'packet/cart_off') {
 			$self->{is_on_stand_by} = 1;
 		}
-		
 	} elsif ($callback_type eq 'recheck') {
 		$self->{is_on_stand_by} = $char->cart->isReady ? 0 : 1;
 	}
