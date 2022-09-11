b0262510e55fe00e06199d2b37259c4ecded769a
Henrique <henriquebkmohan@gmail.com>
2022-09-11 18:27:58 -0300

    fix runFromTarget_maxPathDistance

--- src/Misc.pm
+++ src/Misc.pm
@@ -2512,10 +2512,12 @@ sub meetingPosition {
 	my $runFromTarget;
 	my $runFromTarget_dist;
 	my $runFromTarget_minStep;
+	my $runFromTarget_maxPathDistance;
 
 	# actor is char
 	if ($actorType == 1) {
 		$attackRouteMaxPathDistance = $config{attackRouteMaxPathDistance} || 13;
+		$runFromTarget_maxPathDistance = $config{runFromTarget_maxPathDistance} || 13;
 		$runFromTarget = $config{runFromTarget};
 		$runFromTarget_dist = $config{runFromTarget_dist};
 		$runFromTarget_minStep = $config{runFromTarget_minStep};
@@ -2535,7 +2537,8 @@ sub meetingPosition {
 
 	# actor is a slave
 	} elsif ($actorType == 2) {
-		$attackRouteMaxPathDistance = $config{$actor->{configPrefix}.'attackRouteMaxPathDistance'} || 13;
+		$attackRouteMaxPathDistance = $config{$actor->{configPrefix}.'attackRouteMaxPathDistance'} || 20;
+		$runFromTarget_maxPathDistance = $config{$actor->{configPrefix}.'runFromTarget_maxPathDistance'} || 20;
 		$runFromTarget = $config{$actor->{configPrefix}.'runFromTarget'};
 		$runFromTarget_dist = $config{$actor->{configPrefix}.'runFromTarget_dist'};
 		$runFromTarget_minStep = $config{$actor->{configPrefix}.'runFromTarget_minStep'};
@@ -2565,6 +2568,13 @@ sub meetingPosition {
 		if ($runActive) {
 			$min_destination_dist = $runFromTarget_minStep;
 		}
+	}
+	
+	my $max_path_dist;
+	if ($runActive) {
+		$max_path_dist = $attackRouteMaxPathDistance;
+	} else {
+		$max_path_dist = $runFromTarget_maxPathDistance;
 	}
 
 	# We should not stray further than $attackMaxDistance
@@ -2639,8 +2649,8 @@ sub meetingPosition {
 					max_y => $max_pathfinding_y
 				)->run($solution);
 
-				# 5. It must be reachable and have at max $attackRouteMaxPathDistance of route distance to it from our current position.
-				next unless ($dist >= 0 && $dist <= $attackRouteMaxPathDistance);
+				# 5. It must be reachable and have at max $max_path_dist of route distance to it from our current position.
+				next unless ($dist >= 0 && $dist <= $max_path_dist);
 
 				my $time_actor_to_get_to_spot = calcTime($realMyPos, $spot, $mySpeed);
 				my $time_actor_will_have_to_wait_in_spot_for_target_to_be_at_targetPosInStep;
