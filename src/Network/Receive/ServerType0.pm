da56f46029f15f7575831c8adaabc171803d8817
Alison <alisonserafim@gmail.com>
2023-03-12 14:38:58 -0300

    fix: add support to old items type in item list (#3700)
    * fix: add support to old items type in item list
    
    * code cleanup
    
    ---------
    
    Co-authored-by: ya4ept <ya4ept@ya.ru>

--- src/Network/Receive/ServerType0.pm
+++ src/Network/Receive/ServerType0.pm
@@ -905,11 +905,8 @@ sub items_nonstackable {
 	) {
 		return $items->{type7};
 	} elsif ($args->{switch} eq '0B0A') { # item_list
-		if (grep { $masterServer->{serverType} eq $_ } qw(iRO_Renewal)) {
-			return $items->{type7};
-		} else {
-			return $items->{type8};
-		}
+		return $items->{type7} if ($masterServer->{itemListUseOldType});
+		return $items->{type8};
 	} elsif ($args->{switch} eq '0B39') { # item_list
 		return $items->{type9};
 	} else {
@@ -953,11 +950,8 @@ sub items_stackable {
 	) {
 		return $items->{type6};
 	} elsif ($args->{switch} eq '0B09') { # item_list
-		if (grep { $masterServer->{serverType} eq $_ } qw(iRO_Renewal)) {
-			return $items->{type6};
-		} else {
-			return $items->{type7};
-		}
+		return $items->{type6} if ($masterServer->{itemListUseOldType});
+		return $items->{type7};
 	} else {
 		warning "items_stackable: unsupported packet ($args->{switch})!\n";
 	}
