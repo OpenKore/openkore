78253e51c0c9f207dbff08d203fd9d36ed5b9e3c
Guilherme Garcia <rgui.garcia@gmail.com>
2025-06-05 02:06:09 -0300

    fix: network ROla.pm by gaara

--- src/Network/Send/ROla.pm
+++ src/Network/Send/ROla.pm
@@ -1,5 +1,6 @@
 # File contributed by #gaaradodesertoo, #cry1493, #cry1493, #matheus8666, #megafuji, #ovorei, #__codeplay
 package Network::Send::ROla;
+
 use strict;
 use base qw(Network::Send::ServerType0);
 use Globals qw($net %config);
@@ -10,89 +11,44 @@ use Log qw(debug);
 sub new {
 	my ($class) = @_;
 	my $self = $class->SUPER::new(@_);
-
+	
 	my %packets = (
-		'0C26' => ['master_login', 'a4 Z51 a32 a5', [qw(game_code username password_rijndael flag)]],
-		'0825' => ['token_login', 'v V C Z51 a17 a15 a*', [qw(len version master_version username mac_hyphen_separated ip token)]],
-		'0436' => ['map_login', 'a4 a4 a4 V V C', [qw(accountID charID sessionID unknown tick sex)]],
-		'0360' => ['sync', 'V', [qw(time)]],
 		'0437' => ['actor_action', 'a4 C', [qw(targetID type)]],
 		'0094' => ['actor_info_request', 'a4', [qw(ID)]],
 		'009B' => ['actor_look_at', 'v C', [qw(head body)]],
 		'035F' => ['character_move', 'a3', [qw(coords)]],
-		'009F' => ['item_take', 'a4', [qw(ID)]],
 		'00A2' => ['item_drop', 'a2 v', [qw(ID amount)]],
-		'00A7' => ['item_use', 'a2 a4', [qw(ID targetID)]],
-		'0364' => ['storage_item_add', 'v2', [qw(index amount)]],
-		'0365' => ['storage_item_remove', 'v2', [qw(index amount)]],
-		'0366' => ['skill_use_location', 'v3', [qw(lv skillID x y)]],
-		'0438' => ['skill_use', 'v2 a4', [qw(lv skillID targetID)]],
 		'07E4' => ['item_list_window_selected', 'v V V a*', [qw(len type act itemInfo)]],
-		'098F' => ['char_delete2_accept', 'a4 Z40 Z40 Z40', [qw(charID email1 email2 email3)]],
-		'0998' => ['send_equip', 'v2', [qw(index viewID)]],
-		'08B5' => ['pet_capture', 'a4', [qw(targetID)]],
-		'0202' => ['friend_request', 'a*', [qw(username)]],
-		'02C4' => ['party_join_request_by_name', 'Z24', [qw(playerName)]],
-		'07D7' => ['party_setting', 'C v', [qw(settingFlag value)]],
-		'0811' => ['buy_bulk_openShop', 'v Z*', [qw(limit items)]],
-		'0815' => ['buy_bulk_closeShop', '', []],
-		'0817' => ['buy_bulk_request', 'a4 v', [qw(sellerID itemIndex)]],
-		'0819' => ['buy_bulk_buyer', 'v2', [qw(itemID amount)]],
-		'09F3' => ['rodex_request_items', 'C', [qw(option)]],
-		'0AC0' => ['rodex_open_mailbox', '', []],
-		'0AC1' => ['rodex_refresh_maillist', '', []],
-		'09E9' => ['rodex_close_mailbox', '', []],
-		'022D' => ['homunculus_command', 'C', [qw(command)]],
+		'009F' => ['item_take', 'a4', [qw(ID)]],
+		'00A7' => ['item_use', 'a2 a4', [qw(ID targetID)]],
+		'0436' => ['map_login', 'a4 a4 a4 V V C', [qw(accountID charID sessionID unknown tick sex)]],
+		'0C26' => ['master_login', 'a4 Z51 a32 a5', [qw(game_code username password_rijndael flag)]],
 		'023B' => ['storage_password', 'v a*', [qw(type data)]],
+		'0360' => ['sync', 'V', [qw(time)]],
+		'0825' => ['token_login', 'v V C Z51 a17 a15 a*', [qw(len version master_version username mac_hyphen_separated ip token)]],
 	);
 
 	$self->{packet_list}{$_} = $packets{$_} for keys %packets;
 
 	my %handlers = qw(
-		master_login 0C26
-		token_login 0825
-		map_login 0436
-		char_create 0A39
-		map_loaded 007D
-		character_move 035F
-		sync 0360
 		actor_action 0437
-		actor_info_request 0094
-		actor_look_at 009B
-		item_take 009F
-		item_drop 00A2
-		item_use 00A7
-		blocking_play_cancel 0447
-		storage_item_add 0364
-		storage_item_remove 0365
-		skill_use_location 0366
-		request_cashitems 08C9
-		skill_use 0438
+		actor_info_request 0368
+		actor_look_at 0361
+		actor_name_request 0369
+		character_move 035F
+		item_drop 0363
 		item_list_window_selected 07E4
-		char_delete2_accept 098F
-		gameguard_reply 09D0
-		send_equip 0998
-		pet_capture 08B5
-		friend_request 0202
-		party_join_request_by_name 02C4
-		party_setting 07D7
-		buy_bulk_openShop 0811
-		buy_bulk_closeShop 0815
-		buy_bulk_request 0817
-		buy_bulk_buyer 0819
-		rodex_open_mailbox 0AC0
-		rodex_refresh_maillist 0AC1
-		rodex_close_mailbox 09E9
-		rodex_request_items 09F3
-		homunculus_command 022D
+		item_take 0362
+		map_login 0436
+		master_login 0C26
 		storage_password 023B
-		actor_name_request 0094
+		sync 0360
+		token_login 0825
 	);
 
 	$self->{packet_lut}{$_} = $handlers{$_} for keys %handlers;
-
 	$self->{char_create_version} = 0x0A39;
-
+	
 	return $self;
 }
 
@@ -115,6 +71,33 @@ sub reconstruct_master_login {
 	}
 }
 
+sub sendTokenToServer {
+    my ($self, $username, $password, $master_version, $version, $token, $length, $otp_ip, $otp_port) = @_;
+    my $len = $length + 92;
+    
+    my $ip = '192.168.0.2';
+    my $mac = $config{macAddress} || sprintf("%02x%02x%02x%02x%02x%02x", (int(rand(256)) & 0xFC) | 0x02, map { int(rand(256)) } 1..5);
+    my $mac_hyphen_separated = join '-', $mac =~ /(..)/g;
+    
+    $net->serverDisconnect();
+    $net->serverConnect($otp_ip, $otp_port);
+
+    my $msg = $self->reconstruct({
+        switch => 'token_login',
+        len => $len,
+        version => $version || $self->version,
+        master_version => $master_version,
+        username => $username,
+        mac_hyphen_separated => $mac_hyphen_separated,
+        ip => $ip,
+        token => $token,
+    });
+    
+    $self->sendToServer($msg);
+
+    debug "Sent sendTokenLogin\n", "sendPacket", 2;
+}
+
 sub add_checksum {
 	my ($self, $msg) = @_;
 
@@ -135,33 +118,6 @@ sub add_checksum {
 	return $msg;
 }
 
-sub sendTokenToServer {
-	my ($self, $username, $password, $master_version, $version, $token, $length, $otp_ip, $otp_port) = @_;
-	my $len = $length + 92;
-
-	$net->serverDisconnect();
-	$net->serverConnect($otp_ip, $otp_port);
-
-	my $ip = '192.168.0.2';
-	my $mac = $config{macAddress} || sprintf("%02x%02x%02x%02x%02x%02x", (int(rand(256)) & 0xFC) | 0x02, map { int(rand(256)) } 1..5);
-	my $mac_hyphen_separated = join '-', $mac =~ /(..)/g;
-
-	my $msg = $self->reconstruct({
-		switch => 'token_login',
-		len => $len,
-		version => $version || $self->version,
-		master_version => $master_version,
-		username => $username,
-		mac_hyphen_separated => $mac_hyphen_separated,
-		ip => $ip,
-		token => $token,
-	});
-
-	$self->sendToServer($msg);
-
-	debug "Sent sendTokenLogin\n", "sendPacket", 2;
-}
-
 sub sendMapLogin {
 	my ($self, $accountID, $charID, $sessionID, $sex) = @_;
 	my $msg;
@@ -179,7 +135,7 @@ sub sendMapLogin {
 
 	$msg = $self->add_checksum($msg);
 	$self->sendToServer($msg);
-
+	
 	debug "Sent sendMapLogin\n", "sendPacket", 2;
 }
 
