be457c468b916c987b8e74081b45e783227033fe
roxleopardo <leonardo.leite@smsolucoes.com>
2025-08-22 09:30:38 -0300

    fix: conditional LATAMTranslate load

--- plugins/LATAMTranslate/LATAMTranslate.pl
+++ plugins/LATAMTranslate/LATAMTranslate.pl
@@ -20,18 +20,31 @@ our %strings_cache;
 Plugins::register( 'LATAMTranslate', 'Fixes issues with localized strings.', \&onUnload );
 
 my $hooks = Plugins::addHooks(
-    ['actor_setName', \&setName, undef],
-    ['packet_localBroadcast', \&localBroadcast, undef],
-    ['packet_pre/npc_talk', \&npcTalkPre, undef],
-    ['packet_pre/npc_talk_responses', \&npcTalkRespPre, undef],
+	['start3', \&checkServer, undef],
 );
+my $base_hooks;
 
+my $plugin_path = $Plugins::current_plugin_folder;
+
+sub checkServer {
+	my $master = $masterServers{ $config{master} };
+	if ( grep { $master->{serverType} eq $_ } qw(ROla) ) {
+		my $base_hooks = Plugins::addHooks(
+			['actor_setName', \&setName, undef],
+			['packet_localBroadcast', \&localBroadcast, undef],
+			['packet_pre/npc_talk', \&npcTalkPre, undef],
+			['packet_pre/npc_talk_responses', \&npcTalkRespPre, undef],
+		);
+		loadJSON();
+	}
+
+}
 # Load actor_name.json
 sub loadJSON {
 	message "Loading strings.json...\n", "rolaStrings";
 	%strings_cache = ();
 
-	my $file = "$Plugins::current_plugin_folder/strings.json";
+	my $file = "$plugin_path/strings.json";
 	unless ( -r $file ) {
 		error( "[rolaStrings] Can't read $file\n" );
 		return;
@@ -42,11 +55,11 @@ sub loadJSON {
 		return;
 	};
 
-	local $/;    # Enable slurp mode
+	local $/;	# Enable slurp mode
 	my $json_text = <$fh>;
 	close $fh;
 
-	my $data = eval { from_json( $json_text ) };    # ← FIXED
+	my $data = eval { from_json( $json_text ) };	# ← FIXED
 	if ( $@ || ref( $data ) ne 'HASH' ) {
 		error( "[rolaStrings] Failed to parse JSON: $@\n" );
 		return;
@@ -57,26 +70,24 @@ sub loadJSON {
 	message "[rolaStrings] Loaded $count actor names from strings.json\n", "rolaStrings";
 }
 
-loadJSON();
-
 # Plugin cleanup
-sub onUnload {
-	Plugins::delHooks( $hooks );
+sub unload {
+	Plugins::delHooks( $base_hooks );
+	Plugins::delHooks( $hooks ) if ( $hooks );
 	%strings_cache = ();
 }
 
-
 sub debug {
-    my (undef, $args) = @_;
-    message "[ROLA DEBUG] Debug: " . Data::Dumper::Dumper($args) . "\n", "debug";
+	my (undef, $args) = @_;
+	message "[ROLA DEBUG] Debug: " . Data::Dumper::Dumper($args) . "\n", "debug";
 }
 
 sub translate_token {
 	my ($token) = @_;
 
 	if (exists $strings_cache{$token}) {
-        my $string = $strings_cache{$token};
-        utf8::downgrade($string, 1);
+		my $string = $strings_cache{$token};
+		utf8::downgrade($string, 1);
 		return $string;
 	} else {
 		# print warning of missing token and the hex
@@ -92,7 +103,7 @@ sub setName {
 	my $new_name = $args->{new_name};
 
 	# Check for ∟...∟ (0x1C) encoded strings
-	if (defined $new_name && $new_name =~ /\x1C(.*?)\x1C/) {
+	if (defined $new_name && $new_name =~ /\x1C([^\x1C]+)\x1C/) {
 		my $token = $1;
 		my $translated = translate_token($token);
 
@@ -104,36 +115,36 @@ sub setName {
 }
 
 sub npcTalkPre {
-    my ( $self, $args ) = @_;
-    my $message = $args->{msg} || '';
+	my ( $self, $args ) = @_;
+	my $message = $args->{msg} || '';
 
-    my @tokens = $message =~ /\x1C([A-Za-z0-9]+)\x1C/g;
+	my @tokens = $message =~ /\x1C([^\x1C]+)\x1C/g;
 
-    if (@tokens) {
-        my $last_token = $tokens[-1];
-        my $translated = translate_token($last_token);
-        $args->{msg} = $translated;
-    }
+	if (@tokens) {
+		my $last_token = $tokens[-1];
+		my $translated = translate_token($last_token);
+		$args->{msg} = $translated;
+	}
 }
 
 sub npcTalkRespPre {
-    my (undef, $args) = @_;
+	my (undef, $args) = @_;
 
-    my $raw_msg = $args->{RAW_MSG} || '';
-    my $original_msg = $raw_msg;  # For debug
+	my $raw_msg = $args->{RAW_MSG} || '';
+	my $original_msg = $raw_msg;  # For debug
 
-    my $hex_msg = unpack('H*', $raw_msg);
+	my $hex_msg = unpack('H*', $raw_msg);
 
-    #message Misc::visualDump($raw_msg);
+	#message Misc::visualDump($raw_msg);
 
-    my $translated = $raw_msg;
-    $translated =~ s/\x1C([A-Za-z0-9]+)\x1C/translate_token($1)/ge;
+	my $translated = $raw_msg;
+	$translated =~ s/\x1C([^\x1C]+)\x1C/translate_token($1)/ge;
   
-    my $new_size = length($translated);
-    substr($translated, 2, 2, pack('v', $new_size));
+	my $new_size = length($translated);
+	substr($translated, 2, 2, pack('v', $new_size));
 
-    $args->{RAW_MSG} = $translated;
-    $args->{RAW_MSG_SIZE} = $new_size;
+	$args->{RAW_MSG} = $translated;
+	$args->{RAW_MSG_SIZE} = $new_size;
 }
 
 sub localBroadcast {
