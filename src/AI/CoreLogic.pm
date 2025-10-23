ef8c802c5460cb5a660e545c5aeaef9fb943a04f
Alison <alisonserafim@gmail.com>
2025-10-23 00:27:55 -0300

    Fix formatting in CoreLogic.pm for storageAuto_npc_type

--- src/AI/CoreLogic.pm
+++ src/AI/CoreLogic.pm
@@ -1489,7 +1489,7 @@ sub processAutoStorage {
 						# Use config steps based on storageAuto_npc_type
 						if ($config{'storageAuto_npc_type'} eq "" || $config{'storageAuto_npc_type'} eq "1") {
 							warning T("Warning storageAuto has changed. Please read News.txt\n") if ($config{'storageAuto_npc_type'} eq "");
-							if (grep { $masterServer->{serverType} eq $_ } qw(ROla) && $config{'storageAuto_npc'} =~ /prontera|payon/i;) {
+							if (grep { $masterServer->{serverType} eq $_ } qw(ROla) && $config{'storageAuto_npc'} =~ /prontera|payon/i) {
     							$steps = "c r2";
 								debug "Using standard ROla npc storage steps.\n", "npc";
 							} else {
