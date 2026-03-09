a44723828dc90c2327763b550c950ba055faf2e6
baphomello <baphomello@gmail.com>
2026-03-09 19:07:21 -0300

    feat(receive): allow servers to customize vender item unpack structure

--- src/Network/Receive.pm
+++ src/Network/Receive.pm
@@ -3970,6 +3970,7 @@ sub vender_items_list {
 	$venderCID = $args->{venderCID};
 
 	my $expireDate = 0;
+    my @item_keys = @{ $self->{vender_items_list_item_keys} || [ qw( price amount ID type nameID identified broken upgrade cards options location sprite_id ) ] };
 	my $item_pack = $self->{vender_items_list_item_pack} || 'V v2 C v C3 a8';
 	my $item_len = length pack $item_pack;
 	my $item_list_len = length $args->{itemList};
@@ -3984,7 +3985,7 @@ sub vender_items_list {
 	for (my $i = 0; $i < $item_list_len; $i+=$item_len) {
 		my $item = Actor::Item->new;
 
- 		@$item{qw( price amount ID type nameID identified broken upgrade cards options location sprite_id )} = unpack $item_pack, substr $args->{itemList}, $i, $item_len;
+        @$item{@keys} = unpack $item_pack, substr $args->{itemList}, $i, $item_len;
 
 		$item->{name} = itemName($item);
 		$venderItemList->add($item);
--- src/Network/Receive/laRO.pm
+++ src/Network/Receive/laRO.pm
@@ -1,18 +1,36 @@
 package Network::Receive::laRO;
 use strict;
+use warnings;
 use base qw(Network::Receive::kRO::RagexeRE_2021_11_03);
 
 sub new {
     my ($class) = @_;
     my $self = $class->SUPER::new(@_);
 
-    my %packets = ();
-
+    my %packets = (
+        '0B3D' => ['vender_items_list', 'v a4 a4 a*', [qw(len venderID venderCID itemList)]],
+        '0B62' => ['vender_items_list', 'v a4 a4 C V a*', [qw(len venderID venderCID flag expireDate itemList)]],
+    );
     $self->{packet_list}{$_} = $packets{$_} for keys %packets;
 
-    my %handlers = qw();
-
-    $self->{packet_lut}{$_} = $handlers{$_} for keys %handlers;
+    $self->{vender_items_list_item_pack} = 'V v2 C V C2 a16 a25 V v C2';
+    $self->{vender_items_list_item_keys} = [
+        qw(
+            price
+            amount
+            ID
+            type
+            nameID
+            identified
+            broken
+            upgrade
+            cards
+            options
+            location
+            sprite_id
+            grade
+        )
+    ];
 
     return $self;
 }
